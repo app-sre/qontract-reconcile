a4d4bb297af16a5fdb2c1bdbb1281c790591d49b
Jaime Melis <jmelis@redhat.com>
2019-01-03 15:23:27 +0100

    add build_deploy.sh script

---
+++ build_deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DOCKER_CONF="$PWD/.docker"
+mkdir -p "$DOCKER_CONF"
+docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
+
+# build images
+make build push
+
