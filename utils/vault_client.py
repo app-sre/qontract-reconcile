faa4b33cf95ba33874dc78e19a3b4cfc3ad47835
Maor Friedman <maorfr@gmail.com>
2019-03-07 12:30:40 +0200

    add retry for vault auth (#31)

--- utils/vault_client.py
+++ utils/vault_client.py
@@ -1,3 +1,5 @@
+import time
+import requests
 import hvac
 from utils.config import get_config
 
@@ -12,13 +14,27 @@ class SecretFieldNotFound(Exception):
     pass
 
 
+class VaultConnectionError(Exception):
+    pass
+
+
 def init(server, role_id, secret_id):
     global _client
 
     if _client is None:
         _client = hvac.Client(url=server)
 
-    _client.auth_approle(role_id, secret_id)
+    authenticated = False
+    for i in range(0, 3):
+        try:
+            _client.auth_approle(role_id, secret_id)
+            authenticated = True
+            break
+        except requests.exceptions.ConnectionError:
+            time.sleep(1)
+
+    if not authenticated:
+        raise VaultConnectionError()
 
     return _client
 
