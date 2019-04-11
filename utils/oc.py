e566703dcb351871dbd6e12658428be6f7b99ace
Maor Friedman <maor.friedman@redhat.com>
2019-04-11 18:45:37 +0300

    O s!

--- utils/oc.py
+++ utils/oc.py
@@ -36,7 +36,7 @@ class OC(object):
         identity_file = tempfile.mkdtemp() + '/id'
         with open(identity_file, 'w') as f:
             f.write(identity)
-        s.chmod(identity_file, 0o600)
+        os.chmod(identity_file, 0o600)
         user_host = '{}@{}'.format(user, hostname)
         return ['ssh', '-i', identity_file, user_host]
 
