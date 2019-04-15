4f4fe174abc918802803f2ec5b8aaa9304c5a9d1
Jaime Melis <j.melis@gmail.com>
2019-04-15 23:08:47 +0200

    ignore rsa key (#62)
    * ignore rsa key
    
    * suppress 'Warning: Permanently added' message'

--- utils/jump_host.py
+++ utils/jump_host.py
@@ -31,8 +31,10 @@ class JumpHost(object):
         os.chmod(identity_file, 0o600)
         user_host = '{}@{}'.format(self.user, self.hostname)
 
-        self.set_ssh_base_cmd(['ssh', '-i', identity_file,
-                               '-p', self.port, user_host])
+        self.set_ssh_base_cmd([
+            'ssh', '-o', 'StrictHostKeyChecking=no',
+            '-o', 'UserKnownHostsFile=/dev/null', '-o', 'LogLevel=ERROR',
+            '-i', identity_file, '-p', self.port, user_host])
 
     def get_ssh_base_cmd(self):
         return self.ssh_base_cmd
