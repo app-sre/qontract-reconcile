8b88564b29bd1847f53996e7c87cc6d5dbcf5837
Jaime Melis <jmelis@redhat.com>
2019-04-15 23:05:29 +0200

    suppress 'Warning: Permanently added' message'

--- utils/jump_host.py
+++ utils/jump_host.py
@@ -32,9 +32,9 @@ class JumpHost(object):
         user_host = '{}@{}'.format(self.user, self.hostname)
 
         self.set_ssh_base_cmd([
-            'ssh', '-o', 'StrictHostKeyChecking=no', '-o',
-            'UserKnownHostsFile=/dev/null', '-i', identity_file,
-            '-p', self.port, user_host])
+            'ssh', '-o', 'StrictHostKeyChecking=no',
+            '-o', 'UserKnownHostsFile=/dev/null', '-o', 'LogLevel=ERROR',
+            '-i', identity_file, '-p', self.port, user_host])
 
     def get_ssh_base_cmd(self):
         return self.ssh_base_cmd
