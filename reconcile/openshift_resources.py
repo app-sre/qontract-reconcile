65921e4dffc17ae01a9a13153254fc2e34126cdf
Maor Friedman <maor.friedman@redhat.com>
2019-04-15 17:30:37 +0300

    add try-finally for cleanup

--- reconcile/openshift_resources.py
+++ reconcile/openshift_resources.py
@@ -490,9 +490,11 @@ def run(dry_run=False, thread_pool_size=10):
 
     namespaces_query = gqlapi.query(NAMESPACES_QUERY)['namespaces']
 
-    oc_map, ri = fetch_data(namespaces_query, thread_pool_size)
-    realize_data(dry_run, oc_map, ri)
-    cleanup(oc_map)
+    try:
+        oc_map, ri = fetch_data(namespaces_query, thread_pool_size)
+        realize_data(dry_run, oc_map, ri)
+    finally:
+        cleanup(oc_map)
 
     if ri.has_error_registered():
         sys.exit(1)
