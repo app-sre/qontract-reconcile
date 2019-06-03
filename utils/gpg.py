ed6f79b6eb78d9d10d000115563f76c5fd2d273d
Maor Friedman <maor.friedman@redhat.com>
2019-06-03 16:29:46 +0300

    validate that gpg key is valid

---
+++ utils/gpg.py
@@ -0,0 +1,15 @@
+import os
+import base64
+
+from subprocess import PIPE, Popen, STDOUT
+
+
+def gpg_key_valid(public_gpg_key):
+    DENNULL = open(os.devnull, 'w')
+    public_gpg_key_dec = base64.b64decode(public_gpg_key)
+    proc = Popen(['gpg'], stdin=PIPE, stdout=DENNULL, stderr=STDOUT)
+    proc.communicate(public_gpg_key_dec)
+
+    if proc.returncode != 0:
+        return False
+    return True
