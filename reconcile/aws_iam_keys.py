dc927dc488f1f8e28c7ec6885510d264ee88a19b
Maor Friedman <maor.friedman@redhat.com>
2019-06-03 09:30:16 +0300

    keys_to_delete init to list comprehension

--- reconcile/aws_iam_keys.py
+++ reconcile/aws_iam_keys.py
@@ -16,16 +16,9 @@ def fetch_keys_to_delete():
     gqlapi = gql.get_api()
     accounts = gqlapi.query(QUERY)['accounts']
 
-    keys_to_delete = {}
-    for account in accounts:
-        account_name = account['name']
-        keys = account['deleteKeys']
-        if keys in (None, []):
-            continue
-        keys_to_delete[account_name] = []
-        for key in keys:
-            keys_to_delete[account_name].append(key)
-    return keys_to_delete
+    return {account['name']: account['deleteKeys']
+            for account in accounts
+            if account['deleteKeys'] not in (None, [])}
 
 
 def run(dry_run=False, thread_pool_size=10, enable_deletion=False):
