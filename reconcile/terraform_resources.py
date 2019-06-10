b90043f602542e4ead011c0854e5a1da501b1ec6
Maor Friedman <maor.friedman@redhat.com>
2019-06-10 11:09:11 +0300

    don't take subnet and sg from variables (#97)

--- reconcile/terraform_resources.py
+++ reconcile/terraform_resources.py
@@ -47,7 +47,7 @@ TF_QUERY = """
 """
 
 QONTRACT_INTEGRATION = 'terraform_resources'
-QONTRACT_INTEGRATION_VERSION = semver.format_version(0, 3, 0)
+QONTRACT_INTEGRATION_VERSION = semver.format_version(0, 3, 1)
 QONTRACT_TF_PREFIX = 'qrtf'
 
 
