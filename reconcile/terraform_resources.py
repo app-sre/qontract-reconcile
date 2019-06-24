9632f954e1e08009d87044da4ac2a4202516dbc2
Maor Friedman <maor.friedman@redhat.com>
2019-06-24 17:15:38 +0300

    [terraform-*] reduce verbosity (#121)

--- reconcile/terraform_resources.py
+++ reconcile/terraform_resources.py
@@ -47,7 +47,7 @@ TF_QUERY = """
 """
 
 QONTRACT_INTEGRATION = 'terraform_resources'
-QONTRACT_INTEGRATION_VERSION = semver.format_version(0, 5, 0)
+QONTRACT_INTEGRATION_VERSION = semver.format_version(0, 5, 1)
 QONTRACT_TF_PREFIX = 'qrtf'
 
 
