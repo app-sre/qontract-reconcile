71072a5f445bbce8ba971c2caae2da657b4755cc
Maor Friedman <maor.friedman@redhat.com>
2019-07-04 10:04:34 +0300

    [slack-usergroups] add support for escalation policies (#126)

--- utils/pagerduty_api.py
+++ utils/pagerduty_api.py
@@ -14,20 +14,48 @@ class PagerDutyApi(object):
         pd_api_key = vault_client.read(token_path, token_field)
         pypd.api_key = pd_api_key
 
-    def get_final_schedule(self, schedule_id):
+    def get_pagerduty_users(self, resource_type, resource_id):
         now = datetime.datetime.utcnow()
+
         try:
-            schedule = pypd.Schedule.fetch(
-                id=schedule_id,
-                since=now,
-                until=now,
-                time_zone='UTC')
+            if resource_type == 'schedule':
+                users = self.get_schedule_users(resource_id, now)
+            elif resource_type == 'escalationPolicy':
+                users = self.get_escalation_policy_users(resource_id, now)
         except requests.exceptions.HTTPError:
             return None
 
-        entries = schedule['final_schedule']['rendered_schedule_entries']
-        if len(entries) != 1:
-            return None
+        return users
 
-        [entry] = entries
-        return entry['user']['summary']
+    def get_schedule_users(self, schedule_id, now):
+        s = pypd.Schedule.fetch(
+            id=schedule_id,
+            since=now,
+            until=now,
+            time_zone='UTC')
+        entries = s['final_schedule']['rendered_schedule_entries']
+        return [entry['user']['summary'] for entry in entries]
+
+    def get_escalation_policy_users(self, escalation_policy_id, now):
+        ep = pypd.EscalationPolicy.fetch(
+            id=escalation_policy_id,
+            since=now,
+            until=now,
+            time_zone='UTC')
+        users = []
+        rules = ep['escalation_rules']
+        for rule in rules:
+            targets = rule['targets']
+            for target in targets:
+                target_type = target['type']
+                if target_type == 'schedule_reference':
+                    schedule_users = \
+                        self.get_schedule_users(target['id'], now)
+                    users.extend(schedule_users)
+                elif target_type == 'user_reference':
+                    users.append(target['summary'])
+            if users and rule['escalation_delay_in_minutes'] != 0:
+                # process rules until users are found
+                # and next escalation is not 0 minutes from now
+                break
+        return users
