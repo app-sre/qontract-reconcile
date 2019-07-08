9cc375cd282f301b95311553ddbe05384eb2bd2f
Maor Friedman <maor.friedman@redhat.com>
2019-07-08 16:33:36 +0300

    change loading

--- reconcile/jenkins_job_builder.py
+++ reconcile/jenkins_job_builder.py
@@ -1,20 +1,29 @@
 import sys
 
+from jenkins_jobs.builder import JenkinsManager
+from jenkins_jobs.parser import YamlParser
+from jenkins_jobs.registry import ModuleRegistry
 from jenkins_jobs.cli.entry import JenkinsJobs
 from jenkins_jobs.cli.subcommand.test import TestSubCommand
 
 def run(dry_run=False):
     argv = ['--conf', '/home/mafriedm/.config/jjb/ci-int.ini', 'test', 'jjb-ci-int.yaml']
     jjb = JenkinsJobs(argv)
-    test = TestSubCommand()
 
-    _, xml_jobs, _ = test._generate_xmljobs(
-        jjb.options, jjb.jjb_config)
+    builder = JenkinsManager(jjb.jjb_config)
+    registry = ModuleRegistry(jjb.jjb_config, builder.plugins_list)
+    parser = YamlParser(jjb.jjb_config)
+    parser.load_files(jjb.options.path)
 
-    for job in xml_jobs:
-        print(job.name)
+
+    jobs, views = parser.expandYaml(
+        registry, jjb.options.names)
+
+    for job in jobs:
+        print(job['name'])
         try:
-            project_url = job.xml[9][0][0].text
+            project_url = job['properties'][0]['github']['url']
             print(project_url)
-        except:
-            print("NO PROJECT URL")
+        except KeyError:
+            print('###################################')
+            continue
