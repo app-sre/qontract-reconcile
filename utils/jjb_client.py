ccc2d7ae8d3fe4538110018ab08a31b2575175fe
Maor Friedman <maor.friedman@redhat.com>
2019-07-15 13:05:57 +0300

    [jenkins-webhooks] introduce new integration (#139)

--- utils/jjb_client.py
+++ utils/jjb_client.py
@@ -12,6 +12,10 @@ import utils.gql as gql
 from os import path
 from contextlib import contextmanager
 
+from jenkins_jobs.builder import JenkinsManager
+from jenkins_jobs.parser import YamlParser
+from jenkins_jobs.registry import ModuleRegistry
+
 
 class FetchResourceError(Exception):
     def __init__(self, msg):
@@ -30,11 +34,16 @@ class JJB(object):
 
     def collect_configs(self, configs):
         gqlapi = gql.get_api()
-        instances = {c['instance']['name']: c['instance']['token']
-                     for c in configs}
+        instances = \
+            {c['instance']['name']: {'serverUrl': c['instance']['serverUrl'],
+                                     'token': c['instance']['token']}
+             for c in configs}
 
         working_dirs = {}
-        for name, token in instances.items():
+        instance_urls = {}
+        for name, data in instances.items():
+            token = data['token']
+            server_url = data['serverUrl']
             wd = tempfile.mkdtemp()
             ini = vault_client.read(token['path'], token['field'])
             ini = ini.replace('"', '')
@@ -44,6 +53,7 @@ class JJB(object):
                 f.write(ini)
                 f.write('\n')
             working_dirs[name] = wd
+            instance_urls[name] = server_url
 
         self.sort(configs)
 
@@ -68,6 +78,7 @@ class JJB(object):
                     f.write(config)
                     f.write('\n')
 
+        self.instances = instance_urls
         self.working_dirs = working_dirs
 
     def sort(self, configs):
@@ -167,11 +178,13 @@ class JJB(object):
             args = ['--conf', ini_path, 'update', config_path, '--delete-old']
             self.execute(args)
 
-    def execute(self, args):
-        from jenkins_jobs.cli.entry import JenkinsJobs
+    def get_jjb(self, args):
         os.environ['PYTHONHTTPSVERIFY'] = self.python_https_verify
+        from jenkins_jobs.cli.entry import JenkinsJobs
+        return JenkinsJobs(args)
 
-        jjb = JenkinsJobs(args)
+    def execute(self, args):
+        jjb = self.get_jjb(args)
         with self.toggle_logger():
             jjb.execute()
 
@@ -192,3 +205,42 @@ class JJB(object):
     def cleanup(self):
         for wd in self.working_dirs.values():
             shutil.rmtree(wd)
+
+    def get_job_webhooks_data(self):
+        job_webhooks_data = {}
+        for name, wd in self.working_dirs.items():
+            ini_path = '{}/{}.ini'.format(wd, name)
+            config_path = '{}/config.yaml'.format(wd)
+
+            args = ['--conf', ini_path, 'test', config_path]
+            jjb = self.get_jjb(args)
+            builder = JenkinsManager(jjb.jjb_config)
+            registry = ModuleRegistry(jjb.jjb_config, builder.plugins_list)
+            parser = YamlParser(jjb.jjb_config)
+            parser.load_files(jjb.options.path)
+
+            jobs, _ = parser.expandYaml(
+                registry, jjb.options.names)
+
+            for job in jobs:
+                try:
+                    project_url_raw = job['properties'][0]['github']['url']
+                    if 'https://github.com' in project_url_raw:
+                        continue
+                    job_url = \
+                        '{}/project/{}'.format(self.instances[name],
+                                               job['name'])
+                    project_url = \
+                        project_url_raw.strip('/').replace('.git', '')
+                    gitlab_triggers = job['triggers'][0]['gitlab']
+                    mr_trigger = gitlab_triggers['trigger-merge-request']
+                    trigger = 'mr' if mr_trigger else 'push'
+                    hook = {
+                        'job_url': job_url,
+                        'trigger': trigger,
+                    }
+                    job_webhooks_data.setdefault(project_url, [])
+                    job_webhooks_data[project_url].append(hook)
+                except KeyError:
+                    continue
+        return job_webhooks_data
