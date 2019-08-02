ec5e798fd3895cb51aeb272f279de1a8dc507e73
sdlhopol <sdlhopol@redhat.com>
2019-08-02 13:07:26 -0400

    add threading

--- reconcile/gitlab_permissions.py
+++ reconcile/gitlab_permissions.py
@@ -1,5 +1,7 @@
-import sys
 import logging
+import time
+from multiprocessing.dummy import Pool as ThreadPool
+from functools import partial
 
 import utils.gql as gql
 from utils.config import get_config
@@ -40,25 +42,32 @@ def get_gitlab_api():
     return GitLabApi(server, token, ssl_verify=False)
 
 
+def get_members_to_add(repo, gl, app_sre):
+    maintainers = gl.get_project_maintainers(repo)
+    if gl.user.username not in maintainers:
+        logging.error("'{}' is not shared with {} as 'Maintainer'".format(
+            repo, gl.user.username
+        ))
+        return []
+    members_to_add = [{
+        "user": u, "repo": repo} for u in app_sre
+        if u.username not in maintainers]
+    return members_to_add
+
+
 def run(dry_run=False):
+    start_time = time.time()
+    print("start--- %s seconds ---" % (time.time() - start_time))
     gl = get_gitlab_api()
     repos = get_gitlab_repos(gl.server)
     app_sre = gl.get_app_sre_group_users()
-    error = False
-    for r in repos:
-        maintainers = gl.get_project_maintainers(r)
-        if gl.user.username not in maintainers:
-            error = True
-            logging.error("'{}' is not shared with {} as 'Maintainer'".format(
-                r, gl.user.username
-            ))
-            continue
-        members_to_add = [u for u in app_sre
-                          if u.username not in maintainers]
-        for m in members_to_add:
-            logging.info(['add_maintainer', r, m.username])
-
-            if not dry_run:
-                gl.add_project_member(r, m)
-
-    sys.exit(error)
+    pool = ThreadPool(10)
+    get_members_to_add_partial = \
+        partial(get_members_to_add, gl=gl, app_sre=app_sre)
+    results = pool.map(get_members_to_add_partial, repos)
+    members_to_add = [item for sublist in results for item in sublist]
+    for m in members_to_add:
+        logging.info(['add_maintainer', m["user"], m["user"].username])
+        if not dry_run:
+            gl.add_project_member(m["repo"], m["user"])
+    print("end--- %s seconds ---" % (time.time() - start_time))
