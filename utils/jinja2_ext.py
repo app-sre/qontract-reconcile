3b72417540b4aa058c9a2b939153984d299a32ab
Jean-Francois Chevrette <jfchevrette@gmail.com>
2019-08-14 13:27:44 -0400

    Add jinja2 extension for base64 encoding (#163)
    * add jinja2 extension for base64 encoding

---
+++ utils/jinja2_ext.py
@@ -0,0 +1,25 @@
+import base64
+import textwrap
+
+from jinja2 import nodes
+from jinja2.ext import Extension
+
+
+class B64EncodeExtension(Extension):
+    tags = {'b64encode'}
+
+    def __init__(self, environment):
+        super(B64EncodeExtension, self).__init__(environment)
+
+    def parse(self, parser):
+        lineno = next(parser.stream).lineno
+
+        body = parser.parse_statements(['name:endb64encode'], drop_needle=True)
+
+        return nodes.CallBlock(self.call_method('_b64encode', None),
+                               [], [], body).set_lineno(lineno)
+
+    def _b64encode(self, caller):
+        content = caller()
+        content = textwrap.dedent(content)
+        return base64.b64encode(content)
