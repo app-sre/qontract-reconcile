eeced0516c6b13ab7061bd1fc27b00446e0eddbf
Maor Friedman <maor.friedman@redhat.com>
2019-10-02 10:55:33 +0300

    add default network policy test to create namespace

--- e2e_tests/create_namespace.py
+++ e2e_tests/create_namespace.py
@@ -3,6 +3,7 @@ import logging
 
 import e2e_tests.test_base as tb
 import e2e_tests.dedicated_admin_test_base as dat
+import e2e_tests.network_policy_test_base as npt
 
 from utils.defer import defer
 
@@ -16,6 +17,7 @@ def run(defer=None):
 
     ns_to_create = tb.get_test_namespace_name()
     expected_rolebindings = dat.get_expected_rolebindings()
+    expected_network_policies = npt.get_expected_network_policy_names()
 
     for cluster in oc_map.clusters():
         oc = oc_map.get(cluster)
@@ -25,10 +27,12 @@ def run(defer=None):
 
         try:
             oc.new_project(ns_to_create)
-            time.sleep(2) #  allow time for RoleBindings to be created
+            time.sleep(5) #  allow time for resources to be created
             for expected_rb in expected_rolebindings:
                 rb = oc.get(ns_to_create, 'RoleBinding', expected_rb['name'])
                 tb.assert_rolebinding(expected_rb, rb)
+            for expected_np in expected_network_policies:
+                assert oc.get(ns_to_create, 'NetworkPolicy', expected_np)
         finally:
             logging.info("[{}] Deleting namespace '{}'".format(
                 cluster, ns_to_create
