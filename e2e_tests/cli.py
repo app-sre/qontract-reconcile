da2d4aeb6fe2c061fdd97602cf67b2af99ff4c7f
Maor Friedman <maor.friedman@redhat.com>
2019-10-02 10:24:54 +0300

    add e2e test for default network policies

--- e2e_tests/cli.py
+++ e2e_tests/cli.py
@@ -6,6 +6,7 @@ import utils.config as config
 import utils.gql as gql
 import e2e_tests.create_namespace
 import e2e_tests.dedicated_admin_rolebindings
+import e2e_tests.default_network_policies
 
 from utils.aggregated_list import RunnerException
 
@@ -51,3 +52,8 @@ def create_namespace(ctx):
 @click.pass_context
 def dedicated_admin_rolebindings(ctx):
     run_test(e2e_tests.dedicated_admin_rolebindings.run)
+
+@test.command()
+@click.pass_context
+def default_network_policies(ctx):
+    run_test(e2e_tests.default_network_policies.run)
---
+++ e2e_tests/default_network_policies.py
@@ -0,0 +1,45 @@
+import re
+import logging
+
+import e2e_tests.test_base as tb
+import e2e_tests.dedicated_admin_test_base as dat
+
+from utils.defer import defer
+
+QONTRACT_E2E_TEST = 'dedicated-admin-rolebindings'
+
+
+@defer
+def run(defer=None):
+    oc_map = tb.get_oc_map(QONTRACT_E2E_TEST)
+    defer(lambda: oc_map.cleanup())
+    pattern = \
+        r'^(default|logging|' + \
+        '(openshift|kube-|ops-|dedicated-|management-|{}).*)$'.format(
+            tb.E2E_NS_PFX
+        )
+    for cluster in oc_map.clusters():
+        oc = oc_map.get(cluster)
+        logging.info("[{}] validating default NetworkPolicies".format(cluster))
+
+        projects = [p['metadata']['name']
+                    for p in oc.get_all('Project')['items']
+                    if p['status']['phase'] != 'Terminating' and
+                    not re.search(pattern, p['metadata']['name'])]
+
+        all_network_policies = \
+            oc.get_all('NetworkPolicy', all_namespaces=True)['items']
+        network_policies = [np for np in all_network_policies
+                            if np['metadata']['namespace'] in projects
+                            and np['metadata']['name'] in
+                            ['allow-from-default-namespace',
+                             'allow-from-same-namespace']]
+
+        for project in projects:
+            logging.info("[{}/{}] validating RoleBindings".format(
+                cluster, project))
+            project_nps = [np for np in network_policies
+                           if np['metadata']['namespace'] == project]
+            assert len(project_nps) == 2
+            assert project_nps[0]['metadata']['name'] != \
+                project_nps[1]['metadata']['name']
