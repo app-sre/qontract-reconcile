0a59d312137ff1f9f26f1a4be96ce5a364b3aa55
Jean-Francois Chevrette <jfchevrette@gmail.com>
2019-10-16 18:06:41 -0400

    add full_traceback

--- utils/threaded.py
+++ utils/threaded.py
@@ -1,6 +1,16 @@
 from multiprocessing.dummy import Pool as ThreadPool
 from functools import partial
 
+def full_traceback(func):
+    import traceback, functools
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            msg = "{}\n\nOriginal {}".format(e, traceback.format_exc())
+            raise type(e)(msg)
+    return wrapper
 
 def run(func, iterable, thread_pool_size, **kwargs):
     """run executes a function for each item in the input iterable.
@@ -8,5 +18,5 @@ def run(func, iterable, thread_pool_size, **kwargs):
     kwargs are passed to the input function (optional)."""
 
     pool = ThreadPool(thread_pool_size)
-    func_partial = partial(func, **kwargs)
+    func_partial = partial(full_traceback(func), **kwargs)
     return pool.map(func_partial, iterable)
