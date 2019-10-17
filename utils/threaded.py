b277c99651e6e2fca40adc649f0288de501cffe7
Jean-Francois Chevrette <jfchevrette@gmail.com>
2019-10-17 11:35:13 -0400

    Improve tracebacks for exceptions raised from inside a thread (#247)
    * add full_traceback

--- utils/threaded.py
+++ utils/threaded.py
@@ -1,5 +1,17 @@
+import traceback
+import functools
 from multiprocessing.dummy import Pool as ThreadPool
-from functools import partial
+
+
+def full_traceback(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            msg = "{}\n\nOriginal {}".format(e, traceback.format_exc())
+            raise type(e)(msg)
+    return wrapper
 
 
 def run(func, iterable, thread_pool_size, **kwargs):
@@ -8,5 +20,5 @@ def run(func, iterable, thread_pool_size, **kwargs):
     kwargs are passed to the input function (optional)."""
 
     pool = ThreadPool(thread_pool_size)
-    func_partial = partial(func, **kwargs)
+    func_partial = functools.partial(full_traceback(func), **kwargs)
     return pool.map(func_partial, iterable)
