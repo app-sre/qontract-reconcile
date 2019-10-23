4bbde2a2849e4353b09383d02d31d617ca9c6f61
Jean-Francois Chevrette <jfchevrette@gmail.com>
2019-10-23 11:11:10 -0400

    add openshift-acme integration

---
+++ utils/openshift_acme.py
@@ -0,0 +1,187 @@
+NAMESPACES_QUERY = """
+{
+  namespaces: namespaces_v1 {
+    name
+    cluster {
+      name
+      serverUrl
+      jumpHost {
+          hostname
+          knownHosts
+          user
+          port
+          identity {
+              path
+              field
+              format
+          }
+      }
+      automationToken {
+        path
+        field
+        format
+      }
+      disable {
+        integrations
+      }
+    }
+    openshiftAcme {
+      name
+      image
+      overrides {
+        deploymentName
+        roleName
+        rolebindingName
+        serviceaccountName
+      }
+    }
+  }
+}
+"""
+
+ACME_DEPLOYMENT = """
+apiVersion: extensions/v1beta1
+kind: Deployment
+metadata:
+  name: %(deployment_name)s
+  labels:
+    app: openshift-acme
+spec:
+  progressDeadlineSeconds: 600
+  replicas: 1
+  revisionHistoryLimit: 10
+  selector:
+    matchLabels:
+      app: openshift-acme
+  strategy:
+    type: Recreate
+  template:
+    metadata:
+      creationTimestamp: null
+      labels:
+        app: openshift-acme
+    spec:
+      containers:
+      - env:
+        - name: OPENSHIFT_ACME_EXPOSER_IP
+          valueFrom:
+            fieldRef:
+              apiVersion: v1
+              fieldPath: status.podIP
+        - name: OPENSHIFT_ACME_ACMEURL
+          value: 'https://acme-v01.api.letsencrypt.org/directory'
+        - name: OPENSHIFT_ACME_LOGLEVEL
+          value: '4'
+        - name: OPENSHIFT_ACME_NAMESPACE
+          valueFrom:
+            fieldRef:
+              apiVersion: v1
+              fieldPath: metadata.namespace
+        image: %(image)s
+        imagePullPolicy: Always
+        name: openshift-acme
+        ports:
+        - containerPort: 5001
+          protocol: TCP
+        resources: {}
+        terminationMessagePath: /dev/termination-log
+        terminationMessagePolicy: File
+        volumeMounts:
+        - mountPath: /dapi
+          name: podinfo
+          readOnly: true
+      dnsPolicy: ClusterFirst
+      restartPolicy: Always
+      schedulerName: default-scheduler
+      securityContext: {}
+      serviceAccount: %(serviceaccount_name)s
+      serviceAccountName: %(serviceaccount_name)s
+      terminationGracePeriodSeconds: 30
+      volumes:
+      - name: podinfo
+        downwardAPI:
+          defaultMode: 420
+          items:
+          - path: labels
+            fieldRef:
+              apiVersion: v1
+              fieldPath: metadata.labels
+"""
+
+ACME_SERVICEACCOUNT = """
+kind: ServiceAccount
+apiVersion: v1
+metadata:
+  name: %(serviceaccount_name)s
+  labels:
+    app: openshift-acme
+"""
+
+ACME_ROLE = """
+apiVersion: authorization.openshift.io/v1
+kind: Role
+metadata:
+  name: %(role_name)s
+  labels:
+    app: openshift-acme
+rules:
+- apiGroups:
+  - "route.openshift.io"
+  resources:
+  - routes
+  verbs:
+  - create
+  - delete
+  - get
+  - list
+  - patch
+  - update
+  - watch
+
+- apiGroups:
+  - "route.openshift.io"
+  resources:
+  - routes/custom-host
+  verbs:
+  - create
+
+- apiGroups:
+  - ""
+  resources:
+  - services
+  verbs:
+  - create
+  - delete
+  - get
+  - list
+  - patch
+  - update
+  - watch
+
+- apiGroups:
+  - ""
+  resources:
+  - secrets
+  verbs:
+  - create
+  - delete
+  - get
+  - list
+  - patch
+  - update
+  - watch
+"""
+
+ACME_ROLEBINDING = """
+apiVersion: authorization.openshift.io/v1
+groupNames: null
+kind: RoleBinding
+metadata:
+  name: %(rolebinding_name)s
+roleRef:
+  name: %(role_name)s
+  namespace: %(namespace_name)s
+subjects:
+- kind: ServiceAccount
+  name: %(serviceaccount_name)s
+"""
