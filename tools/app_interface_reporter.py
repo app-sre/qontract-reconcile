7d6cd3e2e6fdd1697d27237005be55038ff53722
Jaime Melis <jmelis@redhat.com>
2019-10-30 16:59:31 +0100

    add decorators to app-interface-exporter

--- tools/app_interface_reporter.py
+++ tools/app_interface_reporter.py
@@ -1,2 +1,11 @@
-def main():
+import click
+
+from reconcile.cli import config_file, log_level, dry_run
+
+
+@click.command()
+@config_file
+@dry_run
+@log_level
+def main(configfile, dry_run, log_level):
     print('app-interface-exporter')
