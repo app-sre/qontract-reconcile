d449d869e69a1345b25309c56c750c49e7f52989
Maor Friedman <maor.friedman@redhat.com>
2019-12-24 16:45:04 +0200

    add origin access identity and bucket policy

--- utils/terrascript_client.py
+++ utils/terrascript_client.py
@@ -27,7 +27,9 @@ from terrascript.aws.r import (aws_db_instance, aws_db_parameter_group,
                                aws_elasticache_parameter_group,
                                aws_iam_user_policy_attachment,
                                aws_sqs_queue, aws_dynamodb_table,
-                               aws_ecr_repository)
+                               aws_ecr_repository, aws_s3_bucket_policy,
+                               aws_cloudfront_origin_access_identity,
+                               aws_cloudfront_distribution)
 
 
 class UnknownProviderError(Exception):
@@ -504,6 +506,8 @@ class TerrascriptClient(object):
         for tf_resource in tf_resources:
             self.add_resource(account, tf_resource)
 
+        return bucket_tf_resource
+
     def populate_tf_resource_elasticache(self, resource, namespace_info,
                                          existing_secrets):
         account, identifier, values, output_prefix, output_resource_name = \
@@ -888,7 +892,59 @@ class TerrascriptClient(object):
             self.add_resource(account, tf_resource)
 
     def populate_tf_resource_s3_cloudfront(self, resource, namespace_info):
-        self.populate_tf_resource_s3(resource, namespace_info)
+        account, identifier, common_values, \
+            output_prefix, output_resource_name = \
+            self.init_values(resource, namespace_info)
+
+        bucket_tf_resource = \
+            self.populate_tf_resource_s3(resource, namespace_info)
+
+        tf_resources = []
+
+        # cloudfront origin access identity
+        values = {}
+        values['comment'] = f'{identifier}-cf-identity'
+        cf_oai_tf_resource = \
+            aws_cloudfront_origin_access_identity(identifier, **values)
+        tf_resources.append(cf_oai_tf_resource)
+
+        # bucket policy for cloudfront
+        values = {}
+        values['bucket'] = identifier
+        policy = {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Sid": "Grant access to CloudFront Origin Identity",
+                    "Effect": "Allow",
+                    "Principal": {
+                        "AWS": "${" + \
+                               cf_oai_tf_resource.fullname + \
+                               ".s3_canonical_user_id}"
+                    },
+                    "Action": "s3:*Object",
+                    "Resource":
+                        [f"arn:aws:s3:::{identifier}/{enable_dir}/*"
+                         for enable_dir
+                         in common_values.get('get_object_enable_dirs', [])]
+                }
+            ]
+        }
+        values['policy'] = json.dumps(policy, sort_keys=True)
+        values['depends_on'] = [bucket_tf_resource]
+        bucket_policy_tf_resource = aws_s3_bucket_policy(identifier, **values)
+        tf_resources.append(bucket_policy_tf_resource)
+
+        # cloud front distribution
+        values = {}
+
+
+        cf_distribution_tf_resource = \
+            aws_cloudfront_distribution(identifier, **values)
+        tf_resources.append(cf_distribution_tf_resource)
+
+        for tf_resource in tf_resources:
+            self.add_resource(account, tf_resource)
 
     @staticmethod
     def get_tf_iam_access_key(user_tf_resource, identifier, output_prefix):
