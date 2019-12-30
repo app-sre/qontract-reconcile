496c1371c959072895c51375df61a5cd8f64ee23
Maor Friedman <maor.friedman@redhat.com>
2019-12-30 11:42:09 +0200

    fix tests

--- reconcile/service_dependencies.py
+++ reconcile/service_dependencies.py
@@ -46,12 +46,12 @@ def get_desired_dependency_names(app, dependency_map):
     code_components = app.get('codeComponents')
     if code_components:
         gitlab_urls = [cc for cc in code_components
-                        if 'gitlab' in cc['url']]
+                       if 'gitlab' in cc['url']]
         if gitlab_urls:
             required_dep_names.update(
                 get_dependency_names(dependency_map, 'gitlab'))
         github_urls = [cc for cc in code_components
-                        if 'github' in cc['url']]
+                       if 'github' in cc['url']]
         if github_urls:
             required_dep_names.update(
                 get_dependency_names(dependency_map, 'github'))
@@ -66,7 +66,7 @@ def get_desired_dependency_names(app, dependency_map):
         required_dep_names.update(
             get_dependency_names(dependency_map, 'openshift'))
         tf_namespaces = [n for n in namespaces
-                            if n.get('managedTerraformResources')]
+                         if n.get('managedTerraformResources')]
         if tf_namespaces:
             required_dep_names.update(
                 get_dependency_names(dependency_map, 'aws'))
@@ -89,8 +89,8 @@ def run(dry_run=False):
         current_deps = \
             [a['name'] for a in app_deps] if app_deps else []
         desired_deps = get_desired_dependency_names(app, dependency_map)
-        missing_deps = list(desired_deps.difference(current_deps))
 
+        missing_deps = list(desired_deps.difference(current_deps))
         if missing_deps:
             error = True
             msg = f"App '{app_name}' has missing dependencies: {missing_deps}"
