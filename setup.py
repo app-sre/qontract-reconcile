e2070877fe641826cb5e9123567a7be4e0497d12
Maor Friedman <maor.friedman@redhat.com>
2020-02-11 10:15:56 +0200

    revert anymarkup for now

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         "requests>=2.21.0,<2.22.0",
         "hvac>=0.7.0,<0.8.0",
         "ldap3>=2.5.2,<2.6.0",
-        "anymarkup>=0.8.0,<0.9.0",
+        "anymarkup>=0.7.0,<0.8.0",
         "python-gitlab>=1.11.0,<1.12.0",
         "semver>=2.8.0,<2.9.0",
         "python-terraform>=0.10.0,<0.11.0",
