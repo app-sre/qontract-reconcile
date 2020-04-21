9ee977f36a98eee55abcec861185690bb2f1d874
Maor Friedman <maor.friedman@redhat.com>
2020-04-21 13:33:00 +0300

    openshift-saas-deploy fetch desired state with mutlithreads (#683)

--- utils/saasherder.py
+++ utils/saasherder.py
@@ -6,6 +6,7 @@ import logging
 from github import Github
 from sretoolbox.container import Image
 
+import utils.threaded as threaded
 import utils.secret_reader as secret_reader
 
 from utils.oc import OC, StatusCodeError
@@ -19,11 +20,13 @@ class SaasHerder():
     """Wrapper around SaaS deployment actions."""
 
     def __init__(self, saas_files,
+                 thread_pool_size,
                  gitlab,
                  integration,
                  integration_version,
                  settings):
         self.saas_files = saas_files
+        self.thread_pool_size = thread_pool_size
         self.gitlab = gitlab
         self.integration = integration
         self.integration_version = integration_version
@@ -59,10 +62,14 @@ class SaasHerder():
                 parameters[k] = 'false'
         return parameters
 
-    def _get_file_contents(self, url, path, ref):
+    def _get_file_contents(self, options):
+        url = options['url']
+        path = options['path']
+        ref = options['ref']
+        github = options['github']
         if 'github' in url:
             repo_name = url.rstrip("/").replace('https://github.com/', '')
-            repo = self.github.get_repo(repo_name)
+            repo = github.get_repo(repo_name)
             f = repo.get_contents(path, ref)
             return f.decoded_content, f.html_url
         elif 'gitlab' in url:
@@ -71,11 +78,15 @@ class SaasHerder():
             html_url = os.path.join(url, 'blob', ref, path)
             return f.decode(), html_url
 
-    def _get_commit_sha(self, url, ref, hash_length):
+    def _get_commit_sha(self, options):
+        url = options['url']
+        ref = options['ref']
+        hash_length = options['hash_length']
+        github = options['github']
         commit_sha = ''
         if 'github' in url:
             repo_name = url.rstrip("/").replace('https://github.com/', '')
-            repo = self.github.get_repo(repo_name)
+            repo = github.get_repo(repo_name)
             commit = repo.get_commit(sha=ref)
             commit_sha = commit.sha
         elif 'gitlab' in url:
@@ -90,17 +101,31 @@ class SaasHerder():
         namespace = target['namespace']['name']
         return cluster, namespace
 
-    def _process_template(self, saas_file_name, resource_template_name,
-                          url, path, hash_length, target, parameters):
+    def _process_template(self, options):
+        saas_file_name = options['saas_file_name']
+        resource_template_name = options['resource_template_name']
+        url = options['url']
+        path = options['path']
+        hash_length = options['hash_length']
+        target = options['target']
+        parameters = options['parameters']
+        github = options['github']
         target_hash = target['hash']
         target_parameters = self._collect_parameters(target)
         target_parameters.update(parameters)
 
         try:
-            content, html_url = self._get_file_contents(url, path, target_hash)
+            get_file_contents_options = {
+                'url': url,
+                'path': path,
+                'ref': target_hash,
+                'github': github
+            }
+            content, html_url = \
+                self._get_file_contents(get_file_contents_options)
         except Exception as e:
             logging.error(
-                f"[{url}/{path}:{target_hash}]" +
+                f"[{url}/{path}:{target_hash}] " +
                 f"error fetching template: {str(e)}")
             return None, None
 
@@ -109,9 +134,13 @@ class SaasHerder():
             for template_parameter in template['parameters']:
                 if template_parameter['name'] == 'IMAGE_TAG':
                     # add IMAGE_TAG only if it is required
-                    image_tag = self._get_commit_sha(url,
-                                                     target_hash,
-                                                     hash_length)
+                    get_commit_sha_options = {
+                        'url': url,
+                        'ref': target_hash,
+                        'hash_length': hash_length,
+                        'github': github
+                    }
+                    image_tag = self._get_commit_sha(get_commit_sha_options)
                     target_parameters['IMAGE_TAG'] = image_tag
         oc = OC('server', 'token')
         try:
@@ -133,28 +162,25 @@ class SaasHerder():
 
         return images
 
-    def _check_images(self, saas_file_name, resource_template_name,
-                      html_url, resource):
+    def _check_images(self, options):
+        saas_file_name = options['saas_file_name']
+        resource_template_name = options['resource_template_name']
+        html_url = options['html_url']
+        resource = options['resource']
+        image_auth = options['image_auth']
         error_prefix = \
             f"[{saas_file_name}/{resource_template_name}] {html_url}:"
         error = False
         images = self._collect_images(resource)
-        if self.image_auth:
-            username = self.image_auth['user']
-            password = self.image_auth['token']
+        if image_auth:
+            username = image_auth['user']
+            password = image_auth['token']
         else:
             username = None
             password = None
         for image in images:
             try:
                 valid = Image(image, username=username, password=password)
-                if not valid:
-                    # check image again without auth, just in case
-                    # only check again if image_auth is defined
-                    # otherwise, we have done that already
-                    if self.image_auth:
-                        valid = Image(image)
-
                 if not valid:
                     error = True
                     logging.error(
@@ -190,57 +216,74 @@ class SaasHerder():
         return creds
 
     def populate_desired_state(self, ri):
-        for saas_file in self.saas_files:
-            saas_file_name = saas_file['name']
-            self.github = self._initiate_github(saas_file)
-            self.image_auth = self._initiate_image_auth(saas_file)
-            managed_resource_types = saas_file['managedResourceTypes']
-            resource_templates = saas_file['resourceTemplates']
-            # iterate over resource templates (multiple per saas_file)
-            for rt in resource_templates:
-                rt_name = rt['name']
-                url = rt['url']
-                path = rt['path']
-                hash_length = rt['hash_length']
-                parameters = self._collect_parameters(rt)
-                # iterate over targets (each target is a namespace)
-                for target in rt['targets']:
-                    cluster, namespace = \
-                        self._get_cluster_and_namespace(target)
-                    resources, html_url = \
-                        self._process_template(saas_file_name,
-                                               rt_name,
-                                               url, path, hash_length,
-                                               target, parameters)
-                    if resources is None:
+        threaded.run(self.populate_desired_state_saas_file,
+                     self.saas_files,
+                     self.thread_pool_size,
+                     ri=ri)
+
+    def populate_desired_state_saas_file(self, saas_file, ri):
+        saas_file_name = saas_file['name']
+        logging.debug(f"populating desired state for {saas_file_name}")
+        github = self._initiate_github(saas_file)
+        image_auth = self._initiate_image_auth(saas_file)
+        managed_resource_types = saas_file['managedResourceTypes']
+        resource_templates = saas_file['resourceTemplates']
+        # iterate over resource templates (multiple per saas_file)
+        for rt in resource_templates:
+            rt_name = rt['name']
+            url = rt['url']
+            path = rt['path']
+            hash_length = rt['hash_length']
+            parameters = self._collect_parameters(rt)
+            # iterate over targets (each target is a namespace)
+            for target in rt['targets']:
+                cluster, namespace = \
+                    self._get_cluster_and_namespace(target)
+                process_template_options = {
+                    'saas_file_name': saas_file_name,
+                    'resource_template_name': rt_name,
+                    'url': url,
+                    'path': path,
+                    'hash_length': hash_length,
+                    'target': target,
+                    'parameters': parameters,
+                    'github': github
+                }
+                resources, html_url = \
+                    self._process_template(process_template_options)
+                if resources is None:
+                    ri.register_error()
+                    continue
+                # add desired resources
+                for resource in resources:
+                    resource_kind = resource['kind']
+                    if resource_kind not in managed_resource_types:
+                        continue
+                    # check images
+                    check_images_options = {
+                        'saas_file_name': saas_file_name,
+                        'resource_template_name': rt_name,
+                        'html_url': html_url,
+                        'resource': resource,
+                        'image_auth': image_auth
+                    }
+                    image_error = self._check_images(check_images_options)
+                    if image_error:
                         ri.register_error()
                         continue
-                    # add desired resources
-                    for resource in resources:
-                        resource_kind = resource['kind']
-                        if resource_kind not in managed_resource_types:
-                            continue
-                        # check images
-                        image_error = self._check_images(saas_file_name,
-                                                         rt_name,
-                                                         html_url,
-                                                         resource)
-                        if image_error:
-                            ri.register_error()
-                            continue
-                        resource_name = resource['metadata']['name']
-                        oc_resource = OR(
-                            resource,
-                            self.integration,
-                            self.integration_version,
-                            error_details=html_url)
-                        ri.add_desired(
-                            cluster,
-                            namespace,
-                            resource_kind,
-                            resource_name,
-                            oc_resource
-                        )
+                    resource_name = resource['metadata']['name']
+                    oc_resource = OR(
+                        resource,
+                        self.integration,
+                        self.integration_version,
+                        error_details=html_url)
+                    ri.add_desired(
+                        cluster,
+                        namespace,
+                        resource_kind,
+                        resource_name,
+                        oc_resource
+                    )
 
     def _init_slack(self, slack_info):
         slack_integrations = slack_info['workspace']['integrations']
@@ -279,7 +322,7 @@ class SaasHerder():
             settings=self.settings
         )
         for saas_file in self.saas_files:
-            self.github = self._initiate_github(saas_file)
+            github = self._initiate_github(saas_file)
             saas_file_name = saas_file['name']
             for resource_template in saas_file['resourceTemplates']:
                 url = resource_template['url']
@@ -291,8 +334,14 @@ class SaasHerder():
                     cluster, namespace = \
                         self._get_cluster_and_namespace(target)
                     target_hash = target['hash']
+                    get_commit_sha_options = {
+                        'url': url,
+                        'ref': target_hash,
+                        'hash_length': hash_length,
+                        'github': github
+                    }
                     desired_commit_sha = \
-                        self._get_commit_sha(url, target_hash, hash_length)
+                        self._get_commit_sha(get_commit_sha_options)
                     state_key_format = "{}/{}/{}/{}"
                     state_key = state_key_format.format(
                         saas_file_name,
