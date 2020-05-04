830b98265cd536b6e7d945c953cfb2fe11d86e7d
Tejas Parikh <tparikh@redhat.com>
2020-05-04 12:46:02 -0400

    elasticsearch integration for app-interface

---
+++ utils/elasticsearch_exceptions.py
@@ -0,0 +1,19 @@
+class ElasticSearchResourceNameInvalidError(Exception):
+    def __init__(self, msg):
+        super(ElasticSearchResourceNameInvalidError, self).__init__(
+            str(msg)
+        )
+
+
+class ElasticSearchResourceMissingSubnetIdError(Exception):
+    def __init__(self, msg):
+        super(ElasticSearchResourceMissingSubnetIdError, self).__init__(
+            str(msg)
+        )
+
+
+class ElasticSearchResourceVersionInvalidError(Exception):
+    def __init__(self, msg):
+        super(ElasticSearchResourceVersionInvalidError, self).__init__(
+            str(msg)
+        )
