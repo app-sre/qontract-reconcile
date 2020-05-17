ee7b132559857c2b82e62b73658e53c7ca34d267
Maor Friedman <maor.friedman@redhat.com>
2020-05-17 11:26:59 +0300

    [jenkins-webhooks-cleaner] introduce new integration (#768)

---
+++ reconcile/jenkins_webhooks_cleaner.py
@@ -0,0 +1,25 @@
+import logging
+
+import reconcile.queries as queries
+
+from utils.gitlab_api import GitLabApi
+
+QONTRACT_INTEGRATION = 'jenkins-webhooks-cleaner'
+
+
+def run(dry_run=False):
+    instance = queries.get_gitlab_instance()
+    settings = queries.get_app_interface_settings()
+    gl = GitLabApi(instance, settings=settings)
+    previous_urls = queries.get_jenkins_instances_previous_urls()
+    repos = queries.get_repos(server=gl.server)
+
+    for repo in repos:
+        hooks = gl.get_project_hooks(repo)
+        for hook in hooks:
+            hook_url = hook.url
+            for previous_url in previous_urls:
+                if hook_url.startswith(previous_url):
+                    logging.info(['delete_hook', repo, hook_url])
+                    if not dry_run:
+                        hook.delete()
