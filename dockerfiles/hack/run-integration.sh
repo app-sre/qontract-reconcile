88699c17c4be5fc55f96728a4f00fec6306614d6
Maor Friedman <maor.friedman@redhat.com>
2020-06-03 15:15:31 +0300

    cronjob run once

--- dockerfiles/hack/run-integration.sh
+++ dockerfiles/hack/run-integration.sh
@@ -6,6 +6,10 @@ while true; do
     qontract-reconcile --config /config/config.toml $DRY_RUN $INTEGRATION_NAME $INTEGRATION_EXTRA_ARGS 2>&1 | tee -a $LOG_FILE
     STATUS=$?
 
+    if [ -n "${RUN_ONCE}" ]; then
+        exit $STATUS
+    fi
+
     if [ $STATUS -ne 3 ]; then
         [ $STATUS -ne 0 ] && exit $STATUS
         sleep ${SLEEP_DURATION_SECS}
