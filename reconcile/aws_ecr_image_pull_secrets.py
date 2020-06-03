f77642230c7dbe4310a4cc3f9b8f837ae08a1ee9
Maor Friedman <maor.friedman@redhat.com>
2020-06-03 16:16:04 +0300

    aws-ecr-image-pull-secrets adjust secrets (#793)

--- reconcile/aws_ecr_image_pull_secrets.py
+++ reconcile/aws_ecr_image_pull_secrets.py
@@ -14,27 +14,39 @@ def enc_dec(data):
     return base64.b64encode(data.encode('utf-8')).decode('utf-8')
 
 
-def construct_secret_data(data):
+def get_password(token):
+    return base64.b64decode(token).decode('utf-8').split(':')[1]
+
+
+def construct_dockercfg_secret_data(data):
     auth_data = data['authorizationData'][0]
     server = auth_data['proxyEndpoint']
     token = auth_data['authorizationToken']
+    password = get_password(token)
     data = {
         'auths': {
             server: {
                 'username': 'AWS',
-                'password': token,
+                'password': password,
                 'email': '',
-                'auth': enc_dec(f'AWS:{token}')
+                'auth': token
             }
         }
     }
 
-    return {'.dockerconfigjson': enc_dec(json.dumps(data))}
+    return {'.dockercfg': enc_dec(json.dumps(data))}
 
 
-def write_output_to_vault(dry_run, vault_path, account, secret_data):
+def construct_basic_auth_secret_data(data):
+    auth_data = data['authorizationData'][0]
+    token = auth_data['authorizationToken']
+    password = get_password(token)
+    return {'username': enc_dec('AWS'), 'token': enc_dec(password)}
+
+
+def write_output_to_vault(dry_run, vault_path, account, secret_data, name):
     integration_name = QONTRACT_INTEGRATION
-    secret_path = f"{vault_path}/{integration_name}/{account}"
+    secret_path = f"{vault_path}/{integration_name}/{account}/{name}"
     secret = {'path': secret_path, 'data': secret_data}
     logging.info(['write_secret', secret_path])
     if not dry_run:
@@ -47,6 +59,9 @@ def run(dry_run=False, vault_output_path=''):
     aws = AWSApi(1, accounts, settings=settings)
     tokens = aws.get_ecr_auth_tokens()
     for account, data in tokens.items():
-        secret_data = construct_secret_data(data)
+        dockercfg_secret_data = construct_dockercfg_secret_data(data)
+        basic_auth_secret_data = construct_basic_auth_secret_data(data)
         write_output_to_vault(dry_run, vault_output_path,
-                              account, secret_data)
+                              account, dockercfg_secret_data, 'dockercfg')
+        write_output_to_vault(dry_run, vault_output_path,
+                              account, basic_auth_secret_data, 'basic-auth')
