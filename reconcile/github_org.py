c9fd7995036840e32255ae5a7ae68d41f7548905
Maor Friedman <maor.friedman@redhat.com>
2020-07-10 14:11:31 +0300

    fix tests

--- reconcile/github_org.py
+++ reconcile/github_org.py
@@ -11,7 +11,6 @@ import reconcile.queries as queries
 
 from utils.aggregated_list import AggregatedList, AggregatedDiffRunner
 from utils.raw_github_api import RawGithubApi
-from utils.defer import defer
 
 
 GH_BASE_URL = os.environ.get('GITHUB_API', 'https://api.github.com')
@@ -184,7 +183,6 @@ def fetch_desired_state(infer_clusters=True):
         return state
 
     clusters = gqlapi.query(CLUSTERS_QUERY)['clusters']
-    settings = queries.get_app_interface_settings()
     openshift_users_desired_state = \
         openshift_users.fetch_desired_state(oc_map=None)
     for cluster in clusters:
