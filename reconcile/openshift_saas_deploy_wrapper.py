0de454d433cc05f52a060222a356eed5330c7ca9
Maor Friedman <maor.friedman@redhat.com>
2020-07-29 09:30:53 +0300

    openshift-saas-deploy don't hang when thread exits

--- reconcile/openshift_saas_deploy_wrapper.py
+++ reconcile/openshift_saas_deploy_wrapper.py
@@ -1,3 +1,4 @@
+import sys
 import semver
 
 import reconcile.openshift_saas_deploy as osd
@@ -13,10 +14,15 @@ QONTRACT_INTEGRATION_VERSION = semver.format_version(0, 1, 0)
 def osd_run_wrapper(diff, dry_run, available_thread_pool_size):
     saas_file_name = diff['saas_file_name']
     env_name = diff['environment']
-    osd.run(dry_run=dry_run,
-            thread_pool_size=available_thread_pool_size,
-            saas_file_name=saas_file_name,
-            env_name=env_name)
+    err = False
+    try:
+        osd.run(dry_run=dry_run,
+                thread_pool_size=available_thread_pool_size,
+                saas_file_name=saas_file_name,
+                env_name=env_name)
+    except SystemExit:
+        err = True
+    return err
 
 
 def run(dry_run, thread_pool_size=10, io_dir='throughput/'):
@@ -29,6 +35,11 @@ def run(dry_run, thread_pool_size=10, io_dir='throughput/'):
             thread_pool_size,
             len(saas_file_owners_diffs))
 
-    threaded.run(osd_run_wrapper, saas_file_owners_diffs, thread_pool_size,
-                 dry_run=dry_run,
-                 available_thread_pool_size=available_thread_pool_size)
+    errors = threaded.run(
+        osd_run_wrapper, saas_file_owners_diffs, thread_pool_size,
+        dry_run=dry_run,
+        available_thread_pool_size=available_thread_pool_size
+    )
+
+    if True in errors:
+        sys.exit(1)
