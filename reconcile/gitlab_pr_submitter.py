abe4ac54c577f3c40a60968f8a2ce71bb45c2017
Amador Pahim <apahim@redhat.com>
2020-09-04 11:16:21 +0100

    Removing gitlab_pr_submitter
    This was replaced by the gitlab-mr-sqs-consumer

--- reconcile/gitlab_pr_submitter.py
+++
@@ -1,7 +0,0 @@
-import reconcile.pull_request_gateway as prg
-
-QONTRACT_INTEGRATION = 'gitlab-pr-submitter'
-
-
-def run(dry_run, gitlab_project_id):
-    prg.submit_to_gitlab(gitlab_project_id, dry_run)
