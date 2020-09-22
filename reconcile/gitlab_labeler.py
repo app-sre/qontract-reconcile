f24ec4c5b07296c54e6a5b7f5bde4e1a20ad81bb
Feng Huang <fehuang@redhat.com>
2020-09-22 11:43:54 -0400

    gitlab-labeler add ignore_tokens (#1081)
    Signed-off-by: Feng Huang <fehuang@redhat.com>

--- reconcile/gitlab_labeler.py
+++ reconcile/gitlab_labeler.py
@@ -17,7 +17,7 @@ def guess_labels(project_labels, changed_paths):
     This is the first form of guessing, which will likely be adjusted.
     """
     not_allowed_labels = MERGE_LABELS_PRIORITY + HOLD_LABELS
-    ignore_tokens = ['cicd']
+    ignore_tokens = ['cicd', 'saas']
     guesses = set()
     for label in project_labels:
         if label in not_allowed_labels:
