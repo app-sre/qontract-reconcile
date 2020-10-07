600ab86bacb03a267b51a00d0bead0882427facc
Maor Friedman <maor.friedman@redhat.com>
2020-10-07 11:18:50 +0300

    slack-cluster-usergroups do not include users with tagged roles

--- reconcile/slack_cluster_usergroups.py
+++ reconcile/slack_cluster_usergroups.py
@@ -10,9 +10,50 @@ from utils.slack_api import UsergroupNotFoundException
 QONTRACT_INTEGRATION = 'slack-cluster-usergroups'
 
 
+def include_user(user, cluster_name, cluster_users):
+    # if user does not have access to the cluster
+    if user['github_username'] not in cluster_users:
+        return False
+    # do nothing when tag_on_cluster_updates is not defined
+    tag_on_cluster_updates = user.get('tag_on_cluster_updates')
+    if tag_on_cluster_updates is True:
+        return True
+    elif tag_on_cluster_updates is False:
+        return False
+
+    # if a user has access via a role
+    # check if that role grants access to the current cluster
+    # if all roles that grant access to the current cluster also
+    # have 'tag_on_cluster_updates: false' - remove the user
+    role_result = None
+    for role in user['roles']:
+        access = role.get('access')
+        if not access:
+            continue
+        for a in access:
+            cluster = a.get('cluster')
+            if cluster:
+                if cluster['name'] == cluster_name:
+                    if role.get('tag_on_cluster_updates') is False:
+                        role_result = role_result or False
+                    else:
+                        role_result = True
+                continue
+            namespace = a.get('namespace')
+            if namespace:
+                if namespace['cluster']['name'] == cluster_name:
+                    if role.get('tag_on_cluster_updates') is False:
+                        role_result = role_result or False
+                    else:
+                        role_result = True
+        
+    result = False if role_result is False else True
+    return result
+
+
 def get_desired_state(slack):
     desired_state = []
-    all_users = queries.get_users()
+    all_users = queries.get_roles()
     all_clusters = queries.get_clusters(minimal=True)
     clusters = [c for c in all_clusters
                 if c.get('auth') and c['auth'].get('team')
@@ -29,9 +70,9 @@ def get_desired_state(slack):
         except UsergroupNotFoundException:
             logging.warning(f'Usergroup {usergroup} not found')
             continue
-        user_names = [slack_usergroups.get_slack_username(u) for u in all_users
-                      if u['github_username'] in cluster_users
-                      and u.get('tag_on_cluster_updates') is not False]
+        user_names = [slack_usergroups.get_slack_username(u)
+                      for u in all_users
+                      if include_user(u, cluster_name, cluster_users)]
         users = slack.get_users_by_names(user_names)
         channels = slack.get_channels_by_names([slack.chat_kwargs['channel']])
         desired_state.append({
