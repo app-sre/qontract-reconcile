6dc42e9d7431758012ebd22b1eef7d83c4971d52
Ari LiVigni <arilivigni@gmail.com>
2020-11-01 20:45:18 -0500

    Removed white space

--- utils/oc.py
+++ utils/oc.py
@@ -70,7 +70,7 @@ class OC(object):
             oc_base_cmd = self.jump_host.get_ssh_base_cmd() + oc_base_cmd
 
         self.oc_base_cmd = oc_base_cmd
-        # calling get_cluster_info to check if cluster is reachable	
+        # calling get_cluster_info to check if cluster is reachable
         self.get_cluster_info()
         self.init_projects = init_projects
         if self.init_projects:
@@ -239,8 +239,8 @@ class OC(object):
         results = self._run(cmd).decode('utf-8').split('\n')
         return [r.split()[-1] for r in results]
 
-    def get_cluster_info(self):	
-        cmd = ['cluster-info']	
+    def get_cluster_info(self):
+        cmd = ['cluster-info']
         return self._run(cmd)
 
     @retry(exceptions=(JobNotRunningError), max_attempts=20)
