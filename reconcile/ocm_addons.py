61d2e5f647481bd94c0c608dab802eb85820d247
Maor Friedman <maor.friedman@redhat.com>
2020-11-16 14:24:39 +0200

    check if addon exists before install

--- reconcile/ocm_addons.py
+++ reconcile/ocm_addons.py
@@ -53,15 +53,23 @@ def calculate_diff(current_state, desired_state):
 
 
 def act(dry_run, diffs, ocm_map):
+    err = False
     for diff in diffs:
         action = diff.pop('action')
         cluster = diff.pop('cluster')
-        logging.info([action, cluster, diff['id']])
+        addon_id = diff['id']
+        logging.info([action, cluster, addon_id])
         if not dry_run:
             ocm = ocm_map.get(cluster)
+            if not ocm.get_addon(addon_id):
+                logging.error(f'Addon {addon_id} does not exist')
+                err = True
+                continue
             if action == 'install':
                 ocm.install_addon(cluster, diff)
             # uninstall is not supported
+
+    return err
 
 
 def run(dry_run, gitlab_project_id=None, thread_pool_size=10):
@@ -74,4 +82,7 @@ def run(dry_run, gitlab_project_id=None, thread_pool_size=10):
     ocm_map, current_state = fetch_current_state(clusters)
     desired_state = fetch_desired_state(clusters)
     diffs = calculate_diff(current_state, desired_state)
-    act(dry_run, diffs, ocm_map)
+    err = act(dry_run, diffs, ocm_map)
+
+    if err:
+        sys.exit(ExitCodes.ERROR)
