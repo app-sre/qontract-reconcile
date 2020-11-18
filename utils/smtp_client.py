9c2495d51fbb2848d41b3f827457813e5efedbc8
Amador Pahim <apahim@redhat.com>
2020-11-18 12:48:26 +0000

    SmtpClient refactoring
    This is yet another refactoring to avoid the global initialization of
    objects.
    
    Signed-off-by: Amador Pahim <apahim@redhat.com>

--- utils/smtp_client.py
+++ utils/smtp_client.py
@@ -11,137 +11,79 @@ from email.mime.text import MIMEText
 from email.header import Header
 from email.utils import formataddr
 
-_client = None
-_server = None
-_username = None
-_mail_address = None
 
+class SmtpClient:
+    def __init__(self, settings):
+        config = get_config()
 
-def init(host, port, username, password, client_only):
-    global _client
-    global _server
+        smtp_secret_path = config['smtp']['secret_path']
+        smtp_config = self.get_smtp_config(smtp_secret_path, settings)
+        self.host = smtp_config['server']
+        self.port = str(smtp_config['port'])
+        self.user = smtp_config['username']
+        self.passwd = smtp_config['password']
+        self.mail_address = config['smtp']['mail_address']
 
-    if _client is None:
-        s = smtplib.SMTP(
-            host=host,
-            port=str(port)
-        )
-        s.send
-        s.starttls()
-        s.login(username, password)
-        _client = s
-    if _server is None and not client_only:
-        s = imaplib.IMAP4_SSL(
-            host=host
-        )
-        s.login(username, password)
-        _server = s
+        self.client = smtplib.SMTP(host=self.host,
+                                   port=self.port)
+        self.client.send
+        self.client.starttls()
+        self.client.login(self.user, self.passwd)
 
-    return _client, _server
+        self._server = None
 
+    @property
+    def server(self):
+        if self._server is None:
+            self._server = imaplib.IMAP4_SSL(host=self.host)
+            self._server.login(self.user, self.passwd)
+        return self._server
 
-def teardown():
-    global _client
+    def get_smtp_config(self, path, settings):
+        config = {}
+        required_keys = ('password', 'port', 'require_tls', 'server',
+                         'username')
+        secret_reader = SecretReader(settings=settings)
+        data = secret_reader.read_all({'path': path})
+        try:
+            for k in required_keys:
+                config[k] = data[k]
+        except KeyError as e:
+            raise Exception(f'Missing expected SMTP config '
+                            f'key in vault secret: {e}')
+        return config
 
-    _client.quit()
+    def get_mails(self, folder='INBOX', criteria='ALL'):
+        self.server.select(f'"{folder}"')
+        result, data = self.server.uid('search', None, criteria)
+        uids = [s for s in data[0].split()]
+        results = []
+        for uid in uids:
+            result, data = self.server.uid('fetch', uid, '(RFC822)')
+            msg = data[0][1].decode('utf-8')
+            results.append({'uid': uid, 'msg': msg})
+        return results
 
-
-def init_from_config(settings, client_only=True):
-    global _username
-    global _mail_address
-
-    config = get_config()
-    smtp_secret_path = config['smtp']['secret_path']
-    smtp_config = get_smtp_config(smtp_secret_path, settings)
-    host = smtp_config['server']
-    port = smtp_config['port']
-    _username = smtp_config['username']
-    password = smtp_config['password']
-    _mail_address = config['smtp']['mail_address']
-
-    return init(host, port, _username, password, client_only=client_only)
-
-
-def get_smtp_config(path, settings):
-    config = {}
-
-    required_keys = ('password', 'port', 'require_tls', 'server', 'username')
-    secret_reader = SecretReader(settings=settings)
-    data = secret_reader.read_all({'path': path})
-
-    try:
-        for k in required_keys:
-            config[k] = data[k]
-    except KeyError as e:
-        raise Exception("Missing expected SMTP config key in vault secret: {}"
-                        .format(e))
-
-    return config
-
-
-def get_mails(folder='INBOX', criteria='ALL', settings=None):
-    global _server
-
-    if _server is None:
-        init_from_config(settings, client_only=False)
-
-    _server.select(f'"{folder}"')
-
-    result, data = _server.uid('search', None, criteria)
-    uids = [s for s in data[0].split()]
-    results = []
-    for uid in uids:
-        result, data = _server.uid('fetch', uid, '(RFC822)')
-        msg = data[0][1].decode('utf-8')
-        results.append({'uid': uid, 'msg': msg})
-
-    return results
-
-
-@retry()
-def send_mail(names, subject, body, settings=None):
-    global _client
-    global _username
-    global _mail_address
-
-    if _client is None:
-        init_from_config(settings)
-
-    msg = MIMEMultipart()
-    from_name = str(Header('App SRE team automation', 'utf-8'))
-    msg['From'] = formataddr((from_name, _username))
-    to = set()
-    for name in names:
-        if '@' in name:
-            to.add(name)
-        else:
-            to.add(f"{name}@{_mail_address}")
-    msg['To'] = ', '.join(to)
-    msg['Subject'] = subject
-
-    # add in the message body
-    msg.attach(MIMEText(body, 'plain'))
-
-    # send the message via the server set up earlier.
-    _client.sendmail(_username, to, msg.as_string())
-
-
-def send_mails(mails, settings=None):
-    global _client
-
-    init_from_config(settings)
-    try:
+    def send_mails(self, mails):
         for name, subject, body in mails:
-            send_mail([name], subject, body)
-    finally:
-        teardown()
+            self.send_mail([name], subject, body)
 
+    @retry()
+    def send_mail(self, names, subject, body):
+        msg = MIMEMultipart()
+        from_name = str(Header('App SRE team automation', 'utf-8'))
+        msg['From'] = formataddr((from_name, self.user))
+        to = set()
+        for name in names:
+            if '@' in name:
+                to.add(name)
+            else:
+                to.add(f"{name}@{self.mail_address}")
+        msg['To'] = ', '.join(to)
+        msg['Subject'] = subject
+        # add in the message body
+        msg.attach(MIMEText(body, 'plain'))
+        self.client.sendmail(self.user, to, msg.as_string())
 
-def get_recepient(org_username, settings):
-    global _client
-    global _mail_address
-
-    if _client is None:
-        init_from_config(settings)
-
-    return f"{org_username}@{_mail_address}"
+    def get_recipient(self, org_username):
+        return f'{org_username}@{self.mail_address}'
