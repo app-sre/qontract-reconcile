0c6db556498015747390fc3dcb1da5fc13a6afa5
Amador Pahim <apahim@redhat.com>
2020-11-30 13:08:38 +0000

    Collecting the commit info in a shared object
    Signed-off-by: Amador Pahim <apahim@redhat.com>

--- reconcile/status.py
+++ reconcile/status.py
@@ -4,3 +4,19 @@ class ExitCodes:
     DATA_CHANGED = 3
     INTEGRATION_NOT_FOUND = 4
     FORBIDDEN_SCHEMA = 5
+
+
+class _RunningState:
+
+    _state = {}
+
+    def __init__(self):
+        self.__dict__ = self._state
+
+
+class RunningState(_RunningState):
+    """
+    Simple Borg class to share information about
+    the running state. Attributes will be populated
+    by the callers.
+    """
