7af52ff18818add2df852a6a174cb90503067de4
Jaime Melis <jmelis@redhat.com>
2021-03-04 12:36:38 +0100

    Revert "Sticky sessions (#1416)" (#1429)
    This reverts commit d7215a5b3758587ab182e414b40d6b5cf323e880.

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
     install_requires=[
         "sretoolbox==0.9.2",
         "Click>=7.0,<8.0",
+        "graphqlclient>=0.2.4,<0.3.0",
         "toml>=0.10.0,<0.11.0",
         "jsonpath-rw>=1.4.0,<1.5.0",
         "PyGithub>=1.47,<1.48",
