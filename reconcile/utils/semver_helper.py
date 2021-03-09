430bdb87b533144eb3b2dc4c8939b4f504a7b5a2
Jaime Melis <jmelis@redhat.com>
2021-03-09 18:38:05 +0100

    typo

--- reconcile/utils/semver_helper.py
+++ reconcile/utils/semver_helper.py
@@ -2,4 +2,4 @@ import semver
 
 
 def make_semver(major, minor, patch):
-    return str(semver.VersionInfo.parse(major=major, minor=minor, patch=patch))
+    return str(semver.VersionInfo(major=major, minor=minor, patch=patch))
