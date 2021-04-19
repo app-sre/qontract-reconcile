3790a26caa1c892e11236759a2ef03a2d0b7b3be
Feng Huang <fehuang@redhat.com>
2021-04-19 17:06:44 -0400

    fix KeyError of use_channel_in_image_tag
    Signed-off-by: Feng Huang <fehuang@redhat.com>

--- reconcile/saas_file_owners.py
+++ reconcile/saas_file_owners.py
@@ -62,7 +62,8 @@ def collect_state():
         saas_file_definitions = {
             'managed_resource_types': saas_file['managedResourceTypes'],
             'image_patterns': saas_file['imagePatterns'],
-            'use_channel_in_image_tag': saas_file['use_channel_in_image_tag'],
+            'use_channel_in_image_tag':
+                saas_file.get('use_channel_in_image_tag', False),
         }
         resource_templates = saas_file['resourceTemplates']
         for resource_template in resource_templates:
