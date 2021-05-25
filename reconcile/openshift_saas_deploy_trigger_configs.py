d3642c45e747ae35f21d0eb5d24329c7b56e08ef
Rafa Porres Molina <rporresm@redhat.com>
2021-05-25 11:44:20 +0200

    Add comment to better understand a list usage
    Signed-off-by: Rafa Porres Molina <rporresm@redhat.com>

--- reconcile/openshift_saas_deploy_trigger_configs.py
+++ reconcile/openshift_saas_deploy_trigger_configs.py
@@ -34,6 +34,8 @@ def run(dry_run, thread_pool_size=10, internal=None,
     defer(lambda: oc_map.cleanup())
 
     trigger_specs = saasherder.get_configs_diff()
+    # This will be populated by osdt_base.trigger in the below loop and
+    # we need it to be consistent across all iterations
     already_triggered = []
 
     error = True  # enter loop
