0be5c46eff82ae9ed85faf560f9c74a42bd9c5ca
Maor Friedman <maor.friedman@redhat.com>
2021-05-27 20:48:56 +0300

    [openshift-saas-deploy-trigger-configs] remove trigger loop (#1588)

--- reconcile/openshift_saas_deploy_trigger_configs.py
+++ reconcile/openshift_saas_deploy_trigger_configs.py
@@ -1,11 +1,10 @@
 import logging
 import sys
-import time
-
 
 import reconcile.queries as queries
 import reconcile.openshift_saas_deploy_trigger_base as osdt_base
 
+from reconcile.status import ExitCodes
 from reconcile.utils.defer import defer
 from reconcile.utils.semver_helper import make_semver
 
@@ -20,7 +19,7 @@ def run(dry_run, thread_pool_size=10, internal=None,
     saas_files = queries.get_saas_files(v1=True, v2=True)
     if not saas_files:
         logging.error('no saas files found')
-        sys.exit(1)
+        sys.exit(ExitCodes.ERROR)
 
     setup_options = {
         'saas_files': saas_files,
@@ -38,24 +37,22 @@ def run(dry_run, thread_pool_size=10, internal=None,
     # we need it to be consistent across all iterations
     already_triggered = []
 
-    error = True  # enter loop
-    while error:
-        error = False
-        for job_spec in trigger_specs:
-            trigger_options = {
-                'dry_run': dry_run,
-                'spec': job_spec,
-                'jenkins_map': jenkins_map,
-                'oc_map': oc_map,
-                'already_triggered': already_triggered,
-                'settings': settings,
-                'state_update_method': saasherder.update_config,
-                'integration': QONTRACT_INTEGRATION,
-                'integration_version': QONTRACT_INTEGRATION_VERSION,
-            }
-            trigger_error = osdt_base.trigger(trigger_options)
-            if trigger_error:
-                error = True
+    error = False
+    for job_spec in trigger_specs:
+        trigger_options = {
+            'dry_run': dry_run,
+            'spec': job_spec,
+            'jenkins_map': jenkins_map,
+            'oc_map': oc_map,
+            'already_triggered': already_triggered,
+            'settings': settings,
+            'state_update_method': saasherder.update_config,
+            'integration': QONTRACT_INTEGRATION,
+            'integration_version': QONTRACT_INTEGRATION_VERSION,
+        }
+        trigger_error = osdt_base.trigger(trigger_options)
+        if trigger_error:
+            error = True
 
-        if error:
-            time.sleep(10)  # add to contants module once created
+    if error:
+        sys.exit(ExitCodes.ERROR)
