34470004beb4fd37386fd517b0de259e38a6c523
Maor Friedman <maor.friedman@redhat.com>
2021-06-06 10:10:48 +0300

    [openshift-saas-deploy-triggers] threaded run + refactor (#1602)

--- reconcile/openshift_saas_deploy_trigger_upstream_jobs.py
+++ reconcile/openshift_saas_deploy_trigger_upstream_jobs.py
@@ -1,8 +1,9 @@
 import sys
 import logging
 
-import reconcile.openshift_saas_deploy_trigger_base as osdt_base
 import reconcile.queries as queries
+import reconcile.openshift_saas_deploy_trigger_base as osdt_base
+import reconcile.utils.threaded as threaded
 
 from reconcile.status import ExitCodes
 from reconcile.utils.defer import defer
@@ -28,25 +29,29 @@ def run(dry_run, thread_pool_size=10, internal=None,
         sys.exit(ExitCodes.ERROR)
     saas_files = [sf for sf in saas_files if is_in_shard(sf['name'])]
 
-    setup_options = {
-        'saas_files': saas_files,
-        'thread_pool_size': thread_pool_size,
-        'internal': internal,
-        'use_jump_host': use_jump_host,
-        'integration': QONTRACT_INTEGRATION,
-        'integration_version': QONTRACT_INTEGRATION_VERSION,
-    }
-    saasherder, jenkins_map, oc_map, settings = osdt_base.setup(setup_options)
+    saasherder, jenkins_map, oc_map, settings = \
+        osdt_base.setup(
+            saas_files=saas_files,
+            thread_pool_size=thread_pool_size,
+            internal=internal,
+            use_jump_host=use_jump_host,
+            integration=QONTRACT_INTEGRATION,
+            integration_version=QONTRACT_INTEGRATION_VERSION
+        )
     defer(lambda: oc_map.cleanup())
 
     current_state = fetch_current_state(jenkins_map)
     trigger_specs = saasherder.get_upstream_jobs_diff(dry_run, current_state)
-    already_triggered = []
-    error = False
-    for job_spec in trigger_specs:
-        trigger_error = osdt_base.trigger(
+    # This will be populated by osdt_base.trigger in the below loop and
+    # we need it to be consistent across all iterations
+    already_triggered = set()
+
+    errors = \
+        threaded.run(
+            osdt_base.trigger,
+            trigger_specs,
+            thread_pool_size,
             dry_run=dry_run,
-            spec=job_spec,
             jenkins_map=jenkins_map,
             oc_map=oc_map,
             already_triggered=already_triggered,
@@ -55,8 +60,6 @@ def run(dry_run, thread_pool_size=10, internal=None,
             integration=QONTRACT_INTEGRATION,
             integration_version=QONTRACT_INTEGRATION_VERSION
         )
-        if trigger_error:
-            error = True
 
-    if error:
+    if any(errors):
         sys.exit(ExitCodes.ERROR)
