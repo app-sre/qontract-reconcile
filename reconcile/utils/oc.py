4d15d6f8315641cecc36d0eab5b60617e141f079
Maor Friedman <maor.friedman@redhat.com>
2021-06-13 09:34:43 +0300

    use method name to log

--- reconcile/utils/oc.py
+++ reconcile/utils/oc.py
@@ -463,7 +463,7 @@ class OC:
 
     @retry(max_attempts=20)
     def validate_pod_ready(self, namespace, name):
-        logging.info(['validate_pod_ready', namespace, name])
+        logging.info([validate_pod_ready.__name__, namespace, name])
         pod = self.get(namespace, 'Pod', name)
         for status in pod['status']['containerStatuses']:
             if not status['ready']:
