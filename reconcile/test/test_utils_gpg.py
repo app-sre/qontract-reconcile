71e736f5e408875335ca2f4690711852571043ad
Luis Fernando Muñoz Mejías <lmunozme@redhat.com>
2021-06-16 18:29:36 +0200

    Add tests for gpg_encrypt
    First step on a small refactoring

--- reconcile/test/test_utils_gpg.py
+++ reconcile/test/test_utils_gpg.py
@@ -1,4 +1,7 @@
 from unittest import TestCase
+from unittest.mock import patch, MagicMock
+
+import subprocess
 
 import reconcile.utils.gpg as gpg
 
@@ -23,3 +26,32 @@ class TestGpgKeyValid(TestCase):
         valid, msg = gpg.gpg_key_valid(key)
         self.assertFalse(valid)
         self.assertEqual(msg, gpg.ERR_BASE64)
+
+
+# We have to mangle the namespace of the gpg module, since it imports
+# Popen. Had that module chosen "import subprocess;
+# subprocess.Popen(...)" we'd be patching subprocess instead.
+@patch.object(gpg, 'Popen')
+class TestGpgEncrypt(TestCase):
+    def test_gpg_encrypt_all_ok(self, popen):
+        popen.return_value.communicate.return_value = (b"stdout", b"stderr")
+        popen.return_value.returncode = 0
+
+        self.assertEqual(gpg.gpg_encrypt('acontent', 'arecipient', 'akey'),
+                         'stdout')
+
+    def test_gpg_encrypt_import_fail(self, popen):
+        popen.return_value.communicate.return_value = (b"stdout", b"stderr")
+        popen.return_value.returncode = 1
+
+        self.assertEqual(gpg.gpg_encrypt('acontent', 'arecipient', 'akey'),
+                         None)
+        popen.assert_called_once()
+
+    def test_gpg_encrypt_encrypt_fail(self, popen):
+        popen.return_value.communicate.return_value = (b"stdout", b"stderr")
+        popen.side_effect = (MagicMock(returncode=0), MagicMock(returncode=1))
+
+        self.assertEqual(gpg.gpg_encrypt('acontent', 'arecipient', 'akey'), None)
+        print(popen.call_args_list)
+        self.assertEqual(popen.call_count, 2)
