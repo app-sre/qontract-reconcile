4a709065752471cee940429e0d427e4c38fe1beb
Piojo <47329970+Piojo@users.noreply.github.com>
2021-06-17 11:18:40 +0200

    [APPSRE-3369]Improve error handling and reporting in requests_sender (#1645)
    If, instead of relying on return values we use exceptions, we can
    pass a lot of context to the part of the stack best suited to handle
    it or to produce the most useful reporting.
    
    So, I have replaced Popen with run in gpg_encrypt, so that I can
    get such an exception while removing code. I have not updated
    gpg_key_valid since I'd need to update its callers and they are out
    of the scope of this task.
    
    I have also replaced some calls to defer with a more Pythonic
    context manager. In this case I have adapted gpg_key_valid, for
    consistency and because it doesn't change its behaviour or API. This
    removes some code, more notably two imports.
    
    Finally, I have tweaked the integration's run to keep the happy path
    uninterrupted - that's the beauty of exceptions. We also guarantee
    that we perform as much work as possible, reporting failures but
    continuing processing. The dry_run flag now lets us get the encrypted credentials, skipping only the state update and email delivery. This way the flag becomes even more useful.

--- reconcile/test/test_utils_gpg.py
+++ reconcile/test/test_utils_gpg.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from unittest.mock import patch
 
 import reconcile.utils.gpg as gpg
 
@@ -23,3 +24,15 @@ class TestGpgKeyValid(TestCase):
         valid, msg = gpg.gpg_key_valid(key)
         self.assertFalse(valid)
         self.assertEqual(msg, gpg.ERR_BASE64)
+
+
+# We have to mangle the namespace of the gpg module, since it imports
+# Popen. Had that module chosen "import subprocess;
+# subprocess.Popen(...)" we'd be patching subprocess instead.
+@patch.object(gpg, 'run')
+class TestGpgEncrypt(TestCase):
+    def test_gpg_encrypt_all_ok(self, popen):
+        popen.return_value.communicate.return_value = (b"stdout", b"stderr")
+
+        self.assertEqual(gpg.gpg_encrypt('acontent', 'arecipient', 'akey'),
+                         'stdout')
