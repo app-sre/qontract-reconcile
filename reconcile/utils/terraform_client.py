918fe90a47fb92957097d0c717a9215589eb4635
Maor Friedman <maor.friedman@redhat.com>
2021-06-28 14:39:44 +0300

    terraform-resources add caller to desired resources

--- reconcile/utils/terraform_client.py
+++ reconcile/utils/terraform_client.py
@@ -264,7 +264,8 @@ class TerraformClient:
                 output_resource_name = data['{}_output_resource_name'.format(
                     self.integration_prefix)]
                 oc_resource = \
-                    self.construct_oc_resource(output_resource_name, data)
+                    self.construct_oc_resource(output_resource_name, data,
+                                               account)
                 ri.add_desired(
                     cluster,
                     namespace,
@@ -396,7 +397,7 @@ class TerraformClient:
             return data[list(data.keys())[0]]
         return data
 
-    def construct_oc_resource(self, name, data):
+    def construct_oc_resource(self, name, data, account):
         body = {
             "apiVersion": "v1",
             "kind": "Secret",
@@ -422,7 +423,8 @@ class TerraformClient:
             body['data'][k] = v
 
         return OR(body, self.integration, self.integration_version,
-                  error_details=name)
+                  error_details=name,
+                  caller_name=account)
 
     def check_output(self, name, cmd, return_code, stdout, stderr):
         error_occured = False
