a1cd670aea6dfb06d03fa3f414569bf2524b6ab6
Maor Friedman <maor.friedman@redhat.com>
2021-06-30 14:27:31 +0300

    add sharding

--- reconcile/terraform_resources_wrapper.py
+++ reconcile/terraform_resources_wrapper.py
@@ -4,6 +4,7 @@ import reconcile.queries as queries
 import reconcile.terraform_resources as tfr
 import reconcile.utils.threaded as threaded
 
+from reconcile.utils.sharding import is_in_shard_round_robin
 from reconcile.utils.semver_helper import make_semver
 from reconcile.status import ExitCodes
 
@@ -42,12 +43,20 @@ def run(dry_run, print_only=False,
         enable_deletion=False, io_dir='throughput/',
         thread_pool_size=10, internal=None, use_jump_host=True,
         light=False, vault_output_path=''):
-    account_names = [a['name'] for a in queries.get_aws_accounts()]
-    if len(account_names) == 0:
+    account_names = sorted([a['name'] for a in queries.get_aws_accounts()])
+
+    in_shard_account_names = [
+        name for name in account_names
+        if is_in_shard_round_robin(name, index)
+        for index, name in enumerate(account_names)
+    ]
+
+    if not in_shard_account_names:
+        logging.warning("No accounts in shards")
         return
 
     exit_codes = threaded.run(
-        tfr_run_wrapper, account_names, thread_pool_size,
+        tfr_run_wrapper, in_shard_account_names, thread_pool_size,
         dry_run=dry_run,
         print_only=print_only,
         enable_deletion=enable_deletion,
