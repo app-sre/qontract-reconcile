ced74fd17a189f8248d5d1e24dfb9f001975bbf8
Maor Friedman <maor.friedman@redhat.com>
2021-06-30 18:10:40 +0300

    fix tox

--- reconcile/utils/metrics.py
+++ reconcile/utils/metrics.py
@@ -3,16 +3,17 @@ from prometheus_client import Histogram
 
 
 extra_labels = {'key': None}
+label_keys = list(extra_labels.keys())
 
-run_time = Gauge(name='qontract_reconcile_last_run_seconds',
-                 documentation='Last run duration in seconds',
-                 labelnames=['integration', 'shards', 'shard_id'] + \
-                      list(extra_labels.keys()))
+run_time = Gauge(
+     name='qontract_reconcile_last_run_seconds',
+     documentation='Last run duration in seconds',
+     labelnames=['integration', 'shards', 'shard_id'] + label_keys)
 
-run_status = Gauge(name='qontract_reconcile_last_run_status',
-                   documentation='Last run status',
-                   labelnames=['integration', 'shards', 'shard_id'] + \
-                      list(extra_labels.keys()))
+run_status = Gauge(
+     name='qontract_reconcile_last_run_status',
+     documentation='Last run status',
+     labelnames=['integration', 'shards', 'shard_id'] + label_keys)
 
 reconcile_time = Histogram(name='qontract_reconcile_function_'
                                 'elapsed_seconds_since_bundle_commit',
