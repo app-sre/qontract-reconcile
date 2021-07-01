b69e8a93786d3af0c44a6ff49d8df50f47abb7ee
Maor Friedman <maor.friedman@redhat.com>
2021-07-01 15:58:59 +0300

    consider disabled accounts

--- reconcile/terraform_resources_wrapper.py
+++ reconcile/terraform_resources_wrapper.py
@@ -7,11 +7,23 @@ import reconcile.utils.threaded as threaded
 
 from reconcile.utils.sharding import is_in_shard_round_robin
 from reconcile.utils.semver_helper import make_semver
+from reconcile.utils.terrascript_client import TerrascriptClient as Terrascript
 from reconcile.status import ExitCodes
 
 QONTRACT_INTEGRATION = 'terraform-resources-wrapper'
 QONTRACT_INTEGRATION_VERSION = make_semver(0, 1, 0)
 
+
+def get_accounts_names():
+    accounts = queries.get_aws_accounts()
+    settings = queries.get_app_interface_settings()
+    # using Terrascript to filter out disabled accounts
+    ts = Terrascript(tfr.QONTRACT_INTEGRATION,
+                     tfr.QONTRACT_INTEGRATION_VERSION,
+                     1,
+                     accounts,
+                     settings=settings)
+    return ts.uids.keys()
 
 def tfr_run_wrapper(account_name,
                     dry_run,
@@ -44,14 +56,12 @@ def run(dry_run, print_only=False,
         enable_deletion=False, io_dir='throughput/',
         thread_pool_size=10, internal=None, use_jump_host=True,
         light=False, vault_output_path=''):
-    account_names = sorted([a['name'] for a in queries.get_aws_accounts()])
-
-    in_shard_account_names = [
-        name for index, name in enumerate(account_names)
+    account_names = [
+        name for index, name in enumerate(sorted(get_accounts_names()))
         if is_in_shard_round_robin(name, index)
     ]
 
-    if not in_shard_account_names:
+    if not account_names:
         logging.warning("No accounts in shards")
         return
 
