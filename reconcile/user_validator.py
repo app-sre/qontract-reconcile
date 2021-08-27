7c3277be73f8d918c4421013316666b8ff0123ff
Feng Huang <fehuang@redhat.com>
2021-08-27 15:24:43 -0400

    [user-validator] add validate_users_github (#1675)
    Signed-off-by: Feng Huang <fehuang@redhat.com>

--- reconcile/user_validator.py
+++ reconcile/user_validator.py
@@ -1,11 +1,19 @@
+import os
 import sys
 import logging
+
+from github.GithubException import GithubException
+from requests.exceptions import ReadTimeout
+from sretoolbox.utils import retry
 
 from reconcile.utils.gpg import gpg_key_valid
 import reconcile.queries as queries
 from reconcile.utils.smtp_client import SmtpClient
+from reconcile.github_users import init_github
+import reconcile.utils.threaded as threaded
 
 
+GH_BASE_URL = os.environ.get('GITHUB_API', 'https://api.github.com')
 QONTRACT_INTEGRATION = 'user-validator'
 
 
@@ -47,12 +55,34 @@ def validate_users_gpg_key(users):
     return ok
 
 
-def run(dry_run):
+@retry(exceptions=(GithubException, ReadTimeout))
+def get_github_user(user, github):
+    gh_user = github.get_user(user['github_username'])
+    return user['org_username'], user['github_username'], gh_user.login
+
+
+def validate_users_github(users, thread_pool_size):
+    ok = True
+    g = init_github()
+    results = threaded.run(get_github_user, users,
+                           thread_pool_size, github=g)
+    for org_username, gb_username, gh_login in results:
+        if gb_username != gh_login:
+            logging.error(
+                "Github username is case sensitive in OSD. "
+                f"User {org_username} github_username should be: {gh_login}.")
+            ok = False
+
+    return ok
+
+
+def run(dry_run, thread_pool_size=10):
     users = queries.get_users()
 
     single_path_ok = validate_users_single_path(users)
     gpg_ok = validate_users_gpg_key(users)
+    github_ok = validate_users_github(users, thread_pool_size)
 
-    ok = single_path_ok and gpg_ok
+    ok = single_path_ok and gpg_ok and github_ok
     if not ok:
         sys.exit(1)
