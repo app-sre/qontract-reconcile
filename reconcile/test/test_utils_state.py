755931c131c35271e61dc8c5266b0725636d5114
Esron Silva <essilva@redhat.com>
2021-09-21 16:56:11 -0300

    Test when State is initiated with integration as an empty string

--- reconcile/test/test_utils_state.py
+++ reconcile/test/test_utils_state.py
@@ -50,6 +50,36 @@ def test_ls_returns_correct_file(accounts, s3_client, mocker):
     assert keys == expected
 
 
+def test_ls_when_integration_is_empty_string(accounts, s3_client, mocker):
+    s3_client.create_bucket(Bucket='some-bucket')
+    s3_client.put_object(Bucket='some-bucket',
+                         Key='state/integration-name-1/some-file-1',
+                         Body='test')
+    s3_client.put_object(Bucket='some-bucket',
+                         Key='state/integration-name-2/some-file-2',
+                         Body='test')
+    s3_client.put_object(Bucket='some-bucket',
+                         Key='state/integration-name-3/nested/some-file-2',
+                         Body='test')
+
+    mock_aws_api = mocker.patch('reconcile.utils.state.AWSApi', autospec=True)
+    mock_aws_api.return_value \
+        .get_session.return_value \
+        .client.return_value = s3_client
+
+    state = State('', accounts)
+
+    keys = state.ls()
+
+    expected = [
+        '/integration-name-1/some-file-1',
+        '/integration-name-2/some-file-2',
+        '/integration-name-3/nested/some-file-2',
+    ]
+
+    assert keys == expected
+
+
 def test_ls_when_state_is_empty(accounts, s3_client, mocker):
     s3_client.create_bucket(Bucket='some-bucket')
 
