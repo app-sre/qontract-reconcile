app-sre/qontract-reconcile

commit ae619fed38b6d4e6e4b0dd09c5af680f03ed2a11

Upgrade Slack SDK from v1 to v3 (#1824)
* Upgrade Slack SDK from v1 to v3

slackclient was deprecated, so it made sense to get to a supported
version of the SDK. In addition, we get these other benefits:

  1. Exceptions are now raised on failure, rather than needing to
     manually parse the response dict.
  2. Access to higher-level methods (ex. chat_postMessage) instead
     of just api_call(). This makes it easier to validate that the
     correct args have been passed in.
  3. Built-in retry handling for throttled requests. In addition,
     a simple mechanism for adding custom retry handlers (which was
     done for 5xx responses).

* Add a few more tests

* Improve test fixtures and some doc updates

reconcile/utils/slack_api.py
@@ -1,11 +1,15 @@
-import time
 import logging
+from typing import Sequence, Dict, Any, Mapping, Optional
 
-from slackclient import SlackClient
-from sretoolbox.utils import retry
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+from slack_sdk.http_retry import RateLimitErrorRetryHandler, RetryHandler, \
+    RetryState, HttpRequest, HttpResponse
 
 from reconcile.utils.secret_reader import SecretReader
 from reconcile.utils.config import get_config
+
+MAX_RETRIES = 5
 
 
 class UserNotFoundException(Exception):
@@ -16,36 +20,85 @@ class UsergroupNotFoundException(Exception):
     pass
 
 
-class SlackAPICallException(Exception):
-    """Raised for general error cases when calling the Slack API."""
-
-
-class SlackAPIRateLimitedException(SlackAPICallException):
-    """Raised when a call to the Slack API has been rate-limited."""
+class ServerErrorRetryHandler(RetryHandler):
+    """Retry handler for 5xx errors."""
+    def _can_retry(
+        self,
+        *,
+        state: RetryState,
+        request: HttpRequest,
+        response: Optional[HttpResponse] = None,
+        error: Optional[Exception] = None
+    ) -> bool:
+        return response is not None and response.status_code >= 500
 
 
 class SlackApi:
     """Wrapper around Slack API calls"""
 
-    def __init__(self, workspace_name, token,
-                 settings=None,
+    def __init__(self,
+                 workspace_name: str,
+                 token: Mapping[str, str],
+                 settings: Optional[Mapping[str, Any]] = None,
                  init_usergroups=True,
-                 **chat_kwargs):
+                 channel: Optional[str] = None,
+                 **chat_kwargs) -> None:
+        """
+        :param workspace_name: Slack workspace name (ex. coreos)
+        :param token: data to pass to SecretReader.read() to get the token
+        :param settings: settings to pass to SecretReader
+        :param init_usergroups: whether or not to get a list of all Slack
+        usergroups when instantiated
+        :param channel: the Slack channel to post messages to, only used
+        when posting messages to a channel
+        :param chat_kwargs: any other kwargs that can be used to post Slack
+        channel messages
+        """
         self.workspace_name = workspace_name
+
         secret_reader = SecretReader(settings=settings)
         slack_token = secret_reader.read(token)
-        self.sc = SlackClient(slack_token)
-        self.results = {}
+
+        self._sc = WebClient(token=slack_token)
+        self._configure_client_retry()
+
+        self._results: Dict[str, Any] = {}
+
+        self.channel = channel
         self.chat_kwargs = chat_kwargs
+
         if init_usergroups:
             self._initiate_usergroups()
 
-    def chat_post_message(self, text):
-        self.sc.api_call(
-            "chat.postMessage",
-            text=text,
-            **self.chat_kwargs
-        )
+    def _configure_client_retry(self) -> None:
+        """
+        Add retry handlers in addition to the defaults provided by the Slack
+        client.
+        """
+        rate_limit_handler = RateLimitErrorRetryHandler(
+            max_retry_count=MAX_RETRIES)
+        server_error_handler = ServerErrorRetryHandler(
+            max_retry_count=MAX_RETRIES)
+
+        self._sc.retry_handlers.append(rate_limit_handler)
+        self._sc.retry_handlers.append(server_error_handler)
+
+    def chat_post_message(self, text: str) -> None:
+        """
+        Sends a message to a channel.
+
+        :param text: message to send to channel
+        :raises ValueError: when Slack channel wasn't provided
+        :raises slack_sdk.errors.SlackApiError: if unsuccessful response
+        from Slack API
+        """
+
+        if not self.channel:
+            raise ValueError('Slack channel name must be provided when '
+                             'posting messages.')
+
+        self._sc.chat_postMessage(channel=self.channel, text=text,
+                                  **self.chat_kwargs)
 
     def describe_usergroup(self, handle):
         usergroup = self.get_usergroup(handle)
@@ -63,14 +116,14 @@ class SlackApi:
         usergroup = self.get_usergroup(handle)
         return usergroup['id']
 
-    @retry()
-    def _initiate_usergroups(self):
-        result = self.sc.api_call(
-            "usergroups.list",
-            include_users=True
-        )
-        if not result['ok']:
-            raise Exception(result['error'])
+    def _initiate_usergroups(self) -> None:
+        """
+        Initiates usergroups list.
+
+        :raises slack_sdk.errors.SlackApiError: if unsuccessful response from
+        Slack API
+        """
+        result = self._sc.usergroups_list(include_users=True)
         self.usergroups = result['usergroups']
 
     def get_usergroup(self, handle):
@@ -80,56 +133,38 @@ class SlackApi:
         [usergroup] = usergroup
         return usergroup
 
-    @retry(exceptions=SlackAPIRateLimitedException, max_attempts=5)
-    def update_usergroup(self, id, channels_list, description):
-        channels = ','.join(channels_list)
-        response = self.sc.api_call(
-            "usergroups.update",
-            usergroup=id,
-            channels=channels,
-            description=description,
-        )
+    def update_usergroup(self, id: str, channels_list: Sequence[str],
+                         description: str) -> None:
+        """
+        Update an existing usergroup.
 
-        error = response.get('error')
+        :param id: encoded usergroup ID
+        :param channels_list: encoded channel IDs that the usergroup uses by
+        default
+        :param description: short description of the usergroup
+        :raises slack_sdk.errors.SlackApiError: if unsuccessful response from
+        Slack API
+        """
+        self._sc.usergroups_update(usergroup=id, channels=channels_list,
+                                   description=description)
 
-        if error == 'ratelimited':
-            retry_after = response['headers']['retry-after']
-            time.sleep(int(retry_after))
-            raise SlackAPIRateLimitedException(
-                f"Slack API throttled after max retry attempts - "
-                f"method=usergroups.update usergroup={id}")
-        elif error:
-            raise SlackAPICallException(
-                f"Slack returned error: {error} - "
-                f"method=usergroups.update usergroup={id}")
+    def update_usergroup_users(self, id: str,
+                               users_list: Sequence[str]) -> None:
+        """
+        Update the list of users for a usergroup.
 
-    @retry(exceptions=SlackAPIRateLimitedException, max_attempts=5)
-    def update_usergroup_users(self, id, users_list):
+        :param id: encoded usergroup ID
+        :param users_list: encoded user IDs that represents the entire list
+        of users for the usergroup
+        :raises slack_sdk.errors.SlackApiError: if unsuccessful response from
+        Slack API
+        """
         # since Slack API does not support empty usergroups
         # we can trick it by passing a deleted user
         if len(users_list) == 0:
             users_list = [self.get_random_deleted_user()]
-        users = ','.join(users_list)
-        response = self.sc.api_call(
-            "usergroups.users.update",
-            usergroup=id,
-            users=users,
-        )
 
-        error = response.get('error')
-
-        if error == 'ratelimited':
-            retry_after = response['headers']['retry-after']
-            time.sleep(int(retry_after))
-            raise SlackAPIRateLimitedException(
-                f"Slack API throttled after max retry attempts - "
-                f"method=usergroups.users.update usergroup={id} users={users}")
-        # Slack can throw an invalid_users error when emptying groups, but it
-        # will still empty the group (so this can be ignored).
-        elif error and error != 'invalid_users':
-            raise SlackAPICallException(
-                f"Slack returned error: {error} - "
-                f"method=usergroups.users.update usergroup={id} users={users}")
+        self._sc.usergroups_users_update(usergroup=id, users=users_list)
 
     def get_random_deleted_user(self):
         for user_id, user_data in self._get('users').items():
@@ -140,15 +175,29 @@ class SlackApi:
                       'empty usergroup will not work')
         return ''
 
-    def get_user_id_by_name(self, user_name):
+    def get_user_id_by_name(self, user_name: str) -> str:
+        """
+        Get user id from their username.
+
+        :param user_name: Slack user name
+        :return: encoded user ID (ex. W012A3CDE)
+        :raises slack_sdk.errors.SlackApiError: if unsuccessful response from
+        Slack API
+        :raises UserNotFoundException: if the Slack user is not found
+        """
         config = get_config()
         mail_address = config['smtp']['mail_address']
-        result = self.sc.api_call(
-            "users.lookupByEmail",
-            email=f"{user_name}@{mail_address}"
-        )
-        if not result['ok']:
-            raise UserNotFoundException(result['error'])
+
+        try:
+            result = self._sc.users_lookupByEmail(
+                email=f"{user_name}@{mail_address}"
+            )
+        except SlackApiError as e:
+            if e.response['error'] == 'users_not_found':
+                raise UserNotFoundException(e.response['error'])
+            else:
+                raise
+
         return result['user']['id']
 
     def get_channels_by_names(self, channels_names):
@@ -178,44 +227,43 @@ class SlackApi:
 
         return api_limits.get(resource_type)
 
-    @retry()
-    def _get(self, type):
+    def _get(self, resource: str) -> Dict[str, Any]:
         """
         Get Slack resources by type. This method uses a cache to ensure that
         each resource type is only fetched once.
 
-        :param type: resource type
+        :param resource: resource type
         :return: data from API call
         """
-        result_key = 'members' if type == 'users' else type
-        api_key = 'conversations' if type == 'channels' else type
+        result_key = 'members' if resource == 'users' else resource
+        api_key = 'conversations' if resource == 'channels' else resource
         results = {}
-        cursor = ''
-        additional_kwargs = {}
+        additional_kwargs = {'cursor': ''}
 
-        api_result_limit = self._get_api_results_limit(type)
+        api_result_limit = self._get_api_results_limit(resource)
 
         if api_result_limit:
             additional_kwargs['limit'] = api_result_limit
 
-        if type in self.results:
-            return self.results[type]
+        if resource in self._results:
+            return self._results[resource]
 
         while True:
-            result = self.sc.api_call(
+            result = self._sc.api_call(
                 "{}.list".format(api_key),
-                cursor=cursor,
-                **additional_kwargs
+                http_verb='GET',
+                params=additional_kwargs
             )
-            if 'error' in result and result['error'] == 'ratelimited':
-                retry_after = result['headers']['retry-after']
-                time.sleep(int(retry_after))
-                continue
+
             for r in result[result_key]:
                 results[r['id']] = r
+
             cursor = result['response_metadata']['next_cursor']
+
             if cursor == '':
                 break
 
-        self.results[type] = results
+            additional_kwargs['cursor'] = cursor
+
+        self._results[resource] = results
         return results

commit e3ca842f445dd4d24706ccf0abf98f9bfe214a1a

Ignore expected invalid_users errors
This fixes a regression from #1824 that was missed due to refactoring
of tests. The current impact is only that a single error message is
displayed each time we empty a group.

reconcile/utils/slack_api.py
@@ -164,7 +164,13 @@ class SlackApi:
         if len(users_list) == 0:
             users_list = [self.get_random_deleted_user()]
 
-        self._sc.usergroups_users_update(usergroup=id, users=users_list)
+        try:
+            self._sc.usergroups_users_update(usergroup=id, users=users_list)
+        except SlackApiError as e:
+            # Slack can throw an invalid_users error when emptying groups, but
+            # it will still empty the group (so this can be ignored).
+            if e.response['error'] != 'invalid_users':
+                raise
 
     def get_random_deleted_user(self):
         for user_id, user_data in self._get('users').items():

reconcile/test/test_utils_slack_api.py
@@ -154,6 +154,33 @@ def test_get_user_id_by_name_reraise(get_config_mock, slack_api):
     with pytest.raises(SlackApiError):
         slack_api.client.get_user_id_by_name('someuser')
 
+
+def test_update_usergroups_users_empty_no_raise(mocker, slack_api):
+    """
+    invalid_users errors shouldn't be raised because providing an empty
+    list is actually removing users from the usergroup.
+    """
+    mocker.patch.object(SlackApi, 'get_random_deleted_user', autospec=True)
+
+    slack_api.mock_slack_client.return_value.usergroups_users_update\
+        .side_effect = SlackApiError('Some error message',
+                                     {'error': 'invalid_users'})
+
+    slack_api.client.update_usergroup_users('ABCD', [])
+
+
+def test_update_usergroups_users_raise(slack_api):
+    """
+    Any errors other than invalid_users should result in an exception being
+    raised.
+    """
+    slack_api.mock_slack_client.return_value.usergroups_users_update \
+        .side_effect = SlackApiError('Some error message',
+                                     {'error': 'internal_error'})
+
+    with pytest.raises(SlackApiError):
+        slack_api.client.update_usergroup_users('ABCD', ['USERA'])
+
 #
 # Slack WebClient retry tests
 #