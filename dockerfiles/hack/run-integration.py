494c60e79bff94d8b01193287e9f98cfec9c4ff3
Gerd Oberlechner <goberlec@redhat.com>
2021-10-21 18:05:07 +0200

    Enable run-integration.py to run all entry point scripts from setup.py
    * all entry point scripts defined in setup.py can be defined with
      COMMAND_NAME env var (fallback to qontract-reconcile if not set)
    * INTEGRATION_NAME is used as subcommand if COMMAND_NAME is a
      click MultiCommand and INTEGRATION_NAME is present
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- dockerfiles/hack/run-integration.py
+++ dockerfiles/hack/run-integration.py
@@ -4,11 +4,14 @@ import logging
 import os
 import sys
 import time
+from typing import Optional
 
 from prometheus_client import start_http_server
+from importlib import metadata
+import click
 
 from reconcile.status import ExitCodes
-from reconcile.cli import integration, LOG_FMT, LOG_DATEFMT
+from reconcile.cli import LOG_FMT, LOG_DATEFMT
 from reconcile.utils.metrics import run_time
 from reconcile.utils.metrics import run_status
 from reconcile.utils.metrics import extra_labels
@@ -18,6 +21,7 @@ SHARDS = int(os.environ.get('SHARDS', 1))
 SHARD_ID = int(os.environ.get('SHARD_ID', 0))
 
 INTEGRATION_NAME = os.environ['INTEGRATION_NAME']
+COMMAND_NAME = os.environ.get('COMMAND_NAME', 'qontract-reconcile')
 
 RUN_ONCE = os.environ.get('RUN_ONCE')
 DRY_RUN = os.environ.get('DRY_RUN')
@@ -47,30 +51,56 @@ logging.basicConfig(level=logging.INFO,
                     handlers=HANDLERS)
 
 
-def build_args():
-    args = ['--config', CONFIG]
-    if DRY_RUN is not None:
-        args.append(DRY_RUN)
-    args.append(INTEGRATION_NAME)
-    if INTEGRATION_EXTRA_ARGS is not None:
-        args.extend(INTEGRATION_EXTRA_ARGS.split())
+def build_entry_point_args(command: click.Command, config: str,
+                           dry_run: str, integration_name: str,
+                           extra_args: str) -> list[str]:
+    args = ['--config', config]
+    if dry_run is not None:
+        args.append(dry_run)
+
+    # if the integration_name is a known sub command, we add it right before the extra_args
+    if integration_name and isinstance(command, click.MultiCommand) and \
+            command.get_command(None, integration_name):
+        args.append(integration_name)
+
+    if extra_args is not None:
+        args.extend(extra_args.split())
     return args
+
+
+def build_entry_point_func(command_name: str) -> click.Command:
+    """
+    Use the entry point information from setup.py to
+    find the function to invoke for a command.
+    """
+    console_script_entry_points = {
+        ep.name: ep
+        for ep in metadata.entry_points()["console_scripts"]
+    }
+    entry_point: Optional[metadata.EntryPoint] = console_script_entry_points.get(command_name, None)
+    if entry_point:
+        return entry_point.load()
+    else:
+        raise ValueError(
+            f"Command {command_name} unknown."
+            f"Have a look at setup.py for valid entry points.")
 
 
 if __name__ == "__main__":
     start_http_server(9090)
 
+    command = build_entry_point_func(COMMAND_NAME)
+    args = build_entry_point_args(command, CONFIG, DRY_RUN, INTEGRATION_NAME, INTEGRATION_EXTRA_ARGS)
     while True:
         sleep = SLEEP_DURATION_SECS
         start_time = time.monotonic()
         # Running the integration via Click, so we don't have to replicate
         # the CLI logic here
         try:
-            with integration.make_context(info_name='qontract-reconcile',
-                                          args=build_args()) as ctx:
+            with command.make_context(info_name=COMMAND_NAME, args=args) as ctx:
                 ctx.ensure_object(dict)
                 ctx.obj['extra_labels'] = extra_labels
-                integration.invoke(ctx)
+                command.invoke(ctx)
                 return_code = 0
         # This is for when the integration explicitly
         # calls sys.exit(N)
@@ -80,7 +110,7 @@ if __name__ == "__main__":
         # in the integrations, but we want to continue the loop anyway
         except Exception as exc_obj:
             sleep = SLEEP_ON_ERROR
-            LOG.exception('Error running qontract-reconcile: %s', exc_obj)
+            LOG.exception(f"Error running {COMMAND_NAME}: %s", exc_obj)
             return_code = ExitCodes.ERROR
 
         time_spent = time.monotonic() - start_time
