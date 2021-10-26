0675bbbfd65a0cba893278feb3c5b39f4bbb2665
Patrick Martin <patmarti@redhat.com>
2021-10-26 17:45:55 +0200

    support pod count resource quotas

--- reconcile/openshift_resourcequotas.py
+++ reconcile/openshift_resourcequotas.py
@@ -19,11 +19,13 @@ QONTRACT_INTEGRATION_VERSION = make_semver(0, 1, 0)
 def flatten(d, parent_key='', sep='.'):
     items = []
     for k, v in d.items():
+        if v is None:
+            continue
         new_key = parent_key + sep + k if parent_key else k
         if isinstance(v, collections.MutableMapping):
             items.extend(flatten(v, new_key, sep=sep).items())
         else:
-            items.append((new_key, v))
+            items.append((new_key, str(v)))
     return dict(items)
 
 
