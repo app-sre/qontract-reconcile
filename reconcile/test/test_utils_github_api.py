21e52bce6c0f8562aebb3952f50a2dae6eb162b3
Jordi Piriz <jpiriz@redhat.com>
2021-11-04 18:11:59 +0100

    Remove unused import

--- reconcile/test/test_utils_github_api.py
+++ reconcile/test/test_utils_github_api.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from requests.exceptions import ConnectTimeout
 from reconcile.utils.github_api import GithubApi
 
