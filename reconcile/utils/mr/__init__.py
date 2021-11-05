04f62d277b3d1673b9525946b3746e1be45df4c8
Gerd Oberlechner <goberlec@redhat.com>
2021-11-05 08:09:53 +0100

    SQS message handling for gitlab PRs (#1980)
    Handle SQS and MR processing so messages don't get lost
    
    * SQS messages are only deleted when they have been processed
      without any issues
    * we keep unknown pr_types in the queue for future processing
      for cases where the producer is already on a newer version
      of qontract-reconcile as the consumer
    * rely on MergeRequestBase cancel() logic to handle situations
      where an MR is not processed for a good reason, e.g. duplicate
      MR detected, no changes introduced by MR
    * cancelation logic cleans up a temporary branch (best effort)
      and does not signal a problem back to the SQS message handler
      (e.g. gitlab_mr_sqs_consumer.py) so messages can get deleted
      from the queue

--- reconcile/utils/mr/__init__.py
+++ reconcile/utils/mr/__init__.py
@@ -1,4 +1,5 @@
 from reconcile.utils.mr.base import MergeRequestBase
+from reconcile.utils.mr.base import MergeRequestProcessingError
 
 from reconcile.utils.mr.app_interface_reporter \
     import CreateAppInterfaceReporter
@@ -12,6 +13,8 @@ from reconcile.utils.mr.cluster_service_install_config import CSInstallConfig
 
 __all__ = [
     'init_from_sqs_message',
+    'UnknownMergeRequestType',
+    'MergeRequestProcessingError',
     'CreateAppInterfaceReporter',
     'CreateDeleteAwsAccessKey',
     'CreateClustersUpdates',
@@ -20,6 +23,12 @@ __all__ = [
     'AutoPromoter',
     'CSInstallConfig',
 ]
+
+
+class UnknownMergeRequestType(Exception):
+    """
+    Used when the message type from the SQS message is unknown
+    """
 
 
 def init_from_sqs_message(message):
@@ -46,7 +55,7 @@ def init_from_sqs_message(message):
     # and fail early if that type is not on the map.
     msg_type = message.pop('pr_type')
     if msg_type not in types_map:
-        raise TypeError(f'type {msg_type} no supported')
+        raise UnknownMergeRequestType(f'type {msg_type} no supported')
 
     # Finally, get the class mapped to the type
     # and create an instance with all the remaining
