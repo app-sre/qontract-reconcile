e6b821d373e6417746868102552dd09748e61a8c
Luis Fernando Muñoz Mejías <47329970+Piojo@users.noreply.github.com>
2021-11-08 16:55:10 +0100

    Remove py36 tests (#1976)
    * Remove py36 tests
    
    We are py39 from now on
    
    * Require Pytyhon 3.9 on setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
 
     author="Red Hat App-SRE Team",
     author_email="sd-app-sre@redhat.com",
-
+    python_requires=">=3.9",
     description="Collection of tools to reconcile services with their desired "
                 "state as defined in the app-interface DB.",
 
@@ -64,7 +64,7 @@ setup(
         'Development Status :: 2 - Pre-Alpha',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.9',
     ],
     entry_points={
         'console_scripts': [
