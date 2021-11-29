64aaf7f97523576baef28865259af3c9c7d57b6d
Andreu Gallofre <agallofr@redhat.com>
2021-11-29 17:30:07 +0100

    Refactor parent app checking

--- reconcile/gitlab_labeler.py
+++ reconcile/gitlab_labeler.py
@@ -1,7 +1,7 @@
 import os
 import logging
 
-from typing import List, Optional
+from typing import List, Optional, Dict, Set, Iterable
 from reconcile import queries
 
 from reconcile.gitlab_housekeeping import MERGE_LABELS_PRIORITY, HOLD_LABELS
@@ -11,46 +11,58 @@ from reconcile.utils.gitlab_api import GitLabApi
 QONTRACT_INTEGRATION = 'gitlab-labeler'
 
 
-def guess_onboarding_status(changed_paths: List[str]) -> Optional[str]:
+def get_app_list() -> Dict:
     """
-    Guess the onboarding status of a given MR from the changed paths of the
-    MR.
+    Get a list of all services managed by app-interface with the parent app
+    and the onboarding status of the service.
     """
     apps = queries.get_apps()
-    #   logging.info(apps)
-    app_name = get_app_name(changed_paths)
-    if app_name is not None:
-        app = None
-        for a in apps:
-            if a['name'] == app_name:
-                app = a
-
-    if app is not None:
-        logging.info(app['onboardingStatus'])
-        return app['onboardingStatus']
-
-    return None
+    app_info = {a['name']: {'onboardingStatus': a['onboardingStatus'],
+                'parentApp': a['parentApp']} for a in apps}
+    return app_info
 
 
-def get_app_name(changed_paths: List[str]) -> Optional[str]:
+def get_parents_list() -> List[str]:
+    """
+    Get a set of all service names that are parents of another service
+    """
+    parent_set = set()
+    apps = queries.get_apps()
+    for a in apps:
+        if a['parentApp'] is not None:
+            parent_set.add(a['parentApp']['name'])
+
+    return list(parent_set)
+
+
+def guess_onboarding_status(changed_paths: Iterable[str],
+                            apps: Dict[str, Dict], parent_apps:
+                            Set[str]) -> Optional[str]:
     """
     Guess the service name of a given MR from the changed paths of the
     MR. This will allow to add the onboarding status to the MR's as label
     """
+    labels = set()
     for path in changed_paths:
         if 'data/services/' in path:
             path_slices = path.split('/')
             try:
-                if path_slices[2] == "insights":
-                    app_name = path_slices[3]
+                if path_slices[2] in parent_apps:
+                    app = apps[path_slices[3]]
+                    if app is not None:
+                        labels.add(app['onboardingStatus'])
                 else:
-                    app_name = path_slices[2]
-                return app_name
-            except IndexError:
+                    app = apps[path_slices[2]]
+                    if app is not None:
+                        labels.add(app['onboardingStatus'])
+                    else:
+                        return None
+            except (IndexError, KeyError):
                 logging.info("Error getting app name")
                 return None
 
-    return None
+    labels_list = list(labels)
+    return labels_list[0] if len(labels_list) == 1 else None
 
 
 def guess_labels(project_labels, changed_paths):
@@ -73,8 +85,11 @@ def guess_labels(project_labels, changed_paths):
                        and t not in ignore_tokens]
             if matches:
                 guesses.add(label)
-
-    onboarding_status = guess_onboarding_status(changed_paths)
+    apps = get_app_list()
+    parent_apps = get_parents_list()
+    onboarding_status = guess_onboarding_status(changed_paths, apps,
+                                                parent_apps)
+    logging.info(onboarding_status)
     if onboarding_status is not None:
         guesses.add(onboarding_status)
 
@@ -92,6 +107,6 @@ def run(dry_run, gitlab_project_id=None, gitlab_merge_request_id=None):
         gl.get_merge_request_changed_paths(gitlab_merge_request_id)
     guessed_labels = guess_labels(project_labels, changed_paths)
     labels_to_add = [b for b in guessed_labels if b not in labels]
-    if labels_to_add and not dry_run:
+    if labels_to_add:
         logging.info(['add_labels', labels_to_add])
         gl.add_labels_to_merge_request(gitlab_merge_request_id, labels_to_add)
