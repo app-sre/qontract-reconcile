240092ba28393ae1a7ad25533a99b08d7f530347
Rafa Porres Molina <rporresm@redhat.com>
2021-12-07 18:36:49 +0100

    Revert "Revert "openshift-tekton-resources objects used by default (#2023)" (#2030)"
    This reverts commit 042c0b327b9871a283f33f001bb096927de64ce0.

--- reconcile/utils/saasherder.py
+++ reconcile/utils/saasherder.py
@@ -900,7 +900,6 @@ class SaasHerder():
     def get_moving_commits_diff_saas_file(self, saas_file, dry_run):
         saas_file_name = saas_file['name']
         timeout = saas_file.get('timeout') or None
-        configurable_resources = saas_file.get('configurableResources', False)
         pipelines_provider = self._get_pipelines_provider(saas_file)
         github = self._initiate_github(saas_file)
         trigger_specs = []
@@ -947,7 +946,6 @@ class SaasHerder():
                         'saas_file_name': saas_file_name,
                         'env_name': env_name,
                         'timeout': timeout,
-                        'configurable_resources': configurable_resources,
                         'pipelines_provider': pipelines_provider,
                         'rt_name': rt_name,
                         'cluster_name': cluster_name,
@@ -1002,7 +1000,6 @@ class SaasHerder():
                                          current_state):
         saas_file_name = saas_file['name']
         timeout = saas_file.get('timeout') or None
-        configurable_resources = saas_file.get('configurableResources', False)
         pipelines_provider = self._get_pipelines_provider(saas_file)
         trigger_specs = []
         for rt in saas_file['resourceTemplates']:
@@ -1051,7 +1048,6 @@ class SaasHerder():
                             'saas_file_name': saas_file_name,
                             'env_name': env_name,
                             'timeout': timeout,
-                            'configurable_resources': configurable_resources,
                             'pipelines_provider': pipelines_provider,
                             'rt_name': rt_name,
                             'cluster_name': cluster_name,
@@ -1090,7 +1086,6 @@ class SaasHerder():
         saas_file_parameters = saas_file.get('parameters')
         saas_file_managed_resource_types = saas_file['managedResourceTypes']
         timeout = saas_file.get('timeout') or None
-        configurable_resources = saas_file.get('configurableResources', False)
         pipelines_provider = self._get_pipelines_provider(saas_file)
         trigger_specs = []
         for rt in saas_file['resourceTemplates']:
@@ -1125,7 +1120,6 @@ class SaasHerder():
                     'saas_file_name': saas_file_name,
                     'env_name': env_name,
                     'timeout': timeout,
-                    'configurable_resources': configurable_resources,
                     'pipelines_provider': pipelines_provider,
                     'rt_name': rt_name,
                     'cluster_name': cluster_name,
