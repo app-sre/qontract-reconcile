46279d0c29896e2bbe9c5d9a5f740a6741706929
Maor Friedman <mafriedm@redhat.com>
2021-12-28 10:47:49 +0200

    lint you are not my friend

--- reconcile/utils/git.py
+++ reconcile/utils/git.py
@@ -8,19 +8,21 @@ class GitError(Exception):
 
 
 def clone(repo_url, wd):
-    with open(os.devnull, 'w') as dev_null:
-        proc = Popen(['git', 'clone', repo_url, wd],
-                     stdout=dev_null, stderr=dev_null)
-        proc.communicate()
+    # pylint: disable=consider-using-with
+    DEVNULL = open(os.devnull, 'w')
+    proc = Popen(['git', 'clone', repo_url, wd],
+                 stdout=DEVNULL, stderr=DEVNULL)
+    proc.communicate()
     if proc.returncode != 0:
         raise GitError('git clone failed: {}'.format(repo_url))
 
 
 def checkout(commit, wd):
-    with open(os.devnull, 'w') as dev_null:
-        proc = Popen(['git', 'checkout', commit],
-                     cwd=wd, stdout=dev_null, stderr=dev_null)
-        proc.communicate()
+    # pylint: disable=consider-using-with
+    DEVNULL = open(os.devnull, 'w')
+    proc = Popen(['git', 'checkout', commit],
+                 cwd=wd, stdout=DEVNULL, stderr=DEVNULL)
+    proc.communicate()
     if proc.returncode != 0:
         raise GitError('git checkout failed: {}'.format(commit))
 
@@ -28,8 +30,9 @@ def checkout(commit, wd):
 def is_file_in_git_repo(file_path):
     real_path = os.path.realpath(file_path)
     dir_path = os.path.dirname(real_path)
-    with open(os.devnull, 'w') as dev_null:
-        proc = Popen(['git', 'git rev-parse', '--is-inside-work-tree'],
-                     cwd=dir_path, stdout=dev_null, stderr=dev_null)
-        proc.communicate()
+    # pylint: disable=consider-using-with
+    DEVNULL = open(os.devnull, 'w')
+    proc = Popen(['git', 'git rev-parse', '--is-inside-work-tree'],
+                 cwd=dir_path, stdout=DEVNULL, stderr=DEVNULL)
+    proc.communicate()
     return proc.returncode == 0
