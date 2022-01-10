79288e1ec3336e4572141835b2f018b5b312ffcf
Maor Friedman <mafriedm@redhat.com>
2022-01-10 11:00:59 +0200

    reorder some more

--- reconcile/utils/git.py
+++ reconcile/utils/git.py
@@ -7,8 +7,8 @@ class GitError(Exception):
 
 
 def clone(repo_url, wd):
-    cmd = ['git', 'clone', repo_url, wd]
     # pylint: disable=subprocess-run-check
+    cmd = ['git', 'clone', repo_url, wd]
     result = subprocess.run(cmd, cwd=wd,
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE)
@@ -17,8 +17,8 @@ def clone(repo_url, wd):
 
 
 def checkout(commit, wd):
-    cmd = ['git', 'checkout', commit]
     # pylint: disable=subprocess-run-check
+    cmd = ['git', 'checkout', commit]
     result = subprocess.run(cmd, cwd=wd,
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE)
@@ -29,8 +29,8 @@ def checkout(commit, wd):
 def is_file_in_git_repo(file_path):
     real_path = os.path.realpath(file_path)
     dir_path = os.path.dirname(real_path)
-    cmd = ['git', 'rev-parse', '--is-inside-work-tree']
     # pylint: disable=subprocess-run-check
+    cmd = ['git', 'rev-parse', '--is-inside-work-tree']
     result = subprocess.run(cmd, cwd=dir_path,
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE)
