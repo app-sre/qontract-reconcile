a739a63e665f196af61e01c3220e0c493eb06647
Maor Friedman <mafriedm@redhat.com>
2022-02-03 16:44:36 +0200

    add tests

--- reconcile/test/test_saasherder.py
+++ reconcile/test/test_saasherder.py
@@ -129,6 +129,151 @@ class TestCheckSaasFileEnvComboUnique(TestCase):
         self.assertFalse(saasherder.valid)
 
 
+class TestCheckSaasFileUpstreamNotUsedWithCommitSha(TestCase):
+    def test_check_saas_file_upstream_not_used_with_commit_sha(self):
+        saas_files = [
+            {
+                'path': 'path1',
+                'name': 'a1',
+                'managedResourceTypes': [],
+                'resourceTemplates':
+                [
+                    {
+                        'name': 'rt',
+                        'url': 'url',
+                        'targets':
+                        [
+                            {
+                                'namespace': {
+                                    'name': 'ns',
+                                    'environment': {
+                                        'name': 'env1'
+                                    },
+                                    'cluster': {
+                                        'name': 'cluster'
+                                    }
+                                },
+                                'ref': 'main',
+                                'upstream': {
+                                    'instance': {
+                                        'name': 'ci'
+                                    },
+                                    'name': 'job'
+                                },
+                                'parameters': {}
+                            },
+                            {
+                                'namespace': {
+                                    'name': 'ns',
+                                    'environment': {
+                                        'name': 'env2'
+                                    },
+                                    'cluster': {
+                                        'name': 'cluster'
+                                    }
+                                },
+                                'ref': 'master',
+                                'upstream': {
+                                    'instance': {
+                                        'name': 'ci'
+                                    },
+                                    'name': 'job'
+                                },
+                                'parameters': {}
+                            }
+                        ]
+                    }
+                ],
+                'roles': [
+                    {'users': [{'org_username': 'myname'}]}
+                ]
+            }
+        ]
+        saasherder = SaasHerder(
+            saas_files,
+            thread_pool_size=1,
+            gitlab=None,
+            integration='',
+            integration_version='',
+            settings={},
+            validate=True
+        )
+
+        self.assertTrue(saasherder.valid)
+
+    def test_check_saas_file_upstream_used_with_commit_sha(self):
+        saas_files = [
+            {
+                'path': 'path1',
+                'name':
+                'long-name-which-is-too-long-to-produce-unique-combo',
+                'managedResourceTypes': [],
+                'resourceTemplates':
+                [
+                    {
+                        'name': 'rt',
+                        'url': 'url',
+                        'targets':
+                        [
+                            {
+                                'namespace': {
+                                    'name': 'ns',
+                                    'environment': {
+                                        'name': 'env1'
+                                    },
+                                    'cluster': {
+                                        'name': 'cluster'
+                                    }
+                                },
+                                'ref': 'main',
+                                'upstream': {
+                                    'instance': {
+                                        'name': 'ci'
+                                    },
+                                    'name': 'job'
+                                },
+                                'parameters': {}
+                            },
+                            {
+                                'namespace': {
+                                    'name': 'ns',
+                                    'environment': {
+                                        'name': 'env2'
+                                    },
+                                    'cluster': {
+                                        'name': 'cluster'
+                                    }
+                                },
+                                'ref': '2637b6c41bda7731b1bcaaf18b4a50d7c5e63e30',
+                                'upstream': {
+                                    'instance': {
+                                        'name': 'ci'
+                                    },
+                                    'name': 'job'
+                                },
+                                'parameters': {}
+                            }
+                        ]
+                    }
+                ],
+                'roles': [
+                    {'users': [{'org_username': 'myname'}]}
+                ]
+            }
+        ]
+        saasherder = SaasHerder(
+            saas_files,
+            thread_pool_size=1,
+            gitlab=None,
+            integration='',
+            integration_version='',
+            settings={},
+            validate=True
+        )
+
+        self.assertFalse(saasherder.valid)
+
+
 class TestGetMovingCommitsDiffSaasFile(TestCase):
     def setUp(self):
         self.saas_files = [
