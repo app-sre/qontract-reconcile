5bed721f44c83393ed31249068fdb11a72146c0f
Jordi Piriz <jpiriz@redhat.com>
2022-02-15 10:59:37 +0100

    APPSRE-3528: Auto-promote targets only if there are changes (#2172)

--- reconcile/openshift_saas_deploy.py
+++ reconcile/openshift_saas_deploy.py
@@ -175,8 +175,12 @@ def run(dry_run, thread_pool_size=10, io_dir='throughput/',
     success = not ri.has_error_registered()
     # only publish promotions for deployment jobs (a single saas file)
     if notify:
+        # Auto-promote next stages only if there are changes in the
+        # promoting stage. This prevents trigger promotions on job re-runs
+        auto_promote = len(actions) > 0
         mr_cli = mr_client_gateway.init(gitlab_project_id=gitlab_project_id)
-        saasherder.publish_promotions(success, all_saas_files, mr_cli)
+        saasherder.publish_promotions(success, all_saas_files,
+                                      mr_cli, auto_promote)
 
     if not success:
         sys.exit(ExitCodes.ERROR)
