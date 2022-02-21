84c072ef30a8094089a52c33f476fa38c27958e5
Maor Friedman <mafriedm@redhat.com>
2022-02-21 08:45:10 +0200

    lgtm

--- reconcile/gitlab_housekeeping.py
+++ reconcile/gitlab_housekeeping.py
@@ -10,9 +10,9 @@ from sretoolbox.utils import retry
 from reconcile import queries
 
 from reconcile.utils.gitlab_api import GitLabApi
+from reconcile.utils.mr.labels import LGTM
 
-LGTM_LABEL = 'lgtm'
-MERGE_LABELS_PRIORITY = ['bot/approved', LGTM_LABEL, 'bot/automerge']
+MERGE_LABELS_PRIORITY = ['bot/approved', LGTM, 'bot/automerge']
 SAAS_FILE_LABEL = 'saas-file-update'
 REBASE_LABELS_PRIORITY = MERGE_LABELS_PRIORITY
 HOLD_LABELS = ['awaiting-approval', 'blocked/bot-access', 'hold', 'bot/hold',
@@ -217,13 +217,13 @@ def merge_merge_requests(dry_run, gl, merge_limit, rebase,
             if not good_to_merge:
                 continue
 
-            if SAAS_FILE_LABEL in labels and LGTM_LABEL in labels:
+            if SAAS_FILE_LABEL in labels and LGTM in labels:
                 logging.warning(
                     f"[{gl.project.name}/{mr.iid}] 'lgtm' label not " +
                     "suitable for saas file update. removing 'lgtm' label"
                 )
                 if not dry_run:
-                    gl.remove_label_from_merge_request(mr.iid, LGTM_LABEL)
+                    gl.remove_label_from_merge_request(mr.iid, LGTM)
                 continue
 
             target_branch = mr.target_branch
--- reconcile/utils/mr/labels.py
+++ reconcile/utils/mr/labels.py
@@ -1,3 +1,4 @@
 AUTO_MERGE = 'bot/automerge'
 DO_NOT_MERGE = 'do-not-merge/hold'
+LGTM = 'lgtm'
 SKIP_CI = 'bot/skip-ci'
