f4706ef38dafd013fb4a5bd9f6ef9647a640dc24
Maor Friedman <mafriedm@redhat.com>
2022-02-21 12:28:07 +0200

    migrate from unittest

--- reconcile/test/test_utils_terrascript_client.py
+++ reconcile/test/test_utils_terrascript_client.py
@@ -1,52 +1,43 @@
-from unittest import TestCase
 import reconcile.utils.terrascript_client as tsclient
 
 
-class TestSupportFunctions(TestCase):
+def test_sanitize_resource_with_dots():
+    assert tsclient.safe_resource_id("foo.example.com") == "foo_example_com"
 
-    def test_sanitize_resource_with_dots(self):
-        self.assertEqual(
-            tsclient.safe_resource_id("foo.example.com"),
-            "foo_example_com"
-        )
+def test_sanitize_resource_with_wildcard():
+    assert tsclient.safe_resource_id("*.foo.example.com") == "_star_foo_example_com"
 
-    def test_sanitize_resource_with_wildcard(self):
-        self.assertEqual(
-            tsclient.safe_resource_id("*.foo.example.com"),
-            "_star_foo_example_com"
-        )
+def test_aws_username_org():
+    ts = tsclient.TerrascriptClient('', '', 1, [])
+    result = 'org'
+    user = {
+        'org_username': result
+    }
+    assert ts._get_aws_username(user) == result
 
-    def test_aws_username_org(self):
-        ts = tsclient.TerrascriptClient('', '', 1, [])
-        result = 'org'
-        user = {
-            'org_username': result
-        }
-        self.assertEqual(ts._get_aws_username(user), result)
+def test_aws_username_aws():
+    ts = tsclient.TerrascriptClient('', '', 1, [])
+    result = 'aws'
+    user = {
+        'org_username': 'org',
+        'aws_username': result
+    }
+    assert ts._get_aws_username(user) == result
 
-    def test_aws_username_aws(self):
-        ts = tsclient.TerrascriptClient('', '', 1, [])
-        result = 'aws'
-        user = {
-            'org_username': 'org',
-            'aws_username': result
-        }
-        self.assertEqual(ts._get_aws_username(user), result)
-
-    def test_validate_mandatory_policies(self):
-        mandatory_policy = {
-            'name': 'mandatory',
-            'mandatory': True,
-        }
-        not_mandatory_policy = {
-            'name': 'not-mandatory',
-        }
-        account = {
-            'name': 'acc',
-            'policies': [mandatory_policy, not_mandatory_policy]
-        }
-        ts = tsclient.TerrascriptClient('', '', 1, [])
-        self.assertTrue(ts._validate_mandatory_policies(
-            account, [mandatory_policy], 'role'))
-        self.assertFalse(ts._validate_mandatory_policies(
-            account, [not_mandatory_policy], 'role'))
+def test_validate_mandatory_policies():
+    mandatory_policy = {
+        'name': 'mandatory',
+        'mandatory': True,
+    }
+    not_mandatory_policy = {
+        'name': 'not-mandatory',
+    }
+    account = {
+        'name': 'acc',
+        'policies': [mandatory_policy, not_mandatory_policy]
+    }
+    ts = tsclient.TerrascriptClient('', '', 1, [])
+    assert ts._validate_mandatory_policies(
+        account, [mandatory_policy], 'role') is True
+    assert ts._validate_mandatory_policies(
+        account, [not_mandatory_policy], 'role') is False
