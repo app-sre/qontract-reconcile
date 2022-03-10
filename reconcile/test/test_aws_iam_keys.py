91a925bfd0089f8eeffb846823133cdc5ca30c3d
Maor Friedman <mafriedm@redhat.com>
2022-03-10 14:48:37 +0200

    aws-iam-keys filter accounts without delete keys (#2230)

--- reconcile/test/test_aws_iam_keys.py
+++ reconcile/test/test_aws_iam_keys.py
@@ -4,18 +4,25 @@ import reconcile.aws_iam_keys as integ
 
 class TestSupportFunctions(TestCase):
     def test_filter_accounts_with_account_name(self):
-        a = {"name": "a"}
-        b = {"name": "b"}
+        a = {"name": "a", "deleteKeys": ["AKIA"]}
+        b = {"name": "b", "deleteKeys": ["AKIA"]}
         accounts = [a, b]
         filtered = integ.filter_accounts(accounts, a["name"])
         self.assertEqual(filtered, [a])
 
     def test_filter_accounts_without_account_name(self):
-        a = {"name": "a"}
-        b = {"name": "b"}
+        a = {"name": "a", "deleteKeys": ["AKIA"]}
+        b = {"name": "b", "deleteKeys": ["AKIA"]}
         accounts = [a, b]
         filtered = integ.filter_accounts(accounts, None)
         self.assertEqual(filtered, accounts)
+
+    def test_filter_accounts_without_delete_keys(self):
+        a = {"name": "a", "deleteKeys": ["AKIA"]}
+        b = {"name": "b"}
+        accounts = [a, b]
+        filtered = integ.filter_accounts(accounts, None)
+        self.assertEqual(filtered, [a])
 
     def test_get_keys_to_delete(self):
         a = {"name": "a", "deleteKeys": ["k1", "k2"]}
