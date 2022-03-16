e7309ccab0b06c0c6b7651da0aa56d0c9e2b1cee
Maor Friedman <mafriedm@redhat.com>
2022-03-16 10:00:30 +0200

    ocm get cluster aws account id

--- reconcile/test/test_utils_ocm.py
+++ reconcile/test/test_utils_ocm.py
@@ -1,5 +1,8 @@
-from unittest import TestCase
+from cmath import exp
+from unittest import TestCase, expectedFailure
 from unittest.mock import patch
+
+import pytest
 
 from reconcile.utils.ocm import OCM
 
@@ -52,3 +55,28 @@ class TestVersionRegex(TestCase):
     ):
         with self.assertRaises(TypeError):
             OCM("name", "url", "tid", "turl", "ot", blocked_versions=["["])
+
+
+@pytest.fixture
+def ocm(mocker):
+    mocker.patch('reconcile.utils.ocm.OCM._init_access_token')
+    mocker.patch('reconcile.utils.ocm.OCM._init_request_headers')
+    mocker.patch('reconcile.utils.ocm.OCM._init_clusters')
+    mocker.patch('reconcile.utils.ocm.OCM._init_blocked_versions')
+    return OCM("name", "url", "tid", "turl", "ot")
+
+
+def test_get_cluster_aws_account_id_none(mocker, ocm):
+    role_grants_mock = mocker.patch.object(ocm, "get_aws_infrastructure_access_role_grants")
+    role_grants_mock.return_value = []
+    result = ocm.get_cluster_aws_account_id('cluster')
+    assert result == None
+
+
+def test_get_cluster_aws_account_id_ok(mocker, ocm):
+    console_url = "https://signin.aws.amazon.com/switchrole?account=12345&roleName=role-1"
+    expected = "12345"
+    role_grants_mock = mocker.patch.object(ocm, "get_aws_infrastructure_access_role_grants")
+    role_grants_mock.return_value = [(None, None, None, console_url)]
+    result = ocm.get_cluster_aws_account_id('cluster')
+    assert result == expected
