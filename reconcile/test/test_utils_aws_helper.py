3a05c54d1b92c9afae597800e91154dfcf12099a
Maor Friedman <mafriedm@redhat.com>
2022-03-16 09:22:46 +0200

    extract get details method

--- reconcile/test/test_utils_aws_helper.py
+++ reconcile/test/test_utils_aws_helper.py
@@ -8,6 +8,13 @@ def test_get_user_id_from_arn():
     assert result == user_id
 
 
+def test_get_details_from_role_link():
+    role_link = "https://signin.aws.amazon.com/switchrole?account=12345&roleName=role-1"
+    expected = ("12345", "role-1")
+    result = awsh.get_details_from_role_link(role_link)
+    assert result == expected
+
+
 def test_get_role_arn_from_role_link():
     role_link = "https://signin.aws.amazon.com/switchrole?account=12345&roleName=role-1"
     expected = "arn:aws:iam::12345:role/role-1"
--- reconcile/utils/aws_helper.py
+++ reconcile/utils/aws_helper.py
@@ -3,13 +3,21 @@ def get_user_id_from_arn(assume_role):
     return assume_role.split("/")[1]
 
 
+def get_details_from_role_link(role_link):
+    # https://signin.aws.amazon.com/switchrole?
+    # account=<uid>&roleName=<role_name> -->
+    # 12345, role-1
+    details = role_link.split("?")[1].split("&")
+    uid = details[0].split("=")[1]
+    role_name = details[1].split("=")[1]
+    return uid, role_name
+
+
 def get_role_arn_from_role_link(role_link):
     # https://signin.aws.amazon.com/switchrole?
     # account=<uid>&roleName=<role_name> -->
     # arn:aws:iam::12345:role/role-1
-    details = role_link.split("?")[1].split("&")
-    uid = details[0].split("=")[1]
-    role_name = details[1].split("=")[1]
+    uid, role_name = get_details_from_role_link(role_link)
     return f"arn:aws:iam::{uid}:role/{role_name}"
 
 
