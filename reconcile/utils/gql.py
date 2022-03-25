3f30cc67b5a31ade7710dc09adc5e68edfe15ecc
Bhushan Thakur <bhthakur@redhat.com>
2022-03-24 20:57:40 -0400

    format files

--- reconcile/utils/gql.py
+++ reconcile/utils/gql.py
@@ -128,7 +128,9 @@ class GqlApi:
         if self.validate_schemas and not skip_validation:
             forbidden_schemas = [
                 # Here we are explicitly type casting _valid_schemas to list as mypy is unable to infer the type
-                schema for schema in query_schemas if schema not in cast(list, self._valid_schemas)
+                schema
+                for schema in query_schemas
+                if schema not in cast(list, self._valid_schemas)
             ]
             if forbidden_schemas:
                 raise GqlApiErrorForbiddenSchema(forbidden_schemas)
