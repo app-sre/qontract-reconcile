3ab921b6041f595c75024366c9c9e4854780742b
Maor Friedman <mafriedm@redhat.com>
2022-04-07 11:18:54 +0300

    adjust test

--- reconcile/test/test_version_bump.py
+++ reconcile/test/test_version_bump.py
@@ -6,15 +6,15 @@ import pytest
 from reconcile.utils.semver_helper import is_version_bumped
 
 
-class TestMake:
-    @staticmethod
-    @pytest.mark.skipif(
-        os.getuid() != 0,
-        reason="This test is only for CI environments",
-    )
-    def test_version_bump():
-        current_version = pkg_resources.get_distribution("qontract-reconcile").version
-        pypi_version = requests.get(
-            "https://pypi.org/pypi/qontract-reconcile/json"
-        ).json()["info"]["version"]
-        assert is_version_bumped(current_version, pypi_version)
+@pytest.mark.skipif(
+    os.getuid() != 0,
+    reason="This test is only for CI environments",
+)
+def test_version_bump():
+    current_version = pkg_resources.get_distribution("qontract-reconcile").version
+    pypi_version = requests.get("https://pypi.org/pypi/qontract-reconcile/json").json()[
+        "info"
+    ]["version"]
+    assert (
+        is_version_bumped(current_version, pypi_version) is True
+    ), "setup.py version must be bumped. see CONTRIBUTING.md for details"
