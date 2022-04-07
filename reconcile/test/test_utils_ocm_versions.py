e1fbb9f63e29e4e13d9dfa9040337f6a540d841e
Jordi Piriz <jpiriz@redhat.com>
2022-04-07 15:30:11 +0200

    Update OCM tests to pytest (#2330)

---
+++ reconcile/test/test_utils_ocm_versions.py
@@ -0,0 +1,50 @@
+import pytest
+from reconcile.utils.ocm import OCM
+
+
+@pytest.fixture
+def ocm(mocker):
+    mocker.patch("reconcile.utils.ocm.OCM._init_access_token")
+    mocker.patch("reconcile.utils.ocm.OCM._init_request_headers")
+    mocker.patch("reconcile.utils.ocm.OCM._init_clusters")
+    return OCM("name", "url", "tid", "turl", "ot")
+
+
+def test_no_blocked_versions(ocm):
+    result = ocm.version_blocked("1.2.3")
+    assert result is False
+
+
+def test_version_blocked(ocm):
+    ocm.blocked_versions = ["1.2.3"]
+    result = ocm.version_blocked("1.2.3")
+    assert result is True
+
+
+def test_version_not_blocked(ocm):
+    ocm.blocked_versions = ["1.2.3"]
+    result = ocm.version_blocked("1.2.4")
+    assert result is False
+
+
+def test_version_blocked_multiple(ocm):
+    ocm.blocked_versions = ["1.2.3", "1.2.4"]
+    result = ocm.version_blocked("1.2.3")
+    assert result is True
+
+
+def test_version_blocked_regex(ocm):
+    ocm.blocked_versions = [r"^.*-fc\..*$"]
+    result = ocm.version_blocked("1.2.3-fc.1")
+    assert result is True
+
+
+def test_version_not_blocked_regex(ocm):
+    ocm.blocked_versions = [r"^.*-fc\..*$"]
+    result = ocm.version_blocked("1.2.3-rc.1")
+    assert result is False
+
+
+def test_version_invalid_regex(ocm):
+    with pytest.raises(TypeError):
+        OCM("name", "url", "tid", "turl", "ot", blocked_versions=["["])
