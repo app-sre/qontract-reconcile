7688bfd19f696a106d864df98f759bf4d3780901
Maor Friedman <mafriedm@redhat.com>
2022-04-07 11:13:12 +0300

    unittest to pytest

--- reconcile/test/test_utils_semver_helper.py
+++ reconcile/test/test_utils_semver_helper.py
@@ -1,13 +1,10 @@
-from unittest import TestCase
-
 import reconcile.utils.semver_helper as svh
 
 
-class TestSortVersions(TestCase):
-    def test_sort_versions(self):
-        versions = ["4.8.0", "4.8.0-rc.0", "4.8.0-fc.1", "4.8.1", "4.8.0-rc.2"]
-        expected = ["4.8.0-fc.1", "4.8.0-rc.0", "4.8.0-rc.2", "4.8.0", "4.8.1"]
-        self.assertEqual(expected, svh.sort_versions(versions))
+def test_sort_versions():
+    versions = ["4.8.0", "4.8.0-rc.0", "4.8.0-fc.1", "4.8.1", "4.8.0-rc.2"]
+    expected = ["4.8.0-fc.1", "4.8.0-rc.0", "4.8.0-rc.2", "4.8.0", "4.8.1"]
+    assert expected == svh.sort_versions(versions)
 
 
 def test_is_version_bumped_lower():
