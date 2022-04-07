2d1303d32ad74059989ea7ab71a4d788e07c3404
Maor Friedman <mafriedm@redhat.com>
2022-04-07 12:34:23 +0300

    remove test make class

--- reconcile/test/test_make.py
+++ reconcile/test/test_make.py
@@ -6,19 +6,17 @@ from reconcile.utils.git import has_uncommited_changes
 from reconcile.utils.make import generate
 
 
-class TestMake:
-    @staticmethod
-    @pytest.mark.skipif(
-        os.getuid() != 0,
-        reason="This test is only for CI environments",
+@pytest.mark.skipif(
+    os.getuid() != 0,
+    reason="This test is only for CI environments",
+)
+def test_make_generate():
+    assert not has_uncommited_changes(), "No uncommited changes must " "exists"
+
+    result = generate()
+    # Just to make sure the command does not fail
+    assert not result.returncode
+
+    assert not has_uncommited_changes(), (
+        "No uncommited changes must " 'exists after "make generate"'
     )
-    def test_make_generate():
-        assert not has_uncommited_changes(), "No uncommited changes must " "exists"
-
-        result = generate()
-        # Just to make sure the command does not fail
-        assert not result.returncode
-
-        assert not has_uncommited_changes(), (
-            "No uncommited changes must " 'exists after "make generate"'
-        )
