667fb56d834b13c42662d2a72c5181644c4be5f1
Luis Fernando Muñoz Mejías <47329970+Piojo@users.noreply.github.com>
2022-04-07 18:12:45 +0200

    Change the default security policy for ELB (#2321)
    * Change the default security policy for ELB
    
    The current one allows for deprecated, insecure, and banned inside Red
    Hat TLS protocols. We must use at least TLSv1.2, which is 11 years
    old. It should be compatible with everything under the sun.
    
    Refs https://issues.redhat.com/browse/APPSRE-4803
    
    * Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="qontract-reconcile",
-    version="0.5.2",
+    version="0.5.3",
     license="Apache License 2.0",
 
     author="Red Hat App-SRE Team",
