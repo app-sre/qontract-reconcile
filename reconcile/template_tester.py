835cd196f2bea0cd29557876459b94197e652d8b
Maor Friedman <mafriedm@redhat.com>
2022-04-11 16:37:45 +0300

    do not use resource parent

--- reconcile/template_tester.py
+++ reconcile/template_tester.py
@@ -17,21 +17,11 @@ TEMPLATE_TESTS_QUERY = """
 {
   tests: template_tests_v1 {
     name
-    resource {
-      %s
-    }
-    resourceParent {
-      name
-      cluster {
-        name
-      }
-    }
+    resource_path
     expectedResult
   }
 }
-""" % indent(
-    orb.OPENSHIFT_RESOURCE, 2 * " "
-)
+"""
 
 
 def load_resource(path: str) -> dict:
@@ -43,17 +33,31 @@ def run(dry_run):
     template_tests = gqlapi.query(TEMPLATE_TESTS_QUERY)["tests"]
     error = False
     for tt in template_tests:
-        resource = tt["resource"]
-        resource["add_path_to_prom_rules"] = False
-        openshift_resource = orb.fetch_openshift_resource(
-            tt["resource"], tt["resourceParent"]
-        )
+        found = False
+        resource_path = tt["resource_path"]
         expected_result = load_resource(tt["expectedResult"])
-        if openshift_resource.body != expected_result:
+        for n in gqlapi.query(orb.NAMESPACES_QUERY)["namespaces"]:
+            openshift_resources = n.get("openshiftResources")
+            if not openshift_resources:
+                continue
+
+            for r in openshift_resources:
+                if resource_path != r["path"]:
+                    continue
+
+                found = True
+                openshift_resource = orb.fetch_openshift_resource(r, n)
+                if openshift_resource.body != expected_result:
+                    logging.error(
+                        f"rendered template is different from expected result in template test {tt['name']}:\n"
+                        f"rendered:\n{yaml.safe_dump(openshift_resource.body)}\n"
+                        f"expected result:\n{yaml.safe_dump(expected_result)}"
+                    )
+                    error = True
+
+        if not found:
             logging.error(
-                f"rendered template is different from expected result in template test {tt['name']}:\n"
-                f"rendered:\n{yaml.safe_dump(openshift_resource.body)}\n"
-                f"expected result:\n{yaml.safe_dump(expected_result)}"
+                f"resource defined in template tests {tt['name']} is not referenced from any namespaces"
             )
             error = True
 
