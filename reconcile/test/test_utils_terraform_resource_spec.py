252887ed13733e7c8554fefdb6b7a1c54754db9b
Gerd Oberlechner <goberlec@redhat.com>
2022-04-25 23:08:38 +0200

    implement provider driven terraform secret output format
    this PR implements the `generic-secret` provider, that can render output secret content based on jinja2 templates, e.g.
    
    ```yaml
     ---
      $schema: /openshift/namespace-1.yml
      ...
      terraformResources:
      - provider: aws-iam-service-account
        output_format:
          provider: generic-secret
          data:
            aws_access_key_id: {{ aws_access_key_id }}
            aws_secret_access_key: {{ aws_secret_access_key }}
            credentials: >-
              [default]
              aws_access_key_id = {{ aws_access_key_id }}
              aws_secret_access_key = {{ aws_secret_access_key }}
    ```
    
    design doc: https://gitlab.cee.redhat.com/service/app-interface/-/blob/master/docs/app-sre/design-docs/terraform-output-secret-format.md
    part of https://issues.redhat.com/browse/APPSRE-4766
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- reconcile/test/test_utils_terraform_resource_spec.py
+++ reconcile/test/test_utils_terraform_resource_spec.py
@@ -1,5 +1,7 @@
+from typing import Any
 from pydantic import ValidationError
 import pytest
+from reconcile.utils.openshift_resource import base64_encode_secret_field_value
 from reconcile.utils.terraform_resource_spec import (
     TerraformResourceUniqueKey,
     TerraformResourceSpec,
@@ -92,3 +94,142 @@ def test_spec_annotation_parsing_none_present():
         namespace={},
     )
     assert s._annotations() == {}
+
+
+@pytest.fixture
+def resource_secret() -> dict[str, Any]:
+    return {"yakk_name": "Furry", "visual_characteristics": "furry", "mood": "grumpy"}
+
+
+@pytest.fixture
+def spec() -> TerraformResourceSpec:
+    return TerraformResourceSpec(
+        resource={
+            "identifier": "i",
+            "provider": "p",
+            "account": "a",
+        },
+        namespace={},
+    )
+
+
+#
+# tests for terraform output format
+#
+
+
+def test_terraform_generic_secret_output_format(
+    spec: TerraformResourceSpec, resource_secret: dict[str, Any]
+):
+    spec.resource["output_format"] = {  # type: ignore[index]
+        "provider": "generic-secret",
+        "data": """
+            motd: The {{ mood }} Yakk {{ yakk_name }} is {{ visual_characteristics }}.
+        """,
+    }
+    spec.secret = resource_secret
+
+    output_secret = spec.build_oc_secret("int", "1.0")
+    assert output_secret.body["data"]["motd"] == base64_encode_secret_field_value(
+        "The grumpy Yakk Furry is furry."
+    )
+
+
+def test_terraform_generic_secret_output_format_no_data(
+    spec: TerraformResourceSpec, resource_secret: dict[str, Any]
+):
+    """
+    this test shows backwards compatibility with the simple dict output when
+    no data is given but the provider is specified
+    """
+    spec.resource["output_format"] = {  # type: ignore[index]
+        "provider": "generic-secret",
+    }
+    spec.secret = resource_secret
+
+    output_secret = spec.build_oc_secret("int", "1.0")
+    assert len(output_secret.body["data"]) == len(resource_secret)
+    for k, v in resource_secret.items():
+        assert output_secret.body["data"][k] == base64_encode_secret_field_value(v)
+
+
+def test_terraform_no_output_format_provider(
+    spec: TerraformResourceSpec, resource_secret: dict[str, Any]
+):
+    """
+    this test shows full backwards compatibility when no provider has been specified
+    """
+    spec.secret = resource_secret
+
+    output_secret = spec.build_oc_secret("int", "1.0")
+    assert len(output_secret.body["data"]) == len(resource_secret)
+    for k, v in resource_secret.items():
+        assert output_secret.body["data"][k] == base64_encode_secret_field_value(v)
+
+
+def test_terraform_generic_secret_output_format_not_a_dict(
+    spec: TerraformResourceSpec, resource_secret: dict[str, Any]
+):
+    """
+    this test shows how a data template for a generic-secret provider must result
+    in a valid dict and fails otherwise
+    """
+    spec.resource["output_format"] = {  # type: ignore[index]
+        "provider": "generic-secret",
+        "data": "not_a_dict",
+    }
+    spec.secret = resource_secret
+
+    with pytest.raises(ValueError):
+        spec.build_oc_secret("int", "1.0")
+
+
+def test_terraform_generic_secret_output_format_not_str_keys(
+    spec: TerraformResourceSpec, resource_secret: dict[str, Any]
+):
+    """
+    this test shows how a data template for a generic-secret provider must produce
+    string keys
+    """
+    spec.resource["output_format"] = {  # type: ignore[index]
+        "provider": "generic-secret",
+        "data": "1: value",
+    }
+    spec.secret = resource_secret
+
+    with pytest.raises(ValueError):
+        spec.build_oc_secret("int", "1.0")
+
+
+def test_terraform_generic_secret_output_format_not_str_val(
+    spec: TerraformResourceSpec, resource_secret: dict[str, Any]
+):
+    """
+    this test shows how a data template for a generic-secret provider must produce
+    string values
+    """
+    spec.resource["output_format"] = {  # type: ignore[index]
+        "provider": "generic-secret",
+        "data": "key: 1",
+    }
+    spec.secret = resource_secret
+
+    with pytest.raises(ValueError):
+        spec.build_oc_secret("int", "1.0")
+
+
+def test_terraform_generic_secret_output_key_too_long(
+    spec: TerraformResourceSpec, resource_secret: dict[str, Any]
+):
+    """
+    tests for too long secret keys (max length in kubernetes is 253 characters )
+    """
+    long_key = "a" * 254
+    spec.resource["output_format"] = {  # type: ignore[index]
+        "provider": "generic-secret",
+        "data": f"{ long_key }: value",
+    }
+    spec.secret = resource_secret
+
+    with pytest.raises(ValueError):
+        spec.build_oc_secret("int", "1.0")
