e89085a1c0f44c61a78893ec1c3f26b0d1fa0262
Maor Friedman <mafriedm@redhat.com>
2022-05-16 09:24:31 +0300

    more pytest-y cli tests

--- reconcile/test/test_cli.py
+++ reconcile/test/test_cli.py
@@ -3,9 +3,7 @@ from click.testing import CliRunner
 import reconcile.cli as reconcile_cli
 
 
-class TestCli:
-    @staticmethod
-    def test_config_is_required():
-        runner = CliRunner()
-        result = runner.invoke(reconcile_cli.integration)
-        assert result.exit_code == 0
+def test_config_is_required():
+    runner = CliRunner()
+    result = runner.invoke(reconcile_cli.integration)
+    assert result.exit_code == 0
