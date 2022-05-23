3450126a8719375f368816ee300f01bb828c59ce
Maor Friedman <mafriedm@redhat.com>
2022-05-23 13:07:50 +0300

    error handling

--- reconcile/query_validator.py
+++ reconcile/query_validator.py
@@ -1,3 +1,7 @@
+import logging
+import sys
+
+from reconcile.status import ExitCodes
 from reconcile.utils import gql
 from reconcile.utils.semver_helper import make_semver
 
@@ -21,6 +25,14 @@ QUERY_VALIDATIONS_QUERY = """
 def run(dry_run):
     gqlapi = gql.get_api()
     query_validations = gqlapi.query(QUERY_VALIDATIONS_QUERY)["validations"]
+    error = False
     for qv in query_validations:
         for q in qv["queries"]:
-            gqlapi.query(gql.get_resource(q["path"])["content"])
+            try:
+                gqlapi.query(gql.get_resource(q["path"])["content"])
+            except (gql.GqlGetResourceError, gql.GqlApiError) as e:
+                error = True
+                logging.error(f"query validation error in {qv['name']}: {str(e)}")
+
+    if error:
+        sys.exit(ExitCodes.ERROR)
