9b8333d2328293e4273e3af263b76522a0b0ab4c
Maor Friedman <mafriedm@redhat.com>
2022-06-02 10:50:29 +0300

    add external resources common functions and tests

---
+++ reconcile/utils/external_resources.py
@@ -0,0 +1,41 @@
+from typing import Mapping, List, Dict, Any, Optional
+
+
+PROVIDER_AWS = "aws"
+
+
+def get_external_resources(
+    namespace_info: Mapping[str, Any], provision_provider: Optional[str] = None
+) -> List[Dict[str, Any]]:
+    resources = []
+    if not managed_external_resources(namespace_info):
+        return resources
+
+    terraform_resources = namespace_info.get("terraformResources") or []
+    for r in terraform_resources:
+        r["provision_provider"] = PROVIDER_AWS
+        resources.append(r)
+
+    external_resources = namespace_info.get("externalResources") or []
+    for e in external_resources:
+        provisioner_name = e["provisioner"]["name"]
+        for r in e["resources"]:
+            r["provision_provider"] = e["provider"]
+            r["account"] = provisioner_name
+            resources.append(r)
+
+    if provision_provider:
+        resources = [
+            r for r in resources if r["provision_provider"] == provision_provider
+        ]
+
+    return resources
+
+
+def managed_external_resources(namespace_info: Mapping[str, Any]) -> bool:
+    if namespace_info.get("managedTerraformResources"):
+        return True
+    if namespace_info.get("managedExternalResources"):
+        return True
+
+    return False
