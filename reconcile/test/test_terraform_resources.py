8aac3a4cc3af25b7a88c724887178cc0a7e09c5f
Maor Friedman <mafriedm@redhat.com>
2022-06-19 13:43:55 +0300

    update terraform resources tests to use external resources

--- reconcile/test/test_terraform_resources.py
+++ reconcile/test/test_terraform_resources.py
@@ -6,17 +6,19 @@ from reconcile.utils.external_resource_spec import (
 
 
 def test_filter_namespaces_no_managed_tf_resources():
-    ra = {"account": "a", "identifier": "a", "provider": "p"}
+    ra = {"identifier": "a", "provider": "p"}
     ns1 = {
         "name": "ns1",
-        "managedTerraformResources": False,
-        "terraformResources": [],
+        "managedExternalResources": False,
+        "externalResources": [],
         "cluster": {"name": "c"},
     }
     ns2 = {
         "name": "ns2",
-        "managedTerraformResources": True,
-        "terraformResources": [ra],
+        "managedExternalResources": True,
+        "externalResources": [
+            {"provider": "aws", "provisioner": {"name": "a"}, "resources": [ra]}
+        ],
         "cluster": {"name": "c"},
     }
     namespaces = [ns1, ns2]
@@ -25,18 +27,22 @@ def test_filter_namespaces_no_managed_tf_resources():
 
 
 def test_filter_namespaces_with_account_filter():
-    ra = {"account": "a", "identifier": "a", "provider": "p"}
-    rb = {"account": "b", "identifier": "b", "provider": "p"}
+    ra = {"identifier": "a", "provider": "p"}
+    rb = {"identifier": "b", "provider": "p"}
     ns1 = {
         "name": "ns1",
-        "managedTerraformResources": True,
-        "terraformResources": [ra],
+        "managedExternalResources": True,
+        "externalResources": [
+            {"provider": "aws", "provisioner": {"name": "a"}, "resources": [ra]}
+        ],
         "cluster": {"name": "c"},
     }
     ns2 = {
         "name": "ns2",
-        "managedTerraformResources": True,
-        "terraformResources": [rb],
+        "managedExternalResources": True,
+        "externalResources": [
+            {"provider": "aws", "provisioner": {"name": "b"}, "resources": [rb]}
+        ],
         "cluster": {"name": "c"},
     }
     namespaces = [ns1, ns2]
@@ -45,18 +51,22 @@ def test_filter_namespaces_with_account_filter():
 
 
 def test_filter_namespaces_no_account_filter():
-    ra = {"account": "a", "identifier": "a", "provider": "p"}
-    rb = {"account": "b", "identifier": "b", "provider": "p"}
+    ra = {"identifier": "a", "provider": "p"}
+    rb = {"identifier": "b", "provider": "p"}
     ns1 = {
         "name": "ns1",
-        "managedTerraformResources": True,
-        "terraformResources": [ra],
+        "managedExternalResources": True,
+        "externalResources": [
+            {"provider": "aws", "provisioner": {"name": "a"}, "resources": [ra]}
+        ],
         "cluster": {"name": "c"},
     }
     ns2 = {
         "name": "ns2",
-        "managedTerraformResources": True,
-        "terraformResources": [rb],
+        "managedExternalResources": True,
+        "externalResources": [
+            {"provider": "aws", "provisioner": {"name": "b"}, "resources": [rb]}
+        ],
         "cluster": {"name": "c"},
     }
     namespaces = [ns1, ns2]
@@ -70,17 +80,19 @@ def test_filter_namespaces_no_tf_resources_no_account_filter():
     attached. this way we can delete the last terraform resources that might have been
     defined on the namespace previously
     """
-    ra = {"account": "a", "identifier": "a", "provider": "p"}
+    ra = {"identifier": "a", "provider": "p"}
     ns1 = {
         "name": "ns1",
-        "managedTerraformResources": True,
-        "terraformResources": [],
+        "managedExternalResources": True,
+        "externalResources": [],
         "cluster": {"name": "c"},
     }
     ns2 = {
         "name": "ns2",
-        "managedTerraformResources": True,
-        "terraformResources": [ra],
+        "managedExternalResources": True,
+        "externalResources": [
+            {"provider": "aws", "provisioner": {"name": "a"}, "resources": [ra]}
+        ],
         "cluster": {"name": "c"},
     }
 
@@ -95,17 +107,19 @@ def test_filter_tf_namespaces_no_tf_resources_with_account_filter():
     to enable terraform resource deletion. in contrast to that, a namespace with a resource
     that does not match the account will not be returned.
     """
-    ra = {"account": "a", "identifier": "a", "provider": "p"}
+    ra = {"identifier": "a", "provider": "p"}
     ns1 = {
         "name": "ns1",
-        "managedTerraformResources": True,
-        "terraformResources": [],
+        "managedExternalResources": True,
+        "externalResources": [],
         "cluster": {"name": "c"},
     }
     ns2 = {
         "name": "ns2",
-        "managedTerraformResources": True,
-        "terraformResources": [ra],
+        "managedExternalResources": True,
+        "externalResources": [
+            {"provider": "aws", "provisioner": {"name": "a"}, "resources": [ra]}
+        ],
         "cluster": {"name": "c"},
     }
     namespaces = [ns1, ns2]
@@ -118,11 +132,13 @@ def test_tf_disabled_namespace_with_resources():
     even if a namespace has tf resources, they are not considered when the
     namespace is not enabled for tf resource management
     """
-    ra = {"account": "a", "identifier": "a", "provider": "p"}
+    ra = {"identifier": "a", "provider": "p"}
     ns1 = {
         "name": "ns1",
-        "managedTerraformResources": False,
-        "terraformResources": [ra],
+        "managedExternalResources": False,
+        "externalResources": [
+            {"provider": "aws", "provisioner": {"name": "a"}, "resources": [ra]}
+        ],
         "cluster": {"name": "c"},
     }
     namespaces = [ns1]
@@ -135,11 +151,13 @@ def test_resource_specs_without_account_filter():
     if no account filter is given, all resources of namespaces with
     enabled tf resource management are expected to be returned
     """
-    ra = {"account": "a", "identifier": "a", "provider": "p"}
+    ra = {"identifier": "a", "provider": "p"}
     ns1 = {
         "name": "ns1",
-        "managedTerraformResources": True,
-        "terraformResources": [ra],
+        "managedExternalResources": True,
+        "externalResources": [
+            {"provider": "aws", "provisioner": {"name": "a"}, "resources": [ra]}
+        ],
         "cluster": {"name": "c"},
     }
     namespaces = [ns1]
@@ -154,12 +172,15 @@ def test_resource_specs_with_account_filter():
     if an account filter is given only the resources defined for
     that account are expected
     """
-    ra = {"account": "a", "identifier": "a", "provider": "p"}
-    rb = {"account": "b", "identifier": "b", "provider": "p"}
+    ra = {"identifier": "a", "provider": "p"}
+    rb = {"identifier": "b", "provider": "p"}
     ns1 = {
         "name": "ns1",
-        "managedTerraformResources": True,
-        "terraformResources": [ra, rb],
+        "managedExternalResources": True,
+        "externalResources": [
+            {"provider": "aws", "provisioner": {"name": "a"}, "resources": [ra]},
+            {"provider": "aws", "provisioner": {"name": "b"}, "resources": [rb]},
+        ],
         "cluster": {"name": "c"},
     }
     namespaces = [ns1]
