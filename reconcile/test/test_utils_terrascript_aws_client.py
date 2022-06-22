6b89feba480aaa11094102b3f50a72e25023281d
Maor Friedman <mafriedm@redhat.com>
2022-06-22 09:39:21 +0300

    extract ts as fixture

--- reconcile/test/test_utils_terrascript_aws_client.py
+++ reconcile/test/test_utils_terrascript_aws_client.py
@@ -1,3 +1,5 @@
+import pytest
+
 import reconcile.utils.terrascript_aws_client as tsclient
 
 
@@ -9,21 +11,24 @@ def test_sanitize_resource_with_wildcard():
     assert tsclient.safe_resource_id("*.foo.example.com") == "_star_foo_example_com"
 
 
-def test_aws_username_org():
-    ts = tsclient.TerrascriptClient("", "", 1, [])
+@pytest.fixture
+def ts():
+    return tsclient.TerrascriptClient("", "", 1, [])
+
+
+def test_aws_username_org(ts):
     result = "org"
     user = {"org_username": result}
     assert ts._get_aws_username(user) == result
 
 
-def test_aws_username_aws():
-    ts = tsclient.TerrascriptClient("", "", 1, [])
+def test_aws_username_aws(ts):
     result = "aws"
     user = {"org_username": "org", "aws_username": result}
     assert ts._get_aws_username(user) == result
 
 
-def test_validate_mandatory_policies():
+def test_validate_mandatory_policies(ts):
     mandatory_policy = {
         "name": "mandatory",
         "mandatory": True,
@@ -32,7 +37,6 @@ def test_validate_mandatory_policies():
         "name": "not-mandatory",
     }
     account = {"name": "acc", "policies": [mandatory_policy, not_mandatory_policy]}
-    ts = tsclient.TerrascriptClient("", "", 1, [])
     assert ts._validate_mandatory_policies(account, [mandatory_policy], "role") is True
     assert (
         ts._validate_mandatory_policies(account, [not_mandatory_policy], "role")
@@ -48,28 +52,25 @@ class MockJenkinsApi:
         return self.response
 
 
-def test_use_previous_image_id_no_upstream():
-    ts = tsclient.TerrascriptClient("", "", 1, [])
+def test_use_previous_image_id_no_upstream(ts):
     assert ts._use_previous_image_id({}) is False
 
 
-def test_use_previous_image_id_false(mocker):
+def test_use_previous_image_id_false(mocker, ts):
     result = False
     mocker.patch(
         "reconcile.utils.terrascript_aws_client.TerrascriptClient.init_jenkins",
         return_value=MockJenkinsApi(result),
     )
-    ts = tsclient.TerrascriptClient("", "", 1, [])
     image = {"upstream": {"instance": {"name": "ci"}, "name": "job"}}
     assert ts._use_previous_image_id(image) == result
 
 
-def test_use_previous_image_id_true(mocker):
+def test_use_previous_image_id_true(mocker, ts):
     result = True
     mocker.patch(
         "reconcile.utils.terrascript_aws_client.TerrascriptClient.init_jenkins",
         return_value=MockJenkinsApi(result),
     )
-    ts = tsclient.TerrascriptClient("", "", 1, [])
     image = {"upstream": {"instance": {"name": "ci"}, "name": "job"}}
     assert ts._use_previous_image_id(image) == result
