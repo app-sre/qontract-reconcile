3a28f0b7c5b2f5487d33bea0ddcf7509a49f484b
Maor Friedman <mafriedm@redhat.com>
2022-06-22 13:06:56 +0300

    fix/ignore type checks

--- reconcile/utils/helpers.py
+++ reconcile/utils/helpers.py
@@ -7,10 +7,10 @@ DEFAULT_TOGGLE_LEVEL = logging.ERROR
 
 
 @contextmanager
-def toggle_logger(log_level: str = DEFAULT_TOGGLE_LEVEL):
+def toggle_logger(log_level: int = DEFAULT_TOGGLE_LEVEL):
     logger = logging.getLogger()
     default_level = logger.level
     try:
-        yield logger.setLevel(log_level)
+        yield logger.setLevel(log_level)  # type: ignore[func-returns-value]
     finally:
         logger.setLevel(default_level)
