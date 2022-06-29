fa16a514621af97182cc0e497ae4e877c970840d
Jordi Piriz <jpiriz@redhat.com>
2022-06-29 11:44:58 +0200

    Fix Route test

--- reconcile/test/test_openshift_resources_base.py
+++ reconcile/test/test_openshift_resources_base.py
@@ -85,7 +85,7 @@ def test_route(namespaces: list[dict[str, Any]], mocker):
                 ],
             }
         ],
-        ["Route", "Certificate.cert-manager.io"],
+        ["Route"],
     )
     ns, override = canonicalize_namespaces(namespaces, ["route"])
     assert (ns, override) == expected
