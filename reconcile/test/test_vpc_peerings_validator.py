fca04b36e6ae9f7391bafb8537063fa97846d469
Maor Friedman <mafriedm@redhat.com>
2022-07-12 09:38:42 +0300

    stop being lazy

--- reconcile/test/test_vpc_peerings_validator.py
+++ reconcile/test/test_vpc_peerings_validator.py
@@ -16,7 +16,7 @@ from reconcile.vpc_peerings_validator import (
 
 
 @pytest.fixture
-def query_data() -> VpcPeeringsValidatorQueryData:
+def query_data_i2p() -> VpcPeeringsValidatorQueryData:
     return VpcPeeringsValidatorQueryData(
         clusters=[
             ClusterV1(
@@ -43,29 +43,61 @@ def query_data() -> VpcPeeringsValidatorQueryData:
 
 
 def test_validate_no_internal_to_public_peerings_invalid(
-    query_data: VpcPeeringsValidatorQueryData,
+    query_data_i2p: VpcPeeringsValidatorQueryData,
 ):
-    assert validate_no_internal_to_public_peerings(query_data) is False
+    assert validate_no_internal_to_public_peerings(query_data_i2p) is False
 
 
 def test_validate_no_internal_to_public_peerings_valid_private(
-    query_data: VpcPeeringsValidatorQueryData,
+    query_data_i2p: VpcPeeringsValidatorQueryData,
 ):
-    query_data.clusters[0].peering.connections[0].cluster.spec.private = True  # type: ignore[index,union-attr]
-    assert validate_no_internal_to_public_peerings(query_data) is True
+    query_data_i2p.clusters[0].peering.connections[0].cluster.spec.private = True  # type: ignore[index,union-attr]
+    assert validate_no_internal_to_public_peerings(query_data_i2p) is True
 
 
 def test_validate_no_internal_to_public_peerings_valid_internal(
-    query_data: VpcPeeringsValidatorQueryData,
+    query_data_i2p: VpcPeeringsValidatorQueryData,
 ):
-    assert query_data.clusters is not None
-    query_data.clusters[0].peering.connections[0].cluster.internal = True  # type: ignore[index,union-attr]
-    assert validate_no_internal_to_public_peerings(query_data) is True
+    assert query_data_i2p.clusters is not None
+    query_data_i2p.clusters[0].peering.connections[0].cluster.internal = True  # type: ignore[index,union-attr]
+    assert validate_no_internal_to_public_peerings(query_data_i2p) is True
+
+
+@pytest.fixture
+def query_data_p2p() -> VpcPeeringsValidatorQueryData:
+    return VpcPeeringsValidatorQueryData(
+        clusters=[
+            ClusterV1(
+                name="cluster1",
+                spec=ClusterSpecV1(private=False),
+                internal=False,
+                peering=ClusterPeeringV1(
+                    connections=[
+                        ClusterPeeringConnectionClusterAccepterV1(
+                            provider="cluster-vpc-accepter",
+                            cluster=ClusterPeeringConnectionClusterAccepterV1_ClusterV1(
+                                name="cluster2",
+                                spec=ClusterPeeringConnectionClusterAccepterV1_ClusterV1_ClusterSpecV1(
+                                    private=False
+                                ),
+                                internal=False,
+                            ),
+                        ),
+                    ],
+                ),
+            ),
+        ],
+    )
+
+
+def test_validate_no_public_to_public_peerings_invalid(
+    query_data_p2p: VpcPeeringsValidatorQueryData,
+):
+    assert validate_no_public_to_public_peerings(query_data_p2p) is False
 
 
 def test_validate_no_public_to_public_peerings_valid(
-    query_data: VpcPeeringsValidatorQueryData,
+    query_data_p2p: VpcPeeringsValidatorQueryData,
 ):
-    query_data.clusters[0].internal = False  # type: ignore[index]
-    query_data.clusters[0].spec.private = False  # type: ignore[index,union-attr]
-    assert validate_no_public_to_public_peerings(query_data) is False
+    query_data_p2p.clusters[0].peering.connections[0].cluster.spec.private = True  # type: ignore[index,union-attr]
+    assert validate_no_public_to_public_peerings(query_data_p2p) is True
