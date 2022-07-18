aed13af1282430d6f306e521b0eda7f660a8751c
Alex Njaastad <anjaasta@redhat.com>
2022-07-18 16:49:25 -0500

    grab vars

--- reconcile/utils/terrascript_aws_client.py
+++ reconcile/utils/terrascript_aws_client.py
@@ -4530,6 +4530,8 @@ class TerrascriptClient:  # pylint: disable=too-many-public-methods
         account = spec.provisioner_name
         identifier = spec.identifier
         common_values = self.init_values(spec)
+        certificate_arn = common_values.get("certificate_arn")
+        domain_name = common_values.get("domain_name")
         tf_resources = []
         self.init_common_outputs(tf_resources, spec)
 
@@ -5106,8 +5108,8 @@ class TerrascriptClient:  # pylint: disable=too-many-public-methods
         # DOMAIN NAME
         api_gateway_domain_name_resource = aws_api_gateway_domain_name(
             "domain",
-            regional_certificate_arn="local.certificate_arn",
-            domain_name="local.domain_name",
+            regional_certificate_arn=certificate_arn,
+            domain_name=domain_name,
         )
         tf_resources.append(api_gateway_domain_name_resource)
 
