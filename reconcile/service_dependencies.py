77389885f40fe3e15e621a5293c6fc993a4cc4e4
Karl Fischer <kfischer@redhat.com>
2022-07-20 10:02:54 +0200

    APPSRE-5046 make intent more explicit

--- reconcile/service_dependencies.py
+++ reconcile/service_dependencies.py
@@ -45,8 +45,12 @@ def get_desired_dependency_names(
             required_dep_names.update(get_dependency_names(dependency_map, instance))
 
     saas_files = app.saas_files or []
-    if [s for s in saas_files if s.pipelines_provider.provider == "tekton"]:
-        # All our tekton SaaS deployment pipelines are in appsrep05ue1
+    tekton_pipelines = [
+        s for s in saas_files if s.pipelines_provider.provider == "tekton"
+    ]
+    if tekton_pipelines:
+        # All our tekton SaaS deployment pipelines are in appsrep05ue1,
+        # hence openshift is a required dependency.
         required_dep_names.update(get_dependency_names(dependency_map, "openshift"))
 
     quay_repos = app.quay_repos
