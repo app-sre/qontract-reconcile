e9716c1ef214033ff1b8e3dcdc38e6806677d6bd
Esron Silva <essilva@redhat.com>
2022-07-22 15:09:21 -0300

    Replace inline policies for managed policies in ecr

--- reconcile/utils/terrascript_aws_client.py
+++ reconcile/utils/terrascript_aws_client.py
@@ -2438,7 +2438,6 @@ class TerrascriptClient:  # pylint: disable=too-many-public-methods
 
         # iam user policy for bucket
         values = {}
-        values["user"] = identifier
         values["name"] = identifier
         policy = {
             "Version": "2012-10-17",
@@ -2478,8 +2477,16 @@ class TerrascriptClient:  # pylint: disable=too-many-public-methods
         }
         values["policy"] = json.dumps(policy, sort_keys=True)
         values["depends_on"] = self.get_dependencies([user_tf_resource])
-        tf_resource = aws_iam_user_policy(identifier, **values)
-        tf_resources.append(tf_resource)
+        tf_aws_iam_policy = aws_iam_policy(identifier, **values)
+        tf_resources.append(tf_aws_iam_policy)
+
+        tf_aws_iam_user_policy_attachment = aws_iam_user_policy_attachment(
+            identifier,
+            user=identifier,
+            policy_arn=f"${{{tf_aws_iam_policy.arn}}}",
+            depends_on=self.get_dependencies([user_tf_resource, tf_aws_iam_policy]),
+        )
+        tf_resources.append(tf_aws_iam_user_policy_attachment)
 
         self.add_resources(account, tf_resources)
 
