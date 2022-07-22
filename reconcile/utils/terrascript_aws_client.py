2425909419504eece452c0ed88e1b509e2bd2788
Esron Silva <essilva@redhat.com>
2022-07-22 15:10:19 -0300

    Replace inlice police for managed police in cloudwatch

--- reconcile/utils/terrascript_aws_client.py
+++ reconcile/utils/terrascript_aws_client.py
@@ -3026,8 +3026,16 @@ class TerrascriptClient:  # pylint: disable=too-many-public-methods
             "policy": json.dumps(policy, sort_keys=True),
             "depends_on": self.get_dependencies([user_tf_resource]),
         }
-        tf_resource = aws_iam_user_policy(identifier, **values)
-        tf_resources.append(tf_resource)
+        tf_aws_iam_policy = aws_iam_policy(identifier, **values)
+        tf_resources.append(tf_aws_iam_policy)
+
+        tf_aws_iam_user_policy_attachment = aws_iam_user_policy_attachment(
+            identifier,
+            user=identifier,
+            policy_arn=f"${{{tf_aws_iam_policy.arn}}}",
+            depends_on=self.get_dependencies([user_tf_resource, tf_aws_iam_policy]),
+        )
+        tf_resources.append(tf_aws_iam_user_policy_attachment)
 
         self.add_resources(account, tf_resources)
 
