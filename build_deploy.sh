793a2d1aca924f6ce49aa4272817e7170a063013
Maor Friedman <mafriedm@redhat.com>
2022-07-28 11:35:08 +0300

    run make test as part of a push (#2631)

--- build_deploy.sh
+++ build_deploy.sh
@@ -5,7 +5,7 @@ mkdir -p "$DOCKER_CONF"
 docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
 
 # build images
-make build push
+make test build push
 
 # publish to pypi
 ./build_tag.sh
