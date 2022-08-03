b5f6976d8f8ce84ba4a1a7579a75262aa620cdcd
Ryan Wallace <rywallac@redhat.com>
2022-08-03 13:15:23 -0400

    fix log_destination_configs param

--- reconcile/utils/terrascript_aws_client.py
+++ reconcile/utils/terrascript_aws_client.py
@@ -5406,7 +5406,7 @@ class TerrascriptClient:  # pylint: disable=too-many-public-methods
         waf_web_acl_logging_configuration_resource = (
             aws_wafv2_web_acl_logging_configuration(
                 "waf_logging_configuration",
-                log_destination_configs=["${aws_wafv2_web_acl.api_waf.arn}"],
+                log_destination_configs=["${aws_cloudwatch_log_group.waf_log_group.arn}"],
                 resource_arn="${aws_wafv2_web_acl.api_waf.arn}",
             )
         )
