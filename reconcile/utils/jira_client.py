e0d8d6b2c47bc87bb8e860f2e522fbcfbb3336e0
Maor Friedman <mafriedm@redhat.com>
2022-08-08 12:57:14 +0300

    add todo

--- reconcile/utils/jira_client.py
+++ reconcile/utils/jira_client.py
@@ -34,6 +34,7 @@ class JiraClient:
         while True:
             index = block_num * block_size
             issues = self.jira.search_issues(jql, index, block_size, **kwargs)
+            # TODO: investigate why the arg-type ignore is needed in CI
             all_issues.extend(issues)  # type: ignore[arg-type]
             if len(issues) < block_size:
                 break
