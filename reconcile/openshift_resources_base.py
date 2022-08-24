7db9464f49299b059f6af8e9ab34326fae2f4f24
Gerd Oberlechner <goberlec@redhat.com>
2022-08-24 14:11:20 +0200

    openshift-resource/route/prometheus-rule-tester resource resolution and early-exit performance (#2734)
    * use qontract-server based resource resolution
    
    fetching resource content with resource resolution speeds up all integrations relying on openshift-resource resource references
    
    depends on https://github.com/app-sre/qontract-schemas/pull/227
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>
    
    * prometheus-rule-tester reusing get_namespace function to fetch prometheus rule resources
    
    enhanced the get_namespace function to allow filtering based on the schema of resources.
    in the case of proemtheus-rule-tester, only resources with the schema /openshift/prometheus-rule-1.yml are relevant
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>
    
    * generalize early-exit functions for openshift resource based integrations
    
    the early-exit functions from openshift-resources have been moved to obenshift_resource_base and have been generalized to work for all openshift resource based integrations: openshift-resources, openshift-routes and prometheus-rule-tester
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>
    
    * don't parse resources during early-exit checks
    
    during early-exit checks, the content of a resource is sufficient to decide if an integrations dry-run can be aborted
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>
    
    * cache jinja template compilation
    
    template compilation takes a considerable time so caching the results based on env and body will speed up all openshift-resources related integrations

--- reconcile/openshift_resources_base.py
+++ reconcile/openshift_resources_base.py
@@ -1,4 +1,6 @@
 import base64
+from contextlib import contextmanager
+from functools import cache
 import json
 import logging
 import sys
@@ -58,14 +60,22 @@ from reconcile.github_users import init_github
 OPENSHIFT_RESOURCE = """
 provider
 ... on NamespaceOpenshiftResourceResource_v1 {
-  path
+  resource: path {
+    content
+    path
+    schema
+  }
   validate_json
   validate_alertmanager_config
   alertmanager_config_key
   enable_query_support
 }
 ... on NamespaceOpenshiftResourceResourceTemplate_v1 {
-  path
+  resource: path {
+    content
+    path
+    schema
+  }
   type
   variables
   validate_alertmanager_config
@@ -83,7 +93,11 @@ provider
   alertmanager_config_key
 }
 ... on NamespaceOpenshiftResourceRoute_v1 {
-  path
+  resource: path {
+    content
+    path
+    schema
+  }
   vault_tls_secret_path
   vault_tls_secret_version
 }
@@ -234,11 +248,30 @@ def lookup_graphql_query_results(query: str, **kwargs) -> list[Any]:
     return results
 
 
-def process_jinja2_template(body, vars=None, env=None, settings=None):
+@cache
+def compile_jinja2_template(body, extra_curly: bool = False):
+    env: dict = {}
+    if extra_curly:
+        env = {
+            "block_start_string": "{{%",
+            "block_end_string": "%}}",
+            "variable_start_string": "{{{",
+            "variable_end_string": "}}}",
+            "comment_start_string": "{{#",
+            "comment_end_string": "#}}",
+        }
+
+    jinja_env = jinja2.Environment(
+        extensions=[B64EncodeExtension, RaiseErrorExtension],
+        undefined=jinja2.StrictUndefined,
+        **env,
+    )
+    return jinja_env.from_string(body)
+
+
+def process_jinja2_template(body, vars=None, extra_curly: bool = False, settings=None):
     if vars is None:
         vars = {}
-    if env is None:
-        env = {}
     vars.update(
         {
             "vault": lambda p, k, v=None: lookup_secret(
@@ -256,12 +289,7 @@ def process_jinja2_template(body, vars=None, env=None, settings=None):
     vars.update({"query": lookup_graphql_query_results})
     vars.update({"url": url_makes_sense})
     try:
-        env = jinja2.Environment(
-            extensions=[B64EncodeExtension, RaiseErrorExtension],
-            undefined=jinja2.StrictUndefined,
-            **env,
-        )
-        template = env.from_string(body)
+        template = compile_jinja2_template(body, extra_curly)
         r = template.render(vars)
     except Exception as e:
         raise Jinja2TemplateError(e)
@@ -271,15 +299,7 @@ def process_jinja2_template(body, vars=None, env=None, settings=None):
 def process_extracurlyjinja2_template(body, vars=None, env=None, settings=None):
     if vars is None:
         vars = {}
-    env = {
-        "block_start_string": "{{%",
-        "block_end_string": "%}}",
-        "variable_start_string": "{{{",
-        "variable_end_string": "}}}",
-        "comment_start_string": "{{#",
-        "comment_end_string": "#}}",
-    }
-    return process_jinja2_template(body, vars=vars, env=env, settings=settings)
+    return process_jinja2_template(body, vars=vars, extra_curly=True, settings=settings)
 
 
 def check_alertmanager_config(data, path, alertmanager_config_key, decode_base64=False):
@@ -302,26 +322,29 @@ def check_alertmanager_config(data, path, alertmanager_config_key, decode_base64
 
 
 def fetch_provider_resource(
-    path,
+    resource: dict,
     tfunc=None,
     tvars=None,
     validate_json=False,
     validate_alertmanager_config=False,
     alertmanager_config_key="alertmanager.yaml",
     add_path_to_prom_rules=True,
+    skip_validation=False,
     settings=None,
 ) -> OR:
-    gqlapi = gql.get_api()
-
-    # get resource data
-    try:
-        resource = gqlapi.get_resource(path)
-    except gql.GqlGetResourceError as e:
-        raise FetchResourceError(str(e))
-
+    path = resource["path"]
     content = resource["content"]
     if tfunc:
         content = tfunc(body=content, vars=tvars, settings=settings)
+
+    if skip_validation:
+        return OR(
+            content,
+            QONTRACT_INTEGRATION,
+            QONTRACT_INTEGRATION_VERSION,
+            error_details=path,
+            validate_k8s_object=False,
+        )
 
     try:
         body = anymarkup.parse(content, force_types=None)
@@ -431,10 +454,11 @@ def fetch_provider_vault_secret(
         raise FetchResourceError(str(e))
 
 
-def fetch_provider_route(path, tls_path, tls_version, settings=None) -> OR:
+def fetch_provider_route(resource: dict, tls_path, tls_version, settings=None) -> OR:
     global _log_lock
 
-    openshift_resource = fetch_provider_resource(path)
+    path = resource["path"]
+    openshift_resource = fetch_provider_resource(resource)
 
     if tls_path is None or tls_version is None:
         return openshift_resource
@@ -476,11 +500,13 @@ def fetch_provider_route(path, tls_path, tls_version, settings=None) -> OR:
     return openshift_resource
 
 
-def fetch_openshift_resource(resource, parent, settings=None) -> OR:
+def fetch_openshift_resource(
+    resource, parent, settings=None, skip_validation=False
+) -> OR:
     global _log_lock
 
     provider = resource["provider"]
-    path = resource["path"]
+    path = resource["resource"]["path"]
     msg = "Fetching {}: {}".format(provider, path)
     _log_lock.acquire()  # pylint: disable=consider-using-with
     logging.debug(msg)
@@ -496,11 +522,12 @@ def fetch_openshift_resource(resource, parent, settings=None) -> OR:
             resource.get("alertmanager_config_key") or "alertmanager.yaml"
         )
         openshift_resource = fetch_provider_resource(
-            path,
+            resource["resource"],
             validate_json=validate_json,
             validate_alertmanager_config=validate_alertmanager_config,
             alertmanager_config_key=alertmanager_config_key,
             add_path_to_prom_rules=add_path_to_prom_rules,
+            skip_validation=skip_validation,
             settings=settings,
         )
     elif provider == "resource-template":
@@ -526,12 +553,13 @@ def fetch_openshift_resource(resource, parent, settings=None) -> OR:
             UnknownTemplateTypeError(tt)
         try:
             openshift_resource = fetch_provider_resource(
-                path,
+                resource["resource"],
                 tfunc=tfunc,
                 tvars=tv,
                 validate_alertmanager_config=validate_alertmanager_config,
                 alertmanager_config_key=alertmanager_config_key,
                 add_path_to_prom_rules=add_path_to_prom_rules,
+                skip_validation=skip_validation,
                 settings=settings,
             )
         except Exception as e:
@@ -572,7 +600,9 @@ def fetch_openshift_resource(resource, parent, settings=None) -> OR:
     elif provider == "route":
         tls_path = resource["vault_tls_secret_path"]
         tls_version = resource["vault_tls_secret_version"]
-        openshift_resource = fetch_provider_route(path, tls_path, tls_version, settings)
+        openshift_resource = fetch_provider_route(
+            resource["resource"], tls_path, tls_version, settings
+        )
     else:
         raise UnknownProviderError(provider)
 
@@ -742,7 +772,9 @@ def filter_namespaces_by_cluster_and_namespace(
 
 
 def canonicalize_namespaces(
-    namespaces: Iterable[dict[str, Any]], providers: list[str]
+    namespaces: Iterable[dict[str, Any]],
+    providers: list[str],
+    resource_schema_filter: Optional[str] = None,
 ) -> Tuple[list[dict[str, Any]], Optional[list[str]]]:
     canonicalized_namespaces = []
     override = None
@@ -750,7 +782,15 @@ def canonicalize_namespaces(
     for namespace_info in namespaces:
         ob.aggregate_shared_resources(namespace_info, "openshiftResources")
         openshift_resources: list = namespace_info.get("openshiftResources") or []
-        ors = [r for r in openshift_resources if r["provider"] in providers]
+        ors = [
+            r
+            for r in openshift_resources
+            if r["provider"] in providers
+            and (
+                resource_schema_filter is None
+                or r["resource"]["schema"] == resource_schema_filter
+            )
+        ]
         if ors and providers:
             # For the time being we only care about the first item in
             # providers
@@ -769,6 +809,7 @@ def get_namespaces(
     providers: Optional[list[str]] = None,
     cluster_name: Optional[str] = None,
     namespace_name: Optional[str] = None,
+    resource_schema_filter: Optional[str] = None,
 ) -> Tuple[list[dict[str, Any]], Optional[list[str]]]:
     if providers is None:
         providers = []
@@ -783,7 +824,7 @@ def get_namespaces(
     namespaces = filter_namespaces_by_cluster_and_namespace(
         namespaces, cluster_name, namespace_name
     )
-    return canonicalize_namespaces(namespaces, providers)
+    return canonicalize_namespaces(namespaces, providers, resource_schema_filter)
 
 
 @defer
@@ -817,3 +858,87 @@ def run(
         sys.exit(1)
 
     return ri
+
+
+def early_exit_desired_state(
+    providers: list[str], resource_schema_filter: Optional[str] = None
+) -> dict[str, Any]:
+    settings = queries.get_secret_reader_settings()
+    namespaces, _ = get_namespaces(
+        providers, resource_schema_filter=resource_schema_filter
+    )
+    fetch_specs = [
+        (r, ns_info) for ns_info in namespaces for r in ns_info["openshiftResources"]
+    ]
+
+    # this context manager patches functions used during jinja templating
+    # to ignore data that is not part of the desired state in app-interface.
+    # the context manager also ensures this function patching is
+    # reverted afterwards
+    with _early_exit_monkey_patch():
+        resources = threaded.run(
+            _early_exit_fetch_resource,
+            fetch_specs,
+            thread_pool_size=10,
+            settings=settings,
+        )
+
+    return {
+        "namespaces": namespaces,
+        "resources": resources,
+    }
+
+
+def _early_exit_fetch_resource(spec, settings):
+    resource = spec[0]
+    ns_info = spec[1]
+    if resource.get("enable_query_support"):
+        # use the regular resource processing functionality that evaluates templates
+        # and inline queries, if the resource is allowed to use this inline query
+        # functionality. this is crucial in such situations because the result of
+        # the template processing depends heavily on other data in app-interface
+        c = fetch_openshift_resource(
+            resource, ns_info, skip_validation=True, settings=settings
+        ).body
+    else:
+        # for regular resources, the plain content is sufficient enough to
+        # detect changes in desired state
+        c = resource["resource"].get("content")
+    del resource["resource"]
+    return c
+
+
+@contextmanager
+def _early_exit_monkey_patch():
+    """Avoid looking outside of app-interface on early-exit pr-check."""
+    orig_lookup_secret = lookup_secret
+    orig_lookup_github_file_content = lookup_github_file_content
+    orig_url_makes_sense = url_makes_sense
+    orig_check_alertmanager_config = check_alertmanager_config
+
+    try:
+        yield _early_exit_monkey_patch_assign(
+            lambda path, key, version=None, tvars=None, settings=None: f"vault({path}, {key}, {version})",
+            lambda repo, path, ref, tvars=None, settings=None: f"github({repo}, {path}, {ref})",
+            lambda url: False,
+            lambda data, path, alertmanager_config_key, decode_base64=False: True,
+        )
+    finally:
+        _early_exit_monkey_patch_assign(
+            orig_lookup_secret,
+            orig_lookup_github_file_content,
+            orig_url_makes_sense,
+            orig_check_alertmanager_config,
+        )
+
+
+def _early_exit_monkey_patch_assign(
+    lookup_secret,
+    lookup_github_file_content,
+    url_makes_sense,
+    check_alertmanager_config,
+):
+    sys.modules[__name__].lookup_secret = lookup_secret
+    sys.modules[__name__].lookup_github_file_content = lookup_github_file_content
+    sys.modules[__name__].url_makes_sense = url_makes_sense
+    sys.modules[__name__].check_alertmanager_config = check_alertmanager_config
