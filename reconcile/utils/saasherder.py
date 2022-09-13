6e4ae3311b4c8f503e64502c4932005fc12c5f25
Maor Friedman <mafriedm@redhat.com>
2022-09-13 15:10:58 +0300

    remove unused init method for trigger spec (#2789)

--- reconcile/utils/saasherder.py
+++ reconcile/utils/saasherder.py
@@ -97,25 +97,6 @@ class TriggerSpecConfig(TriggerSpecBase):
         )
         return key
 
-    @staticmethod
-    def from_state_key(
-        key: str,
-        state_content: Any,
-        timeout: Optional[str] = None,
-        pipelines_provider: Optional[dict[str, Any]] = None,
-    ) -> "TriggerSpecConfig":
-        saas_file_name, rt_name, cluster_name, namespace_name, env_name = key.split("/")
-        return TriggerSpecConfig(
-            saas_file_name=saas_file_name,
-            env_name=env_name,
-            timeout=timeout,
-            pipelines_provider=pipelines_provider,
-            resource_template_name=rt_name,
-            cluster_name=cluster_name,
-            namespace_name=namespace_name,
-            state_content=state_content,
-        )
-
 
 @dataclass
 class TriggerSpecMovingCommit(TriggerSpecBase):
