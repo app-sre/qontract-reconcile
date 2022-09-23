249137d80f9c8c5a11ac195bc4903cbcde8d68af
Christian Assing <cassing@redhat.com>
2022-09-23 10:11:55 +0200

    move example gql query into seperate directory to avoid clashes with prod ones

--- reconcile/gql_definitions_examples/queries/ocm.py
+++ reconcile/gql_definitions_examples/queries/ocm.py
@@ -15,9 +15,13 @@ from pydantic import (  # noqa: F401 # pylint: disable=W0611
     Json,
 )
 
-from reconcile.gql_definitions.fragments.vault_secret import VaultSecret
-from reconcile.gql_definitions.fragments.vault_secret_version import VaultSecretVersion
-from reconcile.gql_definitions.fragments.vault_secret_path import VaultSecretPath
+from reconcile.gql_definitions_examples.fragments.vault_secret import VaultSecret
+from reconcile.gql_definitions_examples.fragments.vault_secret_version import (
+    VaultSecretVersion,
+)
+from reconcile.gql_definitions_examples.fragments.vault_secret_path import (
+    VaultSecretPath,
+)
 
 
 DEFINITION = """
