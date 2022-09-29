001c8e4378c099ea99e8dff034dbc7ccc2fb64fa
Suzana Nesic <snesic@redhat.com>
2022-09-29 17:00:36 -0400

    [wip] gabi expiration handling
    Signed-off-by: Suzana Nesic <snesic@redhat.com>

--- reconcile/test/test_gabi_authorized_users.py
+++ reconcile/test/test_gabi_authorized_users.py
@@ -90,6 +90,7 @@ class TestGabiAuthorizedUser(TestCase):
         get_settings,
     ):
         expirationDate = date.today()
+        apply["desired"]["data"]["expiration"] = str(expirationDate)
         get_gabi_instances.return_value = mock_get_gabi_instances(expirationDate)
         mock_request.side_effect = apply_request
         gabi_u.run(dry_run=False)
@@ -130,7 +131,7 @@ class TestGabiAuthorizedUser(TestCase):
         secret_read,
         get_settings,
     ):
-        expirationDate = date.today() - timedelta(days=1)
+        expirationDate = "2000-01-01"
         get_gabi_instances.return_value = mock_get_gabi_instances(expirationDate)
         mock_request.side_effect = delete_request
         gabi_u.run(dry_run=False)
