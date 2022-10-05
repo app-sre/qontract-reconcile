6b52abb3a8f286cd0c8cccd82d7f467304129c65
Drew Welch <dwelch@redhat.com>
2022-10-05 11:28:34 -0500

    format

--- reconcile/gitlab_sync_push.py
+++ reconcile/gitlab_sync_push.py
@@ -6,7 +6,7 @@ import logging
 
 from typing import Any, Dict, List
 from git import Repo
-from gnupg import GPG # type: ignore
+from gnupg import GPG  # type: ignore
 
 from reconcile import queries
 from reconcile.utils.gitlab_api import GitLabApi
