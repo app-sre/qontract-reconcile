3eb54d290077d3522824c1a0db8d72f76964d002
Maor Friedman <mafriedm@redhat.com>
2022-11-03 23:51:40 +0200

    placeholders for automated MR

--- reconcile/ocm_upgrade_scheduler_org_updater.py
+++ reconcile/ocm_upgrade_scheduler_org_updater.py
@@ -1,6 +1,9 @@
 import json
 import logging
 
+import reconcile.utils.mr.ocm_upgrade_scheduler_org_updates as ousou
+
+from reconcile import mr_client_gateway
 from reconcile import queries
 from reconcile.utils.ocm import OCMMap
 
@@ -8,10 +11,12 @@ from reconcile.utils.ocm import OCMMap
 QONTRACT_INTEGRATION = "ocm-upgrade-scheduler-org-updater"
 
 
-def run(dry_run):
+def run(dry_run, gitlab_project_id):
     settings = queries.get_app_interface_settings()
     ocms = queries.get_openshift_cluster_managers()
     for ocm_info in ocms:
+        updates = {}
+        create_update_mr = False
         upgrade_policy_defaults = ocm_info.get("upgradePolicyDefaults")
         if not upgrade_policy_defaults:
             continue
@@ -38,6 +43,7 @@ def run(dry_run):
                     default_name = default["name"]
                     match_labels: dict[str, str] = json.loads(default["matchLabels"])
                     if match_labels.items() <= ocm_cluster_labels.items():
+                        create_update_mr = True
                         logging.info(
                             ["add_cluster", ocm_name, ocm_cluster_name, default_name]
                         )
@@ -46,4 +52,10 @@ def run(dry_run):
             up_cluster_name = up_cluster["name"]
             found = [c for c in ocm.clusters if c == up_cluster_name]
             if not found:
+                create_update_mr = True
                 logging.info(["delete_cluster", ocm_name, up_cluster_name])
+
+        if create_update_mr and not dry_run:
+            mr_cli = mr_client_gateway.init(gitlab_project_id=gitlab_project_id)
+            mr = ousou.CreateOCMUpgradeSchedulerOrgUpdates(updates)
+            mr.submit(cli=mr_cli)
