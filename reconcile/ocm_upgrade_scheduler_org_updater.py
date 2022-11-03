5945dd26dc01b829a2d9c5cc6aa67a9f002023fa
Maor Friedman <mafriedm@redhat.com>
2022-11-03 23:36:41 +0200

    select by match labels

--- reconcile/ocm_upgrade_scheduler_org_updater.py
+++ reconcile/ocm_upgrade_scheduler_org_updater.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 from reconcile import queries
@@ -30,7 +31,16 @@ def run(dry_run):
                 c for c in upgrade_policy_clusters if c["name"] == ocm_cluster_name
             ]
             if not found:
-                logging.info(["add_cluster", ocm_name, ocm_cluster_name])
+                ocm_cluster_labels = ocm.get_external_configuration_labels(
+                    ocm_cluster_name
+                )
+                for default in upgrade_policy_defaults:
+                    default_name = default["name"]
+                    match_labels = json.loads(default["matchLabels"])
+                    if match_labels.items() <= ocm_cluster_labels.items():
+                        logging.info(
+                            ["add_cluster", ocm_name, ocm_cluster_name, default_name]
+                        )
 
         for up_cluster in upgrade_policy_clusters:
             up_cluster_name = up_cluster["name"]
