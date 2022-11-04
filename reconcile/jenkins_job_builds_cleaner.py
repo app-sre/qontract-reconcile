dfe8819f16465f88d487dcbd7d406a726ce1375b
Serhii Kryzhnii <skryzhni@redhat.com>
2022-11-04 16:01:21 +0200

    APPSRE-6535 Remove setting of ssl_verify, rely on default

--- reconcile/jenkins_job_builds_cleaner.py
+++ reconcile/jenkins_job_builds_cleaner.py
@@ -86,7 +86,7 @@ def run(dry_run):
         token = instance["token"]
         instance_name = instance["name"]
         jenkins = JenkinsApi.init_jenkins_from_secret(
-            secret_reader, token, ssl_verify=True
+            secret_reader, token
         )
         all_job_names = jenkins.get_job_names()
 
