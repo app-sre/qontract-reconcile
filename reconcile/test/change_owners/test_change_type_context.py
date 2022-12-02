057c345132bc0658e617b0b7df37c78a3e51ba82
Gerd Oberlechner <goberlec@redhat.com>
2022-12-02 12:49:30 +0100

    [change-owners] split tests into topic related files
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

---
+++ reconcile/test/change_owners/test_change_type_context.py
@@ -0,0 +1,178 @@
+from reconcile.change_owners.change_types import (
+    BundleFileType,
+    FileRef,
+    build_change_type_processor,
+    create_bundle_file_change,
+)
+from reconcile.gql_definitions.change_owners.queries.change_types import ChangeTypeV1
+from reconcile.test.change_owners.fixtures import TestFile
+
+pytest_plugins = [
+    "reconcile.test.change_owners.fixtures",
+]
+
+#
+# testcases for context file refs extraction from bundle changes
+#
+
+
+def test_extract_context_file_refs_from_bundle_change(
+    saas_file_changetype: ChangeTypeV1, saas_file: TestFile
+):
+    """
+    in this testcase, a changed datafile matches directly the context schema
+    of the change type, so the change type is directly relevant for the changed
+    datafile
+    """
+    bundle_change = saas_file.create_bundle_change(
+        {"resourceTemplates[0].targets[0].ref": "new-ref"}
+    )
+    file_refs = bundle_change.extract_context_file_refs(
+        build_change_type_processor(saas_file_changetype)
+    )
+    assert file_refs == [saas_file.file_ref()]
+
+
+def test_extract_context_file_refs_from_bundle_change_schema_mismatch(
+    saas_file_changetype: ChangeTypeV1, saas_file: TestFile
+):
+    """
+    in this testcase, the schema of the bundle change and the schema of the
+    change types do not match and hence no file context is extracted.
+    """
+    saas_file.fileschema = "/some/other/schema.yml"
+    bundle_change = saas_file.create_bundle_change(
+        {"resourceTemplates[0].targets[0].ref": "new-ref"}
+    )
+    file_refs = bundle_change.extract_context_file_refs(
+        build_change_type_processor(saas_file_changetype)
+    )
+    assert not file_refs
+
+
+def test_extract_context_file_refs_selector(
+    cluster_owner_change_type: ChangeTypeV1,
+):
+    """
+    this testcase extracts the context file based on the change types context
+    selector
+    """
+    cluster = "/my/cluster.yml"
+    namespace_change = create_bundle_file_change(
+        path="/my/namespace.yml",
+        schema="/openshift/namespace-1.yml",
+        file_type=BundleFileType.DATAFILE,
+        old_file_content={
+            "the_change": "does not matter in this test",
+            "cluster": {
+                "$ref": cluster,
+            },
+        },
+        new_file_content={
+            "cluster": {
+                "$ref": cluster,
+            },
+        },
+    )
+    assert namespace_change
+    file_refs = namespace_change.extract_context_file_refs(
+        build_change_type_processor(cluster_owner_change_type)
+    )
+    assert file_refs == [
+        FileRef(
+            file_type=BundleFileType.DATAFILE,
+            schema="/openshift/cluster-1.yml",
+            path=cluster,
+        )
+    ]
+
+
+def test_extract_context_file_refs_in_list_added_selector(
+    role_member_change_type: ChangeTypeV1,
+):
+    """
+    in this testcase, a changed datafile does not directly belong to the change
+    type, because the context schema does not match (change type reacts to roles,
+    while the changed datafile is a user). but the change type defines a context
+    extraction section that feels responsible for user files and extracts the
+    relevant context, the role, from the users role section, looking out for added
+    roles.
+    """
+    new_role = "/role/new.yml"
+    user_change = create_bundle_file_change(
+        path="/somepath.yml",
+        schema="/access/user-1.yml",
+        file_type=BundleFileType.DATAFILE,
+        old_file_content={
+            "roles": [{"$ref": "/role/existing.yml"}],
+        },
+        new_file_content={
+            "roles": [{"$ref": "/role/existing.yml"}, {"$ref": new_role}],
+        },
+    )
+    assert user_change
+    file_refs = user_change.extract_context_file_refs(
+        build_change_type_processor(role_member_change_type)
+    )
+    assert file_refs == [
+        FileRef(
+            file_type=BundleFileType.DATAFILE,
+            schema="/access/role-1.yml",
+            path=new_role,
+        )
+    ]
+
+
+def test_extract_context_file_refs_in_list_removed_selector(
+    role_member_change_type: ChangeTypeV1,
+):
+    """
+    this testcase is similar to previous one, but detects removed contexts (e.g
+    roles in this example) as the relevant context to extract.
+    """
+    role_member_change_type.changes[0].context.when = "removed"  # type: ignore
+    existing_role = "/role/existing.yml"
+    new_role = "/role/new.yml"
+    user_change = create_bundle_file_change(
+        path="/somepath.yml",
+        schema="/access/user-1.yml",
+        file_type=BundleFileType.DATAFILE,
+        old_file_content={
+            "roles": [{"$ref": existing_role}],
+        },
+        new_file_content={
+            "roles": [{"$ref": new_role}],
+        },
+    )
+    assert user_change
+    file_refs = user_change.extract_context_file_refs(
+        build_change_type_processor(role_member_change_type)
+    )
+    assert file_refs == [
+        FileRef(
+            file_type=BundleFileType.DATAFILE,
+            schema="/access/role-1.yml",
+            path=existing_role,
+        )
+    ]
+
+
+def test_extract_context_file_refs_in_list_selector_change_schema_mismatch(
+    role_member_change_type: ChangeTypeV1,
+):
+    """
+    in this testcase, the changeSchema section of the change types changes does
+    not match the bundle change.
+    """
+    datafile_change = create_bundle_file_change(
+        path="/somepath.yml",
+        schema="/some/other/schema.yml",
+        file_type=BundleFileType.DATAFILE,
+        old_file_content={"field": "old-value"},
+        new_file_content={"field": "new-value"},
+    )
+    assert datafile_change
+    file_refs = datafile_change.extract_context_file_refs(
+        build_change_type_processor(role_member_change_type)
+    )
+    assert not file_refs
---
+++ reconcile/test/change_owners/test_change_type_various.py
@@ -0,0 +1,160 @@
+import pytest
+import yaml
+
+from reconcile.change_owners.change_owners import manage_conditional_label
+from reconcile.change_owners.change_types import (
+    BundleFileType,
+    ChangeTypeContext,
+    FileRef,
+    PathExpression,
+    parse_resource_file_content,
+)
+
+pytest_plugins = [
+    "reconcile.test.change_owners.fixtures",
+]
+
+
+#
+# label management tests
+#
+
+
+def test_label_management_add():
+    assert [
+        "existing-label",
+        "true-label",
+        "another-true-label",
+    ] == manage_conditional_label(
+        current_labels=["existing-label"],
+        conditional_labels={
+            "true-label": True,
+            "another-true-label": True,
+            "false-label": False,
+        },
+        dry_run=False,
+    )
+
+    # dry-run
+    assert ["existing-label"] == manage_conditional_label(
+        current_labels=["existing-label"],
+        conditional_labels={
+            "true-label": True,
+            "another-true-label": True,
+            "false-label": False,
+        },
+        dry_run=True,
+    )
+
+
+def test_label_management_remove():
+    assert ["existing-label"] == manage_conditional_label(
+        current_labels=["existing-label", "false-label"],
+        conditional_labels={
+            "false-label": False,
+        },
+        dry_run=False,
+    )
+
+    # dry-run
+    assert ["existing-label", "false-label"] == manage_conditional_label(
+        current_labels=["existing-label", "false-label"],
+        conditional_labels={
+            "false-label": False,
+        },
+        dry_run=True,
+    )
+
+
+def test_label_management_add_and_remove():
+    assert ["existing-label", "true-label"] == manage_conditional_label(
+        current_labels=["existing-label", "false-label"],
+        conditional_labels={
+            "true-label": True,
+            "false-label": False,
+        },
+        dry_run=False,
+    )
+
+
+#
+# PathExpression tests
+#
+
+
+def test_normal_path_expression():
+    jsonpath_expression = "path.to.some.value"
+    pe = PathExpression(
+        jsonpath_expression=jsonpath_expression,
+    )
+    jsonpath = pe.jsonpath_for_context(
+        ChangeTypeContext(
+            change_type_processor=None,  # type: ignore
+            context="RoleV1 - some-role",
+            approvers=[],
+            context_file=FileRef(
+                BundleFileType.DATAFILE, "some-file.yaml", "schema-1.yml"
+            ),
+        )
+    )
+    assert jsonpath_expression == str(jsonpath)
+
+
+def test_templated_path_expression():
+    jsonpath_expression = "path.to.some.value[?(@.name == '{{ ctx_file_path }}')]"
+    pe = PathExpression(
+        jsonpath_expression=jsonpath_expression,
+    )
+    jsonpath = pe.jsonpath_for_context(
+        ChangeTypeContext(
+            change_type_processor=None,  # type: ignore
+            context="RoleV1 - some-role",
+            approvers=[],
+            context_file=FileRef(
+                BundleFileType.DATAFILE, "some-file.yaml", "schema-1.yml"
+            ),
+        )
+    )
+    assert (
+        "path.to.some.value.[?[Expression(Child(This(), Fields('name')) == 'some-file.yaml')]]"
+        == str(jsonpath)
+    )
+
+
+def test_template_path_expression_unsupported_variable():
+    with pytest.raises(ValueError):
+        PathExpression(
+            jsonpath_expression="path[?(@.name == '{{ unsupported_variable }}')]"
+        )
+
+
+#
+# Test resource file parsing
+#
+
+
+def test_parse_resource_file_content_structured_with_schema():
+    expected_content = {"$schema": "schema-1.yml", "some_field": "some_value"}
+    content, schema = parse_resource_file_content(yaml.dump(expected_content))
+    assert schema == expected_content["$schema"]
+    assert content == expected_content
+
+
+def test_parse_resource_file_content_structured_no_schema():
+    expected_content = {"some_field": "some_value"}
+    content, schema = parse_resource_file_content(yaml.dump(expected_content))
+    assert schema is None
+    assert content == expected_content
+
+
+def test_parse_resource_file_content_unstructured():
+    expected_content = "something something"
+    content, schema = parse_resource_file_content(expected_content)
+    assert schema is None
+    assert content == expected_content
+
+
+def test_parse_resource_file_content_none():
+    content, schema = parse_resource_file_content(None)
+    assert schema is None
+    assert content is None
