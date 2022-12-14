app-sre/qontract-reconcile

commit 8e9eb951822e449096ef364b37b05a676e74bb77

[change-owners] split high level changes into fine grained diffs (#3058)
When the detected changes within a file are too rough/high level, only high level `change-types` would be able to cover them. e.g. if a new file is introduced, the detected change would be the entire file, only coverable by the top-level JSONPath placeholder for the file-root '$'. Such `change-types` are the opposite of what we intend to do with `fine-grained` permissions.

Split high level changes in smaller changes so they can be covered by `change-types`. The process of splitting is driven by the involved change-types, so that the resulting splits are as fine as they are required to allow the change-types to have effect, but not finer than required in order to keep the list of changes as concise as possible.

The center-piece of this change is the list of splits (`_split_into`) within the `DiffCoverage` dataclass which are `DiffCoverage` objects themselves. This forms a tree-like structure of diffs/sub-diffs, with the most fine grained splits represented as leaves. A diff is considered covered, if it is directly covered by a `change-type` or if all the splits of the diff are covered.

The approval process does not know anything about this process since it will operate on a flattened and consolidated list of diffs as if they would have been detected that way to begin with.

Ref: https://issues.redhat.com/browse/APPSRE-6606

reconcile/change_owners/tester.py
@@ -168,7 +168,7 @@ class AppInterfaceRepo:
                     ),
                     old=parsed_yaml,
                     new=parsed_yaml,
-                    diff_coverage=[],
+                    diffs=[],
                 )
         elif file_type == BundleFileType.RESOURCEFILE:
             with open(f"{self.resource_dir()}{path}", "r") as f:
@@ -182,7 +182,7 @@ class AppInterfaceRepo:
                     ),
                     old=parsed_content,
                     new=parsed_content,
-                    diff_coverage=[],
+                    diffs=[],
                 )
         else:
             raise ValueError(f"Unknown file type {file_type}")

reconcile/change_owners/change_owners.py
@@ -112,8 +112,6 @@ def _parse_bundle_changes(bundle_changes) -> list[BundleFileChange]:
     """
     parses the output of the qontract-server /diff endpoint
     """
-
-    # todo - add some logging to remove BundleFileChange file with no changes
     datafiles = bundle_changes["datafiles"].values()
     resourcefiles = bundle_changes["resources"].values()
     logging.debug(
@@ -336,15 +334,12 @@ def run(
         #
         changes = fetch_bundle_changes(comparison_sha)
         logging.info(
-            f"detected {len(changes)} changed files with {sum(len(c.diff_coverage) for c in changes)} differences"
+            f"detected {len(changes)} changed files with {sum(c.raw_diff_count() for c in changes)} differences"
         )
         cover_changes(
             changes,
             change_type_processors,
             comparison_gql_api,
-        )
-        logging.debug(
-            f"bundle files with changes uncovered by change-types: {', '.join([str(c.fileref) for c in changes if not c.all_changes_covered()])}"
         )
         self_serviceable = (
             len(changes) > 0

reconcile/change_owners/change_types.py
@@ -1,9 +1,12 @@
 from collections import defaultdict
 from collections.abc import (
-    Iterable,
+    MutableMapping,
     Sequence,
 )
-from dataclasses import dataclass
+from dataclasses import (
+    dataclass,
+    field,
+)
 from enum import Enum
 from typing import (
     Any,
@@ -19,6 +22,7 @@ import networkx
 
 from reconcile.change_owners.diff import (
     SHA256SUM_FIELD_NAME,
+    SHA256SUM_PATH,
     Diff,
     DiffType,
     extract_diffs,
@@ -62,12 +66,105 @@ class DiffCoverage:
     diff: Diff
     coverage: list["ChangeTypeContext"]
 
+    # if a diff is split into smaller diffs, they are stored here
+    # please notice that the splits are DiffCoverages objects themselves which
+    # can have splits on their own. DiffCoverage objects form a tree-like structure
+    _split_into: list["DiffCoverage"] = field(default_factory=list)
+
     def is_covered(self) -> bool:
         """
-        a diff is considered covered, if there is at least one change-type
-        assosciated that is not disabled
+        a diff is considered covered,
+        * if there is at least one change-type that covers the diff entirely
+        * if there the diff is split into smaller diffs which are covered.
+          the smaller diffs must cover the entire original diff in sum
         """
+        return self.is_directly_covered() or self.is_covered_by_splits()
+
+    def is_directly_covered(self) -> bool:
         return any(not ctx.disabled for ctx in self.coverage)
+
+    def is_covered_by_splits(self) -> bool:
+        """
+        determine if the splits of this DiffCoverage would cover it entirely
+        """
+        # remove the parts of this diffs data that are covered by the splits
+        # if nothing remains, the splits cover the entire diff
+        uncovered_data = self.diff.get_context_data_copy()
+        if uncovered_data and isinstance(uncovered_data, MutableMapping):
+            for s in self._split_into:
+                if s.is_covered():
+                    # this removes the data that matches the path
+                    s.diff.path.filter(lambda x: True, uncovered_data)
+        return uncovered_data == {}
+
+    def changed_path_covered_by_path(self, path: jsonpath_ng.JSONPath) -> bool:
+        return str(self.diff.path).startswith(str(path)) or path == jsonpath_ng.Root()
+
+    def path_under_changed_path(self, path: jsonpath_ng.JSONPath) -> bool:
+        return (
+            str(path).startswith(str(self.diff.path))
+            or str(self.diff.path) == JSON_PATH_ROOT
+        ) and path != str(self.diff.path)
+
+    def split(
+        self, path: jsonpath_ng.JSONPath, ctx: "ChangeTypeContext"
+    ) -> Optional["DiffCoverage"]:
+        """
+        create a new DiffCoverage object that coveres the given path
+        this function also manages the split tree structure in _split_into
+        so that finer diffs are always lower in the tree that higher diffs.
+        """
+        if self.path_under_changed_path(path):
+            # find out if the path fits unter an existing split
+            for s in self._split_into:
+                if sub_cov := s.split(path, ctx):
+                    return sub_cov
+
+            # no suitable existing split found, create a new one
+            split_sub_coverage = DiffCoverage(
+                self.diff.create_subdiff(path), self.coverage.copy()
+            )
+
+            # consolidate existing splits. maybe they should go under the newly created one?
+            consolidated_splits = [split_sub_coverage]
+            for s in self._split_into:
+                if split_sub_coverage.path_under_changed_path(s.diff.path_str()):
+                    split_sub_coverage._split_into.append(s)
+                else:
+                    consolidated_splits.append(s)
+
+            # add the covering context to the new split and all its child splits
+            split_sub_coverage.add_covering_context(ctx)
+
+            self._split_into = consolidated_splits
+            return split_sub_coverage
+        if self.diff.path == path:
+            return self
+        else:
+            return None
+
+    def add_covering_context(self, ctx: "ChangeTypeContext"):
+        self.coverage.append(ctx)
+        for s in self._split_into:
+            s.add_covering_context(ctx)
+
+    def fine_grained_diff_coverages(self) -> dict[str, "DiffCoverage"]:
+        coverages = {}
+        for s in self._split_into:
+            coverages.update(s.fine_grained_diff_coverages())
+
+        # if this diff is not directly covered by a change-type, but is covered
+        # by the splits, this DiffCoverage is discarded because it bears no value
+        # in the coverage process, e.g. for a newly introduced file, the high
+        # level diff is usually '$', which is very rough and might not be covered
+        # by any change-type. the splits of that diff, however, are more fine
+        # and are covered by change-types. so it makes sense to just keep the splits
+        # and discard the high level '$' diff. it would even prevent a change
+        # from being self-serviceable.
+        if not (not self.is_directly_covered() and self.is_covered_by_splits()):
+            coverages[self.diff.path_str()] = self
+
+        return coverages
 
 
 @dataclass
@@ -81,7 +178,11 @@ class BundleFileChange:
     fileref: FileRef
     old: Optional[dict[str, Any]]
     new: Optional[dict[str, Any]]
-    diff_coverage: list[DiffCoverage]
+    diffs: list[Diff]
+    _diff_coverage: dict[str, DiffCoverage] = field(init=False, default_factory=dict)
+
+    def __post_init__(self) -> None:
+        self._diff_coverage = {d.path_str(): DiffCoverage(d, []) for d in self.diffs}
 
     def extract_context_file_refs(
         self, change_type: "ChangeTypeProcessor"
@@ -226,21 +327,58 @@ class BundleFileChange:
                 self.fileref, file_content, change_type_context
             ):
                 for dc in diffs:
-                    if change_path_covered_by_allowed_path(
-                        str(dc.diff.path), allowed_path
-                    ):
-                        covered_diffs[str(dc.diff.path)] = dc.diff
+                    if dc.changed_path_covered_by_path(allowed_path):
+                        covered_diffs[dc.diff.path_str()] = dc.diff
                         dc.coverage.append(change_type_context)
+                    elif SHA256SUM_PATH != allowed_path and dc.path_under_changed_path(
+                        allowed_path
+                    ):
+                        # the self-service path allowed by the change-type is covering
+                        # only parts of the diff. we will split the diff into a
+                        # smaller part, that can be covered by the change-type.
+                        # but the rest of the diff needs to be covered by another
+                        # change-type, either in full or again as a split.
+                        sub_dc = dc.split(allowed_path, change_type_context)
+                        if not sub_dc:
+                            raise Exception(
+                                f"unable to create a subdiff for path {allowed_path} on diff {dc.diff.path_str()}"
+                            )
+                        covered_diffs[str(allowed_path)] = sub_dc.diff
+
         return covered_diffs
 
     def _filter_diffs(self, diff_types: list[DiffType]) -> list[DiffCoverage]:
-        return [d for d in self.diff_coverage if d.diff.diff_type in diff_types]
-
-    def uncovered_changes(self) -> Iterable[DiffCoverage]:
-        return (d for d in self.diff_coverage if not d.is_covered())
+        return [
+            d for d in self._diff_coverage.values() if d.diff.diff_type in diff_types
+        ]
 
     def all_changes_covered(self) -> bool:
-        return not any(self.uncovered_changes())
+        return all(d.is_covered() for d in self.diff_coverage)
+
+    def raw_diff_count(self) -> int:
+        return len(self._diff_coverage)
+
+    @property
+    def diff_coverage(self) -> Sequence[DiffCoverage]:
+        """
+        returns the meaningful set of diffs, potentially more fine grained than
+        what was originally detected for to the BundleFileChange.
+        """
+        coverages: list[DiffCoverage] = []
+        for dc in self._diff_coverage.values():
+            coverages.extend(dc.fine_grained_diff_coverages().values())
+        return coverages
+
+    def involved_change_types(self) -> list["ChangeTypeProcessor"]:
+        """
+        returns all the change-types that are involved in the coverage
+        of all changes
+        """
+        change_types = []
+        for dc in self.diff_coverage:
+            for ctx in dc.coverage:
+                change_types.append(ctx.change_type_processor)
+        return change_types
 
 
 def parse_resource_file_content(content: Optional[Any]) -> tuple[Any, Optional[str]]:
@@ -287,7 +425,7 @@ def create_bundle_file_change(
             fileref=fileref,
             old=old_file_content,
             new=new_file_content,
-            diff_coverage=[DiffCoverage(d, []) for d in diffs],
+            diffs=diffs,
         )
     else:
         return None
@@ -357,7 +495,7 @@ class ChangeTypeProcessor:
 
     def allowed_changed_paths(
         self, file_ref: FileRef, file_content: Any, ctx: "ChangeTypeContext"
-    ) -> list[str]:
+    ) -> list[jsonpath_ng.JSONPath]:
         """
         find all paths within the provide file_content, that are covered by this
         ChangeTypeV1. the paths are represented as jsonpath expressions pinpointing
@@ -373,7 +511,7 @@ class ChangeTypeProcessor:
             ]:
                 paths.extend(
                     [
-                        str(p.full_path)
+                        p.full_path
                         for p in change_type_path_expression.jsonpath_for_context(
                             ctx
                         ).find(file_content)
@@ -509,13 +647,12 @@ def get_priority_for_changes(
     """
     Finds the lowest priority of all change types involved in the provided bundle file changes.
     """
-    prorities: set[ChangeTypePriority] = set()
+    priorities: set[ChangeTypePriority] = set()
     for bfc in bundle_file_changes:
-        for dc in bfc.diff_coverage:
-            for c in dc.coverage:
-                prorities.add(c.change_type_processor.priority)
+        for ct in bfc.involved_change_types():
+            priorities.add(ct.priority)
     # get the lowest priority
     for p in reversed(ChangeTypePriority):
-        if p in prorities:
+        if p in priorities:
             return p
     return None

reconcile/change_owners/diff.py
@@ -1,5 +1,7 @@
+import copy
 import json
 import re
+from collections.abc import MutableMapping
 from dataclasses import dataclass
 from enum import Enum
 from functools import reduce
@@ -31,6 +33,56 @@ class Diff:
     diff_type: DiffType
     old: Optional[Any]
     new: Optional[Any]
+
+    def create_subdiff(self, sub_path: jsonpath_ng.JSONPath) -> "Diff":
+        if sub_path == self.path:
+            # no need to subdiff ... this is the same path
+            return self
+        else:
+            sub_path_str = str(sub_path)
+            if self.path == jsonpath_ng.Root():
+                absolute_sub_path = sub_path
+            elif sub_path_str.startswith(self.path_str()):
+                absolute_sub_path = jsonpath_ng.ext.parse(
+                    f"${sub_path_str[len(self.path_str()):]}"
+                )
+            else:
+                raise Exception(
+                    f"sub_path {sub_path_str} is not prefixed by {self.path_str()}"
+                )
+            sub_old = absolute_sub_path.find(self.old)
+            sub_new = absolute_sub_path.find(self.new)
+            return Diff(
+                path=sub_path,
+                diff_type=self.diff_type,
+                old=sub_old[0].value if len(sub_old) > 0 else None,
+                new=sub_new[0].value if len(sub_new) > 0 else None,
+            )
+
+    def get_context_data_copy(self) -> Optional[Any]:
+        if self.diff_type in [DiffType.ADDED, DiffType.CHANGED]:
+            data_copy = copy.deepcopy(self.new)
+        elif self.diff_type == DiffType.REMOVED:
+            data_copy = copy.deepcopy(self.old)
+        else:
+            raise Exception(f"Unknown diff type {self.diff_type}")
+
+        # remove file metadata
+        if (
+            data_copy
+            and isinstance(data_copy, MutableMapping)
+            and self.path == jsonpath_ng.Root()
+        ):
+            if SCHEMA_FIELD in data_copy:
+                del data_copy[SCHEMA_FIELD]
+            if PATH_FIELD in data_copy:
+                del data_copy[PATH_FIELD]
+            if SHA256SUM_FIELD_NAME in data_copy:
+                del data_copy[SHA256SUM_FIELD_NAME]
+        return data_copy
+
+    def path_str(self) -> str:
+        return str(self.path)
 
     def old_value_repr(self) -> Optional[str]:
         return self._value_repr(self.old)
@@ -98,7 +150,10 @@ def compare_object_ctx_identifier(
     raise CannotCompare() from None
 
 
+SCHEMA_FIELD = "$schema"
+PATH_FIELD = "path"
 SHA256SUM_FIELD_NAME = "$file_sha256sum"
+SHA256SUM_PATH = jsonpath_ng.parse(f"'{SHA256SUM_FIELD_NAME}'")
 
 
 def extract_diffs(old_file_content: Any, new_file_content: Any) -> list[Diff]:

reconcile/test/change_owners/fixtures.py
@@ -16,7 +16,11 @@ from reconcile.change_owners.change_types import (
     create_bundle_file_change,
 )
 from reconcile.gql_definitions.change_owners.queries import self_service_roles
-from reconcile.gql_definitions.change_owners.queries.change_types import ChangeTypeV1
+from reconcile.gql_definitions.change_owners.queries.change_types import (
+    ChangeTypeChangeDetectorContextSelectorV1,
+    ChangeTypeChangeDetectorJsonPathProviderV1,
+    ChangeTypeV1,
+)
 from reconcile.gql_definitions.change_owners.queries.self_service_roles import (
     BotV1,
     DatafileObjectV1,
@@ -141,3 +145,23 @@ def namespace_file() -> TestFile:
 @pytest.fixture
 def rds_defaults_file() -> TestFile:
     return TestFile(**fxt.get_anymarkup("resourcefile_rds_defaults.yaml"))
+
+
+def build_jsonpath_change(
+    selectors: list[str],
+    schema: Optional[str] = None,
+    context_selector: Optional[str] = None,
+    context_when: Optional[str] = None,
+) -> ChangeTypeChangeDetectorJsonPathProviderV1:
+    if context_selector:
+        context = ChangeTypeChangeDetectorContextSelectorV1(
+            selector=context_selector, when=context_when
+        )
+    else:
+        context = None
+    return ChangeTypeChangeDetectorJsonPathProviderV1(
+        provider="jsonPath",
+        changeSchema=schema,
+        jsonPathSelectors=selectors,
+        context=context,
+    )

reconcile/test/change_owners/test_change_type_coverage.py
@@ -1,3 +1,5 @@
+import jsonpath_ng
+
 from reconcile.change_owners.change_types import (
     Approver,
     BundleFileType,
@@ -5,6 +7,10 @@ from reconcile.change_owners.change_types import (
     DiffCoverage,
     build_change_type_processor,
     create_bundle_file_change,
+)
+from reconcile.change_owners.diff import (
+    Diff,
+    DiffType,
 )
 from reconcile.gql_definitions.change_owners.queries.change_types import ChangeTypeV1
 from reconcile.test.change_owners.fixtures import TestFile
@@ -32,7 +38,6 @@ def test_cover_changes_one_file(
     )
     saas_file_change.cover_changes(ctx)
 
-    assert not list(saas_file_change.uncovered_changes())
     assert saas_file_change.all_changes_covered()
     assert saas_file_change.diff_coverage[0].is_covered()
     assert saas_file_change.diff_coverage[0].coverage == [ctx]
@@ -52,11 +57,10 @@ def test_uncovered_change_because_change_type_is_disabled(
         context_file=saas_file.file_ref(),
     )
     saas_file_change.cover_changes(ctx)
-    uncoverd_changes = list(saas_file_change.uncovered_changes())
-    assert uncoverd_changes
     assert not saas_file_change.all_changes_covered()
-    assert not uncoverd_changes[0].is_covered()
-    assert uncoverd_changes[0].coverage[0].disabled
+    for dc in saas_file_change.diff_coverage:
+        if not dc.is_covered():
+            assert dc.coverage[0].disabled
 
 
 def test_uncovered_change_one_file(
@@ -130,13 +134,20 @@ def test_root_change_type(cluster_owner_change_type: ChangeTypeV1, saas_file: Te
 
 
 def test_diff_no_coverage():
-    dc = DiffCoverage(diff=None, coverage=[])  # type: ignore
+    dc = DiffCoverage(
+        diff=Diff(
+            diff_type=DiffType.ADDED, path=jsonpath_ng.parse("$"), new=None, old=None
+        ),
+        coverage=[],
+    )
     assert not dc.is_covered()
 
 
 def test_diff_covered(saas_file_changetype: ChangeTypeV1):
     dc = DiffCoverage(
-        diff=None,  # type: ignore
+        diff=Diff(
+            diff_type=DiffType.ADDED, path=jsonpath_ng.parse("$"), new=None, old=None
+        ),
         coverage=[
             ChangeTypeContext(
                 change_type_processor=build_change_type_processor(saas_file_changetype),
@@ -153,7 +164,9 @@ def test_diff_covered_many(
     saas_file_changetype: ChangeTypeV1, role_member_change_type: ChangeTypeV1
 ):
     dc = DiffCoverage(
-        diff=None,  # type: ignore
+        diff=Diff(
+            diff_type=DiffType.ADDED, path=jsonpath_ng.parse("$"), new=None, old=None
+        ),
         coverage=[
             ChangeTypeContext(
                 change_type_processor=build_change_type_processor(saas_file_changetype),
@@ -179,7 +192,9 @@ def test_diff_covered_partially_disabled(
 ):
     role_member_change_type.disabled = True
     dc = DiffCoverage(
-        diff=None,  # type: ignore
+        diff=Diff(
+            diff_type=DiffType.ADDED, path=jsonpath_ng.parse("$"), new=None, old=None
+        ),
         coverage=[
             ChangeTypeContext(
                 change_type_processor=build_change_type_processor(saas_file_changetype),
@@ -206,7 +221,9 @@ def test_diff_no_coverage_all_disabled(
     role_member_change_type.disabled = True
     saas_file_changetype.disabled = True
     dc = DiffCoverage(
-        diff=None,  # type: ignore
+        diff=Diff(
+            diff_type=DiffType.ADDED, path=jsonpath_ng.parse("$"), new=None, old=None
+        ),
         coverage=[
             ChangeTypeContext(
                 change_type_processor=build_change_type_processor(saas_file_changetype),

reconcile/test/change_owners/test_change_type_diff.py
@@ -52,6 +52,9 @@ def test_deepdiff_path_element_with_dot():
 
 
 def test_bundle_change_diff_value_changed():
+    """
+    detect a change on a top level field
+    """
     bundle_change = create_bundle_file_change(
         path="path",
         schema="schema",
@@ -69,6 +72,9 @@ def test_bundle_change_diff_value_changed():
 
 
 def test_bundle_change_diff_value_changed_deep():
+    """
+    detect a change deeper in the object tree
+    """
     bundle_change = create_bundle_file_change(
         path="path",
         schema="schema",
@@ -87,7 +93,7 @@ def test_bundle_change_diff_value_changed_deep():
 
 def test_bundle_change_diff_value_changed_multiple_in_iterable():
     """
-    this testscenario searches shows how changes can be detected in a list,
+    this testscenario shows how changes can be detected in a list,
     when objects with identifiers and objects without are mixed and shuffled
     """
     bundle_change = create_bundle_file_change(
@@ -184,6 +190,10 @@ def test_bundle_change_diff_value_changed_multiple_in_iterable():
 
 
 def test_bundle_change_diff_property_added():
+    """
+    this test scenario show how a newly added property is correctly
+    detected if the containing object has a clear identity.
+    """
     bundle_change = create_bundle_file_change(
         path="path",
         schema="/openshift/namespace-1.yml",
@@ -229,6 +239,10 @@ def test_bundle_change_diff_property_added():
 
 
 def test_bundle_change_diff_property_removed():
+    """
+    this test scenario show how a removed property is correctly
+    detected if the containing object has a clear identity.
+    """
     bundle_change = create_bundle_file_change(
         path="path",
         schema="/openshift/namespace-1.yml",

commit d515643fc4850ab3d7350d98aefca266e4feb762

[change-type] fix a bug in the change-type-tester
fixes a bug introduced in https://github.com/app-sre/qontract-reconcile/pull/3058

Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

reconcile/change_owners/tester.py
@@ -74,9 +74,11 @@ def test_change_type_in_context(
 SELF_SERVICABLE_MARKER = "self-serviceable"
 
 
-def print_annotated_file(file: BundleFileChange, self_serviceable_paths: list[str]):
+def print_annotated_file(
+    file: BundleFileChange, self_serviceable_paths: list[jsonpath_ng.JSONPath]
+):
     # add a markers to the data to indicate which parts are self serviceable
-    for path_expression in [jsonpath_ng.parse(p) for p in self_serviceable_paths]:
+    for path_expression in self_serviceable_paths:
         for self_serviceable_data in path_expression.find(file.new):
             self_serviceable_data.full_path.update(
                 file.new, {SELF_SERVICABLE_MARKER: self_serviceable_data.value}