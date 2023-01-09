c057d00c4ec8895706dfab31abb10afac2c628c1
Christian Assing <cassing@redhat.com>
2023-01-09 11:32:01 +0100

    [github] don't examine non github usernames
    e.g., if redhat OIDC is configured
    
    APPSRE-6772

--- reconcile/openshift_users.py
+++ reconcile/openshift_users.py
@@ -68,14 +68,16 @@ def fetch_current_state(thread_pool_size, internal, use_jump_host):
     return oc_map, current_state
 
 
-def fetch_desired_state(oc_map):
+def fetch_desired_state(oc_map, enforced_user_keys=None):
     desired_state = []
     flat_rolebindings_desired_state = openshift_rolebindings.fetch_desired_state(
-        ri=None, oc_map=oc_map
+        ri=None, oc_map=oc_map, enforced_user_keys=enforced_user_keys
     )
     desired_state.extend(flat_rolebindings_desired_state)
 
-    groups_desired_state = openshift_groups.fetch_desired_state(oc_map)
+    groups_desired_state = openshift_groups.fetch_desired_state(
+        oc_map, enforced_user_keys=enforced_user_keys
+    )
     flat_groups_desired_state = [
         {"cluster": s["cluster"], "user": s["user"]} for s in groups_desired_state
     ]
