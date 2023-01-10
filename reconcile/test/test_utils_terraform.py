254f64ccc35e96b153d28c556e096657448bc785
Jean-Francois Chevrette <jchevret@redhat.com>
2023-01-10 14:08:34 -0500

    utils/terraform: safe_resource_id adds an underscore at the front of terraform resource name that begins with a number (#3074)
    * append underscore at the front of a terraform resource name if it begins with a
    number

--- reconcile/test/test_utils_terraform.py
+++ reconcile/test/test_utils_terraform.py
@@ -7,3 +7,7 @@ def test_sanitize_resource_with_dots():
 
 def test_sanitize_resource_with_wildcard():
     assert safe_resource_id("*.foo.example.com") == "_star_foo_example_com"
+
+
+def test_sanitize_resource_begins_with_number():
+    assert safe_resource_id("0xyz") == "_0xyz"
