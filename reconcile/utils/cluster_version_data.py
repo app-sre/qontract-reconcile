7a6d3bb91263f922a2751682ff634bf4ed2d8b25
Patrick Martin <patmarti@redhat.com>
2023-01-11 16:16:06 +0100

    stats in version data and cross-org sector inheritance

--- reconcile/utils/cluster_version_data.py
+++ reconcile/utils/cluster_version_data.py
@@ -11,6 +11,7 @@ from pydantic import (
     Field,
 )
 
+from reconcile.utils.semver_helper import parse_semver
 from reconcile.utils.state import State
 
 
@@ -23,12 +24,58 @@ class VersionHistory(BaseModel):
     workloads: dict[str, WorkloadHistory] = Field(default_factory=dict)
 
 
+class Stats(BaseModel):
+    min_version: str
+    min_version_per_workload: dict[str, str] = Field(default_factory=dict)
+    inherited: Optional["Stats"]
+
+    def inherit(self, added: "Stats") -> None:
+        if not self.inherited:
+            self.inherited = added
+            return
+        self.inherited.min_version = min(
+            self.inherited.min_version, added.min_version, key=parse_semver
+        )
+        for workload, added_version in added.min_version_per_workload.items():
+            v = self.inherited.min_version_per_workload.get(workload, added_version)
+            self.inherited.min_version_per_workload[workload] = min(
+                v, added_version, key=parse_semver
+            )
+
+    def validate_against_inherited(self, version: str, workloads: list[str]) -> bool:
+        """Returns True only if version is less than any of the inherited version for these workloads
+        If one of the worloads is not part of the inherited stats, we will check against the global
+        minimum version
+        """
+        if not self.inherited:
+            return True
+        semver = parse_semver(version)
+        all_workloads_found = True
+        all_workload_ok = True
+        # check that inherited orgs run at least that version for our workloads
+        for w in workloads:
+            all_workloads_found = w in self.inherited.min_version_per_workload
+            if not all_workloads_found:
+                break
+            if semver > parse_semver(self.inherited.min_version_per_workload[w]):
+                all_workload_ok = False
+        if all_workloads_found and not all_workload_ok:
+            return False
+        if not all_workloads_found:
+            # if some of our workload is not inherited, check the global min_version
+            # from the other orgs
+            if semver > parse_semver(self.inherited.min_version):
+                return False
+        return True
+
+
 class VersionData(BaseModel):
     check_in: Optional[datetime]
     versions: dict[str, VersionHistory] = Field(default_factory=dict)
+    stats: Optional[Stats]
 
     def jsondict(self) -> dict[str, Any]:
-        return json.loads(self.json())
+        return json.loads(self.json(exclude_unset=True))
 
     def save(self, state: State, ocm_name: str) -> None:
         state.add(ocm_name, self.jsondict(), force=True)
@@ -48,6 +95,22 @@ class VersionData(BaseModel):
             workloads.update(v.workloads.keys())
         return workloads
 
+    def update_stats(self, upgrade_policies: list[dict[str, str]]) -> None:
+        min_version_per_workload: dict[str, str] = {}
+        for item in upgrade_policies:
+            current_version = item["current_version"]
+            for w in item["workloads"]:
+                min_ver = min_version_per_workload.setdefault(w, current_version)
+                if parse_semver(current_version) < parse_semver(min_ver):
+                    min_version_per_workload[w] = current_version
+
+        if min_version_per_workload:
+            min_version = min(min_version_per_workload.values(), key=parse_semver)
+            self.stats = Stats(
+                min_version=min_version,
+                min_version_per_workload=min_version_per_workload,
+            )
+
     def aggregate(self, added: "VersionData", added_org_name: str) -> None:
         known_workloads = self.workloads()
         for version, version_data in added.versions.items():
@@ -61,6 +124,13 @@ class VersionData(BaseModel):
                     f"{added_org_name}/{cluster}" for cluster in workload_data.reporting
                 ]
                 w.reporting += ocm_clusters
+        if self.stats and added.stats:
+            self.stats.inherit(added.stats)
+
+    def validate_against_inherited(self, version: str, workloads: list[str]) -> bool:
+        if not self.stats:
+            return True
+        return self.stats.validate_against_inherited(version, workloads)
 
 
 def get_version_data(state: State, ocm_name: str) -> VersionData:
