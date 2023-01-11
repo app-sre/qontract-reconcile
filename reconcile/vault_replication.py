70abf02dcb15f01b412b8ec80265f8af89f0a5ba
Andreu Gallofre <agallofr@redhat.com>
2023-01-11 10:27:59 +0100

    Add message on exception

--- reconcile/vault_replication.py
+++ reconcile/vault_replication.py
@@ -349,7 +349,9 @@ def replicate_paths(
             if path.policy is None:
                 # Exit if the replication config is empty, this should never happen
                 # as policy is a required field in the schema but makes mypy happy.
-                raise VaultInvalidPolicy
+                raise VaultInvalidPolicy(
+                    "Policy is required when using policy provider"
+                )
             policy_paths = get_policy_paths(
                 path.policy.name,
                 path.policy.instance.name,
