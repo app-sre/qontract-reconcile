b034d40a1cbcdbd0c0902032f2527fb1bd97ea03
Christian Assing <cassing@redhat.com>
2023-02-07 09:38:03 +0100

    pin mypy version to avoid other unrelated changes in this PR

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
         "croniter>=1.0.15,<1.1.0",
         "dyn~=1.8.1",
         "transity-statuspageio>=0.0.3,<0.1",
-        "pydantic~=1.10.0",
+        "pydantic~=1.9.0",
         "MarkupSafe==2.1.1",
         # this is really needed only in lint and type validations.
         # Is there any better place to put this in?
