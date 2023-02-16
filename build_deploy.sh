65648ebf33fe5638771085a9ce940a7829d8e47a
Karl Fischer <kfischer@redhat.com>
2023-02-16 12:57:10 +0100

    APPSRE-7067 containerize pypi publish

--- build_deploy.sh
+++ build_deploy.sh
@@ -2,10 +2,11 @@
 
 DOCKER_CONF="$PWD/.docker"
 mkdir -p "$DOCKER_CONF"
+PYPI_PUSH_IMAGE=quay.io/app-sre/qontract-reconcile-builder:0.3.8
 docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
 
 # build images
 make test build push
 
 # publish to pypi
-./build_tag.sh
+docker run -e TWINE_USERNAME -e TWINE_PASSWORD -v $(pwd):/work --rm $PYPI_PUSH_IMAGE ./build_tag.sh
