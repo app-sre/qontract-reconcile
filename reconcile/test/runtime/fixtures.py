01fcc596743a123b797aec7500a031b85d50a24d
Gerd Oberlechner <goberlec@redhat.com>
2023-02-16 09:58:44 +0100

    [integration-runtime] replace integration args, kwargs run parameters with class (#3198)
    replace the generic `*args: Any, **kwargs: Any` parameterlist on the `run` function of class based integrations with a dedicated parameter class on an integrations `__init__` function.
    wrapping parameters in a dataclass enables validations at runtime (e.g. pydantic) but also at static code analysis (e.g. mypy).
    
    to leverage type safety all the way, the type of the parameter class can be defined as type hint when inheriting from `QontractReconcileIntegration`. the `__init__` function will then accept that type and the `param` property of the class will have that as a verifable type as well.
    
    ```python
    class SimpleTestIntegrationParams(PydanticRunParams):
        int_arg: int
        opt_str_arg: Optional[str] = None
    
    class SimpleTestIntegration(QontractReconcileIntegration[SimpleTestIntegrationParams]):
        def __init__(self, params: SimpleTestIntegrationParams):
            super().__init__(params)
    
        ...
    
        def run(self, dry_run: bool) -> None:
            print(self.params.int_arg)
    ```
    
    in this example, the `print(self.params.int_arg)` statement is completely verifiable with mypy and also VSCode yields proper autocompletion.
    
    in cli.py, the integration is hooked up like this:
    
    ```python
    @integration.command()
    @click.pass_context
    def simple_integration(ctx, int_param: int):
        from reconcile import simple_integration
    
        run_class_integration(
            integration=simple_integration.SimpleIntegration(SimpleTestIntegrationParams(int_arg=int_param)),
            ctx=ctx.obj,
        )
    ```
    
    for the module based integrations, the generic wrapper class `ModuleArgsKwargsRunParams` is available, that makes sure the custom `run` functions in the modules do not need to change. all the wrapping happens in `reconcile.cli` so module integrations do not need to adapt because of this change.
    
    Wrapping all paramters in a single object makes it easier to pass them around. This comes in handy when sharded integration runs are spawned off into threads or processes.
    
    Another option would have been to make the integration a dataclass and defining all parameters as properties. That would make it harder to distinguish between run parameters passed into the integration and potential object properties that are just there to hold some values for the integration run.
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- reconcile/test/runtime/fixtures.py
+++ reconcile/test/runtime/fixtures.py
@@ -7,42 +7,56 @@ import pytest
 
 from reconcile.utils.runtime.integration import (
     DesiredStateShardConfig,
+    PydanticRunParams,
     QontractReconcileIntegration,
 )
 
 
-class SimpleTestIntegration(QontractReconcileIntegration):
-    def __init__(self):
-        self.desired_state_data = {}
+class SimpleTestIntegrationParams(PydanticRunParams):
+    int_arg: int
+    opt_str_arg: Optional[str] = None
+
+
+class SimpleTestIntegration(QontractReconcileIntegration[SimpleTestIntegrationParams]):
+    def __init__(self, params: SimpleTestIntegrationParams):
+        super().__init__(params)
+        self.desired_state_data: dict[str, Any] = {}
 
     @property
     def name(self) -> str:
         return "test-integration"
 
-    def get_early_exit_desired_state(self, *args: Any, **kwargs: Any) -> dict[str, Any]:
+    def get_early_exit_desired_state(self) -> dict[str, Any]:
         return self.desired_state_data
 
     def get_desired_state_shard_config(self) -> Optional[DesiredStateShardConfig]:
         return None
 
-    def run(self, dry_run: bool, *args, **kwargs) -> None:
-        pass
+    def run(self, dry_run: bool) -> None:
+        print(self.params.int_arg)
 
 
 @pytest.fixture
 def simple_test_integration() -> SimpleTestIntegration:
-    return SimpleTestIntegration()
+    return SimpleTestIntegration(params=SimpleTestIntegrationParams(int_arg=1))
 
 
-class ShardableTestIntegration(QontractReconcileIntegration):
-    def __init__(self):
-        self.desired_state_data = {}
+class ShardableTestIntegrationParams(PydanticRunParams):
+    shard: Optional[str] = None
+
+
+class ShardableTestIntegration(
+    QontractReconcileIntegration[ShardableTestIntegrationParams]
+):
+    def __init__(self, params: ShardableTestIntegrationParams):
+        super().__init__(params)
+        self.desired_state_data: dict[str, Any] = {}
 
     @property
     def name(self) -> str:
         return "shardable-test-integration"
 
-    def get_early_exit_desired_state(self, *args: Any, **kwargs: Any) -> dict[str, Any]:
+    def get_early_exit_desired_state(self) -> dict[str, Any]:
         return self.desired_state_data
 
     def get_desired_state_shard_config(self) -> Optional[DesiredStateShardConfig]:
@@ -52,10 +66,10 @@ class ShardableTestIntegration(QontractReconcileIntegration):
             sharded_run_review=lambda srp: True,
         )
 
-    def run(self, dry_run: bool, *args, **kwargs) -> None:
+    def run(self, dry_run: bool) -> None:
         pass
 
 
 @pytest.fixture
 def shardable_test_integration() -> ShardableTestIntegration:
-    return ShardableTestIntegration()
+    return ShardableTestIntegration(params=ShardableTestIntegrationParams())
--- reconcile/test/runtime/test_utils_integration.py
+++ reconcile/test/runtime/test_utils_integration.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import pytest
 
 from reconcile.test.runtime import (
@@ -7,7 +9,10 @@ from reconcile.test.runtime import (
     demo_integration_no_run_func,
     demo_integration_shard_config,
 )
-from reconcile.utils.runtime.integration import ModuleBasedQontractReconcileIntegration
+from reconcile.utils.runtime.integration import (
+    ModuleArgsKwargsRunParams,
+    ModuleBasedQontractReconcileIntegration,
+)
 
 
 def test_module_integration_no_name():
@@ -15,7 +20,9 @@ def test_module_integration_no_name():
     an integration with no QONTRACT_INTEGRATION name variable
     """
     with pytest.raises(NotImplementedError):
-        ModuleBasedQontractReconcileIntegration(demo_integration_no_name)
+        ModuleBasedQontractReconcileIntegration(
+            ModuleArgsKwargsRunParams(module=demo_integration_no_name)
+        )
 
 
 def test_module_integration_no_run_func():
@@ -23,13 +30,17 @@ def test_module_integration_no_run_func():
     an integration with no run function
     """
     with pytest.raises(NotImplementedError):
-        ModuleBasedQontractReconcileIntegration(demo_integration_no_run_func)
+        ModuleBasedQontractReconcileIntegration(
+            ModuleArgsKwargsRunParams(module=demo_integration_no_run_func)
+        )
 
 
 def test_module_integration_run():
     demo_integration.run_calls = []
-    integration = ModuleBasedQontractReconcileIntegration(demo_integration)
-    integration.run(dry_run=True, some_arg=1)
+    integration = ModuleBasedQontractReconcileIntegration(
+        ModuleArgsKwargsRunParams(module=demo_integration, some_arg=1)
+    )
+    integration.run(dry_run=True)
 
     assert {
         "some_arg": 1,
@@ -39,47 +50,54 @@ def test_module_integration_run():
 
 
 def test_demo_integration_no_early_exit_desired_state():
-    integration = ModuleBasedQontractReconcileIntegration(demo_integration)
-    assert not integration.get_early_exit_desired_state(some_arg=1)
+    integration = ModuleBasedQontractReconcileIntegration(
+        ModuleArgsKwargsRunParams(module=demo_integration, some_arg=1)
+    )
+    assert not integration.get_early_exit_desired_state()
 
 
 def test_demo_integration_early_exit_desired_state():
-    integration = ModuleBasedQontractReconcileIntegration(demo_integration_early_exit)
-    data = integration.get_early_exit_desired_state("arg", some_kw_arg="kwarg")
+    integration = ModuleBasedQontractReconcileIntegration(
+        ModuleArgsKwargsRunParams(
+            demo_integration_early_exit, "arg", some_kw_arg="kwarg"
+        )
+    )
+    data = integration.get_early_exit_desired_state()
 
     assert data == {"args": ("arg",), "kwargs": {"some_kw_arg": "kwarg"}}
 
 
 def test_demo_integration_no_shard_config():
-    integration = ModuleBasedQontractReconcileIntegration(demo_integration)
+    integration = ModuleBasedQontractReconcileIntegration(
+        ModuleArgsKwargsRunParams(module=demo_integration, some_arg=1)
+    )
     assert not integration.get_desired_state_shard_config()
 
 
 def test_demo_integration_shard_config():
-    integration = ModuleBasedQontractReconcileIntegration(demo_integration_shard_config)
+    integration = ModuleBasedQontractReconcileIntegration(
+        ModuleArgsKwargsRunParams(demo_integration_shard_config)
+    )
     assert integration.get_desired_state_shard_config()
 
 
-def test_demo_integration_kwargs_have_shard_info():
-    integration = ModuleBasedQontractReconcileIntegration(demo_integration_shard_config)
-    assert integration.kwargs_have_shard_info(
-        **{demo_integration_shard_config.SHARD_ARG_NAME: "shard1"}
+@pytest.mark.parametrize(
+    "kwargs,expected_result",
+    [
+        # no comparison bundle available, so no early exit and no sharding
+        ({demo_integration_shard_config.SHARD_ARG_NAME: "shard1"}, True),
+        ({demo_integration_shard_config.SHARD_ARG_NAME: ["shard1"]}, True),
+        ({demo_integration_shard_config.SHARD_ARG_NAME: None}, False),
+        ({demo_integration_shard_config.SHARD_ARG_NAME: ()}, False),
+        ({demo_integration_shard_config.SHARD_ARG_NAME: []}, False),
+        ({}, False),
+    ],
+)
+def test_demo_integration_params_have_shard_info(kwargs: Any, expected_result: bool):
+    integration = ModuleBasedQontractReconcileIntegration(
+        ModuleArgsKwargsRunParams(
+            demo_integration_shard_config,
+            **kwargs,
+        )
     )
-
-    assert integration.kwargs_have_shard_info(
-        **{demo_integration_shard_config.SHARD_ARG_NAME: ["shard1"]}
-    )
-
-    assert not integration.kwargs_have_shard_info(
-        **{demo_integration_shard_config.SHARD_ARG_NAME: None}
-    )
-
-    assert not integration.kwargs_have_shard_info(
-        **{demo_integration_shard_config.SHARD_ARG_NAME: ()}
-    )
-
-    assert not integration.kwargs_have_shard_info(
-        **{demo_integration_shard_config.SHARD_ARG_NAME: []}
-    )
-
-    assert not integration.kwargs_have_shard_info(**{})
+    assert integration.params_have_shard_info() == expected_result
