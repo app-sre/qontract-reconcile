d4c8a4bb766e6d3570034f716412f73c6dc56bdb
Doran Moppert <dmoppert@redhat.com>
2023-02-28 12:01:57 +0000

    add a passing test of allowed_secret_parameter_paths

--- reconcile/test/test_saasherder_allowed_secret_paths.py
+++ reconcile/test/test_saasherder_allowed_secret_paths.py
@@ -1,7 +1,68 @@
 from reconcile.utils.saasherder import SaasHerder
 
 
-def test_saasherder_allowed_secret_paths():
+def test_saasherder_allowed_secret_paths_parent_directory():
+    """
+    ensure a parent directory in allowed_secret_parameter_paths matches correctly
+    """
+    saas_files = [
+        {
+            "path": "path1",
+            "name": "a1",
+            "managedResourceTypes": [],
+            "allowedSecretParameterPaths": [
+                "foobar",
+            ],
+            "resourceTemplates": [
+                {
+                    "name": "test",
+                    "url": "url",
+                    "targets": [
+                        {
+                            "namespace": {
+                                "name": "ns",
+                                "environment": {"name": "env1", "parameters": "{}"},
+                                "cluster": {"name": "cluster"},
+                            },
+                            "ref": "main",
+                            "upstream": {"instance": {"name": "ci"}, "name": "job"},
+                            "parameters": {},
+                            "secretParameters": [
+                                {
+                                    "name": "secret",
+                                    "secret": {
+                                        "path": "foobar/baz",
+                                        "field": "db.endpoint",
+                                    },
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+            "selfServiceRoles": [
+                {"users": [{"org_username": "theirname"}], "bots": []}
+            ],
+        },
+    ]
+
+    saasherder = SaasHerder(
+        saas_files,
+        thread_pool_size=1,
+        gitlab=None,
+        integration="",
+        integration_version="",
+        settings={},
+        validate=True,
+    )
+
+    assert saasherder.valid
+
+
+def test_saasherder_allowed_secret_paths_invalid_parent_directory():
+    """
+    ensure a prefix directory allowed_secret_parameter_paths is not incorrectly identified as a parent
+    """
     saas_files = [
         {
             "path": "path1",
