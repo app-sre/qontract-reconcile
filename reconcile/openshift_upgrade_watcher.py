1048846bc19925f252de6959ea9d90a584f207d1
Maor Friedman <mafriedm@redhat.com>
2023-03-01 13:23:39 +0200

    openshift-upgrade-watcher ignore hypershift clusters

--- reconcile/openshift_upgrade_watcher.py
+++ reconcile/openshift_upgrade_watcher.py
@@ -128,7 +128,7 @@ def run(
         secret_reader=secret_reader,
     )
 
-    clusters = [c for c in get_clusters() if c.ocm]
+    clusters = [c for c in get_clusters() if c.ocm and not c.spec.hypershift]
 
     slack: Optional[SlackApi] = None
     if not dry_run:
