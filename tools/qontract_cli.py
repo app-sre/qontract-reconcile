1ba72d47cac0a3b3859d62067926cc424e7e79b1
Jan-Hendrik Boll <jboll@redhat.com>
2023-03-06 12:17:54 +0100

    Output to s3 (#3239)
    * Switch aws user output to s3
    
    Vault is just to slow to list all the secrets. Read encrypted passwords from s3 instead
    
    * Use s3 instead of vault
    
    * parallel s3 get calls

--- tools/qontract_cli.py
+++ tools/qontract_cli.py
@@ -7,14 +7,12 @@ import sys
 from collections import defaultdict
 from datetime import datetime
 from operator import itemgetter
-from typing import (
-    Optional,
-    cast,
-)
+from typing import Optional
 
 import click
 import requests
 import yaml
+from sretoolbox.utils import threaded
 
 import reconcile.gitlab_housekeeping as glhk
 import reconcile.ocm_upgrade_scheduler as ous
@@ -70,10 +68,6 @@ from reconcile.utils.secret_reader import SecretReader
 from reconcile.utils.semver_helper import parse_semver
 from reconcile.utils.state import State
 from reconcile.utils.terraform_client import TerraformClient as Terraform
-from reconcile.utils.vault import (
-    VaultClient,
-    _VaultClient,
-)
 from tools.cli_commands.gpg_encrypt import (
     GPGEncryptCommand,
     GPGEncryptCommandData,
@@ -883,9 +877,11 @@ def clusters_aws_account_ids(ctx):
 @get.command()
 @click.pass_context
 def terraform_users_credentials(ctx) -> None:
-    vc = cast(_VaultClient, VaultClient())
+    settings = queries.get_app_interface_settings()
+    accounts = queries.get_state_aws_accounts()
+    state = State("account-notifier", accounts, settings=settings)
 
-    skip_accounts, appsre_pgp_key, reencrypt_settings = tfu.get_reencrypt_settings()
+    skip_accounts, appsre_pgp_key, _ = tfu.get_reencrypt_settings()
 
     accounts, working_dirs, _, aws_api = tfu.setup(
         False,
@@ -919,13 +915,24 @@ def terraform_users_credentials(ctx) -> None:
                 }
                 credentials.append(item)
 
-    for secret in vc.list(reencrypt_settings.aws_account_output_vault_path):
-        secret_request = {
-            "path": f"{reencrypt_settings.aws_account_output_vault_path}/{secret}"
-        }
-        secret_data = vc.read_all(secret_request)
-        if secret_data["account"] not in skip_accounts:
-            credentials.append(secret_data)
+    secrets = state.ls()
+
+    def _get_secret(secret_key: str):
+        if secret_key.startswith("/output/"):
+            secret_data = state.get(secret_key[1:])
+            if secret_data["account"] not in skip_accounts:
+                return secret_data
+        return None
+
+    secret_result = threaded.run(
+        _get_secret,
+        secrets,
+        10,
+    )
+
+    for secret in secret_result:
+        if secret and secret["account"] not in skip_accounts:
+            credentials.append(secret)
 
     columns = ["account", "console_url", "user_name", "encrypted_password"]
     print_output(ctx.obj["options"], credentials, columns)
@@ -933,10 +940,11 @@ def terraform_users_credentials(ctx) -> None:
 
 @root.command()
 @click.argument("account_name")
-@click.argument("output_path")
 @click.pass_context
-def user_credentials_migrate_output(ctx, account_name, output_path) -> None:
-    vc = cast(_VaultClient, VaultClient())
+def user_credentials_migrate_output(ctx, account_name) -> None:
+    settings = queries.get_app_interface_settings()
+    accounts = queries.get_state_aws_accounts()
+    state = State("account-notifier", accounts, settings=settings)
 
     skip_accounts, appsre_pgp_key, _ = tfu.get_reencrypt_settings()
 
@@ -960,24 +968,19 @@ def user_credentials_migrate_output(ctx, account_name, output_path) -> None:
     )
     credentials = []
     for account, output in tf.outputs.items():
-        if account in skip_accounts:
-            user_passwords = tf.format_output(output, tf.OUTPUT_TYPE_PASSWORDS)
-            console_urls = tf.format_output(output, tf.OUTPUT_TYPE_CONSOLEURLS)
-            for user_name, enc_password in user_passwords.items():
-                item = {
-                    "account": account,
-                    "console_url": console_urls[account],
-                    "user_name": user_name,
-                    "encrypted_password": enc_password,
-                }
-                credentials.append(item)
+        user_passwords = tf.format_output(output, tf.OUTPUT_TYPE_PASSWORDS)
+        console_urls = tf.format_output(output, tf.OUTPUT_TYPE_CONSOLEURLS)
+        for user_name, enc_password in user_passwords.items():
+            item = {
+                "account": account,
+                "console_url": console_urls[account],
+                "user_name": user_name,
+                "encrypted_password": enc_password,
+            }
+            credentials.append(item)
 
     for cred in credentials:
-        new_secret = {
-            "path": f"{output_path}/{cred['user_name']}_{cred['account']}",
-            "data": cred,
-        }
-        vc.write(new_secret, decode_base64=False)
+        state.add(f"output/{cred['account']}/{cred['user_name']}", cred)
 
 
 @get.command()
