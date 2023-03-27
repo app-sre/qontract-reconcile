1e5f2730e6c1720c391f1edfce7b4db684b60b66
Bhushan Thakur <bhthakur@redhat.com>
2023-03-27 08:55:08 -0400

    format

--- reconcile/test/test_utils_terrascript_cloudflare_client.py
+++ reconcile/test/test_utils_terrascript_cloudflare_client.py
@@ -168,7 +168,7 @@ def test_create_cloudflare_resources_terraform_json(account_config, backend_conf
             "ownership_challenge": "some-challenge",
             "dataset": "http_requests",
             "frequency": "high",
-            "kind": "edge"
+            "kind": "edge",
         },
         {},
     )
@@ -185,7 +185,7 @@ def test_create_cloudflare_resources_terraform_json(account_config, backend_conf
             "dataset": "http_requests",
             "frequency": "high",
             "job_name": "Logpush job for account",
-            "filter": "{\"key\":\"BotScore\",\"operator\":\"lt\",\"value\":\"30\"}"
+            "filter": '{"key":"BotScore","operator":"lt","value":"30"}',
         },
         {},
     )
@@ -325,7 +325,7 @@ def test_create_cloudflare_resources_terraform_json(account_config, backend_conf
                     "frequency": "high",
                     "logpull_options": "fields=RayID,ClientIP,EdgeStartTimestamp&timestamps=rfc3339",
                     "ownership_challenge": "some-challenge",
-                    "kind": "edge"
+                    "kind": "edge",
                 },
                 "logpush_job_account": {
                     "account_id": "${var.account_id}",
@@ -336,7 +336,7 @@ def test_create_cloudflare_resources_terraform_json(account_config, backend_conf
                     "logpull_options": "fields=RayID,ClientIP,EdgeStartTimestamp&timestamps=rfc3339",
                     "ownership_challenge": "some-challenge",
                     "name": "Logpush job for account",
-                    "filter": "{\"key\":\"BotScore\",\"operator\":\"lt\",\"value\":\"30\"}"
+                    "filter": '{"key":"BotScore","operator":"lt","value":"30"}',
                 },
             },
             "cloudflare_logpull_retention": {
