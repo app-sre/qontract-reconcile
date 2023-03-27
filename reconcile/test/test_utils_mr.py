0e908ee8c2fb564fb8aff1c4d92273115600ea9f
Christian Assing <cassing@redhat.com>
2023-03-27 12:54:37 +0200

    [utils.mr.notificator] Enhance CreateAppInterfaceNotificator
    * Fully typed
    * Replace dict parameter by a typed class
    * Refactor `app-interface-email` rendering + tests

--- reconcile/test/test_utils_mr.py
+++ reconcile/test/test_utils_mr.py
@@ -1,13 +1,19 @@
-from typing import Optional
+from typing import (
+    Any,
+    Optional,
+)
 from unittest import TestCase
 from unittest.mock import MagicMock
 
+import pytest
+import yaml
 from gitlab.exceptions import GitlabError
 
 from reconcile.utils.gitlab_api import GitLabApi
 from reconcile.utils.mr.base import (
     MergeRequestBase,
     MergeRequestProcessingError,
+    app_interface_email,
 )
 
 
@@ -87,3 +93,134 @@ class TestMergeRequestBaseProcessContractTests(TestCase):
         with self.assertRaises(MergeRequestProcessingError):
             mr.submit_to_gitlab(cli)
         cli.project.mergerequests.create.assert_not_called()
+
+
+@pytest.mark.parametrize(
+    "aliases, expected_aliases",
+    [
+        pytest.param(
+            {},
+            {"aliases": []},
+            marks=pytest.mark.xfail(strict=True, raises=(KeyError, TypeError)),
+        ),
+        pytest.param(
+            {"aliases": []},
+            {"aliases": []},
+            marks=pytest.mark.xfail(strict=True, raises=(KeyError, TypeError)),
+        ),
+        (
+            {"aliases": ["alias1", "alias2"]},
+            {"aliases": ["alias1", "alias2"]},
+        ),
+    ],
+)
+@pytest.mark.parametrize(
+    "users, expected_users",
+    [
+        pytest.param(
+            {},
+            {"users": []},
+            marks=pytest.mark.xfail(strict=True, raises=(KeyError, TypeError)),
+        ),
+        pytest.param(
+            {"users": []},
+            {"users": []},
+            marks=pytest.mark.xfail(strict=True, raises=(KeyError, TypeError)),
+        ),
+        (
+            {"users": ["/path/to/user1", "/path/to/user2"]},
+            {
+                "users": [
+                    {"$ref": "/path/to/user1"},
+                    {"$ref": "/path/to/user2"},
+                ]
+            },
+        ),
+    ],
+)
+@pytest.mark.parametrize(
+    "aws_accounts, expected_aws_accounts",
+    [
+        pytest.param(
+            {},
+            {"aws_accounts": []},
+            marks=pytest.mark.xfail(strict=True, raises=(KeyError, TypeError)),
+        ),
+        pytest.param(
+            {"aws_accounts": []},
+            {"aws_accounts": []},
+            marks=pytest.mark.xfail(strict=True, raises=(KeyError, TypeError)),
+        ),
+        (
+            {"aws_accounts": ["/path/to/aws_account1", "/path/to/aws_account2"]},
+            {
+                "aws_accounts": [
+                    {"$ref": "/path/to/aws_account1"},
+                    {"$ref": "/path/to/aws_account2"},
+                ]
+            },
+        ),
+    ],
+)
+@pytest.mark.parametrize(
+    "services, expected_services",
+    [
+        pytest.param(
+            {},
+            {"services": []},
+            marks=pytest.mark.xfail(strict=True, raises=(KeyError, TypeError)),
+        ),
+        pytest.param(
+            {"apps": []},
+            {"services": []},
+            marks=pytest.mark.xfail(strict=True, raises=(KeyError, TypeError)),
+        ),
+        (
+            {"apps": ["/path/to/aws_account1", "/path/to/aws_account2"]},
+            {
+                "services": [
+                    {"$ref": "/path/to/aws_account1"},
+                    {"$ref": "/path/to/aws_account2"},
+                ]
+            },
+        ),
+    ],
+)
+def test_email_template(
+    aliases: dict[str, Any],
+    expected_aliases: dict[str, Any],
+    users: dict[str, Any],
+    expected_users: dict[str, Any],
+    aws_accounts: dict[str, Any],
+    expected_aws_accounts: dict[str, Any],
+    services: dict[str, Any],
+    expected_services: dict[str, Any],
+) -> None:
+    """Test email template"""
+
+    content = app_interface_email(
+        name="test-name",
+        subject="test-subject",
+        body="test-body",
+        **aliases,
+        **users,
+        **aws_accounts,
+        **services,
+    )
+
+    email = yaml.safe_load(content)
+    email["NAME"] = "test-name"
+    email["SUBJECT"] = "test-subject"
+    email["BODY"] = "test-body"
+
+    if not aliases and not users and not aws_accounts and not services:
+        assert not email["to"]
+
+    for key, value in expected_aliases.items():
+        assert email["to"][key] == value
+    for key, value in expected_users.items():
+        assert email["to"][key] == value
+    for key, value in expected_aws_accounts.items():
+        assert email["to"][key] == value
+    for key, value in expected_services.items():
+        assert email["to"][key] == value
