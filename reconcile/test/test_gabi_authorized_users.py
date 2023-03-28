97fd4bb5d61fa950c3a3b28c18121e7691e62214
Feng Huang <fehuang@redhat.com>
2023-03-28 10:20:38 -0400

    decoupling gabi-authorized-users tests from OCNative
    Signed-off-by: Feng Huang <fehuang@redhat.com>

--- reconcile/test/test_gabi_authorized_users.py
+++ reconcile/test/test_gabi_authorized_users.py
@@ -1,161 +1,72 @@
-import json
-import os
 from datetime import (
     date,
     timedelta,
 )
-from unittest import TestCase
-from unittest.mock import patch
+
+import pytest
+from pytest_mock import MockerFixture
 
 import reconcile.gabi_authorized_users as gabi_u
 import reconcile.openshift_base as ob
-from reconcile import queries
 from reconcile.test.fixtures import Fixtures
 from reconcile.utils.aggregated_list import RunnerException
-from reconcile.utils.oc import (
-    ApiClient,
-    OCDeprecated,
-)
 from reconcile.utils.openshift_resource import OpenshiftResource as OR
-from reconcile.utils.secret_reader import SecretReader
+from reconcile.utils.openshift_resource import ResourceInventory
 
-apply = Fixtures("gabi_authorized_users").get_anymarkup("apply.yml")
-delete = Fixtures("gabi_authorized_users").get_anymarkup("delete.yml")
-
-
-class RespMock:
-    def __init__(self, data):
-        self.data = json.dumps(data).encode()
+fixture = Fixtures("gabi_authorized_users").get_anymarkup("api.yml")
 
 
-def apply_request(
-    method,
-    url,
-    query_params=None,
-    headers=None,
-    post_params=None,
-    body=None,
-    _preload_content=True,
-    _request_timeout=None,
-):
-    url_list = url.split("/", 1)
-    cluster = apply.get(url_list[0])
-    data = cluster.get(url_list[1], None)
-    return RespMock(data)
+def test_gabi_authorized_users_apply(mocker: MockerFixture) -> None:
+    ri = ResourceInventory()
+    ri.initialize_resource_type("server", "gabi-db", "ConfigMap")
+    gabi_instance = fixture["gql_response"][0]
+    gabi_u.fetch_desired_state([gabi_instance], ri)
+    mock_apply = mocker.patch.object(ob, "apply", autospec=True)
+    oc_map = mocker.patch("reconcile.utils.oc_map.OCMap", autospec=True)
+    ob.realize_data(True, oc_map, ri, 1)
+    expected = OR(
+        fixture["desired"],
+        gabi_u.QONTRACT_INTEGRATION,
+        gabi_u.QONTRACT_INTEGRATION_VERSION,
+    )
+    args, _ = mock_apply.call_args
+    assert args[5] == expected
 
 
-def delete_request(
-    method,
-    url,
-    query_params=None,
-    headers=None,
-    post_params=None,
-    body=None,
-    _preload_content=True,
-    _request_timeout=None,
-):
-    url_list = url.split("/", 1)
-    cluster = delete.get(url_list[0])
-    data = cluster.get(url_list[1], None)
-    return RespMock(data)
+def test_gabi_authorized_users_exist(mocker: MockerFixture) -> None:
+    ri = ResourceInventory()
+    ri.initialize_resource_type("server", "gabi-db", "ConfigMap")
+    current = OR(
+        fixture["current"],
+        gabi_u.QONTRACT_INTEGRATION,
+        gabi_u.QONTRACT_INTEGRATION_VERSION,
+    )
+    ri.add_current(
+        "server",
+        "gabi-db",
+        "ConfigMap",
+        current.name,
+        current,
+    )
+    gabi_instance = fixture["gql_response"][0]
+    gabi_u.fetch_desired_state([gabi_instance], ri)
+    mock_apply = mocker.patch.object(ob, "apply", autospec=True)
+    oc_map = mocker.patch("reconcile.utils.oc_map.OCMap", autospec=True)
+    ob.realize_data(True, oc_map, ri, 1)
+    mock_apply.assert_not_called()
 
 
-def mock_get_gabi_instances(expiration_date: str) -> list[dict]:
-    gabi_instances = apply["gql_response"]
-    gabi_instances[0]["expirationDate"] = str(expiration_date)
-    return gabi_instances
+def test_gabi_authorized_users_exceed() -> None:
+    expiration_date = date.today() + timedelta(days=(gabi_u.EXPIRATION_DAYS_MAX + 1))
+    gabi_instance = fixture["gql_response"][0]
+    gabi_instance["expirationDate"] = str(expiration_date)
+    ri = ResourceInventory()
+    with pytest.raises(RunnerException):
+        gabi_u.fetch_desired_state([gabi_instance], ri)
 
 
-@patch.object(queries, "get_app_interface_settings", autospec=True)
-@patch.object(SecretReader, "read", autospec=True)
-@patch.object(SecretReader, "read_all", autospec=True)
-@patch.object(OCDeprecated, "get_version", autospec=True)
-@patch.object(queries, "get_gabi_instances", autospec=True)
-@patch.object(ApiClient, "request")
-@patch.dict(os.environ, {"USE_NATIVE_CLIENT": "True"}, clear=True)
-class TestGabiAuthorizedUser(TestCase):
-    def test_gabi_authorized_users_exceed(
-        self,
-        mock_request,
-        get_gabi_instances,
-        oc_version,
-        secret_read,
-        secrect_read_all,
-        get_settings,
-    ):
-        expiration_date = date.today() + timedelta(
-            days=(gabi_u.EXPIRATION_DAYS_MAX + 1)
-        )
-        get_gabi_instances.return_value = mock_get_gabi_instances(expiration_date)
-        mock_request.side_effect = apply_request
-        with self.assertRaises(RunnerException):
-            gabi_u.run(dry_run=False)
-
-    @patch.object(ob, "apply", autospec=True)
-    def test_gabi_authorized_users_apply(
-        self,
-        mock_apply,
-        mock_request,
-        get_gabi_instances,
-        oc_version,
-        secrect_read_all,
-        secret_read,
-        get_settings,
-    ):
-        expiration_date = date(2023, 1, 1)
-        get_gabi_instances.return_value = mock_get_gabi_instances(expiration_date)
-        secrect_read_all.return_value = {"server": "server", "token": "foo"}
-        mock_request.side_effect = apply_request
-        gabi_u.run(dry_run=False)
-        expected = OR(
-            apply["desired"],
-            gabi_u.QONTRACT_INTEGRATION,
-            gabi_u.QONTRACT_INTEGRATION_VERSION,
-        )
-        args, _ = mock_apply.call_args
-        self.assertEqual(args[5], expected)
-
-    @patch.object(OR, "calculate_sha256sum", autospec=True)
-    @patch.object(OCDeprecated, "apply", autospec=True)
-    def test_gabi_authorized_users_exist(
-        self,
-        mock_apply,
-        sha,
-        mock_request,
-        get_gabi_instances,
-        oc_version,
-        secrect_read_all,
-        secret_read,
-        get_settings,
-    ):
-        expiration_date = date.today()
-        get_gabi_instances.return_value = mock_get_gabi_instances(expiration_date)
-        secrect_read_all.return_value = {"server": "server", "token": "foo"}
-        mock_request.side_effect = delete_request
-        sha.return_value = "abc"
-        gabi_u.run(dry_run=False)
-        mock_apply.assert_not_called()
-
-    @patch.object(ob, "apply", autospec=True)
-    def test_gabi_authorized_users_expire(
-        self,
-        mock_apply,
-        mock_request,
-        get_gabi_instances,
-        oc_version,
-        secrect_read_all,
-        secret_read,
-        get_settings,
-    ):
-        expiration_date = date(2023, 1, 1) - timedelta(days=1)
-        get_gabi_instances.return_value = mock_get_gabi_instances(expiration_date)
-        secrect_read_all.return_value = {"server": "server", "token": "foo"}
-        mock_request.side_effect = delete_request
-        gabi_u.run(dry_run=False)
-        expected = OR(
-            delete["desired"],
-            gabi_u.QONTRACT_INTEGRATION,
-            gabi_u.QONTRACT_INTEGRATION_VERSION,
-        )
-        args, _ = mock_apply.call_args
-        self.assertEqual(args[5], expected)
+def test_gabi_authorized_users_rds_not_found() -> None:
+    gabi_instance = fixture["gql_response"][1]
+    ri = ResourceInventory()
+    with pytest.raises(RunnerException):
+        gabi_u.fetch_desired_state([gabi_instance], ri)
