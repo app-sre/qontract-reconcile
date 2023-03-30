app-sre/qontract-reconcile

commit 15870b239aba26209eacc703271f707bba7bd2a6

[ocm-upgrade-scheduler] upgrade spec fetching and processing shardable by OCM env (#3377)
make the upgrade spec fetching of the upgrade scheduler integrations shardable by
OCM environment and org.

adapt the qontract-cli commands to new refactored spec fetching

why: preparation for AUS where we want to be able to handle an OCM environment as a failure domain

refactor ticket: APPSRE-7356
epic: SDE-2789

Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

reconcile/aus/ocm_upgrade_scheduler.py
@@ -10,6 +10,7 @@ from reconcile.aus.models import (
 from reconcile.gql_definitions.advanced_upgrade_service.aus_clusters import (
     query as aus_clusters_query,
 )
+from reconcile.gql_definitions.fragments.ocm_environment import OCMEnvironment
 from reconcile.utils import gql
 from reconcile.utils.disabled_integrations import integration_is_enabled
 from reconcile.utils.ocm import (
@@ -59,38 +60,34 @@ class OCMClusterUpgradeSchedulerIntegration(
         )
         aus.act(dry_run, diffs, ocm_map)
 
-    def get_organization_upgrade_spec(
-        self, org_name: Optional[str] = None
+    def get_ocm_env_upgrade_specs(
+        self, ocm_env: OCMEnvironment, org_name: Optional[str] = None
     ) -> dict[str, OrganizationUpgradeSpec]:
-        return organization_upgrade_spec(org_name=org_name)
-
-
-def organization_upgrade_spec(
-    org_name: Optional[str] = None,
-) -> dict[str, OrganizationUpgradeSpec]:
-    specs_per_org: dict[str, list[ClusterUpgradeSpec]] = defaultdict(list)
-    for cluster in aus_clusters_query(query_func=gql.get_api().query).clusters or []:
-        supported_product = (
-            cluster.spec and cluster.spec.product in SUPPORTED_OCM_PRODUCTS
-        )
-        in_org_shard = org_name is None or (
-            cluster.ocm and cluster.ocm.name == org_name
-        )
-        if (
-            integration_is_enabled(QONTRACT_INTEGRATION, cluster)
-            and cluster.ocm
-            and cluster.upgrade_policy
-            and supported_product
-            and in_org_shard
+        specs_per_org: dict[str, list[ClusterUpgradeSpec]] = defaultdict(list)
+        for cluster in (
+            aus_clusters_query(query_func=gql.get_api().query).clusters or []
         ):
-            specs_per_org[cluster.ocm.name].append(
-                ClusterUpgradeSpec(
-                    name=cluster.name,
-                    ocm=cluster.ocm,
-                    upgradePolicy=cluster.upgrade_policy,
-                )
+            supported_product = (
+                cluster.spec and cluster.spec.product in SUPPORTED_OCM_PRODUCTS
             )
-    return {
-        org_name: OrganizationUpgradeSpec(org=specs[0].ocm, specs=specs)
-        for org_name, specs in specs_per_org.items()
-    }
+            in_org_shard = org_name is None or (
+                cluster.ocm and cluster.ocm.name == org_name
+            )
+            if (
+                integration_is_enabled(QONTRACT_INTEGRATION, cluster)
+                and cluster.ocm
+                and cluster.upgrade_policy
+                and supported_product
+                and in_org_shard
+            ):
+                specs_per_org[cluster.ocm.name].append(
+                    ClusterUpgradeSpec(
+                        name=cluster.name,
+                        ocm=cluster.ocm,
+                        upgradePolicy=cluster.upgrade_policy,
+                    )
+                )
+        return {
+            org_name: OrganizationUpgradeSpec(org=specs[0].ocm, specs=specs)
+            for org_name, specs in specs_per_org.items()
+        }

tools/qontract_cli.py
@@ -22,6 +22,10 @@ import reconcile.terraform_resources as tfr
 import reconcile.terraform_users as tfu
 import reconcile.terraform_vpc_peerings as tfvpc
 from reconcile import queries
+from reconcile.aus.base import (
+    AdvancedUpgradeSchedulerBaseIntegration,
+    AdvancedUpgradeSchedulerBaseIntegrationParams,
+)
 from reconcile.change_owners.bundle import NoOpFileDiffResolver
 from reconcile.change_owners.change_owners import (
     fetch_change_type_processors,
@@ -295,6 +299,9 @@ def get_upgrade_policies_data(
     show_only_soaking_upgrades=False,
     by_workload=False,
 ):
+    if not clusters:
+        return []
+
     settings = queries.get_app_interface_settings()
     ocm_map = OCMMap(
         clusters=clusters,
@@ -467,14 +474,38 @@ def cluster_upgrade_policies(
     show_only_soaking_upgrades=False,
     by_workload=False,
 ):
-    import reconcile.aus.ocm_upgrade_scheduler as ous
+    from reconcile.aus.ocm_upgrade_scheduler import (
+        OCMClusterUpgradeSchedulerIntegration,
+    )
 
+    integration = OCMClusterUpgradeSchedulerIntegration(
+        AdvancedUpgradeSchedulerBaseIntegrationParams()
+    )
+    generate_cluster_upgrade_policies_report(
+        ctx,
+        integration=integration,
+        cluster=cluster,
+        workload=workload,
+        show_only_soaking_upgrades=show_only_soaking_upgrades,
+        by_workload=by_workload,
+    )
+
+
+def generate_cluster_upgrade_policies_report(
+    ctx,
+    integration: AdvancedUpgradeSchedulerBaseIntegration,
+    cluster: Optional[str],
+    workload: Optional[str],
+    show_only_soaking_upgrades: bool,
+    by_workload: bool,
+) -> None:
     md_output = ctx.obj["options"]["output"] == "md"
 
-    upgrade_policies_per_org = ous.organization_upgrade_spec()
+    upgrade_specs = integration.get_upgrade_specs()
     clusters = [
         s.dict(by_alias=True)
-        for org_upgrade_spec in upgrade_policies_per_org.values()
+        for org_upgrade_specs in upgrade_specs.values()
+        for org_upgrade_spec in org_upgrade_specs.values()
         for s in org_upgrade_spec.specs
     ]
 
@@ -488,7 +519,7 @@ def cluster_upgrade_policies(
     results = get_upgrade_policies_data(
         clusters,
         md_output,
-        ous.QONTRACT_INTEGRATION,
+        integration.name,
         workload,
         show_only_soaking_upgrades,
         by_workload,
@@ -560,23 +591,55 @@ def inherit_version_data_text(ocm_org: str, ocm_specs: list[dict]) -> str:
 def ocm_fleet_upgrade_policies(
     ctx,
 ):
-    import reconcile.aus.ocm_upgrade_scheduler_org as ouso
+    from reconcile.aus.ocm_upgrade_scheduler_org import (
+        OCMClusterUpgradeSchedulerOrgIntegration,
+    )
+
+    generate_fleet_upgrade_policices_report(
+        ctx,
+        OCMClusterUpgradeSchedulerOrgIntegration(
+            AdvancedUpgradeSchedulerBaseIntegrationParams()
+        ),
+    )
+
+
+@get.command()
+@click.pass_context
+def aus_fleet_upgrade_policies(
+    ctx,
+):
+    from reconcile.aus.advanced_upgrade_service import AdvancedUpgradeServiceIntegration
+
+    generate_fleet_upgrade_policices_report(
+        ctx,
+        AdvancedUpgradeServiceIntegration(
+            AdvancedUpgradeSchedulerBaseIntegrationParams()
+        ),
+    )
+
+
+def generate_fleet_upgrade_policices_report(
+    ctx, aus_integration: AdvancedUpgradeSchedulerBaseIntegration
+):
 
     md_output = ctx.obj["options"]["output"] == "md"
 
-    upgrade_policies_per_org = ouso.organization_upgrade_spec()
+    upgrade_specs = aus_integration.get_upgrade_specs()
     upgrade_policies = [
         s.dict(by_alias=True)
-        for org_upgrade_spec in upgrade_policies_per_org.values()
+        for org_upgrade_specs in upgrade_specs.values()
+        for org_upgrade_spec in org_upgrade_specs.values()
         for s in org_upgrade_spec.specs
     ]
+
     ocm_org_specs = [
         org_upgrade_spec.org.dict(by_alias=True)
-        for org_upgrade_spec in upgrade_policies_per_org.values()
+        for org_upgrade_specs in upgrade_specs.values()
+        for org_upgrade_spec in org_upgrade_specs.values()
     ]
 
     results = get_upgrade_policies_data(
-        upgrade_policies, md_output, integration=ouso.QONTRACT_INTEGRATION
+        upgrade_policies, md_output, integration=aus_integration.name
     )
 
     if md_output:

reconcile/aus/advanced_upgrade_service.py
@@ -4,13 +4,18 @@ from reconcile.aus.models import OrganizationUpgradeSpec
 from reconcile.aus.ocm_upgrade_scheduler_org import (
     OCMClusterUpgradeSchedulerOrgIntegration,
 )
+from reconcile.gql_definitions.fragments.ocm_environment import OCMEnvironment
+
+QONTRACT_INTEGRATION = "advanced-upgrade-scheduler"
 
 
-class LabelBasedOCMClusterUpgradeSchedulerOrgIntegration(
-    OCMClusterUpgradeSchedulerOrgIntegration
-):
-    def get_organization_upgrade_spec(
-        self, org_name: Optional[str] = None
+class AdvancedUpgradeServiceIntegration(OCMClusterUpgradeSchedulerOrgIntegration):
+    @property
+    def name(self) -> str:
+        return QONTRACT_INTEGRATION
+
+    def get_ocm_env_upgrade_specs(
+        self, ocm_env: OCMEnvironment, org_name: Optional[str] = None
     ) -> dict[str, OrganizationUpgradeSpec]:
         return {
             # todo

reconcile/aus/base.py
@@ -17,6 +17,11 @@ from croniter import croniter
 from semver import VersionInfo
 
 from reconcile.aus.models import OrganizationUpgradeSpec
+from reconcile.gql_definitions.common.ocm_environments import (
+    query as ocm_environment_query,
+)
+from reconcile.gql_definitions.fragments.ocm_environment import OCMEnvironment
+from reconcile.utils import gql
 from reconcile.utils.cluster_version_data import (
     VersionData,
     WorkloadHistory,
@@ -29,7 +34,7 @@ from reconcile.utils.ocm import (
     Sector,
 )
 from reconcile.utils.runtime.integration import (
-    NoParams,
+    PydanticRunParams,
     QontractReconcileIntegration,
 )
 from reconcile.utils.semver_helper import (
@@ -39,20 +44,43 @@ from reconcile.utils.semver_helper import (
 from reconcile.utils.state import init_state
 
 
-class AdvancedUpgradeSchedulerBaseIntegration(QontractReconcileIntegration[NoParams]):
-    def run(self, dry_run: bool) -> None:
-        upgrade_specs_per_org = self.get_organization_upgrade_spec()
-        if not upgrade_specs_per_org:
-            logging.debug("No upgrade policy definitions found")
-            sys.exit(0)
+class AdvancedUpgradeSchedulerBaseIntegrationParams(PydanticRunParams):
 
-        for org_name, org_upgrade_spec in upgrade_specs_per_org.items():
-            if org_upgrade_spec.specs:
-                self.process_upgrade_policies_in_org(dry_run, org_upgrade_spec)
-            else:
-                logging.debug(
-                    f"Skip org {org_name} because it defines no upgrade policies"
-                )
+    ocm_environment: Optional[str] = None
+    ocm_organization: Optional[str] = None
+
+
+class AdvancedUpgradeSchedulerBaseIntegration(
+    QontractReconcileIntegration[AdvancedUpgradeSchedulerBaseIntegrationParams]
+):
+    def run(self, dry_run: bool) -> None:
+        upgrade_specs = self.get_upgrade_specs()
+        for ocm_env, env_upgrade_specs in upgrade_specs.items():
+            for org_name, org_upgrade_spec in env_upgrade_specs.items():
+                if org_upgrade_spec.specs:
+                    self.process_upgrade_policies_in_org(dry_run, org_upgrade_spec)
+                else:
+                    logging.debug(
+                        f"Skip org {org_name} in {ocm_env} because it defines no upgrade policies"
+                    )
+        sys.exit(0)
+
+    def get_upgrade_specs(self) -> dict[str, dict[str, OrganizationUpgradeSpec]]:
+        return {
+            ocm_env.name: self.get_ocm_env_upgrade_specs(
+                ocm_env,
+                self.params.ocm_organization,
+            )
+            for ocm_env in self.get_ocm_environments()
+        }
+
+    def get_ocm_environments(self) -> list[OCMEnvironment]:
+        return ocm_environment_query(
+            gql.get_api().query,
+            variables={"name": self.params.ocm_environment}
+            if self.params.ocm_environment
+            else None,
+        ).environments
 
     @abstractmethod
     def process_upgrade_policies_in_org(
@@ -61,8 +89,8 @@ class AdvancedUpgradeSchedulerBaseIntegration(QontractReconcileIntegration[NoPar
         ...
 
     @abstractmethod
-    def get_organization_upgrade_spec(
-        self, org_name: Optional[str] = None
+    def get_ocm_env_upgrade_specs(
+        self, ocm_env: OCMEnvironment, org_name: Optional[str] = None
     ) -> dict[str, OrganizationUpgradeSpec]:
         ...
 

reconcile/aus/ocm_addons_upgrade_scheduler_org.py
@@ -14,6 +14,7 @@ from reconcile.aus.models import (
 from reconcile.gql_definitions.advanced_upgrade_service.aus_organization import (
     query as aus_organizations_query,
 )
+from reconcile.gql_definitions.fragments.ocm_environment import OCMEnvironment
 from reconcile.utils import gql
 from reconcile.utils.cluster_version_data import VersionData
 from reconcile.utils.ocm import OCMMap
@@ -58,10 +59,27 @@ class OCMAddonsUpgradeSchedulerOrgIntegration(
             )
             aus.act(dry_run, diffs, ocm_map, addon_id=addon_state.addon_id)
 
-    def get_organization_upgrade_spec(
-        self, org_name: Optional[str] = None
+    def get_ocm_env_upgrade_specs(
+        self, ocm_env: OCMEnvironment, org_name: Optional[str] = None
     ) -> dict[str, OrganizationUpgradeSpec]:
-        return organization_upgrade_spec(org_name=org_name)
+        return {
+            org.name: OrganizationUpgradeSpec(
+                org=org,
+                specs=[
+                    ClusterUpgradeSpec(
+                        name=cluster.name,
+                        ocm=org,
+                        upgradePolicy=cluster.upgrade_policy,
+                    )
+                    for cluster in org.upgrade_policy_clusters or []
+                ],
+            )
+            for org in aus_organizations_query(
+                query_func=gql.get_api().query
+            ).organizations
+            or []
+            if org.addon_managed_upgrades and (org_name is None or org.name == org_name)
+        }
 
 
 def get_state_for_org_spec(
@@ -108,27 +126,6 @@ def get_state_for_org_spec_per_addon(
             )
         )
     return ocm_map, result
-
-
-def organization_upgrade_spec(
-    org_name: Optional[str] = None,
-) -> dict[str, OrganizationUpgradeSpec]:
-    return {
-        org.name: OrganizationUpgradeSpec(
-            org=org,
-            specs=[
-                ClusterUpgradeSpec(
-                    name=cluster.name,
-                    ocm=org,
-                    upgradePolicy=cluster.upgrade_policy,
-                )
-                for cluster in org.upgrade_policy_clusters or []
-            ],
-        )
-        for org in aus_organizations_query(query_func=gql.get_api().query).organizations
-        or []
-        if org.addon_managed_upgrades and (org_name is None or org.name == org_name)
-    }
 
 
 def calculate_diff(

reconcile/aus/ocm_upgrade_scheduler_org.py
@@ -8,6 +8,7 @@ from reconcile.aus.ocm_upgrade_scheduler import OCMClusterUpgradeSchedulerIntegr
 from reconcile.gql_definitions.advanced_upgrade_service.aus_organization import (
     query as aus_organizations_query,
 )
+from reconcile.gql_definitions.fragments.ocm_environment import OCMEnvironment
 from reconcile.utils import gql
 
 QONTRACT_INTEGRATION = "ocm-upgrade-scheduler-org"
@@ -18,28 +19,24 @@ class OCMClusterUpgradeSchedulerOrgIntegration(OCMClusterUpgradeSchedulerIntegra
     def name(self) -> str:
         return QONTRACT_INTEGRATION
 
-    def get_organization_upgrade_spec(
-        self, org_name: Optional[str] = None
+    def get_ocm_env_upgrade_specs(
+        self, ocm_env: OCMEnvironment, org_name: Optional[str] = None
     ) -> dict[str, OrganizationUpgradeSpec]:
-        return organization_upgrade_spec(org_name=org_name)
-
-
-def organization_upgrade_spec(
-    org_name: Optional[str] = None,
-) -> dict[str, OrganizationUpgradeSpec]:
-    return {
-        org.name: OrganizationUpgradeSpec(
-            org=org,
-            specs=[
-                ClusterUpgradeSpec(
-                    name=cluster.name,
-                    ocm=org,
-                    upgradePolicy=cluster.upgrade_policy,
-                )
-                for cluster in org.upgrade_policy_clusters or []
-            ],
-        )
-        for org in aus_organizations_query(query_func=gql.get_api().query).organizations
-        or []
-        if org_name is None or org.name == org_name
-    }
+        return {
+            org.name: OrganizationUpgradeSpec(
+                org=org,
+                specs=[
+                    ClusterUpgradeSpec(
+                        name=cluster.name,
+                        ocm=org,
+                        upgradePolicy=cluster.upgrade_policy,
+                    )
+                    for cluster in org.upgrade_policy_clusters or []
+                ],
+            )
+            for org in aus_organizations_query(
+                query_func=gql.get_api().query
+            ).organizations
+            or []
+            if org_name is None or org.name == org_name
+        }

reconcile/cli.py
@@ -29,7 +29,6 @@ from reconcile.utils.runtime.environment import init_env
 from reconcile.utils.runtime.integration import (
     ModuleArgsKwargsRunParams,
     ModuleBasedQontractReconcileIntegration,
-    NoParams,
     QontractReconcileIntegration,
 )
 from reconcile.utils.runtime.meta import IntegrationMeta
@@ -1958,11 +1957,14 @@ def ocm_machine_pools(ctx, thread_pool_size):
 @environ(["APP_INTERFACE_STATE_BUCKET", "APP_INTERFACE_STATE_BUCKET_ACCOUNT"])
 @click.pass_context
 def ocm_upgrade_scheduler(ctx):
-    import reconcile.aus.ocm_upgrade_scheduler
+    from reconcile.aus.base import AdvancedUpgradeSchedulerBaseIntegrationParams
+    from reconcile.aus.ocm_upgrade_scheduler import (
+        OCMClusterUpgradeSchedulerIntegration,
+    )
 
     run_class_integration(
-        integration=reconcile.aus.ocm_upgrade_scheduler.OCMClusterUpgradeSchedulerIntegration(
-            NoParams()
+        integration=OCMClusterUpgradeSchedulerIntegration(
+            AdvancedUpgradeSchedulerBaseIntegrationParams()
         ),
         ctx=ctx.obj,
     )
@@ -1972,11 +1974,16 @@ def ocm_upgrade_scheduler(ctx):
 @environ(["APP_INTERFACE_STATE_BUCKET", "APP_INTERFACE_STATE_BUCKET_ACCOUNT"])
 @click.pass_context
 def ocm_upgrade_scheduler_org(ctx):
-    import reconcile.aus.ocm_upgrade_scheduler_org
+    from reconcile.aus.base import AdvancedUpgradeSchedulerBaseIntegrationParams
+    from reconcile.aus.ocm_upgrade_scheduler_org import (
+        OCMClusterUpgradeSchedulerOrgIntegration,
+    )
 
     run_class_integration(
-        integration=reconcile.aus.ocm_upgrade_scheduler_org.OCMClusterUpgradeSchedulerOrgIntegration(
-            NoParams()
+        integration=OCMClusterUpgradeSchedulerOrgIntegration(
+            AdvancedUpgradeSchedulerBaseIntegrationParams(
+                # pass in env or org here for sharding
+            )
         ),
         ctx=ctx.obj,
     )
@@ -2001,11 +2008,32 @@ def ocm_upgrade_scheduler_org_updater(ctx, gitlab_project_id):
 @click.pass_context
 def ocm_addons_upgrade_scheduler_org(ctx):
 
-    import reconcile.aus.ocm_addons_upgrade_scheduler_org
+    from reconcile.aus.base import AdvancedUpgradeSchedulerBaseIntegrationParams
+    from reconcile.aus.ocm_addons_upgrade_scheduler_org import (
+        OCMAddonsUpgradeSchedulerOrgIntegration,
+    )
 
     run_class_integration(
-        integration=reconcile.aus.ocm_addons_upgrade_scheduler_org.OCMAddonsUpgradeSchedulerOrgIntegration(
-            NoParams()
+        integration=OCMAddonsUpgradeSchedulerOrgIntegration(
+            AdvancedUpgradeSchedulerBaseIntegrationParams()
+        ),
+        ctx=ctx.obj,
+    )
+
+
+@integration.command(
+    short_help="Manage Cluster Upgrade Policy schedules in OCM organizations based on OCM labels."
+)
+@environ(["APP_INTERFACE_STATE_BUCKET", "APP_INTERFACE_STATE_BUCKET_ACCOUNT"])
+@click.pass_context
+def aus_upgrade_scheduler_org(ctx):
+
+    from reconcile.aus.advanced_upgrade_service import AdvancedUpgradeServiceIntegration
+    from reconcile.aus.base import AdvancedUpgradeSchedulerBaseIntegrationParams
+
+    run_class_integration(
+        integration=AdvancedUpgradeServiceIntegration(
+            AdvancedUpgradeSchedulerBaseIntegrationParams()
         ),
         ctx=ctx.obj,
     )

reconcile/gql_definitions/common/ocm_environments.gql
@@ -1,0 +1,7 @@
+# qenerate: plugin=pydantic_v1
+
+query OCMEnvironments($name: String) {
+  environments: ocm_environments_v1(name: $name) {
+    ... OCMEnvironment
+  }
+}

commit 52d4fcb1e57a9312ac820cce48251e3bd5e1cf2d

fix ocm-addons-upgrade-policies report
fixes a bug introduced in https://github.com/app-sre/qontract-reconcile/pull/3377

Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

reconcile/aus/ocm_upgrade_scheduler.py
@@ -74,7 +74,7 @@ class OCMClusterUpgradeSchedulerIntegration(
                 cluster.ocm and cluster.ocm.name == org_name
             )
             if (
-                integration_is_enabled(QONTRACT_INTEGRATION, cluster)
+                integration_is_enabled(self.name, cluster)
                 and cluster.ocm
                 and cluster.upgrade_policy
                 and supported_product

tools/qontract_cli.py
@@ -697,43 +697,51 @@ def generate_fleet_upgrade_policices_report(
 @get.command()
 @click.pass_context
 def ocm_addon_upgrade_policies(ctx):
+
     import reconcile.aus.ocm_addons_upgrade_scheduler_org as oauso
+
+    integration = oauso.OCMAddonsUpgradeSchedulerOrgIntegration(
+        AdvancedUpgradeSchedulerBaseIntegrationParams()
+    )
 
     md_output = ctx.obj["options"]["output"] == "md"
     if not md_output:
         print("We only support md output for now")
         sys.exit(1)
 
-    upgrade_policies_per_org = oauso.organization_upgrade_spec()
+    upgrade_specs = integration.get_upgrade_specs()
 
     output = {}
-    for org_name, org_spec in upgrade_policies_per_org.items():
-        ocm_map, addon_states = oauso.get_state_for_org_spec_per_addon(
-            org_spec, fetch_current_state=False
-        )
-        ocm = ocm_map[org_name]
-        for addon_state in addon_states:
-            next_version = ocm.get_addon_version(addon_state.addon_id)
-            ocm_output = output.setdefault(org_name, [])
-            for d in addon_state.desired_state:
-                sector = ""
-                conditions = d.get("conditions") or {}
-                if conditions.get("sector"):
-                    sector = conditions["sector"].name
-                version = d["current_version"]
-                ocm_output.append(
-                    {
-                        "cluster": d["cluster"],
-                        "addon_id": addon_state.addon_id,
-                        "current_version": version,
-                        "schedule": d["schedule"],
-                        "sector": sector,
-                        "mutexes": ", ".join(conditions.get("mutexes") or []),
-                        "soak_days": conditions.get("soakDays"),
-                        "workloads": ", ".join(d["workloads"]),
-                        "next_version": next_version if next_version != version else "",
-                    }
-                )
+    for upgrade_policies_per_org in upgrade_specs.values():
+        for org_name, org_spec in upgrade_policies_per_org.items():
+            ocm_map, addon_states = oauso.get_state_for_org_spec_per_addon(
+                org_spec, fetch_current_state=False
+            )
+            ocm = ocm_map[org_name]
+            for addon_state in addon_states:
+                next_version = ocm.get_addon_version(addon_state.addon_id)
+                ocm_output = output.setdefault(org_name, [])
+                for d in addon_state.desired_state:
+                    sector = ""
+                    conditions = d.get("conditions") or {}
+                    if conditions.get("sector"):
+                        sector = conditions["sector"].name
+                    version = d["current_version"]
+                    ocm_output.append(
+                        {
+                            "cluster": d["cluster"],
+                            "addon_id": addon_state.addon_id,
+                            "current_version": version,
+                            "schedule": d["schedule"],
+                            "sector": sector,
+                            "mutexes": ", ".join(conditions.get("mutexes") or []),
+                            "soak_days": conditions.get("soakDays"),
+                            "workloads": ", ".join(d["workloads"]),
+                            "next_version": next_version
+                            if next_version != version
+                            else "",
+                        }
+                    )
     fields = [
         {"key": "cluster", "sortable": True},
         {"key": "addon_id", "sortable": True},
@@ -754,7 +762,8 @@ def ocm_addon_upgrade_policies(ctx):
     """
     ocm_org_specs = [
         org_upgrade_spec.org.dict(by_alias=True)
-        for org_upgrade_spec in upgrade_policies_per_org.values()
+        for org_upgrade_specs in upgrade_specs.values()
+        for org_upgrade_spec in org_upgrade_specs.values()
     ]
     for ocm_name in sorted(output.keys()):
         json_data = json.dumps(