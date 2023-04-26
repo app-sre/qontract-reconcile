fdcae6cfb21851a15fcb6eadb0ab777b8a1a6f99
Suzana Nesic <snesic@redhat.com>
2023-04-26 00:29:46 -0400

    revising vpc peerings validator class

--- reconcile/gql_definitions/vpc_peerings_validator/vpc_peerings_validator.py
+++ reconcile/gql_definitions/vpc_peerings_validator/vpc_peerings_validator.py
@@ -107,7 +107,7 @@ class AWSVPCV1(ConfiguredBaseModel):
     name: str = Field(..., alias="name")
 
 
-class ClusterPeeringConnectionAccountV1(ClusterPeeringConnectionV1):
+class ClusterPeeringConnectionAccountV1(ConfiguredBaseModel):
     vpc: AWSVPCV1 = Field(..., alias="vpc")
 
 
@@ -116,7 +116,7 @@ class AWSAccountV1(ConfiguredBaseModel):
     uid: str = Field(..., alias="uid")
 
 
-class ClusterPeeringConnectionAccountVPCMeshV1(ClusterPeeringConnectionV1):
+class ClusterPeeringConnectionAccountVPCMeshV1(ConfiguredBaseModel):
     name: str = Field(..., alias="name")
     account: AWSAccountV1 = Field(..., alias="account")
     tags: Optional[Json] = Field(..., alias="tags")
@@ -129,14 +129,14 @@ class ClusterPeeringConnectionClusterRequesterV1_ClusterV1_ClusterNetworkV1(
 
 
 class ClusterPeeringConnectionClusterRequesterV1_ClusterV1(
-    VpcPeeringsValidatorPeeredCluster
+    ConfiguredBaseModel
 ):
     network: Optional[
         ClusterPeeringConnectionClusterRequesterV1_ClusterV1_ClusterNetworkV1
     ] = Field(..., alias="network")
 
 
-class ClusterPeeringConnectionClusterRequesterV1(ClusterPeeringConnectionV1):
+class ClusterPeeringConnectionClusterRequesterV1(ConfiguredBaseModel):
     cluster: ClusterPeeringConnectionClusterRequesterV1_ClusterV1 = Field(
         ..., alias="cluster"
     )
@@ -149,14 +149,14 @@ class ClusterPeeringConnectionClusterAccepterV1_ClusterV1_ClusterNetworkV1(
 
 
 class ClusterPeeringConnectionClusterAccepterV1_ClusterV1(
-    VpcPeeringsValidatorPeeredCluster
+    ConfiguredBaseModel
 ):
     network: Optional[
         ClusterPeeringConnectionClusterAccepterV1_ClusterV1_ClusterNetworkV1
     ] = Field(..., alias="network")
 
 
-class ClusterPeeringConnectionClusterAccepterV1(ClusterPeeringConnectionV1):
+class ClusterPeeringConnectionClusterAccepterV1(ConfiguredBaseModel):
     cluster: ClusterPeeringConnectionClusterAccepterV1_ClusterV1 = Field(
         ..., alias="cluster"
     )
