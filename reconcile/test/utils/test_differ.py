28f5381201f0aeb78515a545592068b5e797e931
Di Wang <dwan@redhat.com>
2023-04-27 19:35:15 +1000

    Extend differ to support lists
    Signed-off-by: Di Wang <dwan@redhat.com>

--- reconcile/test/utils/test_differ.py
+++ reconcile/test/utils/test_differ.py
@@ -1,27 +1,157 @@
+from dataclasses import dataclass
+
 from reconcile.utils import differ
 
 
-def test_diff_with_default_equal():
+def test_diff_mappings_with_default_equal():
     current = {"a": 1, "b": 2, "c": 3}
     desired = {"a": 1, "b": 20, "d": 30}
 
-    result = differ.diff(current, desired)
+    result = differ.diff_mappings(current, desired)
 
-    assert result == differ.DiffResult(
+    assert result == differ.DiffKeyedResult(
         add={"d": 30},
         delete={"c": 3},
         change={"b": (2, 20)},
     )
 
 
-def test_diff_with_custom_equal():
+def test_diff_mappings_with_custom_equal():
     current = {"a": 1, "b": 2, "c": 3}
     desired = {"a": [1], "b": [20], "d": [30]}
 
-    result = differ.diff(current, desired, equal=lambda x, y: x == y[0])
+    result = differ.diff_mappings(current, desired, equal=lambda c, d: c == d[0])
 
-    assert result == differ.DiffResult(
+    assert result == differ.DiffKeyedResult(
         add={"d": [30]},
         delete={"c": 3},
         change={"b": (2, [20])},
     )
+
+
+def test_diff_by_key_with_default_equal():
+    current = [
+        {"name": "a", "value": 1},
+        {"name": "b", "value": 2},
+        {"name": "c", "value": 3},
+    ]
+
+    desired = [
+        {"name": "a", "value": 1},
+        {"name": "b", "value": 20},
+        {"name": "d", "value": 30},
+    ]
+
+    result = differ.diff_by_key(
+        current,
+        desired,
+        current_key=lambda c: c["name"],
+        desired_key=lambda d: d["name"],
+    )
+
+    assert result == differ.DiffKeyedResult(
+        add={
+            "d": {"name": "d", "value": 30},
+        },
+        delete={
+            "c": {"name": "c", "value": 3},
+        },
+        change={
+            "b": (
+                {"name": "b", "value": 2},
+                {"name": "b", "value": 20},
+            )
+        },
+    )
+
+
+def test_diff_by_key_with_custom_equal():
+    current = [
+        {"name": "a", "value": 1},
+        {"name": "b", "value": 2},
+        {"name": "c", "value": 3},
+    ]
+
+    desired = [
+        {"name": "a", "value": [1]},
+        {"name": "b", "value": [20]},
+        {"name": "d", "value": [30]},
+    ]
+
+    result = differ.diff_by_key(
+        current,
+        desired,
+        current_key=lambda c: c["name"],
+        desired_key=lambda d: d["name"],
+        equal=lambda c, d: c["value"] == d["value"][0],
+    )
+
+    assert result == differ.DiffKeyedResult(
+        add={
+            "d": {"name": "d", "value": [30]},
+        },
+        delete={
+            "c": {"name": "c", "value": 3},
+        },
+        change={
+            "b": (
+                {"name": "b", "value": 2},
+                {"name": "b", "value": [20]},
+            )
+        },
+    )
+
+
+def test_diff_lists_with_scalar_types():
+    current = ["a", "b"]
+    desired = ["a", "c"]
+
+    result = differ.diff_lists(current, desired)
+
+    assert result == differ.DiffListsResult(
+        add=["c"],
+        delete=["b"],
+        identical=[("a", "a")],
+    )
+
+
+@dataclass
+class Foo:
+    name: str
+    value: int
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Foo):
+            return False
+
+        return self.name == other.name
+
+    def __hash__(self) -> int:
+        return hash(self.name)
+
+
+def test_diff_lists_with_custom_types():
+    current = [
+        Foo(name="a", value=1),
+        Foo(name="b", value=2),
+    ]
+    desired = [
+        Foo(name="a", value=10),
+        Foo(name="c", value=30),
+    ]
+
+    result = differ.diff_lists(current, desired)
+
+    assert len(result.add) == 1
+    assert result.add[0].name == "c"
+    assert result.add[0].value == 30
+
+    assert len(result.delete) == 1
+    assert result.delete[0].name == "b"
+    assert result.delete[0].value == 2
+
+    assert len(result.identical) == 1
+    assert result.identical[0][0].name == "a"
+    assert result.identical[0][0].value == 1
+    assert result.identical[0][1].name == "a"
+    assert result.identical[0][1].value == 10
--- reconcile/utils/differ.py
+++ reconcile/utils/differ.py
@@ -1,6 +1,8 @@
 from collections.abc import (
     Callable,
+    Iterable,
     Mapping,
+    Sequence,
 )
 from dataclasses import dataclass
 from typing import (
@@ -15,19 +17,26 @@ K = TypeVar("K")
 
 
 @dataclass(frozen=True, eq=True)
-class DiffResult(Generic[C, D, K]):
+class DiffKeyedResult(Generic[C, D, K]):
     add: dict[K, D]
     delete: dict[K, C]
     change: dict[K, tuple[C, D]]
 
 
-def diff(
+@dataclass(frozen=True, eq=True)
+class DiffListsResult(Generic[C, D]):
+    add: list[D]
+    delete: list[C]
+    identical: list[tuple[C, D]]
+
+
+def diff_mappings(
     current: Mapping[K, C],
     desired: Mapping[K, D],
     equal: Optional[Callable[[C, D], bool]] = None,
-) -> DiffResult[C, D, K]:
+) -> DiffKeyedResult[C, D, K]:
     """
-    Compare two mappings and return a `DiffResult` instance containing the differences between them.
+    Compare two mappings and return a `DiffKeyedResult` instance containing the differences between them.
 
     :param current: The current mapping to compare.
     :type current: Mapping[K, C]
@@ -38,10 +47,16 @@ def diff(
     :type equal: Optional[Callable[[C, D], bool]]
     :return: A `DiffResult` instance containing the differences between the `current` and `desired` mappings,
         including elements that were added, deleted, or changed.
-    :rtype: DiffResult[C, D, K]
+    :rtype: DiffKeyedResult[C, D, K]
     :raises: None
+
+    Example:
+        >>> current = {"a": 1, "b": 2, "c": 3}
+        >>> desired = {"a": 1, "b": 20, "d": 30}
+        >>> result = diff_mappings(current, desired, equal=lambda c, d: c == d)
+        DiffKeyedResult(add={'d': 30}, delete={'c': 3}, change={'b': (2, 20)})
     """
-    eq = equal or (lambda x, y: x == y)
+    eq = equal or (lambda c, d: c == d)
     add = {k: desired[k] for k in desired.keys() - current.keys()}
     delete = {k: current[k] for k in current.keys() - desired.keys()}
     change = {
@@ -49,4 +64,96 @@ def diff(
         for k in current.keys() & desired.keys()
         if not eq(current[k], desired[k])
     }
-    return DiffResult(add=add, delete=delete, change=change)
+    return DiffKeyedResult(add=add, delete=delete, change=change)
+
+
+def diff_by_key(
+    current: Iterable[C],
+    desired: Iterable[D],
+    current_key: Callable[[C], K],
+    desired_key: Callable[[D], K],
+    equal: Optional[Callable[[C, D], bool]] = None,
+) -> DiffKeyedResult[C, D, K]:
+    """
+    Compare two iterables and return a `DiffKeyedResult` instance containing the differences between them.
+
+    :param current: The current iterable to compare.
+    :type current: Iterable[C]
+    :param desired: The desired iterable to compare.
+    :type desired: Iterable[D]
+    :param current_key: A function that returns the key for an element in the `current` iterable.
+    :type current_key: Callable[[C], K]
+    :param desired_key: A function that returns the key for an element in the `desired` iterable.
+    :type desired_key: Callable[[D], K]
+    :param equal: An optional function that compares two elements in the iterables and returns True if they are equal.
+        If not provided, the default behavior is to use the `==` operator.
+    :type equal: Optional[Callable[[C, D], bool]]
+    :return: A `DiffKeyedResult` instance containing the differences between the `current` and `desired` mappings,
+        including elements that were added, deleted, or changed.
+    :rtype: DiffKeyedResult[C, D, K]
+    :raises: None
+
+    Example:
+    >>> current = [
+    ...     {"name": "a", "value": 1},
+    ...     {"name": "b", "value": 2},
+    ...     {"name": "c", "value": 3},
+    ... ]
+    >>> desired = [
+    ...     {"name": "a", "value": 1},
+    ...     {"name": "b", "value": 20},
+    ...     {"name": "d", "value": 30},
+    ... ]
+    >>> result = diff_by_key(
+    ...     current,
+    ...     desired,
+    ...     lambda c: c["name"],
+    ...     lambda d: d["name"],
+    ...     equal=lambda c, d: c["value"] == d["value"],
+    ... )
+    DiffKeyedResult(
+        add={'d': {'name': 'd', 'value': 30}},
+        delete={'c': {'name': 'c', 'value': 3}},
+        change={'b': ({'name': 'b', 'value': 2}, {'name': 'b', 'value': 20}))
+    )
+    """
+    current_dict = {current_key(x): x for x in current}
+    desired_dict = {desired_key(x): x for x in desired}
+    return diff_mappings(current_dict, desired_dict, equal=equal)
+
+
+def diff_lists(
+    current: Sequence[C],
+    desired: Sequence[D],
+) -> DiffListsResult[C, D]:
+    """
+    Compare two iterables and return a `DiffListsResult` instance containing the differences between them.
+
+    :param current: The current sequence to compare.
+    :type current: Sequence[C]
+    :param desired: The desired sequence to compare.
+    :type desired: Sequence[D]
+    :return: A `DiffListsResult` instance containing the differences between the `current` and `desired` mappings,
+        including elements that were added ,deleted or identical.
+    :rtype: DiffListsResult[C, D]
+    :raises: None
+
+    Example:
+    >>> current = [1, 2]
+    >>> desired = [1, 3]
+    >>> result = diff_lists(current, desired)
+    DiffListsResult(add=[3], delete=[2], identical=[(1, 1)])
+    """
+    current_with_index = {x: idx for idx, x in enumerate(current)}
+    desired_with_index = {x: idx for idx, x in enumerate(desired)}
+    add = list(desired_with_index.keys() - current_with_index.keys())
+    delete = list(current_with_index.keys() - desired_with_index.keys())
+    identical = [
+        (current[current_with_index[k]], desired[desired_with_index[k]])
+        for k in current_with_index.keys() & desired_with_index.keys()
+    ]
+    return DiffListsResult(
+        add=add,
+        delete=delete,
+        identical=identical,
+    )
