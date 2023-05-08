e14a75652a3fbdc8a28f918364d7ac2d27108faa
Jan-Hendrik Boll <jboll@redhat.com>
2023-05-08 10:01:16 +0200

    Improve runtime of aws-iam-password-reset (#3488)
    * Improve runtime of aws-iam-password-reset
    
    Remove saas files from roles query
    Initialize AWS client only if it is required
    
    * Introduce Dataclass for storing accounts to update
    
    * Review remarks, initialize AWSApi only once

--- reconcile/aws_iam_password_reset.py
+++ reconcile/aws_iam_password_reset.py
@@ -9,6 +9,8 @@ from typing import (
     Optional,
 )
 
+from pydantic import BaseModel
+
 from reconcile import queries
 from reconcile.utils.aws_api import AWSApi
 from reconcile.utils.defer import defer
@@ -34,37 +36,74 @@ def account_in_roles(roles: Iterable[Mapping[str, Any]], aws_account: str) -> bo
     return False
 
 
+class AwsProfileToReset(BaseModel):
+    user_name: str
+    state_key: str
+
+
+class AwsAccountWithResets(BaseModel):
+    account: Mapping[str, Any]
+    resetPasswords: list[AwsProfileToReset]
+
+
 @defer
 def run(dry_run, defer=None):
     accounts = queries.get_aws_accounts(reset_passwords=True)
     settings = queries.get_app_interface_settings()
-    roles = queries.get_roles(aws=True)
+    roles = queries.get_roles(aws=True, saas_files=False)
     state = init_state(integration=QONTRACT_INTEGRATION)
     defer(state.cleanup)
+
+    accounts_to_reset: list[AwsAccountWithResets] = []
 
     for a in accounts:
         account_name = a["name"]
         reset_passwords = a.get("resetPasswords")
         if not reset_passwords:
             continue
-        with AWSApi(1, [a], settings=settings) as aws_api:
-            for r in reset_passwords:
-                user_name = r["user"]["org_username"]
-                request_id = r["requestId"]
-                state_key = f"{account_name}/{user_name}/{request_id}"
-                if state.exists(state_key):
-                    continue
 
-                role = get_roles(roles, user_name)
-                if not role:
-                    logging.error(f"Expected a role to be found with name {user_name}")
-                    sys.exit(1)
+        account_reset = AwsAccountWithResets(
+            account=a,
+            resetPasswords=[],
+        )
+        accounts_to_reset.append(account_reset)
 
-                if not account_in_roles(role["roles"], account_name):
-                    logging.error(f"User {user_name} is not in account {account_name}")
-                    sys.exit(1)
+        for r in reset_passwords:
+            user_name = r["user"]["org_username"]
+            request_id = r["requestId"]
+            state_key = f"{account_name}/{user_name}/{request_id}"
+            if state.exists(state_key):
+                continue
+
+            role = get_roles(roles, user_name)
+            if not role:
+                logging.error(f"Expected a role to be found with name {user_name}")
+                sys.exit(1)
+
+            if not account_in_roles(role["roles"], account_name):
+                logging.error(f"User {user_name} is not in account {account_name}")
+                sys.exit(1)
+
+            account_reset.resetPasswords.append(
+                AwsProfileToReset(
+                    user_name=user_name,
+                    state_key=state_key,
+                )
+            )
+
+    for a in accounts_to_reset:
+        if not a.resetPasswords:
+            continue
+
+        with AWSApi(1, [a.account], settings=settings) as aws_api:
+            for r in a.resetPasswords:
+                user_name = r.user_name
+                state_key = r.state_key
+
+                account_name = a.account["name"]
 
                 logging.info(["reset_password", account_name, user_name])
+
                 if dry_run:
                     continue
 
