4724477568964ca1a4efaff10f9ac5f53f61b100
Maor Friedman <mafriedm@redhat.com>
2023-05-10 21:57:07 +0300

    test example

--- reconcile/utils/aws_api.py
+++ reconcile/utils/aws_api.py
@@ -169,8 +169,8 @@ class AWSApi:  # pylint: disable=too-many-public-methods
             secret_key = secret["aws_secret_access_key"]
             region_name = account["resourcesDefaultRegion"]
             session = Session(
-                aws_access_key_id="AKIAZAP3LFING6SRUXHM",
-                aws_secret_access_key="mak1ngth1sup+thi5i5fak3",
+                aws_access_key_id=" AKIAIOSFODNN7EXAMPLE",
+                aws_secret_access_key="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
                 region_name=region_name,
             )
             self.sessions[account_name] = session
