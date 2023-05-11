eab05b410677b186be0b278324515ab4f2b2fb93
Jan-Hendrik Boll <jboll@redhat.com>
2023-05-11 11:00:14 +0200

    Revert "Add Control Plane upgrade (#3494)" (#3499)
    This reverts commit ff8d6ea8cd6a93e148a892f4d900e67ae0c9854a.

--- reconcile/openshift_upgrade_watcher.py
+++ reconcile/openshift_upgrade_watcher.py
@@ -127,7 +127,9 @@ def run(
     if defer:
         defer(state.cleanup)
 
-    clusters = [c for c in get_clusters() if c.ocm]
+    clusters = [
+        c for c in get_clusters() if c.ocm and not (c.spec and c.spec.hypershift)
+    ]
 
     slack: Optional[SlackApi] = None
     if not dry_run:
