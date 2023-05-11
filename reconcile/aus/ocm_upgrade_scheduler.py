ceab063e5d74e73d1f3e8ebd6ae595243af9e074
Jan-Hendrik Boll <jboll@redhat.com>
2023-05-11 11:00:14 +0200

    Revert "Add Control Plane upgrade (#3494)" (#3499)
    This reverts commit ff8d6ea8cd6a93e148a892f4d900e67ae0c9854a.

--- reconcile/aus/ocm_upgrade_scheduler.py
+++ reconcile/aus/ocm_upgrade_scheduler.py
@@ -15,12 +15,11 @@ from reconcile.utils import gql
 from reconcile.utils.disabled_integrations import integration_is_enabled
 from reconcile.utils.ocm import (
     OCM_PRODUCT_OSD,
-    OCM_PRODUCT_ROSA,
     OCMMap,
 )
 
 QONTRACT_INTEGRATION = "ocm-upgrade-scheduler"
-SUPPORTED_OCM_PRODUCTS = [OCM_PRODUCT_OSD, OCM_PRODUCT_ROSA]
+SUPPORTED_OCM_PRODUCTS = [OCM_PRODUCT_OSD]
 
 
 class OCMClusterUpgradeSchedulerIntegration(
