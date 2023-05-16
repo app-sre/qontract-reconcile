8d3b0c5d8bae0bc7f8943d09c77c6cac5eaedca9
Christian Assing <cassing@redhat.com>
2023-05-16 09:17:30 +0200

    bulletproof dict lookup

--- reconcile/gitlab_owners.py
+++ reconcile/gitlab_owners.py
@@ -325,7 +325,8 @@ def act(repo, dry_run, instance, settings, defer=None):
             merge_request=mr,
             owners=project_owners,
             dry_run=dry_run,
-            persistent_lgtm=repo["gitlabRepoOwners"]["persistentLgtm"] or False,
+            persistent_lgtm=repo.get("gitlabRepoOwners", {}).get("persistentLgtm", None)
+            or False,
         )
 
         if mr_approval.top_commit_created_at is None:
