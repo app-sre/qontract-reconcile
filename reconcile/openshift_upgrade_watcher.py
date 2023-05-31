4371bbdc732dfe02bb8fc7a0164fc34b78f2906a
Jan-Hendrik Boll <jboll@redhat.com>
2023-05-31 12:38:55 +0200

    Quick fix, disable upgrade watcher for hypershift clusters (#3540)
    * Quick fix, disable upgrade watcher for hypershift clusters
    
    * Make type check happy

--- reconcile/openshift_upgrade_watcher.py
+++ reconcile/openshift_upgrade_watcher.py
@@ -127,7 +127,7 @@ def run(
     if defer:
         defer(state.cleanup)
 
-    clusters = [c for c in get_clusters() if c.ocm]
+    clusters = [c for c in get_clusters() if c.ocm and c.spec and not c.spec.hypershift]
 
     slack: Optional[SlackApi] = None
     if not dry_run:
