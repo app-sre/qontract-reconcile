9fd405f23c4fbc2eac4e2cd8ab5c689ef73b8d2e
Rafael Porres Molina <rporresm@redhat.com>
2023-06-06 10:53:10 +0200

    Check typing in dockerfiles/hack and release dirs (#3554)
    and fix trivial mypy errors.
    
    Signed-off-by: Rafa Porres Molina <rporresm@redhat.com>

--- release/test_version.py
+++ release/test_version.py
@@ -4,12 +4,12 @@ from reconcile.utils.semver_helper import parse_semver
 from release import version
 
 
-def test_version_semver():
+def test_version_semver() -> None:
     assert version.semver("0.4.0") == "0.4.0"
     assert version.semver("0.4.0-6-gaaaaaaa") == "0.4.1-6+aaaaaaa"
 
 
-def test_semver_ordering():
+def test_semver_ordering() -> None:
     # 6 commits after 0.4.0
     assert parse_semver("0.4.1-6+aaaaaaa") > parse_semver("0.4.0")
     # prerelease version are lower than release ones
@@ -22,13 +22,13 @@ def test_semver_ordering():
     assert parse_semver("0.4.1-50+aaaaaaa") > parse_semver("0.4.1-6+aaaaaaa")
 
 
-def test_version_pip():
+def test_version_pip() -> None:
     assert version.pip("0.4.0") == "0.4.0"
     assert version.pip("0.4.0-6-gaaaaaaa") == "0.4.1.pre6"
     # assert version.pip("0.4.0-6-gaaaaaaa") == "0.4.0.post6"
 
 
-def test_pep440_ordering():
+def test_pep440_ordering() -> None:
     assert pep440.Version("0.4.1") > pep440.Version("0.4.0")
 
     # using pre-releases
@@ -44,7 +44,7 @@ def test_pep440_ordering():
     assert pep440.Version("0.4.0.post50") > pep440.Version("0.4.0.post6")
 
 
-def test_version_docker():
+def test_version_docker() -> None:
     assert version.docker("0.4.0") == "0.4.0"
     assert version.docker("0.4.0-6-gaaaaaaa") == "0.4.1.pre6"
     # assert version.docker("0.4.0-6-gaaaaaaa") == "0.4.0.post6"
