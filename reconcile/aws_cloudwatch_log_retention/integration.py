2ae4f822aa37573a27434c5b942fe787afc742b6
Suzana Nesic <snesic@redhat.com>
2023-06-28 01:27:38 -0400

    catching any KeyErrors from log groups that does not have retention days already set

--- reconcile/aws_cloudwatch_log_retention/integration.py
+++ reconcile/aws_cloudwatch_log_retention/integration.py
@@ -1,3 +1,4 @@
+from cmath import log
 from collections.abc import Callable
 import logging
 import re
@@ -39,35 +40,32 @@ def get_app_interface_cloudwatch_retention_period() -> list:
     return results
 
 
-# def parse_log_retention_date(retention_period: str) -> int:
-#     if retention_period[-1] == "d":
-#         return int(retention_period[:-1])
-#     raise ValueError(
-#         "Invalid retention period format. Expected format is <numeric value>d"
-#     )
-
-
 def run(dry_run: bool, thread_pool_size: int, defer: Optional[Callable] = None) -> None:
     cloudwatch_cleanup_list = get_app_interface_cloudwatch_retention_period()
-    logging.debug()
     for cloudwatch_cleanup_entry in cloudwatch_cleanup_list:
         settings = queries.get_secret_reader_settings()
         accounts = queries.get_aws_accounts(uid=cloudwatch_cleanup_entry.acct_uid)
         awsapi = AWSApi(1, accounts, settings=settings, init_users=False)
-        # transformed_retention_day_length = parse_log_retention_date(
-        #     cloudwatch_cleanup_entry.log_retention_day_length
-        # )
         log_groups = awsapi.get_cloudwatch_logs(accounts[0])
         regex_pattern = re.compile(cloudwatch_cleanup_entry.log_regex)
         for log_group in log_groups:
             group_name = log_group["logGroupName"]
-            retention_days = log_group["retentionInDays"]
-            if regex_pattern.match(group_name) and retention_days != cloudwatch_cleanup_entry.log_retention_day_length:
-                logging.info(f" Setting {group_name} retention days to {cloudwatch_cleanup_entry.log_retention_day_length}")
+            # retention_days = log_group["retentionInDays"]
+            retention_days = log_group.get('retentionInDays')
+            if retention_days is not None:
+                if regex_pattern.match(group_name) and retention_days != cloudwatch_cleanup_entry.log_retention_day_length:
+                    logging.info(f" Setting {group_name} retention days to {cloudwatch_cleanup_entry.log_retention_day_length}")
+                    if not dry_run:
+                        logging.info("we are in here")
+                        awsapi.set_cloudwatch_log_retention(group_name, )
+                        # cloudwatch_logs.put_retention_policy(logGroupName=group_name, retentionInDays=retention_days)
+            else:
+                logging.info(f"log group {log_group} retentionInDays not specified")
                 if not dry_run:
-                    logging.info("we are in here")
-                    awsapi.set_cloudwatch_log_retention(group_name, )
-                    # cloudwatch_logs.put_retention_policy(logGroupName=group_name, retentionInDays=retention_days)
+                        logging.info("we are in here")
+                        awsapi.set_cloudwatch_log_retention(group_name, )
+                        # cloudwatch_logs.put_retention_policy(logGroupName=group_name, retentionInDays=retention_days)
+
 
 
         # awsapi.set_cloudwatch_log_retention(
