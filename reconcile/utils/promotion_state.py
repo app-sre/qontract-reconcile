541c993340a88cb84b8fd237a18c301c9f19dc87
Karl Fischer <kfischer@redhat.com>
2023-07-06 14:08:35 +0200

    APPSRE-7307 promotion state target_uid

--- reconcile/utils/promotion_state.py
+++ reconcile/utils/promotion_state.py
@@ -39,6 +39,9 @@ class PromotionState:
         self._state = state
         self._commits_by_channel: dict[str, set[str]] = defaultdict(set)
 
+    def _target_key(self, channel: str, target_uid: str) -> str:
+        return f"{channel}/{target_uid}"
+
     def cache_commit_shas_from_s3(self) -> None:
         """
         Caching commit shas locally - this is used
@@ -47,24 +50,56 @@ class PromotionState:
         """
         all_keys = self._state.ls()
         for commit in all_keys:
+            # BACKWARDS-COMPAT BLOCK
             # Format: /promotions/{channel}/{commit-sha}
-            if not commit.startswith("/promotions/"):
+            if commit.startswith("/promotions/"):
+                # Keep this for backwards-compatibility with v1 promotions
+                # We wait a couple of months to be sure all pipelines have
+                # been triggered since.
+                # This can be deleted after 30.09.2023
+                _, _, channel_name, commit_sha = commit.split("/")
+                self._commits_by_channel[channel_name].add(commit_sha)
+            # / BACKWARDS-COMPAT BLOCK
+
+            # Format: /promotions_v2/{channel}/{publisher-target-uid}/{commit-sha}
+            if not commit.startswith("/promotions_v2/"):
                 continue
-            _, _, channel_name, commit_sha = commit.split("/")
-            self._commits_by_channel[channel_name].add(commit_sha)
+            _, _, channel_name, publisher_uid, commit_sha = commit.split("/")
+            key = self._target_key(channel=channel_name, target_uid=publisher_uid)
+            self._commits_by_channel[key].add(commit_sha)
 
     def get_promotion_data(
-        self, sha: str, channel: str, local_lookup: bool = True
+        self, sha: str, channel: str, target_uid: str = "", local_lookup: bool = True
     ) -> Optional[PromotionData]:
-        if local_lookup and sha not in self._commits_by_channel[channel]:
+        cache_key_v1 = channel
+        cache_key_v2 = self._target_key(channel=channel, target_uid=target_uid)
+        if (
+            local_lookup
+            and sha not in self._commits_by_channel[cache_key_v1]
+            and sha not in self._commits_by_channel[cache_key_v2]
+        ):
             # Lets reduce unecessary calls to S3
             return None
-        key = f"promotions/{channel}/{sha}"
+
+        # BACKWARDS-COMPAT BLOCK
+        # Keep for backwards-compatibility with v1 promotions
+        # We wait a couple of months to be sure all pipelines have
+        # been triggered since.
+        # This can be deleted after 30.09.2023
+        path_v1 = f"promotions/{channel}/{sha}"
         try:
-            data = self._state.get(key)
+            data = self._state.get(path_v1)
+            return PromotionData(**data)
+        except KeyError:
+            pass
+        # / BACKWARDS-COMPAT BLOCK
+
+        path_v2 = f"promotions_v2/{channel}/{target_uid}/{sha}"
+        try:
+            data = self._state.get(path_v2)
+            return PromotionData(**data)
         except KeyError:
             return None
-        return PromotionData(**data)
 
     def publish_promotion_data(
         self, sha: str, channel: str, target_uid: str, data: PromotionData
