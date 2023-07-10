6e01e3463012e809d91aed92b3d6387abde4483a
Karl Fischer <fishi0x01@users.noreply.github.com>
2023-07-10 14:25:58 +0200

    typed queries for jira-permissions-validator (#3657)

--- reconcile/jira_permissions_validator.py
+++ reconcile/jira_permissions_validator.py
@@ -1,18 +1,36 @@
 import logging
 import sys
 
-from reconcile import queries
+from reconcile.gql_definitions.jira_permissions_validator.jira_boards_for_permissions_validator import (
+    query as query_jira_boards,
+)
 from reconcile.status import ExitCodes
+from reconcile.typed_queries.app_interface_vault_settings import (
+    get_app_interface_vault_settings,
+)
+from reconcile.typed_queries.jira_settings import get_jira_settings
+from reconcile.utils import gql
 from reconcile.utils.jira_client import JiraClient
+from reconcile.utils.secret_reader import create_secret_reader
 
 QONTRACT_INTEGRATION = "jira-permissions-validator"
 
 
 def run(dry_run: bool) -> None:
-    settings = queries.get_app_interface_settings()
+    gql_api = gql.get_api()
+    settings = get_jira_settings(gql_api=gql_api)
+    vault_settings = get_app_interface_vault_settings()
+    secret_reader = create_secret_reader(use_vault=vault_settings.vault)
+    jira_boards = query_jira_boards(query_func=gql_api.query).jira_boards or []
     error = False
-    for jira_board in queries.get_jira_boards(with_slack=False):
-        jira = JiraClient(jira_board, settings=settings)
+    for jira_board in jira_boards:
+        token = secret_reader.read_secret(jira_board.server.token)
+        jira = JiraClient.create(
+            project_name=jira_board.name,
+            token=token,
+            server_url=jira_board.server.server_url,
+            jira_watcher_settings=settings.jira_watcher,
+        )
         if not jira.can_create_issues():
             error = True
             logging.error(f"can not create issues in project {jira.project}")
