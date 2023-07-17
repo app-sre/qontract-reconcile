c38cf4fb9794921ad5717bd9cf1ec833c0a861b1
Karl Fischer <kfischer@redhat.com>
2023-07-17 10:50:02 +0200

    Revert "typed jira_watcher (#3681)"
    This reverts commit 0b039f77397efc374577ca4fcb488dfbfc984e4c.

--- reconcile/jira_watcher.py
+++ reconcile/jira_watcher.py
@@ -1,34 +1,10 @@
 import logging
-from collections.abc import (
-    Callable,
-    Iterable,
-    Mapping,
-)
-from typing import Optional
 
-from reconcile.gql_definitions.common.jira_settings import AppInterfaceSettingsV1
-from reconcile.gql_definitions.jira_watcher.jira_watcher_boards import (
-    JiraBoardV1,
-    SlackOutputV1,
-)
-from reconcile.gql_definitions.jira_watcher.jira_watcher_boards import (
-    query as query_jira_boards,
-)
-from reconcile.slack_base import (
-    SlackApi,
-    slackapi_from_slack_workspace,
-)
-from reconcile.typed_queries.app_interface_vault_settings import (
-    get_app_interface_vault_settings,
-)
-from reconcile.typed_queries.jira_settings import get_jira_settings
-from reconcile.utils import gql
+from reconcile import queries
+from reconcile.slack_base import slackapi_from_slack_workspace
 from reconcile.utils.defer import defer
 from reconcile.utils.jira_client import JiraClient
-from reconcile.utils.secret_reader import (
-    SecretReaderBase,
-    create_secret_reader,
-)
+from reconcile.utils.secret_reader import SecretReader
 from reconcile.utils.sharding import is_in_shard_round_robin
 from reconcile.utils.state import (
     State,
@@ -38,18 +14,8 @@ from reconcile.utils.state import (
 QONTRACT_INTEGRATION = "jira-watcher"
 
 
-def fetch_current_state(
-    jira_board: JiraBoardV1,
-    settings: AppInterfaceSettingsV1,
-    secret_reader: SecretReaderBase,
-) -> tuple[JiraClient, dict[str, dict]]:
-    token = secret_reader.read_secret(jira_board.server.token)
-    jira = JiraClient.create(
-        project_name=jira_board.name,
-        token=token,
-        server_url=jira_board.server.server_url,
-        jira_watcher_settings=settings.jira_watcher,
-    )
+def fetch_current_state(jira_board, settings):
+    jira = JiraClient(jira_board, settings=settings)
     issues = jira.get_issues(fields=["key", "status", "summary"])
     return jira, {
         issue.key: {"status": issue.fields.status.name, "summary": issue.fields.summary}
@@ -57,18 +23,11 @@ def fetch_current_state(
     }
 
 
-def fetch_previous_state(state: State, project: str) -> dict[str, dict]:
+def fetch_previous_state(state, project):
     return state.get(project, {})
 
 
-def format_message(
-    server: str,
-    key: str,
-    data: Mapping,
-    event: str,
-    previous_state: Optional[Mapping] = None,
-    current_state: Optional[Mapping] = None,
-) -> str:
+def format_message(server, key, data, event, previous_state=None, current_state=None):
     summary = data["summary"]
     info = (
         ": {} -> {}".format(previous_state["status"], current_state["status"])
@@ -79,12 +38,8 @@ def format_message(
     return "{} ({}) {}{}".format(url, summary, event, info)
 
 
-def calculate_diff(
-    server: str,
-    current_state: Mapping[str, Mapping],
-    previous_state: Mapping[str, Mapping],
-) -> list[str]:
-    messages: list[str] = []
+def calculate_diff(server, current_state, previous_state):
+    messages = []
     new_issues = [
         format_message(server, key, data, "created")
         for key, data in current_state.items()
@@ -111,74 +66,52 @@ def calculate_diff(
     return messages
 
 
-def init_slack(slack: SlackOutputV1, secret_reader: SecretReaderBase) -> SlackApi:
+def init_slack(jira_board):
+    secret_reader = SecretReader(queries.get_secret_reader_settings())
+    slack_info = jira_board["slack"]
+
     return slackapi_from_slack_workspace(
-        slack.dict(by_alias=True),
+        slack_info,
         secret_reader,
         QONTRACT_INTEGRATION,
-        channel=slack.channel,
+        channel=slack_info.get("channel"),
         init_usergroups=False,
     )
 
 
-def act(
-    dry_run: bool,
-    slack: SlackOutputV1,
-    diffs: Iterable[str],
-    secret_reader: SecretReaderBase,
-) -> None:
-    slack_api: Optional[SlackApi] = None
+def act(dry_run, jira_board, diffs):
     if not dry_run and diffs:
-        slack_api = init_slack(slack=slack, secret_reader=secret_reader)
+        slack = init_slack(jira_board)
 
-    for diff in reversed(list(diffs)):
+    for diff in reversed(diffs):
         logging.info(diff)
         if not dry_run:
-            if not slack_api:
-                raise RuntimeError("Slack API not initialized")
-            slack_api.chat_post_message(diff)
+            slack.chat_post_message(diff)
 
 
-def write_state(
-    state: State, project: str, state_to_write: Mapping[str, Mapping]
-) -> None:
+def write_state(state: State, project, state_to_write):
     state.add(project, value=state_to_write, force=True)
 
 
 @defer
-def run(dry_run: bool, defer: Optional[Callable]) -> None:
-    gql_api = gql.get_api()
-    vault_settings = get_app_interface_vault_settings()
-    secret_reader = create_secret_reader(use_vault=vault_settings.vault)
-    jira_boards = query_jira_boards(query_func=gql_api.query).jira_boards or []
-    settings = get_jira_settings(gql_api=gql_api)
+def run(dry_run, defer):
+    jira_boards = [j for j in queries.get_jira_boards() if j.get("slack")]
+    settings = queries.get_app_interface_settings()
     state = init_state(integration=QONTRACT_INTEGRATION)
-    if defer:
-        defer(state.cleanup)
+    defer(state.cleanup)
     for index, jira_board in enumerate(jira_boards):
-        if not jira_board.slack:
+        if not is_in_shard_round_robin(jira_board["name"], index):
             continue
-        if not is_in_shard_round_robin(jira_board.name, index):
-            continue
-        jira, current_state = fetch_current_state(
-            jira_board=jira_board, settings=settings, secret_reader=secret_reader
-        )
+        jira, current_state = fetch_current_state(jira_board, settings)
         if not current_state:
             logging.warning(
                 "not acting on empty Jira boards. "
                 + "please create a ticket to get started."
             )
             continue
-        previous_state = fetch_previous_state(state=state, project=jira.project)
+        previous_state = fetch_previous_state(state, jira.project)
         if previous_state:
-            diffs = calculate_diff(
-                jira_board.server.server_url, current_state, previous_state
-            )
-            act(
-                dry_run=dry_run,
-                slack=jira_board.slack,
-                diffs=diffs,
-                secret_reader=secret_reader,
-            )
+            diffs = calculate_diff(jira.server, current_state, previous_state)
+            act(dry_run, jira_board, diffs)
         if not dry_run:
             write_state(state, jira.project, current_state)
