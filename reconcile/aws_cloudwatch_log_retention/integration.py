e7fe2076e0038df8c58a5e9762d23b77ca705e8c
Suzana Nesic <snesic@redhat.com>
2023-08-01 20:29:58 -0400

    black format

--- reconcile/aws_cloudwatch_log_retention/integration.py
+++ reconcile/aws_cloudwatch_log_retention/integration.py
@@ -98,7 +98,10 @@ def run(dry_run: bool, thread_pool_size: int, defer: Optional[Callable] = None) 
                                 awsapi.create_cloudwatch_tag(
                                     aws_acct, group_name, MANAGED_TAG
                                 )
-                    if retention_days != cloudwatch_cleanup_entry.log_retention_day_length:
+                    if (
+                        retention_days
+                        != cloudwatch_cleanup_entry.log_retention_day_length
+                    ):
                         logging.info(
                             f" Setting {group_name} retention days to {cloudwatch_cleanup_entry.log_retention_day_length}"
                         )
