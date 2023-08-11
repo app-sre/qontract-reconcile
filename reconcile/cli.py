d626fc16b10e8260e8685bff8b2ffc8e30298a49
Jan-Hendrik Boll <jboll@redhat.com>
2023-08-11 14:54:54 +0200

    wip

--- reconcile/cli.py
+++ reconcile/cli.py
@@ -584,14 +584,16 @@ def integration(
 
 @integration.result_callback()
 def exit_integration(
-    ctx,
-    configfile,
-    dry_run,
-    validate_schemas,
-    dump_schemas_file,
-    log_level,
-    gql_sha_url,
-    gql_url_print,
+        ctx,
+        configfile,
+        dry_run,
+        early_exit_compare_sha,
+        check_only_affected_shards,
+        validate_schemas,
+        dump_schemas_file,
+        log_level,
+        gql_sha_url,
+        gql_url_print,
 ):
     global_session_cache.close_all()
 
