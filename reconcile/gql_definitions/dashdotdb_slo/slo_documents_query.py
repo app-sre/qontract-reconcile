5ee01b4efa60d25ec50ef955cd83309f09b8a5af
Jan-Hendrik Boll <jboll@redhat.com>
2023-08-23 14:42:28 +0200

    Make SLO Reporter RHOBS compatible (#3755)
    * Update DashdotSLO, allow configuration of external reference
    
    * Fix type
    
    * Remove token, cause we use tokenrefresher
    
    * Remove redundant non check

--- reconcile/gql_definitions/dashdotdb_slo/slo_documents_query.py
+++ reconcile/gql_definitions/dashdotdb_slo/slo_documents_query.py
@@ -32,18 +32,23 @@ query SLODocuments {
   slo_documents: slo_document_v1 {
     name
     namespaces {
-      name
-      app {
-        name
+      prometheusAccess {
+         url
       }
-      cluster {
+      namespace {
         name
-        automationToken {
-          ... VaultSecret
+        app {
+          name
         }
-        prometheusUrl
-        spec {
-          private
+        cluster {
+          name
+          automationToken {
+          ... VaultSecret
+          }
+          prometheusUrl
+          spec {
+            private
+          }
         }
       }
     }
@@ -68,6 +73,10 @@ class ConfiguredBaseModel(BaseModel):
         extra = Extra.forbid
 
 
+class SLOExternalPrometheusAccessV1(ConfiguredBaseModel):
+    url: str = Field(..., alias="url")
+
+
 class AppV1(ConfiguredBaseModel):
     name: str = Field(..., alias="name")
 
@@ -89,6 +98,13 @@ class NamespaceV1(ConfiguredBaseModel):
     cluster: ClusterV1 = Field(..., alias="cluster")
 
 
+class SLONamespacesV1(ConfiguredBaseModel):
+    prometheus_access: Optional[SLOExternalPrometheusAccessV1] = Field(
+        ..., alias="prometheusAccess"
+    )
+    namespace: NamespaceV1 = Field(..., alias="namespace")
+
+
 class SLODocumentSLOSLOParametersV1(ConfiguredBaseModel):
     window: str = Field(..., alias="window")
 
@@ -104,7 +120,7 @@ class SLODocumentSLOV1(ConfiguredBaseModel):
 
 class SLODocumentV1(ConfiguredBaseModel):
     name: str = Field(..., alias="name")
-    namespaces: list[NamespaceV1] = Field(..., alias="namespaces")
+    namespaces: list[SLONamespacesV1] = Field(..., alias="namespaces")
     slos: Optional[list[SLODocumentSLOV1]] = Field(..., alias="slos")
 
 
