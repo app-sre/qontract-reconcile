c2744565f8e0bb2b5acc5d1a4aa892bd52e0ead8
Christian Assing <cassing@redhat.com>
2023-09-04 15:49:22 +0200

    [qontract-cli] sso-client management command

--- reconcile/utils/keycloak.py
+++ reconcile/utils/keycloak.py
@@ -1,4 +1,5 @@
 from typing import (
+    Any,
     Iterable,
     Optional,
     Sequence,
@@ -35,14 +36,21 @@ class KeycloakInstance(BaseModel):
 
 class KeycloakAPI:
     def __init__(
-        self, url: str, initial_access_token: Optional[str] = None, timeout: int = 30
+        self,
+        url: str | None = None,
+        initial_access_token: str | None = None,
+        timeout: int = 30,
     ) -> None:
         self.url = url
         self.initial_access_token = initial_access_token
         self.timeout = timeout
-        self._init_openid_configuration()
+        self._openid_configuration: dict[str, Any] | None = None
+        if self.url:
+            self._init_openid_configuration()
 
     def _init_openid_configuration(self) -> None:
+        if not self.url:
+            raise ValueError("url is required")
         self._openid_configuration = requests.get(
             f"{self.url}/.well-known/openid-configuration",
             timeout=self.timeout,
@@ -59,6 +67,8 @@ class KeycloakAPI:
         """Create a new SSO client."""
         if not self.initial_access_token:
             raise ValueError("initial_access_token is required")
+        if not self._openid_configuration:
+            raise ValueError("openid_configuration is required")
 
         response = requests.post(
             self._openid_configuration["registration_endpoint"],
