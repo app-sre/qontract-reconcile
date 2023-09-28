f390bbda23a845cbb1dacc5978cceba12611977a
Maor Friedman <mafriedm@redhat.com>
2023-09-28 09:59:14 +0300

    init clusters

--- reconcile/overview_metrics_exporter.py
+++ reconcile/overview_metrics_exporter.py
@@ -1,8 +1,34 @@
 from typing import Optional
 
+from pydantic import BaseModel
+
+from reconcile.typed_queries.clusters import get_clusters
+from reconcile.utils import metrics
+from reconcile.utils.metrics import GaugeMetric
+
+
+class OverviewBaseMetric(BaseModel):
+    "Base class for overview metrics"
+
+    integration: str
+
+
+class OverviewClustersGauge(OverviewBaseMetric, GaugeMetric):
+    "Overview of clusters"
+
+    @classmethod
+    def name(cls) -> str:
+        return "overview_clusters"
+
 
 QONTRACT_INTEGRATION = "overview-metrics-exporter"
 
 
 def run(dry_run: Optional[bool]):
-    print("hello world!")
+    clusters = get_clusters()
+    metrics.set_gauge(
+        OverviewClustersGauge(
+            integration=QONTRACT_INTEGRATION,
+        ),
+        len(clusters),
+    )
