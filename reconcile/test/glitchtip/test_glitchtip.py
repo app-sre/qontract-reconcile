b65bfe3351470369d2c8919967ead6f98dd6382e
Christian Assing <cassing@redhat.com>
2023-10-02 09:30:55 +0200

    [glitchtip] team.ldapGroups support (#3842)
    For our new SRE error-tracking capability, we also need to get Glitchtip users from LDAP. The PR introduces this feature and reactors the glitchtip tests a little bit.
    
    Ticket: [SRE Capability] Error tracking
    Design-doc: https://gitlab.cee.redhat.com/service/app-interface/-/blob/master/docs/app-sre/design-docs/sre-capability-error-tracking.md
    Depends on: app-sre/qontract-schemas#525

--- reconcile/test/glitchtip/test_glitchtip.py
+++ reconcile/test/glitchtip/test_glitchtip.py
@@ -1,4 +1,7 @@
-from pytest_mock import MockerFixture
+from collections.abc import Callable
+from unittest.mock import Mock
+
+from deepdiff import DeepDiff
 
 from reconcile.glitchtip.integration import (
     fetch_current_state,
@@ -6,26 +9,12 @@ from reconcile.glitchtip.integration import (
 )
 from reconcile.gql_definitions.glitchtip.glitchtip_project import GlitchtipProjectsV1
 from reconcile.test.fixtures import Fixtures
-from reconcile.utils.glitchtip import (
-    GlitchtipClient,
-    Organization,
+from reconcile.utils.glitchtip import GlitchtipClient
+from reconcile.utils.internal_groups.models import (
+    Entity,
+    EntityType,
+    Group,
 )
-
-
-def sort_all(orgs: list[Organization]) -> list[Organization]:
-    """sort everything for easier comparison"""
-    for org in orgs:
-        for proj in org.projects:
-            proj.teams.sort()
-            for team in proj.teams:
-                team.users.sort()
-        org.projects.sort()
-        for team in org.teams:
-            team.users.sort()
-        org.teams.sort()
-        org.users.sort()
-    orgs.sort()
-    return orgs
 
 
 def test_fetch_current_state(
@@ -36,21 +25,56 @@ def test_fetch_current_state(
     current_state = fetch_current_state(
         glitchtip_client, ignore_users=["sd-app-sre+glitchtip@nasa.com"]
     )
-    expected_current_state = [
-        Organization(**i) for i in fx.get_anymarkup("current_state_expected.yml")
-    ]
 
-    assert sort_all(current_state) == sort_all(expected_current_state)
-
-
-def test_desire_state(mocker: MockerFixture, fx: Fixtures) -> None:
-    projects = [
-        GlitchtipProjectsV1(**i) for i in fx.get_anymarkup("desire_state_projects.yml")
-    ]
-    desired_state = fetch_desired_state(
-        glitchtip_projects=projects, mail_domain="nasa.com"
+    assert (
+        DeepDiff(
+            [s.dict() for s in current_state],
+            fx.get_anymarkup("current_state_expected.yml"),
+            ignore_order=True,
+        )
+        == {}
     )
-    expected_desire_state = [
-        Organization(**i) for i in fx.get_anymarkup("desire_state_expected.yml")
+
+
+def test_desire_state(
+    fx: Fixtures,
+    internal_groups_client: Mock,
+    gql_class_factory: Callable,
+) -> None:
+    projects = [
+        gql_class_factory(GlitchtipProjectsV1, i)
+        for i in fx.get_anymarkup("desire_state_projects.yml")
     ]
-    assert sort_all(desired_state)[1] == sort_all(expected_desire_state)[1]
+    from_emea_ldap_group = Group(
+        name="from_emea",
+        description="foo bar",
+        member_approval_type="self-service",
+        contact_list="email@example.org",
+        owners=[Entity(type=EntityType.SERVICE_ACCOUNT, id="service-account-1")],
+        display_name="ai-dev-test-group (App-Interface))",
+        notes=None,
+        rover_group_member_query=None,
+        rover_group_inclusions=None,
+        rover_group_exclusions=None,
+        members=[
+            Entity(type=EntityType.USER, id="pike"),
+            Entity(type=EntityType.USER, id="uhura"),
+        ],
+        member_of=None,
+        namespace=None,
+    )
+    internal_groups_client.group.side_effect = [from_emea_ldap_group]
+    desired_state = fetch_desired_state(
+        glitchtip_projects=projects,
+        mail_domain="nasa.com",
+        internal_groups_client=internal_groups_client,
+    )
+
+    assert (
+        DeepDiff(
+            [s.dict() for s in desired_state],
+            fx.get_anymarkup("desire_state_expected.yml"),
+            ignore_order=True,
+        )
+        == {}
+    )
