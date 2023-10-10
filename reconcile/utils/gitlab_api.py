03669d65731341b348f3dba517eb3a8164dabe70
Gerd Oberlechner <goberlec@redhat.com>
2023-10-10 12:22:38 +0200

    cached_property
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- reconcile/utils/gitlab_api.py
+++ reconcile/utils/gitlab_api.py
@@ -4,6 +4,7 @@ from collections.abc import (
     Iterable,
     Set,
 )
+from functools import cached_property
 from operator import (
     attrgetter,
     itemgetter,
@@ -36,6 +37,7 @@ MR_DESCRIPTION_COMMENT_ID = 0
 
 # The default value is there for unit test
 INTEGRATION_NAME = os.getenv("INTEGRATION_NAME", "")
+DEFAULT_MAIN_BRANCH = "master"
 
 
 class MRState:
@@ -100,22 +102,17 @@ class GitLabApi:  # pylint: disable=too-many-public-methods
             gitlab_request.labels(integration=INTEGRATION_NAME).inc()
             self.project = self.gl.projects.get(project_id)
         self.saas_files = saas_files
-        self._main_branch = None
+
+    @cached_property
+    def project_main_branch(self) -> str:
+        return next(
+            (b.name for b in self.project.branches.list() if b.default),
+            DEFAULT_MAIN_BRANCH,
+        )
 
     @property
     def main_branch(self) -> str:
-        if self._main_branch is None:
-            self._main_branch = self._determine_main_branch()
-        return self._main_branch or "master"
-
-    def _determine_main_branch(self) -> Optional[str]:
-        if self.project:
-            main_branch_obj = next(
-                (b for b in self.project.branches.list() if b.default), None
-            )
-            if main_branch_obj:
-                return main_branch_obj.name
-        return None
+        return self.project_main_branch if self.project else DEFAULT_MAIN_BRANCH
 
     def __enter__(self):
         return self
