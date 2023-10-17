206a9d0e5fccabea8a0e0b2ae2891e2e191682a4
Karl Fischer <kfischer@redhat.com>
2023-10-17 13:00:23 +0200

    APPSRE-7307 remove prom v1 publishing

--- reconcile/utils/promotion_state.py
+++ reconcile/utils/promotion_state.py
@@ -75,11 +75,6 @@ class PromotionState:
     def publish_promotion_data(
         self, sha: str, channel: str, target_uid: str, data: PromotionData
     ) -> None:
-        # TODO: this will be deprecated once we fully moved to promotions_v2
-        state_key = f"promotions/{channel}/{sha}"
-        self._state.add(state_key, data.dict(), force=True)
-        logging.info("Uploaded %s to %s", data, state_key)
-
         state_key_v2 = f"promotions_v2/{channel}/{target_uid}/{sha}"
         self._state.add(state_key_v2, data.dict(), force=True)
         logging.info("Uploaded %s to %s", data, state_key_v2)
