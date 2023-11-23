d3cf3c49bd613ec97980eee83e89a9a8b9e179bd
Jan-Hendrik Boll <jboll@redhat.com>
2023-11-23 15:26:30 +0100

    Fix assertion

--- reconcile/test/test_database_access_manager.py
+++ reconcile/test/test_database_access_manager.py
@@ -145,7 +145,7 @@ def _assert_delete_script(script: str) -> None:
 
 
 def _assert_revoke_access(script: str) -> None:
-    assert '\nREVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA "foo" FROM "test";\n'
+    assert '\nREVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA "foo" FROM "test";\n' in script
 
 
 def test_generate_create_user(
