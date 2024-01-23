112d63e30146e71e9a6a38e841621dbd0f9e2662
Karl Fischer <kfischer@redhat.com>
2024-01-23 17:00:02 +0100

    APPSRE-8505 decouple parsing logic

--- reconcile/saas_auto_promotions_manager/merge_request_manager/merge_request_manager.py
+++ reconcile/saas_auto_promotions_manager/merge_request_manager/merge_request_manager.py
@@ -2,37 +2,28 @@ import logging
 import re
 from collections import defaultdict
 from collections.abc import Iterable
-from dataclasses import dataclass
 
 from gitlab.exceptions import GitlabGetError
-from gitlab.v4.objects import ProjectMergeRequest
 
 from reconcile.saas_auto_promotions_manager.merge_request_manager.merge_request import (
     SAPMMR,
+)
+from reconcile.saas_auto_promotions_manager.merge_request_manager.mr_parser import (
+    MRParser,
+    OpenMergeRequest,
 )
 from reconcile.saas_auto_promotions_manager.merge_request_manager.renderer import (
     CHANNELS_REF,
     CONTENT_HASHES,
     IS_BATCHABLE,
-    PROMOTION_DATA_SEPARATOR,
     SAPM_LABEL,
-    SAPM_VERSION,
     VERSION_REF,
     Renderer,
 )
 from reconcile.saas_auto_promotions_manager.subscriber import Subscriber
-from reconcile.utils.vcs import VCS, MRCheckStatus
+from reconcile.utils.vcs import VCS
 
 ITEM_SEPARATOR = ","
-
-
-@dataclass
-class OpenMergeRequest:
-    raw: ProjectMergeRequest
-    content_hashes: str
-    channels: str
-    failed_mr_check: bool
-    is_batchable: bool
 
 
 class MergeRequestManager:
@@ -49,162 +40,16 @@ class MergeRequestManager:
     amount of MRs.
     """
 
-    def __init__(self, vcs: VCS, renderer: Renderer):
+    def __init__(self, vcs: VCS, mr_parser: MRParser, renderer: Renderer):
         self._vcs = vcs
+        self._mr_parser = mr_parser
         self._renderer = renderer
         self._version_ref_regex = re.compile(rf"{VERSION_REF}: (.*)$", re.MULTILINE)
         self._content_hash_regex = re.compile(rf"{CONTENT_HASHES}: (.*)$", re.MULTILINE)
         self._channels_regex = re.compile(rf"{CHANNELS_REF}: (.*)$", re.MULTILINE)
         self._is_batchable_regex = re.compile(rf"{IS_BATCHABLE}: (.*)$", re.MULTILINE)
         self._open_mrs: list[OpenMergeRequest] = []
-        self._open_mrs_with_problems: list[OpenMergeRequest] = []
-        self._open_raw_mrs: list[ProjectMergeRequest] = []
         self._unbatchable_hashes: set[str] = set()
-
-    def _apply_regex(self, pattern: re.Pattern, promotion_data: str) -> str:
-        matches = pattern.search(promotion_data)
-        if not matches:
-            return ""
-        groups = matches.groups()
-        if len(groups) != 1:
-            return ""
-        return groups[0]
-
-    def _fetch_sapm_managed_open_merge_requests(self) -> None:
-        all_open_mrs = self._vcs.get_open_app_interface_merge_requests()
-        self._open_raw_mrs = [
-            mr for mr in all_open_mrs if SAPM_LABEL in mr.attributes.get("labels")
-        ]
-
-    def _parse_raw_mrs(self) -> None:
-        """
-        We store state in MR descriptions.
-        This function parses the state and stores a list of valid, parsed open MRs (current state).
-        If any issue is encountered during parsing, we consider this MR
-        to be broken and close it. Information we want to parse includes:
-        - SAPM_VERSION -> Close if it doesnt match current version
-        - CHANNELS
-        - CONTENT_HASHES
-        - IS_BATCHABLE flag
-        """
-        seen: set[tuple[str, str, str]] = set()
-        for mr in self._open_raw_mrs:
-            attrs = mr.attributes
-            desc = attrs.get("description")
-            has_conflicts = attrs.get("has_conflicts", False)
-            if has_conflicts:
-                logging.info(
-                    "Merge-conflict detected. Closing %s",
-                    mr.attributes.get("web_url", "NO_WEBURL"),
-                )
-                self._vcs.close_app_interface_mr(
-                    mr, "Closing this MR because of a merge-conflict."
-                )
-                continue
-            parts = desc.split(PROMOTION_DATA_SEPARATOR)
-            if not len(parts) == 2:
-                logging.info(
-                    "Bad data separator format. Closing %s",
-                    mr.attributes.get("web_url", "NO_WEBURL"),
-                )
-                self._vcs.close_app_interface_mr(
-                    mr, "Closing this MR because of bad data separator format."
-                )
-                continue
-            promotion_data = parts[1]
-
-            version_ref = self._apply_regex(
-                pattern=self._version_ref_regex, promotion_data=promotion_data
-            )
-            if not version_ref:
-                logging.info(
-                    "Bad %s format. Closing %s",
-                    VERSION_REF,
-                    mr.attributes.get("web_url", "NO_WEBURL"),
-                )
-                self._vcs.close_app_interface_mr(
-                    mr, f"Closing this MR because of bad {VERSION_REF} format."
-                )
-                continue
-
-            if version_ref != SAPM_VERSION:
-                logging.info(
-                    "Old MR version detected: %s. Closing %s",
-                    version_ref,
-                    mr.attributes.get("web_url", "NO_WEBURL"),
-                )
-                self._vcs.close_app_interface_mr(
-                    mr,
-                    f"Closing this MR because it has an outdated SAPM version {version_ref}",
-                )
-                continue
-
-            content_hashes = self._apply_regex(
-                pattern=self._content_hash_regex, promotion_data=promotion_data
-            )
-            if not content_hashes:
-                logging.info(
-                    "Bad %s format. Closing %s",
-                    CONTENT_HASHES,
-                    mr.attributes.get("web_url", "NO_WEBURL"),
-                )
-                self._vcs.close_app_interface_mr(
-                    mr, f"Closing this MR because of bad {CONTENT_HASHES} format."
-                )
-                continue
-
-            channels_refs = self._apply_regex(
-                pattern=self._channels_regex, promotion_data=promotion_data
-            )
-            if not channels_refs:
-                logging.info(
-                    "Bad %s format. Closing %s",
-                    CHANNELS_REF,
-                    mr.attributes.get("web_url", "NO_WEBURL"),
-                )
-                self._vcs.close_app_interface_mr(
-                    mr, f"Closing this MR because of bad {CHANNELS_REF} format."
-                )
-                continue
-
-            key = (version_ref, channels_refs, content_hashes)
-            if key in seen:
-                logging.info(
-                    "Duplicate MR detected. Closing %s",
-                    mr.attributes.get("web_url", "NO_WEBURL"),
-                )
-                self._vcs.close_app_interface_mr(
-                    mr,
-                    "Closing this MR because there is already another MR open with identical content.",
-                )
-                continue
-            seen.add(key)
-
-            is_batchable_str = self._apply_regex(
-                pattern=self._is_batchable_regex, promotion_data=promotion_data
-            )
-            if is_batchable_str not in set(["True", "False"]):
-                logging.info(
-                    "Bad %s format. Closing %s",
-                    IS_BATCHABLE,
-                    mr.attributes.get("web_url", "NO_WEBURL"),
-                )
-                self._vcs.close_app_interface_mr(
-                    mr, f"Closing this MR because of bad {IS_BATCHABLE} format."
-                )
-                continue
-
-            mr_check_status = self._vcs.get_gitlab_mr_check_status(mr)
-
-            self._open_mrs.append(
-                OpenMergeRequest(
-                    raw=mr,
-                    content_hashes=content_hashes,
-                    channels=channels_refs,
-                    failed_mr_check=mr_check_status == MRCheckStatus.FAILED,
-                    is_batchable=bool(is_batchable_str),
-                )
-            )
 
     def _unbatch_failed_mrs(self) -> None:
         """
@@ -230,8 +75,7 @@ class MergeRequestManager:
         self._open_mrs = open_mrs_after_unbatching
 
     def housekeeping(self) -> None:
-        self._fetch_sapm_managed_open_merge_requests()
-        self._parse_raw_mrs()
+        self._open_mrs = self._mr_parser.retrieve_open_mrs()
         self._unbatch_failed_mrs()
 
     def _aggregate_subscribers_per_channel_combo(
--- reconcile/test/saas_auto_promotions_manager/merge_request_manager/merge_request_manager/test_merge_request_manager.py
+++ reconcile/test/saas_auto_promotions_manager/merge_request_manager/merge_request_manager/test_merge_request_manager.py
@@ -1,13 +1,19 @@
 from collections.abc import (
     Callable,
+    Iterable,
     Mapping,
 )
+from unittest.mock import create_autospec
 
 import pytest
 from gitlab.v4.objects import ProjectMergeRequest
 
 from reconcile.saas_auto_promotions_manager.merge_request_manager.merge_request_manager import (
     MergeRequestManager,
+)
+from reconcile.saas_auto_promotions_manager.merge_request_manager.mr_parser import (
+    MRParser,
+    OpenMergeRequest,
 )
 from reconcile.saas_auto_promotions_manager.merge_request_manager.renderer import (
     Renderer,
@@ -16,9 +22,7 @@ from reconcile.saas_auto_promotions_manager.subscriber import Subscriber
 from reconcile.utils.vcs import VCS
 
 from .data_keys import (
-    OPEN_MERGE_REQUESTS,
     SUBSCRIBER_CHANNELS,
-    SUBSCRIBER_CONTENT_HASH,
     SUBSCRIBER_DESIRED_CONFIG_HASHES,
     SUBSCRIBER_DESIRED_REF,
     SUBSCRIBER_TARGET_NAMESPACE,
@@ -27,7 +31,7 @@ from .data_keys import (
 
 
 def test_close_old_content(
-    vcs_builder: Callable[[Mapping], tuple[VCS, list[ProjectMergeRequest]]],
+    mr_parser_builder: Callable[[Iterable], MRParser],
     renderer: Renderer,
     subscriber_builder: Callable[[Mapping], Subscriber],
 ):
@@ -41,26 +45,29 @@ def test_close_old_content(
         })
     ]
 
-    vcs, _ = vcs_builder({
-        OPEN_MERGE_REQUESTS: [
-            {
-                SUBSCRIBER_CONTENT_HASH: "oldcontent",
-                SUBSCRIBER_CHANNELS: "channel-a,channel-b",
-            }
-        ]
-    })
-
+    open_mrs = [
+        OpenMergeRequest(
+            raw=create_autospec(ProjectMergeRequest),
+            content_hashes="oldcontent",
+            channels="channel-a,channel-b",
+            failed_mr_check=False,
+            is_batchable=True,
+        )
+    ]
+    mr_parser = mr_parser_builder(open_mrs)
+    vcs = create_autospec(spec=VCS)
     merge_request_manager = MergeRequestManager(
         vcs=vcs,
+        mr_parser=mr_parser,
         renderer=renderer,
     )
     merge_request_manager.housekeeping()
     merge_request_manager.create_promotion_merge_requests(subscribers=subscribers)
 
-    # There was is an open MR with old content for that subscriber
+    # There is an open MR with old content for that subscriber
     # Close old content and open new MR with new content
-    vcs.close_app_interface_mr.assert_called_once()  # type: ignore[attr-defined]
-    vcs.open_app_interface_merge_request.assert_called_once()  # type: ignore[attr-defined]
+    vcs.close_app_interface_mr.assert_called_once()
+    vcs.open_app_interface_merge_request.assert_called_once()
 
 
 @pytest.mark.parametrize(
@@ -75,7 +82,7 @@ def test_close_old_content(
     ],
 )
 def test_merge_request_already_opened(
-    vcs_builder: Callable[[Mapping], tuple[VCS, list[ProjectMergeRequest]]],
+    mr_parser_builder: Callable[[Iterable], MRParser],
     renderer: Renderer,
     subscriber_builder: Callable[[Mapping], Subscriber],
     hash_prefix: str,
@@ -95,21 +102,21 @@ def test_merge_request_already_opened(
     ]
     content_hash = Subscriber.combined_content_hash(subscribers=subscribers)
 
-    vcs, _ = vcs_builder({
-        OPEN_MERGE_REQUESTS: [
-            {
-                # Note, that the hash/channel can be embedded within a concatenated string.
-                # This is required to allow aggregating multiple MRs into a single MR,
-                # while still keeping track of whether the desired content is already part
-                # of an MR or not.
-                SUBSCRIBER_CONTENT_HASH: f"{hash_prefix}{content_hash}{hash_suffix}",
-                SUBSCRIBER_CHANNELS: f"{channel_prefix}{subscriber_channel}{channel_suffix}",
-            }
-        ]
-    })
+    open_mrs = [
+        OpenMergeRequest(
+            raw=create_autospec(spec=ProjectMergeRequest),
+            content_hashes=f"{hash_prefix}{content_hash}{hash_suffix}",
+            channels=f"{channel_prefix}{subscriber_channel}{channel_suffix}",
+            is_batchable=True,
+            failed_mr_check=False,
+        )
+    ]
+    mr_parser = mr_parser_builder(open_mrs)
 
+    vcs = create_autospec(spec=VCS)
     merge_request_manager = MergeRequestManager(
         vcs=vcs,
+        mr_parser=mr_parser,
         renderer=renderer,
     )
     merge_request_manager.housekeeping()
@@ -117,8 +124,8 @@ def test_merge_request_already_opened(
 
     # There is already an open merge request for this subscriber content
     # Do not open another one
-    vcs.close_app_interface_mr.assert_not_called()  # type: ignore[attr-defined]
-    vcs.open_app_interface_merge_request.assert_not_called()  # type: ignore[attr-defined]
+    vcs.close_app_interface_mr.assert_not_called()
+    vcs.open_app_interface_merge_request.assert_not_called()
 
 
 @pytest.mark.parametrize(
@@ -133,7 +140,7 @@ def test_merge_request_already_opened(
     ],
 )
 def test_ignore_unrelated_channels(
-    vcs_builder: Callable[[Mapping], tuple[VCS, list[ProjectMergeRequest]]],
+    mr_parser_builder: Callable[[Iterable], MRParser],
     renderer: Renderer,
     subscriber_builder: Callable[[Mapping], Subscriber],
     hash_prefix: str,
@@ -152,21 +159,21 @@ def test_ignore_unrelated_channels(
     ]
     content_hash = Subscriber.combined_content_hash(subscribers=subscribers)
 
-    vcs, _ = vcs_builder({
-        OPEN_MERGE_REQUESTS: [
-            {
-                # Note, that the hash/channel can be embedded within a concatenated string.
-                # This is required to allow aggregating multiple MRs into a single MR,
-                # while still keeping track of whether the desired content is already part
-                # of an MR or not.
-                SUBSCRIBER_CONTENT_HASH: f"{hash_prefix}{content_hash}{hash_suffix}",
-                SUBSCRIBER_CHANNELS: f"{channel_prefix}other-channel{channel_suffix}",
-            }
-        ]
-    })
+    open_mrs = [
+        OpenMergeRequest(
+            raw=create_autospec(spec=ProjectMergeRequest),
+            content_hashes=f"{hash_prefix}{content_hash}{hash_suffix}",
+            channels=f"{channel_prefix}other-channel{channel_suffix}",
+            is_batchable=True,
+            failed_mr_check=False,
+        )
+    ]
+    mr_parser = mr_parser_builder(open_mrs)
 
+    vcs = create_autospec(spec=VCS)
     merge_request_manager = MergeRequestManager(
         vcs=vcs,
+        mr_parser=mr_parser,
         renderer=renderer,
     )
     merge_request_manager.housekeeping()
@@ -174,5 +181,5 @@ def test_ignore_unrelated_channels(
 
     # There is already an open merge request for this subscriber content
     # Do not open another one, because the channels do not match
-    vcs.close_app_interface_mr.assert_not_called()  # type: ignore[attr-defined]
-    vcs.open_app_interface_merge_request.assert_called_once()  # type: ignore[attr-defined]
+    vcs.close_app_interface_mr.assert_not_called()
+    vcs.open_app_interface_merge_request.assert_called_once()
