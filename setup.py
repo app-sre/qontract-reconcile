c523baab810793489aa45a352b2f4d5270625d3c
Di Wang <dwan@redhat.com>
2024-01-25 19:59:29 +1100

    Bump sretoolbox to 2.5.0
    Signed-off-by: Di Wang <dwan@redhat.com>

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     packages=find_packages(exclude=("tests",)),
     package_data={"reconcile": ["templates/*.j2", "gql_queries/*/*.gql"]},
     install_requires=[
-        "sretoolbox~=2.4.3",
+        "sretoolbox~=2.5.0",
         "Click>=7.0,<9.0",
         "gql==3.1.0",
         "toml>=0.10.0,<0.11.0",
