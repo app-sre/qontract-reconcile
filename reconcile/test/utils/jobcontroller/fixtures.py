fbd2c64fc0a5ec3b21a7026cd883818ea42af695
Gerd Oberlechner <goberlec@redhat.com>
2024-02-14 12:46:20 +0100

    [job-controller] support secret data for jobs
    the `K8sJob` class can define secret data it relies on and the job
    controller will manage the lifecycle of a kubernetes Secret based on
    that data.
    
    ```python
    
    class MyJob(K8sJob):
    
       ...
    
       def secret_data(self) -> dict[str: str]:
         return {
           "super": "secret"
         }
    
    ```
    
    the resulting secret has an `ownerReference` towards the job, which
    implies that the secret gets cleaned up when the job gets cleaned up.
    
    since these kind of jobs leverage `ttlSecondsAfterFinished` to
    self-destroy, also the secrets lifecycle depends on the job ones.
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- reconcile/test/utils/jobcontroller/fixtures.py
+++ reconcile/test/utils/jobcontroller/fixtures.py
@@ -1,5 +1,4 @@
 from typing import Any, Optional
-from unittest.mock import create_autospec
 
 from kubernetes.client import (  # type: ignore[attr-defined]
     ApiClient,
@@ -8,14 +7,13 @@ from kubernetes.client import (  # type: ignore[attr-defined]
 )
 from pydantic import BaseModel
 
-from reconcile.utils.jobcontroller.controller import K8sJobController
 from reconcile.utils.jobcontroller.models import K8sJob
-from reconcile.utils.oc import OCCli
 
 
 class SomeJob(K8sJob, BaseModel):
     identifying_attribute: str
     backoff_limit: int = 0
+    credentials: Optional[dict[str, str]] = None
 
     def name_prefix(self) -> str:
         return "some-job"
@@ -30,9 +28,13 @@ class SomeJob(K8sJob, BaseModel):
             template=V1PodTemplateSpec(),
         )
 
+    def secret_data(self) -> dict[str, str]:
+        return self.credentials or {}
+
 
 class SomeJobV2(K8sJob, BaseModel):
     identifying_attribute: str
+    credentials: Optional[dict[str, str]] = None
 
     def name_prefix(self) -> str:
         return "some-job"
@@ -65,34 +67,3 @@ def build_job_resource(
     if status:
         job_resource["status"] = status
     return job_resource
-
-
-def build_oc_fixture(job_specs: Optional[list[list[dict[str, Any]]]] = None) -> OCCli:
-    mocked_oc_client = create_autospec(OCCli)
-    mocked_oc_client.get_items.side_effect = job_specs or [[]]
-    return mocked_oc_client
-
-
-def build_job_controller_fixture(oc: OCCli, dry_run: bool) -> K8sJobController:
-    return K8sJobController(
-        oc=oc,
-        cluster="some-cluster",
-        namespace="some-ns",
-        integration="some-integration",
-        integration_version="0.1",
-        dry_run=dry_run,
-        time_module=TimeMock(),
-    )
-
-
-class TimeMock:
-    def __init__(self) -> None:
-        self.current_time = 0.0
-
-    def time(self) -> float:
-        return self.current_time
-
-    def sleep(self, seconds: float) -> None:
-        if seconds < 0:
-            raise ValueError("Negative value for sleep seconds not allowed")
-        self.current_time += seconds
