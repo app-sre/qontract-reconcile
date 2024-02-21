bf29889a3e97b2e6d2231f165858112f915876a9
Ryan Wallace <rywallac@redhat.com>
2024-02-21 16:16:11 -0500

    update image name

--- build_deploy.sh
+++ build_deploy.sh
@@ -11,6 +11,6 @@ docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
 make test build push
 
 # and fedramp
-make IMAGE_NAME=quay.io/app-sre/qontract-reconcile-fedramp BUILD_TARGET=fedramp-prod-image build push
+make IMAGE_NAME=quay.io/app-sre/qontract-reconcile-fips BUILD_TARGET=fedramp-prod-image build push
 
 make pypi-release
