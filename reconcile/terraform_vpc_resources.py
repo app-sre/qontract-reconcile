8783a41f8b9fed2e2e015fa223a9379235ec1086
Esron Silva <essilva@essilva-thinkpadp16vgen1.rmtbr.csb>
2024-02-26 15:41:20 -0300

    Creates an aws api object

--- reconcile/terraform_vpc_resources.py
+++ reconcile/terraform_vpc_resources.py
@@ -1,9 +1,17 @@
 import logging
+from typing import Optional
+from reconcile import queries
+from reconcile.utils import aws_api
 
 from reconcile.utils.semver_helper import make_semver
 
 QONTRACT_INTEGRATION = "terraform_vpc_resources"
 QONTRACT_INTEGRATION_VERSION = make_semver(0, 1, 0)
 
-def run(dry_run):
+def run(dry_run, account_name: Optional[str] = None):
+    settings = queries.get_secret_reader_settings()
+    accounts = queries.get_aws_accounts(terraform_state=True,)
+
+    awsapi = aws_api.AWSApi(1, accounts, settings=settings, init_users=False)
+
     logging.info(f"Running {QONTRACT_INTEGRATION} version {QONTRACT_INTEGRATION_VERSION}")
