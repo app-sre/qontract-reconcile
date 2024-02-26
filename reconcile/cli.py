76731b40405f2fc292d8f4442ce2b0103a9ab74b
Esron Silva <essilva@essilva-thinkpadp16vgen1.rmtbr.csb>
2024-02-26 15:41:02 -0300

    Add account name argument

--- reconcile/cli.py
+++ reconcile/cli.py
@@ -2287,11 +2287,12 @@ def terraform_users(
     )
 
 @integration.command(short_help="Manage VPC creation")
+@account_name
 @click.pass_context
 def terraform_vpc_resources(ctx):
     import reconcile.terraform_vpc_resources
 
-    run_integration(reconcile.terraform_vpc_resources, ctx.obj)
+    run_integration(reconcile.terraform_vpc_resources, ctx.obj, account_name)
 
 @integration.command(
     short_help="Manage VPC peerings between OSD clusters and AWS accounts or other OSD clusters."
