bec7354f8ca38467523dbc6a6e17638e5395a81b
Maor Friedman <mafriedm@redhat.com>
2024-03-03 12:42:04 +0200

    early_exit_desired_state

--- reconcile/ocm_aws_infrastructure_access.py
+++ reconcile/ocm_aws_infrastructure_access.py
@@ -203,3 +203,6 @@ def run(dry_run):
     act(
         dry_run, ocm_map, current_state, current_failed, desired_state, current_deleting
     )
+
+def early_exit_desired_state(*args: Any, **kwargs: Any) -> dict[str, Any]:
+    return {"state": fetch_desired_state(get_clusters())}
