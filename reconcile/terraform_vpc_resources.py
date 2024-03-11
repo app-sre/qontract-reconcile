439f19fbfd716e13622a08879cf5feba5d4e3264
Esron Silva <essilva@essilva-thinkpadp16vgen1.rmtbr.csb>
2024-03-11 10:00:13 -0300

    Creates a terrascript client

--- reconcile/terraform_vpc_resources.py
+++ reconcile/terraform_vpc_resources.py
@@ -14,11 +14,13 @@ from reconcile.typed_queries.app_interface_vault_settings import (
 )
 from reconcile.utils import gql
 from reconcile.utils.runtime.integration import (
+    DesiredStateShardConfig,
     PydanticRunParams,
     QontractReconcileIntegration,
 )
 from reconcile.utils.secret_reader import create_secret_reader
 from reconcile.utils.semver_helper import make_semver
+from reconcile.utils.terrascript_aws_client import TerrascriptClient
 
 QONTRACT_INTEGRATION = "terraform_vpc_resources"
 QONTRACT_INTEGRATION_VERSION = make_semver(0, 1, 0)
@@ -58,6 +60,10 @@ class TerraformVpcResources(QontractReconcileIntegration[TerraformVpcResourcesPa
 
         if data:
             accounts = self._filter_accounts(data)
+            if account_name:
+                accounts = [
+                    account for account in accounts if account.name == account_name
+                ]
             if not accounts:
                 logging.error(
                     "No AWS accounts with 'terraform-vpc-resources' state found, nothing to do."
@@ -66,3 +72,23 @@ class TerraformVpcResources(QontractReconcileIntegration[TerraformVpcResourcesPa
         else:
             logging.error("No AWS accounts found, nothing to do.")
             sys.exit(ExitCodes.SUCCESS)
+
+        accounts_untyped: list[dict] = [acc.dict(by_alias=True) for acc in accounts]
+        ts_client = TerrascriptClient(
+            integration=QONTRACT_INTEGRATION,
+            integration_prefix="",
+            thread_pool_size=1,
+            accounts=accounts_untyped,
+            secret_reader=secret_reader,
+        )
+
+        working_dir = ts_client.dump()
+
+    def get_desired_state_shard_config(self) -> DesiredStateShardConfig:
+        return DesiredStateShardConfig(
+            shard_arg_name="account_name",
+            shard_path_selectors={
+                "accounts[*].name",
+            },
+            sharded_run_review=lambda proposal: len(proposal.proposed_shards) <= 2,
+        )
