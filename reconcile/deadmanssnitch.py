5bedba51b3ab3898323046b7a692edf3f6e0a9da
Mehfuz Khan <mekhan@redhat.com>
2024-03-15 12:44:31 +0530

    build fix

--- reconcile/deadmanssnitch.py
+++ reconcile/deadmanssnitch.py
@@ -179,7 +179,7 @@ class DeadMansSnitchIntegration(QontractReconcileIntegration[NoParams]):
             )
             for cluster in desired_cluster_enabled_true & current_state.keys()
             # add to update set only if they exists, flag is set to true but vault and snitch url mismatch
-            if (cluster_data := current_state[cluster]) is not None
+            if (cluster_data := current_state.get(cluster)) is not None
             and cluster_data.vault_data != cluster_data.check_in_url
         ]
         return create_snitches + delete_snitches + update_vault
