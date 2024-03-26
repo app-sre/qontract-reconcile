390ebb39f77d111dade5c7c67b8b9c394fedd0bd
Di Wang <dwan@redhat.com>
2024-03-26 19:42:14 +1100

    Fetch config and secrets
    Signed-off-by: Di Wang <dwan@redhat.com>

--- tools/cli_commands/cost_report/command.py
+++ tools/cli_commands/cost_report/command.py
@@ -3,8 +3,13 @@ from collections.abc import Iterable, Mapping, MutableMapping
 from decimal import Decimal
 from typing import List, Self
 
+from reconcile.typed_queries.app_interface_vault_settings import (
+    get_app_interface_vault_settings,
+)
 from reconcile.typed_queries.cost_report.app_names import App, get_app_names
+from reconcile.typed_queries.cost_report.settings import get_cost_report_settings
 from reconcile.utils import gql
+from reconcile.utils.secret_reader import create_secret_reader
 from tools.cli_commands.cost_report.cost_management_api import CostManagementApi
 from tools.cli_commands.cost_report.model import ChildAppReport, Report, ServiceReport
 from tools.cli_commands.cost_report.response import ReportCostResponse
@@ -143,16 +148,19 @@ class CostReportCommand:
         cls,
     ) -> Self:
         gql_api = gql.get_api()
-        # TODO: fetch from config
+        vault_settings = get_app_interface_vault_settings(gql_api.query)
+        secret_reader = create_secret_reader(use_vault=vault_settings.vault)
+        cost_report_settings = get_cost_report_settings(gql_api)
+        secret = secret_reader.read_all_secret(cost_report_settings.credentials)
         cost_management_api = CostManagementApi(
-            base_url="",
-            token_url="",
-            client_id="",
-            client_secret="",
-            scope=None,
+            base_url=secret["api_base_url"],
+            token_url=secret["token_url"],
+            client_id=secret["client_id"],
+            client_secret=secret["client_secret"],
+            scope=secret["scope"].split(" "),
         )
         return cls(
             gql_api=gql_api,
             cost_management_api=cost_management_api,
-            cost_management_console_base_url="https://console.redhat.com/openshift/cost-management",
+            cost_management_console_base_url=secret["console_base_url"],
         )
--- tools/cli_commands/test/test_cost_report.py
+++ tools/cli_commands/test/test_cost_report.py
@@ -5,6 +5,11 @@ from typing import Any
 import pytest
 from pytest_mock import MockerFixture
 
+from reconcile.gql_definitions.common.app_interface_vault_settings import (
+    AppInterfaceSettingsV1,
+)
+from reconcile.gql_definitions.cost_report.settings import CostReportSettingsV1
+from reconcile.gql_definitions.fragments.vault_secret import VaultSecret
 from reconcile.typed_queries.cost_report.app_names import App
 from tools.cli_commands.cost_report.command import CostReportCommand
 from tools.cli_commands.cost_report.model import ChildAppReport, Report, ServiceReport
@@ -28,20 +33,82 @@ def mock_cost_management_api(mocker: MockerFixture) -> Any:
     )
 
 
+VAULT_SETTINGS = AppInterfaceSettingsV1(vault=True)
+COST_REPORT_SETTINGS = CostReportSettingsV1(
+    credentials=VaultSecret(
+        path="some-path",
+        field="all",
+        version=None,
+        format=None,
+    )
+)
+
+
+@pytest.fixture
+def mock_get_app_interface_vault_settings(mocker: MockerFixture) -> Any:
+    return mocker.patch(
+        "tools.cli_commands.cost_report.command.get_app_interface_vault_settings",
+        return_value=VAULT_SETTINGS,
+    )
+
+
+@pytest.fixture
+def mock_create_secret_reader(mocker: MockerFixture) -> Any:
+    mock = mocker.patch(
+        "tools.cli_commands.cost_report.command.create_secret_reader",
+        autospec=True,
+    )
+    mock.return_value.read_all_secret.return_value = {
+        "api_base_url": "base_url",
+        "token_url": "token_url",
+        "client_id": "client_id",
+        "client_secret": "client_secret",
+        "scope": "scope",
+        "console_base_url": COST_MANAGEMENT_CONSOLE_BASE_URL,
+    }
+    return mock
+
+
+@pytest.fixture
+def mock_get_cost_report_settings(mocker: MockerFixture) -> Any:
+    return mocker.patch(
+        "tools.cli_commands.cost_report.command.get_cost_report_settings",
+        return_value=COST_REPORT_SETTINGS,
+    )
+
+
 def test_cost_report_create(
     mock_gql: Any,
     mock_cost_management_api: Any,
+    mock_get_app_interface_vault_settings: Any,
+    mock_create_secret_reader: Any,
+    mock_get_cost_report_settings: Any,
 ) -> None:
     cost_report_command = CostReportCommand.create()
 
     assert isinstance(cost_report_command, CostReportCommand)
     assert cost_report_command.gql_api == mock_gql.get_api.return_value
     assert (
-        cost_report_command.cost_management_api == mock_cost_management_api.return_value
-    )
-    assert (
         cost_report_command.cost_management_console_base_url
         == COST_MANAGEMENT_CONSOLE_BASE_URL
+    )
+    assert (
+        cost_report_command.cost_management_api == mock_cost_management_api.return_value
+    )
+    mock_cost_management_api.assert_called_once_with(
+        base_url="base_url",
+        token_url="token_url",
+        client_id="client_id",
+        client_secret="client_secret",
+        scope=["scope"],
+    )
+    mock_get_app_interface_vault_settings.assert_called_once_with(
+        mock_gql.get_api.return_value.query
+    )
+    mock_get_cost_report_settings.assert_called_once_with(mock_gql.get_api.return_value)
+    mock_create_secret_reader.assert_called_once_with(use_vault=True)
+    mock_create_secret_reader.return_value.read_all_secret.assert_called_once_with(
+        COST_REPORT_SETTINGS.credentials
     )
 
 
@@ -49,6 +116,9 @@ def test_cost_report_create(
 def cost_report_command(
     mock_gql: Any,
     mock_cost_management_api: Any,
+    mock_get_app_interface_vault_settings: Any,
+    mock_create_secret_reader: Any,
+    mock_get_cost_report_settings: Any,
 ) -> CostReportCommand:
     return CostReportCommand.create()
 
