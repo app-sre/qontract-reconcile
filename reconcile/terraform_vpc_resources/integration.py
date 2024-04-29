e480be8a4cb8c96206c184468780716a06c9f2d1
Esron Silva <essilva@essilva-thinkpadp16vgen1.rmtbr.csb>
2024-04-29 14:17:22 -0300

    Create the MR in app-interface

--- reconcile/terraform_vpc_resources/integration.py
+++ reconcile/terraform_vpc_resources/integration.py
@@ -1,17 +1,28 @@
 import logging
 import sys
-from collections.abc import Mapping
+from collections.abc import Mapping, MutableMapping
 from typing import Any, Iterable, Optional
+
+import jinja2
+from pydantic import BaseModel
 
 from reconcile.gql_definitions.fragments.aws_vpc_request import (
     AWSAccountV1,
     VPCRequest,
 )
 from reconcile.status import ExitCodes
+from reconcile.terraform_vpc_resources.merge_request import Renderer, create_parser
+from reconcile.terraform_vpc_resources.merge_request_manager import (
+    MergeRequestManager,
+    MrData,
+)
+from reconcile.typed_queries.app_interface_repo_url import get_app_interface_repo_url
 from reconcile.typed_queries.app_interface_vault_settings import (
     get_app_interface_vault_settings,
 )
 from reconcile.typed_queries.aws_vpc_requests import get_aws_vpc_requests
+from reconcile.typed_queries.github_orgs import get_github_orgs
+from reconcile.typed_queries.gitlab_instances import get_gitlab_instances
 from reconcile.utils import gql
 from reconcile.utils.runtime.integration import (
     DesiredStateShardConfig,
@@ -22,6 +33,7 @@ from reconcile.utils.secret_reader import create_secret_reader
 from reconcile.utils.semver_helper import make_semver
 from reconcile.utils.terraform_client import TerraformClient
 from reconcile.utils.terrascript_aws_client import TerrascriptClient
+from reconcile.utils.vcs import VCS
 
 QONTRACT_INTEGRATION = "terraform_vpc_resources"
 QONTRACT_INTEGRATION_VERSION = make_semver(0, 1, 0)
@@ -37,6 +49,12 @@ class TerraformVpcResourcesParams(PydanticRunParams):
 
 class NoManagedVPCForAccount(Exception):
     pass
+
+
+class OutputData(BaseModel):
+    vpc_id: str
+    private_subnet_ids: Iterable[str]
+    public_subnet_ids: Iterable[str]
 
 
 class TerraformVpcResources(QontractReconcileIntegration[TerraformVpcResourcesParams]):
@@ -60,7 +78,7 @@ class TerraformVpcResources(QontractReconcileIntegration[TerraformVpcResourcesPa
         self, requests: Iterable[VPCRequest], outputs: Mapping[str, Any]
     ) -> Mapping[str, Any]:
         """Receives a terraform outputs dict and returns a map of outputs per VPC requests"""
-        outputs_per_request = {}
+        outputs_per_request: MutableMapping[str, Any] = {}
         for request in requests:
             outputs_per_request[request.identifier] = []
             outputs_per_account = outputs[request.account.name]
@@ -68,19 +86,35 @@ class TerraformVpcResources(QontractReconcileIntegration[TerraformVpcResourcesPa
             # If the output exists for that request get its value
             # Else get None
             values = {
-                "vpc_id": outputs_per_account.get(
-                    f"{request.identifier}-vpc_id", {}
-                ).get("value"),
-                "private_subnet_ids": outputs_per_account.get(
-                    f"{request.identifier}-private_subnets", {}
-                ).get("value"),
-                "public_subnet_ids": outputs_per_account.get(
-                    f"{request.identifier}-public_subnets", {}
-                ).get("value"),
+                "static": {
+                    "vpc_id": outputs_per_account.get(
+                        f"{request.identifier}-vpc_id", {}
+                    ).get("value"),
+                    "private_subnet_ids": outputs_per_account.get(
+                        f"{request.identifier}-private_subnets", {}
+                    ).get("value", []),
+                    "public_subnet_ids": outputs_per_account.get(
+                        f"{request.identifier}-public_subnets", {}
+                    ).get("value", []),
+                    "account_name": request.account.name,
+                    "region": request.region,
+                    "cidr_block": request.cidr_block.network_address,
+                    "identifier": request.identifier,
+                }
             }
-            outputs_per_request[request.identifier].append(values)
+
+            outputs_per_request[request.identifier] = values
 
         return outputs_per_request
+
+    def _render_template(self, template: str, data: Mapping[str, Any]) -> str:
+        return jinja2.Template(
+            template,
+            undefined=jinja2.StrictUndefined,
+            trim_blocks=False,
+            lstrip_blocks=False,
+            keep_trailing_newline=False,
+        ).render(data)
 
     def run(self, dry_run: bool) -> None:
         account_name = self.params.account_name
@@ -90,7 +124,8 @@ class TerraformVpcResources(QontractReconcileIntegration[TerraformVpcResourcesPa
         vault_settings = get_app_interface_vault_settings()
         secret_reader = create_secret_reader(use_vault=vault_settings.vault)
 
-        data = get_aws_vpc_requests(gql_api=gql.get_api())
+        gql_api = gql.get_api()
+        data = get_aws_vpc_requests(gql_api=gql_api)
 
         if data:
             accounts = self._filter_accounts(data, account_name)
@@ -134,6 +169,41 @@ class TerraformVpcResources(QontractReconcileIntegration[TerraformVpcResourcesPa
         tf_client.apply()
 
         handled_output = self._handle_outputs(data, tf_client.outputs)
+
+        # MR and template Management
+        vcs = VCS(
+            secret_reader=secret_reader,
+            github_orgs=get_github_orgs(),
+            gitlab_instances=get_gitlab_instances(),
+            app_interface_repo_url=get_app_interface_repo_url(),
+            dry_run=dry_run,
+            allow_deleting_mrs=False,
+            allow_opening_mrs=True,
+        )
+
+        mr_manager = MergeRequestManager(
+            vcs=vcs,
+            renderer=Renderer(),
+            parser=create_parser(),
+            auto_merge_enabled=True,
+        )
+
+        mr_manager._fetch_managed_open_merge_requests()
+
+        # Create a MR for each vpc request if the MR don't exist yet
+        for _, outputs in handled_output.items():
+            template = gql_api.get_template(
+                path="/templating/templates/terraform-vpc-resources/vpc.yml"
+            )["template"]
+            content = self._render_template(template=template, data=outputs)
+
+            mr_manager.create_merge_request(
+                MrData(
+                    account=outputs["static"]["account_name"],
+                    content=content,
+                    path=f"data/aws/{outputs['static']['account_name']}/vpcs/{outputs['static']['identifier']}.yml",
+                )
+            )
 
     def get_desired_state_shard_config(self) -> DesiredStateShardConfig:
         return DesiredStateShardConfig(
