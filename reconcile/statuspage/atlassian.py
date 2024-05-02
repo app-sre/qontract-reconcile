a898acbec62a6c96b5e923c7423ddab8972377da
Maor Friedman <mafriedm@redhat.com>
2024-05-02 12:32:32 +0300

    statuspage-components atlassian retry get

--- reconcile/statuspage/atlassian.py
+++ reconcile/statuspage/atlassian.py
@@ -8,6 +8,8 @@ from typing import (
 import requests
 import statuspageio  # type: ignore
 from pydantic import BaseModel
+from requests import Response
+from sretoolbox.utils import retry
 
 from reconcile.gql_definitions.statuspage.statuspages import StatusPageV1
 from reconcile.statuspage.page import (
@@ -64,16 +66,20 @@ class LegacyLibAtlassianAPI:
             api_key=self.token, page_id=self.page_id, organization_id="unset"
         )
 
+    @retry(max_attempts=10)
+    def _do_get(self, url: str, params: dict[str, Any]) -> Response:
+        response = requests.get(url, params=params, headers=self.auth_headers)
+        response.raise_for_status()
+        return response
+
     def list_components(self) -> list[AtlassianRawComponent]:
         url = f"{self.api_url}/v1/pages/{self.page_id}/components"
-        headers = {"Authorization": f"OAuth {self.token}"}
         all_components: list[AtlassianRawComponent] = []
         page = 1
         per_page = 100
         while True:
             params = {"page": page, "per_page": per_page}
-            response = requests.get(url, params=params, headers=headers)
-            response.raise_for_status()
+            response = self._do_get(url, params=params)
             components = [AtlassianRawComponent(**c) for c in response.json()]
             all_components += components
             if len(components) < per_page:
