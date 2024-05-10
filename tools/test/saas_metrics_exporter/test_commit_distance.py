a1b825b6d7bf082a643e63c74771a5a87b78d70f
Karl Fischer <kfischer@redhat.com>
2024-05-10 14:06:28 +0200

    APPSRE-10053 add test case

--- tools/test/saas_metrics_exporter/test_commit_distance.py
+++ tools/test/saas_metrics_exporter/test_commit_distance.py
@@ -178,3 +178,114 @@ def test_commit_distance_single_pub_sub_pair(
             ),
         )
     ]
+
+
+def test_commit_distance_multiple_distances(
+    saas_files_builder: Callable[[Iterable[Mapping]], list[SaasFile]],
+    vcs_builder: Callable[[Mapping], VCS],
+) -> None:
+    saas_files = saas_files_builder([
+        {
+            "path": "/saas1.yml",
+            "name": "saas_1",
+            "app": {
+                "name": "APP",
+            },
+            "resourceTemplates": [
+                {
+                    "name": "template_1",
+                    "url": "repo1/url",
+                    "targets": [
+                        {
+                            "ref": "main",
+                            "namespace": {
+                                "path": "/namespace1.yml",
+                                "name": "publisher_namespace1",
+                            },
+                            "promotion": {
+                                "publish": ["channel-a"],
+                            },
+                        },
+                        {
+                            "ref": "refabc",
+                            "namespace": {
+                                "path": "/namespace1.yml",
+                                "name": "publisher_namespace2",
+                            },
+                            "promotion": {
+                                "publish": ["channel-b"],
+                            },
+                        },
+                    ],
+                }
+            ],
+        },
+        {
+            "path": "/saas2.yml",
+            "name": "saas_2",
+            "app": {
+                "name": "APP",
+            },
+            "publishJobLogs": True,
+            "resourceTemplates": [
+                {
+                    "name": "template_2",
+                    "url": "repo2/url",
+                    "targets": [
+                        {
+                            "ref": "test123",
+                            "namespace": {
+                                "path": "/namespace2.yml",
+                                "name": "subscriber_namespace1",
+                            },
+                            "promotion": {
+                                "subscribe": ["channel-a"],
+                            },
+                        },
+                        {
+                            "ref": "test456",
+                            "namespace": {
+                                "path": "/namespace3.yml",
+                                "name": "subscriber_namespace2",
+                            },
+                            "promotion": {
+                                "subscribe": ["channel-b"],
+                            },
+                        },
+                    ],
+                }
+            ],
+        },
+    ])
+    vcs = vcs_builder({
+        "test123/main": 2,
+        "test456/refabc": 4,
+    })
+    commit_distance_fetcher = CommitDistanceFetcher(vcs=vcs)
+    commit_distance_metrics = commit_distance_fetcher.fetch(
+        saas_files=saas_files, thread_pool_size=1
+    )
+    assert commit_distance_metrics == [
+        CommitDistanceMetric(
+            value=2.0,
+            metric=SaasCommitDistanceGauge(
+                channel="channel-a",
+                app="APP",
+                publisher="NoName",
+                publisher_namespace="publisher_namespace1",
+                subscriber="NoName",
+                subscriber_namespace="subscriber_namespace1",
+            ),
+        ),
+        CommitDistanceMetric(
+            value=4.0,
+            metric=SaasCommitDistanceGauge(
+                channel="channel-b",
+                app="APP",
+                publisher="NoName",
+                publisher_namespace="publisher_namespace2",
+                subscriber="NoName",
+                subscriber_namespace="subscriber_namespace2",
+            ),
+        ),
+    ]
