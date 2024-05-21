79356605b77d7266565c882bfabb6e5dd2e40817
Mehfuz Khan <mekhan@redhat.com>
2024-05-21 19:30:21 +0530

    mypy

--- reconcile/utils/batches.py
+++ reconcile/utils/batches.py
@@ -1,7 +1,9 @@
+from collections.abc import Generator, Iterable
 from itertools import islice
+from typing import Any
 
 
-def batched(iterable, size):
+def batched(iterable: Iterable[Any], size: int) -> Generator:
     if size < 1:
         raise ValueError("n must be at least one")
     it = iter(iterable)
