126b22794081618941e572c4e28fc161fc568deb
Maor Friedman <mafriedm@redhat.com>
2024-05-30 08:15:36 +0300

    do not raise on no pagerduty instances

--- reconcile/typed_queries/pagerduty_instances.py
+++ reconcile/typed_queries/pagerduty_instances.py
@@ -6,7 +6,6 @@ from reconcile.gql_definitions.common.pagerduty_instances import (
     query,
 )
 from reconcile.utils import gql
-from reconcile.utils.exceptions import AppInterfaceSettingsError
 
 
 def get_pagerduty_instances(
@@ -16,7 +15,4 @@ def get_pagerduty_instances(
     if not query_func:
         gqlapi = gql.get_api()
         query_func = gqlapi.query
-    pagerduty_instances = query(query_func=query_func).pagerduty_instances
-    if not pagerduty_instances:
-        raise AppInterfaceSettingsError("no pagerduty instance(s) configured")
-    return pagerduty_instances
+    return query(query_func=query_func).pagerduty_instances or []
