64ea350e9a104f41c32458db9a4f43fdab072342
Mehfuz Khan <mekhan@redhat.com>
2024-05-31 00:24:35 +0530

    added tests

--- reconcile/gitlab_permissions.py
+++ reconcile/gitlab_permissions.py
@@ -17,7 +17,8 @@ def run(dry_run, defer=None):
         defer(gl.cleanup)
     repos = queries.get_repos(server=gl.server, exclude_manage_permissions=True)
     group_id, shared_projects = gl.get_group_id_and_shared_projects(APP_SRE_GROUP_NAME)
-    repos_to_share = set(repos) - {project["web_url"] for project in shared_projects}
+    shared_project_repos = {project["web_url"] for project in shared_projects}
+    repos_to_share = set(repos) - shared_project_repos
     for repo in repos_to_share:
         gl.share_project_with_group(repo_url=repo, group_id=group_id, dry_run=dry_run)
 
