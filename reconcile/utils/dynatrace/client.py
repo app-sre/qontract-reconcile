931aa93f0cd3406d2e3252edcb46d66d0c377083
Karl Fischer <kfischer@redhat.com>
2024-06-10 14:24:19 +0200

    APPSRE-10229 dynatrace client

---
+++ reconcile/utils/dynatrace/client.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from collections.abc import Iterable
+
+from dynatrace import Dynatrace
+
+
+class DynatraceTokenCreationError(Exception):
+    pass
+
+
+class DynatraceClient:
+    def __init__(self, environment_url: str, api: Dynatrace) -> None:
+        self._environment_url = environment_url
+        self._api = api
+
+    def create_api_token(self, name: str, scopes: Iterable[str]) -> str:
+        try:
+            token = self._api.tokens.create(name=name, scopes=scopes)
+        except Exception as e:
+            raise DynatraceTokenCreationError(
+                f"{self._environment_url=} Failed to create token for {name=}", e
+            ) from e
+        return token.token
+
+    @staticmethod
+    def create(
+        environment_url: str, token: str | None, api: Dynatrace | None
+    ) -> DynatraceClient:
+        if not api:
+            api = Dynatrace(base_url=environment_url, token=token)
+        return DynatraceClient(environment_url=environment_url, api=api)
