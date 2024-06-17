2cf7da892281f025a5282b31055106a852aa9852
Di Wang <dwan@redhat.com>
2024-06-17 18:05:34 +1000

    Populate node pools in spec for ocm-upgrade-schedulr-org
    Signed-off-by: Di Wang <dwan@redhat.com>

--- reconcile/aus/node_pool_spec.py
+++ reconcile/aus/node_pool_spec.py
@@ -1,5 +1,8 @@
+from collections.abc import Iterable, Mapping
+
 from reconcile.aus.models import NodePoolSpec
 from reconcile.utils.ocm import get_node_pools
+from reconcile.utils.ocm.base import ClusterDetails
 from reconcile.utils.ocm.clusters import get_version
 from reconcile.utils.ocm_base_client import OCMBaseClient
 
@@ -14,3 +17,21 @@ def get_node_pool_specs(ocm_api: OCMBaseClient, cluster_id: str) -> list[NodePoo
         for p in node_pools
         if (version_id := p.get("version", {}).get("id"))
     ]
+
+
+def get_node_pool_specs_by_org_cluster(
+    ocm_api: OCMBaseClient,
+    clusters_by_org: Mapping[str, Iterable[ClusterDetails]],
+) -> dict[str, dict[str, list[NodePoolSpec]]]:
+    """
+    Fetch node pool specs for all rosa hypershift clusters
+    Returns a dict with org IDs as keys, the values are dicts with cluster id as key
+    """
+    return {
+        org_id: {
+            c.ocm_cluster.id: get_node_pool_specs(ocm_api, c.ocm_cluster.id)
+            for c in clusters
+            if c.ocm_cluster.is_rosa_hypershift()
+        }
+        for org_id, clusters in clusters_by_org.items()
+    }
