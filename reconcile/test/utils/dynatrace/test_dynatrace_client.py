4a826af9d79070fc7615ace3f09ce8405475ff8a
Karl Fischer <fishi0x01@users.noreply.github.com>
2024-07-19 13:05:59 +0200

    APPSRE-10615 DT token scope updates (#4558)

--- reconcile/test/utils/dynatrace/test_dynatrace_client.py
+++ reconcile/test/utils/dynatrace/test_dynatrace_client.py
@@ -1,9 +1,8 @@
-from collections.abc import Callable, Mapping
-
-from dynatrace import Dynatrace
 from pytest import raises
 
+from reconcile.test.utils.dynatrace.fixtures import build_dynatrace_api
 from reconcile.utils.dynatrace.client import (
+    DynatraceAPIToken,
     DynatraceAPITokenCreated,
     DynatraceClient,
     DynatraceTokenCreationError,
@@ -11,12 +10,10 @@ from reconcile.utils.dynatrace.client import (
 )
 
 
-def test_dynatrace_create_token_success(
-    dynatrace_api_builder: Callable[[Mapping], Dynatrace],
-) -> None:
-    api = dynatrace_api_builder({"CREATE_TOKEN_RESULT": ("id1", "test-token")})
+def test_dynatrace_create_token_success() -> None:
+    api = build_dynatrace_api(create_token_id="id1", create_token_token="test-token")
 
-    client = DynatraceClient.create(environment_url="test-env", token=None, api=api)
+    client = DynatraceClient(environment_url="test-env", api=api)
     token = client.create_api_token(name="test-token-name", scopes=["test-scope"])
 
     assert token == DynatraceAPITokenCreated(token="test-token", id="id1")
@@ -25,12 +22,10 @@ def test_dynatrace_create_token_success(
     )
 
 
-def test_dynatrace_create_token_error(
-    dynatrace_api_builder: Callable[[Mapping], Dynatrace],
-) -> None:
-    api = dynatrace_api_builder({"CREATE_TOKEN_RESULT": Exception("test-error")})
+def test_dynatrace_create_token_error() -> None:
+    api = build_dynatrace_api(create_error=Exception("test-error"))
 
-    client = DynatraceClient.create(environment_url="test-env", token=None, api=api)
+    client = DynatraceClient(environment_url="test-env", api=api)
 
     with raises(DynatraceTokenCreationError):
         client.create_api_token(name="test-token", scopes=["test-scope"])
@@ -38,50 +33,65 @@ def test_dynatrace_create_token_error(
     api.tokens.create.assert_called_once_with(name="test-token", scopes=["test-scope"])
 
 
-def test_dynatrace_token_ids_for_name_prefix_success(
-    dynatrace_api_builder: Callable[[Mapping], Dynatrace],
-) -> None:
-    api = dynatrace_api_builder({
-        "LIST_TOKEN_RESULT": [
+def test_dynatrace_token_ids_for_name_prefix_success() -> None:
+    api = build_dynatrace_api(
+        list_tokens=[
             ("test-prefix-1", "123"),
             ("test-prefix-2", "456"),
             ("filter-this", "789"),
         ]
-    })
+    )
 
-    client = DynatraceClient.create(environment_url="test-env", token=None, api=api)
+    client = DynatraceClient(environment_url="test-env", api=api)
     token_ids = client.get_token_ids_for_name_prefix(prefix="test-prefix")
 
     assert token_ids == ["123", "456"]
     api.tokens.list.assert_called_once_with()
 
 
-def test_dynatrace_token_ids_for_name_empty_prefix_success(
-    dynatrace_api_builder: Callable[[Mapping], Dynatrace],
-) -> None:
-    api = dynatrace_api_builder({
-        "LIST_TOKEN_RESULT": [
+def test_dynatrace_token_ids_for_name_empty_prefix_success() -> None:
+    api = build_dynatrace_api(
+        list_tokens=[
             ("test-prefix-1", "123"),
             ("test-prefix-2", "456"),
             ("filter-this", "789"),
         ]
-    })
+    )
 
-    client = DynatraceClient.create(environment_url="test-env", token=None, api=api)
+    client = DynatraceClient(environment_url="test-env", api=api)
     token_ids = client.get_token_ids_for_name_prefix(prefix="")
 
     assert token_ids == ["123", "456", "789"]
     api.tokens.list.assert_called_once_with()
 
 
-def test_dynatrace_token_ids_for_name_prefix_error(
-    dynatrace_api_builder: Callable[[Mapping], Dynatrace],
-) -> None:
-    api = dynatrace_api_builder({"LIST_TOKEN_RESULT": Exception("test-error")})
+def test_dynatrace_token_ids_for_name_prefix_error() -> None:
+    api = build_dynatrace_api(list_error=Exception("test-error"))
 
-    client = DynatraceClient.create(environment_url="test-env", token=None, api=api)
+    client = DynatraceClient(environment_url="test-env", api=api)
 
     with raises(DynatraceTokenRetrievalError):
         client.get_token_ids_for_name_prefix(prefix="test-prefix")
 
     api.tokens.list.assert_called_once_with()
+
+
+def test_dynatrace_get_token_success() -> None:
+    api = build_dynatrace_api(get_token_id="id1", get_token_scopes=["test-scope"])
+
+    client = DynatraceClient(environment_url="test-env", api=api)
+    token = client.get_token_by_id(token_id="id1")
+
+    assert token == DynatraceAPIToken(id="id1", scopes=["test-scope"])
+    api.tokens.get.assert_called_once_with(token_id="id1")
+
+
+def test_dynatrace_get_token_error() -> None:
+    api = build_dynatrace_api(get_error=Exception("test-error"))
+
+    client = DynatraceClient(environment_url="test-env", api=api)
+
+    with raises(DynatraceTokenRetrievalError):
+        client.get_token_by_id(token_id="id1")
+
+    api.tokens.get.assert_called_once_with(token_id="id1")
