99938de78c55bc513d8aade0643c0b2a2aaa3fcb
Maor Friedman <mafriedm@redhat.com>
2024-07-29 11:08:09 +0300

    [saas-file-validator] validate quay image patterns in quay organizations (#4569)
    * saas-file-validator validate quay image patterns in quay organizations
    
    * better, faster, stronger

--- reconcile/typed_queries/quay.py
+++ reconcile/typed_queries/quay.py
@@ -1,7 +1,23 @@
-from reconcile.gql_definitions.common.quay_instances import QuayInstanceV1, query
+from reconcile.gql_definitions.common.quay_instances import (
+    QuayInstanceV1,
+)
+from reconcile.gql_definitions.common.quay_instances import (
+    query as quay_instances_query,
+)
+from reconcile.gql_definitions.common.quay_orgs import (
+    QuayOrgV1,
+)
+from reconcile.gql_definitions.common.quay_orgs import (
+    query as quay_orgs_query,
+)
 from reconcile.utils import gql
 
 
 def get_quay_instances() -> list[QuayInstanceV1]:
-    data = query(gql.get_api().query)
+    data = quay_instances_query(gql.get_api().query)
     return list(data.instances or [])
+
+
+def get_quay_orgs() -> list[QuayOrgV1]:
+    data = quay_orgs_query(gql.get_api().query)
+    return list(data.orgs or [])
