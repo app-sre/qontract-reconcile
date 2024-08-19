3f7994dce13aa805f02220b4254257bc15d4e0c2
Maor Friedman <mafriedm@redhat.com>
2024-08-19 13:05:17 +0300

    more rename

--- reconcile/cli.py
+++ reconcile/cli.py
@@ -3559,7 +3559,7 @@ def change_owners(
 
 @integration.command(short_help="Analyze bundle diffs by change types.")
 @click.pass_context
-def change_management(ctx):
+def change_management_tracking(ctx):
     import reconcile.change_owners.change_management_tracking
 
     run_class_integration(
