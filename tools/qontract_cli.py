f925c045aa49a1477f4d0ddaf8dbd96fb9bffa2d
Maor Friedman <mafriedm@redhat.com>
2024-08-27 10:58:00 +0300

    cleanup

--- tools/qontract_cli.py
+++ tools/qontract_cli.py
@@ -2868,10 +2868,8 @@ def change_log_tracking(ctx):
     repo_url = get_app_interface_repo_url()
     change_types = fetch_change_type_processors(gql.get_api(), NoOpFileDiffResolver())
     state = init_state(integration=ChangeLogIntegration().name)
-    # change_log: dict[str, Any] = state.get("bundle-diffs.json")
     change_log = ChangeLog(**state.get("bundle-diffs.json"))
     data: list[dict[str, str]] = []
-    # for commit, covered_change_types in change_log.items():
     for change_log_item in change_log.items:
         commit = change_log_item.commit
         covered_change_types_descriptions = [
