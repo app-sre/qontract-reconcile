9f8ba1e8be9e84e1f66d1665b017d98bf8ee8f66
Jordi Piriz <jpiriz@redhat.com>
2024-10-09 10:50:46 +0200

    Change Mapping to static inventories list

--- reconcile/utils/external_resources.py
+++ reconcile/utils/external_resources.py
@@ -8,6 +8,7 @@ from typing import Any
 
 import anymarkup
 
+from reconcile.external_resources.model import ExternalResourcesInventory
 from reconcile.utils import (
     gql,
     metrics,
@@ -16,6 +17,7 @@ from reconcile.utils.exceptions import FetchResourceError
 from reconcile.utils.external_resource_spec import (
     ExternalResourceInventoryGauge,
     ExternalResourceSpec,
+    ExternalResourceSpecInventory,
 )
 
 PROVIDER_AWS = "aws"
@@ -65,14 +67,17 @@ def managed_external_resources(namespace_info: Mapping[str, Any]) -> bool:
 
 
 def get_inventory_count_combinations(
-    inventory: Mapping,
+    inventory: ExternalResourceSpecInventory | ExternalResourcesInventory,
 ) -> Counter[tuple]:
     return Counter(
         (k.provision_provider, k.provisioner_name, k.provider) for k in inventory
     )
 
 
-def publish_metrics(inventory: Mapping, integration: str) -> None:
+def publish_metrics(
+    inventory: ExternalResourceSpecInventory | ExternalResourcesInventory,
+    integration: str,
+) -> None:
     count_combinations = get_inventory_count_combinations(inventory)
     integration_name = metrics.normalize_integration_name(integration)
     for combination, count in count_combinations.items():
