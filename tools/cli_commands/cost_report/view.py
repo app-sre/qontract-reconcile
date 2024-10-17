a1efb2d9ac392915b7f05694f768c33bcd6a04d2
Di Wang <dwan@redhat.com>
2024-10-17 16:53:12 +1100

    feat(cost-report): add more info to optimization report
    Signed-off-by: Di Wang <dwan@redhat.com>

--- tools/cli_commands/cost_report/view.py
+++ tools/cli_commands/cost_report/view.py
@@ -32,6 +32,15 @@ OPENSHIFT_HEADER = """\
 
 OPTIMIZATION_HEADER = """\
 # OpenShift Cost Optimization Report
+
+Report is generated for optimization enabled namespaces
+(`insights_cost_management_optimizations='true'` in namespace `labels`).
+
+In Cost optimizations, recommendations get generated when
+CPU usage is at or above the 60th percentile and
+memory usage is at the 100th percentile.
+
+View details in [Cost Management Optimizations](https://console.redhat.com/openshift/cost-management/optimizations).
 """
 
 AWS_SUMMARY = """\
--- tools/cli_commands/test/fixtures/openshift_cost_optimization_report.md
+++ tools/cli_commands/test/fixtures/openshift_cost_optimization_report.md
@@ -2,6 +2,15 @@
 
 # OpenShift Cost Optimization Report
 
+Report is generated for optimization enabled namespaces
+(`insights_cost_management_optimizations='true'` in namespace `labels`).
+
+In Cost optimizations, recommendations get generated when
+CPU usage is at or above the 60th percentile and
+memory usage is at the 100th percentile.
+
+View details in [Cost Management Optimizations](https://console.redhat.com/openshift/cost-management/optimizations).
+
 ## app
 
 ```json:table
