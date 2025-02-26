1ded7ee064806a5730c6622e8a796f253ea77ea5
Suzana Nesic <snesic@redhat.com>
2025-02-26 11:48:16 -0500

    reformatting for cleaner lines

--- reconcile/utils/terraform_client.py
+++ reconcile/utils/terraform_client.py
@@ -716,24 +716,18 @@ class TerraformClient:  # pylint: disable=too-many-public-methods
                 response = self._aws_api.describe_rds_db_instance(
                     account_name, resource_name, region_name=region_name
                 )
-                if (
-                    len(
-                        response["DBInstances"][0].get(
-                            "ReadReplicaDBInstanceIdentifiers"
-                        )
-                    )
-                    > 0
-                ):
-                    replica_db = response["DBInstances"][0].get(
-                        "ReadReplicaDBInstanceIdentifiers"
-                    )[0]
+                replica_identifiers = response["DBInstances"][0].get(
+                    "ReadReplicaDBInstanceIdentifiers"
+                )
+                if len(replica_identifiers) > 0:
+                    replica_db = replica_identifiers[0]
                     replica_db_response = self._aws_api.describe_rds_db_instance(
                         account_name, replica_db, region_name=region_name
                     )
-                    replica_db_engine_version = replica_db_response["DBInstances"][
-                        0
-                    ].get("EngineVersion")
-                    if after.get("engine_version") > replica_db_engine_version:
+                    replica_version = replica_db_response["DBInstances"][0].get(
+                        "EngineVersion"
+                    )
+                    if after.get("engine_version") > replica_version:
                         raise RdsUpgradeValidationError(
                             "The replica database is running a previous version from the primary and needs to have an upgrade first."
                         )
