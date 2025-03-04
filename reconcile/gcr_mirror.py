61e1771f116b70441f07138e6c2d70b7eac8b981
Ryan Wallace <rywallac@redhat.com>
2025-03-04 16:24:02 -0500

    add more explicit typing & logic to handle artifact registry

--- reconcile/gcr_mirror.py
+++ reconcile/gcr_mirror.py
@@ -4,10 +4,10 @@ import os
 import re
 import tempfile
 import time
-from collections import defaultdict
 from typing import Any, Self
 
 import requests
+from pydantic import BaseModel
 from sretoolbox.container import (
     Image,
     Skopeo,
@@ -15,57 +15,31 @@ from sretoolbox.container import (
 from sretoolbox.container.image import ImageComparisonError
 from sretoolbox.container.skopeo import SkopeoCmdError
 
+import reconcile.gql_definitions.gcr_mirror.gcp_projects as gql_gcp_projects
+import reconcile.gql_definitions.gcr_mirror.gcr_repos as gql_gcr_repos
 from reconcile import queries
 from reconcile.utils import gql
 from reconcile.utils.secret_reader import SecretReader
-
-_LOG = logging.getLogger(__name__)
 
 QONTRACT_INTEGRATION = "gcr-mirror"
 REQUEST_TIMEOUT = 60
 
 
+class GcrSyncItem(BaseModel):
+    name: str
+    mirror: gql_gcr_repos.ContainerImageMirrorV1
+    server_url: str
+    org_name: str
+
+
+class SyncTask(BaseModel):
+    mirror_creds: str
+    mirror_url: str
+    image_url: str
+    org_name: str
+
+
 class QuayMirror:
-    GCR_PROJECT_CATALOG_QUERY = """
-    {
-      projects: gcp_projects_v1 {
-        name
-        pushCredentials {
-          path
-          field
-          version
-          format
-        }
-      }
-    }
-    """
-
-    GCR_REPOS_QUERY = """
-    {
-      apps: apps_v1 {
-        gcrRepos {
-          project {
-            name
-          }
-          items {
-            name
-            mirror {
-              url
-              pullCredentials {
-                path
-                field
-                version
-                format
-              }
-              tags
-              tagsExclude
-            }
-          }
-        }
-      }
-    }
-    """
-
     def __init__(self, dry_run: bool = False) -> None:
         self.dry_run = dry_run
         self.gqlapi = gql.get_api()
@@ -83,41 +57,43 @@ class QuayMirror:
 
     def run(self) -> None:
         sync_tasks = self.process_sync_tasks()
-        for org, data in sync_tasks.items():
-            for item in data:
-                try:
-                    self.skopeo_cli.copy(
-                        src_image=item["mirror_url"],
-                        src_creds=item["mirror_creds"],
-                        dst_image=item["image_url"],
-                        dest_creds=self.push_creds[org],
-                    )
-                except SkopeoCmdError as details:
-                    _LOG.error("[%s]", details)
+        for task in sync_tasks:
+            try:
+                self.skopeo_cli.copy(
+                    src_image=task.mirror_url,
+                    src_creds=task.mirror_creds,
+                    dst_image=task.image_url,
+                    dest_creds=self.push_creds[task.org_name],
+                )
+            except SkopeoCmdError as details:
+                logging.error("[%s]", details)
 
-    def process_repos_query(self) -> dict[str, list[dict[str, Any]]]:
-        result = self.gqlapi.query(self.GCR_REPOS_QUERY)
+    def process_repos_query(self) -> list[GcrSyncItem]:
+        result = gql_gcr_repos.query(self.gqlapi.query())
+        summary = list[GcrSyncItem]()
 
-        summary = defaultdict(list)
+        if result.apps:
+            for app in result.apps:
+                if app.gcr_repos:
+                    for project in app.gcr_repos:
+                        for gcr_repo in project.items:
+                            if gcr_repo.mirror:
+                                project_name = project.project.name
+                                server_url = "gcr.io"
+                                artifact_registry = gcr_repo.artifact_registry
+                                if artifact_registry and artifact_registry.enabled:
+                                    server_url = (
+                                        f"{artifact_registry.location}-docker.pkg.dev"
+                                    )
 
-        for app in result["apps"]:
-            gcr_repos = app.get("gcrRepos")
-
-            if gcr_repos is None:
-                continue
-
-            for gcr_repo in gcr_repos:
-                project = gcr_repo["project"]["name"]
-                server_url = gcr_repo["project"].get("serverUrl") or "gcr.io"
-                for item in gcr_repo["items"]:
-                    if item["mirror"] is None:
-                        continue
-
-                    summary[project].append({
-                        "name": item["name"],
-                        "mirror": item["mirror"],
-                        "server_url": server_url,
-                    })
+                                summary.append(
+                                    GcrSyncItem(
+                                        name=gcr_repo.name,
+                                        mirror=gcr_repo.mirror,
+                                        server_url=server_url,
+                                        org_name=project_name,
+                                    )
+                                )
 
         return summary
 
@@ -139,99 +115,104 @@ class QuayMirror:
         # tag must be synced
         return True
 
-    def process_sync_tasks(self) -> dict[str, list[dict[str, Any]]]:
+    def process_sync_tasks(self) -> list[SyncTask]:
         eight_hours = 28800  # 60 * 60 * 8
         is_deep_sync = self._is_deep_sync(interval=eight_hours)
 
         summary = self.process_repos_query()
 
-        sync_tasks = defaultdict(list)
-        for org, data in summary.items():
-            for item in data:
-                image = Image(
-                    f"{item['server_url']}/{org}/{item['name']}",
-                    session=self.session,
-                    timeout=REQUEST_TIMEOUT,
-                )
+        sync_tasks = list[SyncTask]()
+        for item in summary:
+            image = Image(
+                f"{item.server_url}/{item.org_name}/{item.name}",
+                session=self.session,
+                timeout=REQUEST_TIMEOUT,
+            )
 
-                mirror_url = item["mirror"]["url"]
+            mirror_url = item.mirror.url
 
-                username = None
-                password = None
-                mirror_creds = None
-                if item["mirror"]["pullCredentials"] is not None:
-                    pull_credentials = item["mirror"]["pullCredentials"]
-                    raw_data = self.secret_reader.read_all(pull_credentials)
-                    username = raw_data["user"]
-                    password = raw_data["token"]
-                    mirror_creds = f"{username}:{password}"
+            username = None
+            password = None
+            mirror_creds = None
+            pull_credentials = item.mirror.pull_credentials
+            if pull_credentials:
+                raw_data = self.secret_reader.read_all(pull_credentials.dict())
+                username = raw_data["user"]
+                password = raw_data["token"]
+                mirror_creds = f"{username}:{password}"
 
-                image_mirror = Image(
-                    mirror_url,
-                    username=username,
-                    password=password,
-                    session=self.session,
-                    timeout=REQUEST_TIMEOUT,
-                )
+            image_mirror = Image(
+                mirror_url,
+                username=username,
+                password=password,
+                session=self.session,
+                timeout=REQUEST_TIMEOUT,
+            )
 
-                tags = item["mirror"].get("tags")
-                tags_exclude = item["mirror"].get("tagsExclude")
-
-                for tag in image_mirror:
+            if item.mirror.tags:
+                for tag in item.mirror.tags:
                     if not self.sync_tag(
-                        tags=tags, tags_exclude=tags_exclude, candidate=tag
+                        tags=item.mirror.tags,
+                        tags_exclude=item.mirror.tags_exclude,
+                        candidate=tag,
                     ):
                         continue
 
-                    upstream = image_mirror[tag]
-                    downstream = image[tag]
+                    upstream = image_mirror.tag
+                    downstream = image.tag
                     if tag not in image:
-                        _LOG.debug(
+                        logging.debug(
                             "Image %s and mirror %s are out off sync",
                             downstream,
                             upstream,
                         )
-                        sync_tasks[org].append({
-                            "mirror_url": str(upstream),
-                            "mirror_creds": mirror_creds,
-                            "image_url": str(downstream),
-                        })
+                        sync_tasks.append(
+                            SyncTask(
+                                mirror_url=str(upstream),
+                                mirror_creds=mirror_creds,
+                                image_url=str(downstream),
+                                org_name=item.org_name,
+                            )
+                        )
                         continue
 
                     # Deep (slow) check only in non dry-run mode
                     if self.dry_run:
-                        _LOG.debug(
+                        logging.debug(
                             "Image %s and mirror %s are in sync", downstream, upstream
                         )
                         continue
 
                     # Deep (slow) check only from time to time
                     if not is_deep_sync:
-                        _LOG.debug(
+                        logging.debug(
                             "Image %s and mirror %s are in sync", downstream, upstream
                         )
                         continue
 
                     try:
                         if downstream == upstream:
-                            _LOG.debug(
+                            logging.debug(
                                 "Image %s and mirror %s are in sync",
                                 downstream,
                                 upstream,
                             )
                             continue
                     except ImageComparisonError as details:
-                        _LOG.error("[%s]", details)
+                        logging.error("[%s]", details)
                         continue
 
-                    _LOG.debug(
+                    logging.debug(
                         "Image %s and mirror %s are out of sync", downstream, upstream
                     )
-                    sync_tasks[org].append({
-                        "mirror_url": str(upstream),
-                        "mirror_creds": mirror_creds,
-                        "image_url": str(downstream),
-                    })
+                    sync_tasks.append(
+                        SyncTask(
+                            mirror_url=str(upstream),
+                            mirror_creds=mirror_creds,
+                            image_url=str(downstream),
+                            org_name=item.org_name,
+                        )
+                    )
 
         return sync_tasks
 
@@ -258,18 +239,17 @@ class QuayMirror:
             file_object.write(str(time.time()))
 
     def _get_push_creds(self) -> dict[str, str]:
-        result = self.gqlapi.query(self.GCR_PROJECT_CATALOG_QUERY)
+        result = gql_gcp_projects.query(self.gqlapi.query())
 
-        creds = {}
-        for project_data in result["projects"]:
-            push_secret = project_data["pushCredentials"]
-            if push_secret is None:
-                continue
-
-            raw_data = self.secret_reader.read_all(push_secret)
-            project = project_data["name"]
-            token = base64.b64decode(raw_data["token"]).decode()
-            creds[project] = f"{raw_data['user']}:{token}"
+        creds = dict[str, str]()
+        if result.projects:
+            for project_data in result.projects:
+                push_secret = project_data.push_credentials
+                if push_secret:
+                    raw_data = self.secret_reader.read_all(push_secret.dict())
+                    project_name = project_data.name
+                    token = base64.b64decode(raw_data["token"]).decode()
+                    creds[project_name] = f"{raw_data['user']}:{token}"
         return creds
 
 
