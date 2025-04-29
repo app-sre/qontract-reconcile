e8e73e48cfb2f502e9cf34b15a14559fb071bdef
Karl Fischer <fishi0x01@users.noreply.github.com>
2025-04-29 14:47:00 +0200

    APPSRE-11142 allow multiple specs per cluster (#4965)

--- reconcile/dynatrace_token_provider/model.py
+++ reconcile/dynatrace_token_provider/model.py
@@ -12,3 +12,16 @@ class K8sSecret(BaseModel):
     namespace_name: str
     secret_name: str
     tokens: list[DynatraceAPIToken]
+
+
+class TokenSpecTenantBinding(BaseModel):
+    spec_name: str
+    tenant_id: str
+
+
+class Cluster(BaseModel):
+    id: str
+    external_id: str
+    organization_id: str
+    is_hcp: bool
+    dt_token_bindings: list[TokenSpecTenantBinding]
