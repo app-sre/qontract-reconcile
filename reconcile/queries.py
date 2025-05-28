f5f31e8717183e3eaaba1e555bd5a6102dd29f04
biswassri <srijoni.biswas1994@gmail.com>
2025-05-28 12:02:54 -0400

    Deprecate slackubername

--- reconcile/queries.py
+++ reconcile/queries.py
@@ -1776,7 +1776,6 @@ USERS_QUERY = """
     name
     org_username
     github_username
-    slack_username
     pagerduty_username
     public_gpg_key
     {% if refs %}
@@ -1807,7 +1806,6 @@ ROLES_QUERY = """
     name
     org_username
     github_username
-    slack_username
     tag_on_cluster_updates
     labels
     roles {
