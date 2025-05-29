72556a5b931c2c2ad0e5b4e64e06e67f7424adbb
Jordi Freixa Serrabassa <jfreixa@redhat.com>
2025-05-29 12:22:52 +0200

    Improve asserts

--- reconcile/test/ldap_users/test_ldap_users.py
+++ reconcile/test/ldap_users/test_ldap_users.py
@@ -70,34 +70,47 @@ def test_run(
     ldap_users.run(False, app_interface_gitlab_project_id, infra_gitlab_project_id)
 
     assert mocked_mr_client_gateway.call_count == 2
-    assert mocked_mr_client_gateway.call_args_list == [
-        call(gitlab_project_id=app_interface_gitlab_project_id, sqs_or_gitlab="gitlab"),
-        call(gitlab_project_id=infra_gitlab_project_id, sqs_or_gitlab="gitlab"),
-    ]
+    mocked_mr_client_gateway.assert_has_calls(
+        [
+            call(
+                gitlab_project_id=app_interface_gitlab_project_id,
+                sqs_or_gitlab="gitlab",
+            ),
+            call(gitlab_project_id=infra_gitlab_project_id, sqs_or_gitlab="gitlab"),
+        ],
+        any_order=True,
+    )
     assert mocked_create_delete_user_app_interface.call_count == 2
-    assert mocked_create_delete_user_app_interface.call_args_list == [
-        call(
-            "username2",
-            [
-                PathSpec(type=PathTypes.USER, path="blah"),
-                PathSpec(type=PathTypes.REQUEST, path="test_path2"),
-                PathSpec(type=PathTypes.QUERY, path="another_test_path2"),
-                PathSpec(type=PathTypes.GABI, path="yet_another_test_path2"),
-                PathSpec(type=PathTypes.SCHEDULE, path="and_yet_another_test_path2"),
-            ],
-        ),
-        call(
-            "username3",
-            [
-                PathSpec(type=PathTypes.USER, path="blah"),
-                PathSpec(type=PathTypes.REQUEST, path="test_path3"),
-                PathSpec(type=PathTypes.QUERY, path="another_test_path3"),
-                PathSpec(type=PathTypes.GABI, path="yet_another_test_path3"),
-                PathSpec(type=PathTypes.SCHEDULE, path="and_yet_another_test_path3"),
-            ],
-        ),
-    ]
+    mocked_create_delete_user_app_interface.assert_has_calls(
+        [
+            call(
+                "username2",
+                [
+                    PathSpec(type=PathTypes.USER, path="blah"),
+                    PathSpec(type=PathTypes.REQUEST, path="test_path2"),
+                    PathSpec(type=PathTypes.QUERY, path="another_test_path2"),
+                    PathSpec(type=PathTypes.GABI, path="yet_another_test_path2"),
+                    PathSpec(
+                        type=PathTypes.SCHEDULE, path="and_yet_another_test_path2"
+                    ),
+                ],
+            ),
+            call(
+                "username3",
+                [
+                    PathSpec(type=PathTypes.USER, path="blah"),
+                    PathSpec(type=PathTypes.REQUEST, path="test_path3"),
+                    PathSpec(type=PathTypes.QUERY, path="another_test_path3"),
+                    PathSpec(type=PathTypes.GABI, path="yet_another_test_path3"),
+                    PathSpec(
+                        type=PathTypes.SCHEDULE, path="and_yet_another_test_path3"
+                    ),
+                ],
+            ),
+        ],
+        any_order=True,
+    )
     assert mocked_create_delete_user_app_interface.return_value.submit.call_count == 2
-    assert mocked_create_delete_user_infra.call_count == 1
-    assert mocked_create_delete_user_infra.call_args == call(["username2", "username3"])
-    assert mocked_create_delete_user_infra.return_value.submit.call_count == 1
+    mocked_create_delete_user_infra.assert_called_once_with(["username2", "username3"])
+    mocked_create_delete_user_infra.return_value.submit.assert_called_once
+
