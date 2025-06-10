3a39be976ea8c5331dc15840ed5d6e01114df698
Christian Assing <cassing@redhat.com>
2025-06-10 11:51:48 +0200

    :label: jira-watcher: type hints

--- reconcile/jira_watcher.py
+++ reconcile/jira_watcher.py
@@ -1,4 +1,6 @@
 import logging
+from collections.abc import Callable, Mapping, Sequence
+from typing import Any
 
 from reconcile import queries
 from reconcile.slack_base import slackapi_from_slack_workspace
@@ -6,6 +8,7 @@ from reconcile.utils.defer import defer
 from reconcile.utils.jira_client import JiraClient
 from reconcile.utils.secret_reader import SecretReader
 from reconcile.utils.sharding import is_in_shard_round_robin
+from reconcile.utils.slack_api import SlackApi
 from reconcile.utils.state import (
     State,
     init_state,
@@ -14,7 +17,9 @@ from reconcile.utils.state import (
 QONTRACT_INTEGRATION = "jira-watcher"
 
 
-def fetch_current_state(jira_board, settings):
+def fetch_current_state(
+    jira_board: Mapping, settings: Mapping
+) -> tuple[JiraClient, dict[str, dict[str, str]]]:
     jira = JiraClient(jira_board, settings=settings)
     issues = jira.get_issues(fields=["key", "status", "summary"])
     return jira, {
@@ -23,11 +28,18 @@ def fetch_current_state(jira_board, settings):
     }
 
 
-def fetch_previous_state(state, project):
+def fetch_previous_state(state: State, project: str) -> dict:
     return state.get(project, {})
 
 
-def format_message(server, key, data, event, previous_state=None, current_state=None):
+def format_message(
+    server: str,
+    key: str,
+    data: Mapping,
+    event: str,
+    previous_state: Mapping | None = None,
+    current_state: Mapping | None = None,
+) -> str:
     summary = data["summary"]
     info = (
         ": {} -> {}".format(previous_state["status"], current_state["status"])
@@ -38,7 +50,9 @@ def format_message(server, key, data, event, previous_state=None, current_state=
     return f"{url} ({summary}) {event}{info}"
 
 
-def calculate_diff(server, current_state, previous_state):
+def calculate_diff(
+    server: str, current_state: Mapping, previous_state: Mapping
+) -> list[str]:
     messages = []
     new_issues = [
         format_message(server, key, data, "created")
@@ -64,7 +78,7 @@ def calculate_diff(server, current_state, previous_state):
     return messages
 
 
-def init_slack(jira_board):
+def init_slack(jira_board: Mapping[str, Any]) -> SlackApi:
     secret_reader = SecretReader(queries.get_secret_reader_settings())
     slack_info = jira_board["slack"]
 
@@ -77,7 +91,7 @@ def init_slack(jira_board):
     )
 
 
-def act(dry_run, jira_board, diffs):
+def act(dry_run: bool, jira_board: Mapping[str, str], diffs: Sequence[str]) -> None:
     if not dry_run and diffs:
         slack = init_slack(jira_board)
 
@@ -87,16 +101,17 @@ def act(dry_run, jira_board, diffs):
             slack.chat_post_message(diff)
 
 
-def write_state(state: State, project, state_to_write):
+def write_state(state: State, project: str, state_to_write: Mapping) -> None:
     state.add(project, value=state_to_write, force=True)
 
 
 @defer
-def run(dry_run, defer):
+def run(dry_run: bool, defer: Callable | None = None) -> None:
     jira_boards = [j for j in queries.get_jira_boards() if j.get("slack")]
     settings = queries.get_app_interface_settings()
     state = init_state(integration=QONTRACT_INTEGRATION)
-    defer(state.cleanup)
+    if defer:
+        defer(state.cleanup)
     for index, jira_board in enumerate(jira_boards):
         if not is_in_shard_round_robin(jira_board["name"], index):
             continue
@@ -109,6 +124,7 @@ def run(dry_run, defer):
             continue
         previous_state = fetch_previous_state(state, jira.project)
         if previous_state:
+            assert jira.server
             diffs = calculate_diff(jira.server, current_state, previous_state)
             act(dry_run, jira_board, diffs)
         if not dry_run:
