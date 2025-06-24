74687591233a7eb8145b2c76eae9940302859d5a
Ryan Wallace <rywallac@redhat.com>
2025-06-24 14:31:12 -0400

    Revert "feat: Add target_ref to saas deploy trigger (#5041)"
    This reverts commit 138b31078679880cf52c038db0420c6568c3d77d.

--- reconcile/openshift_saas_deploy_trigger_base.py
+++ reconcile/openshift_saas_deploy_trigger_base.py
@@ -38,7 +38,7 @@ from reconcile.utils.saasherder import (
     TriggerSpecUnion,
 )
 from reconcile.utils.saasherder.interfaces import SaasPipelinesProviderTekton
-from reconcile.utils.saasherder.models import TriggerSpecConfig, TriggerTypes
+from reconcile.utils.saasherder.models import TriggerTypes
 from reconcile.utils.secret_reader import create_secret_reader
 from reconcile.utils.sharding import is_in_shard
 from reconcile.utils.state import init_state
@@ -267,10 +267,6 @@ def _trigger_tekton(
         )
         return False
 
-    target_ref = None
-    if isinstance(spec, TriggerSpecConfig):
-        target_ref = spec.target_ref
-
     tkn_trigger_resource, tkn_name = _construct_tekton_trigger_resource(
         spec.saas_file_name,
         spec.env_name,
@@ -282,7 +278,6 @@ def _trigger_tekton(
         integration_version,
         saasherder.include_trigger_trace,
         spec.reason,
-        target_ref,
     )
 
     error = False
@@ -339,7 +334,6 @@ def _construct_tekton_trigger_resource(
     integration_version: str,
     include_trigger_trace: bool,
     reason: str | None,
-    target_ref: str | None,
 ) -> tuple[OR, str]:
     """Construct a resource (PipelineRun) to trigger a deployment via Tekton.
 
@@ -354,7 +348,6 @@ def _construct_tekton_trigger_resource(
         integration_version (string): Version of calling integration
         include_trigger_trace (bool): Should include traces of the triggering integration and reason
         reason (string): The reason this trigger was created
-        target_ref (string): the SHA ref of the target
 
     Returns:
         OpenshiftResource: OpenShift resource to be applied
@@ -392,7 +385,6 @@ def _construct_tekton_trigger_resource(
             "labels": {
                 "qontract.saas_file_name": saas_file_name,
                 "qontract.env_name": env_name,
-                "qontract.target_ref": target_ref or "",
             },
         },
         "spec": {
