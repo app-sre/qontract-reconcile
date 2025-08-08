fb07664f33573d1012cc8631e2b15e8f45e76e39
Jordi Freixa Serrabassa <jfreixa@redhat.com>
2025-08-08 12:32:32 +0200

    Add types to tests terraform and terrascript (#5164)

--- reconcile/test/utils/test_terraform.py
+++ reconcile/test/utils/test_terraform.py
@@ -1,13 +1,13 @@
 from reconcile.utils.terraform import safe_resource_id
 
 
-def test_sanitize_resource_with_dots():
+def test_sanitize_resource_with_dots() -> None:
     assert safe_resource_id("foo.example.com") == "foo_example_com"
 
 
-def test_sanitize_resource_with_wildcard():
+def test_sanitize_resource_with_wildcard() -> None:
     assert safe_resource_id("*.foo.example.com") == "_star_foo_example_com"
 
 
-def test_sanitize_resource_begins_with_number():
+def test_sanitize_resource_begins_with_number() -> None:
     assert safe_resource_id("0xyz") == "_0xyz"
