b95768d96ed36dac74521414ba7c1a24b5af8155
Jordi Freixa Serrabassa <jfreixa@redhat.com>
2025-08-13 16:21:29 +0200

    Add test to test_jenkins_api trigger_job

--- reconcile/test/test_jenkins_api.py
+++ reconcile/test/test_jenkins_api.py
@@ -50,3 +50,89 @@ def test_get_jobs_state(
     job_state = jenkins_api.get_jobs_state()
 
     assert job_state == expected_job_state
+
+
+def test_trigger_job_build(
+    jenkins_api: JenkinsApi,
+    mocker: MockerFixture,
+) -> None:
+    mocked_requests = mocker.patch("reconcile.utils.jenkins_api.requests")
+    first_mocked_response = create_autospec(Response)
+    first_mocked_response.json.return_value = {
+        "_class": "hudson.security.csrf.DefaultCrumbIssuer",
+        "crumb": "058d22a8f4805175d41425b4ad8afa81117aa813a19d4918a6af5f83abe42e37",
+        "crumbRequestField": "Jenkins-Crumb",
+    }
+    second_mocked_response = create_autospec(Response)
+    second_mocked_response.json.return_value = {
+        "_class": "hudson.model.FreeStyleProject",
+        "property": [
+            {"_class": "com.coravy.hudson.plugins.github.GithubProjectProperty"},
+            {
+                "_class": "com.dabsquared.gitlabjenkins.connection.GitLabConnectionProperty"
+            },
+        ],
+    }
+
+    mocked_requests.get.side_effect = [
+        first_mocked_response,
+        second_mocked_response,
+    ]
+
+    jenkins_api.trigger_job("test")
+
+    assert mocked_requests.post.call_args.args[0] == "http://example.com/job/test/build"
+
+
+def test_trigger_job_build_with_parameters(
+    jenkins_api: JenkinsApi,
+    mocker: MockerFixture,
+) -> None:
+    mocked_requests = mocker.patch("reconcile.utils.jenkins_api.requests")
+    first_mocked_response = create_autospec(Response)
+    first_mocked_response.json.return_value = {
+        "_class": "hudson.security.csrf.DefaultCrumbIssuer",
+        "crumb": "78ad8e9fb370431c261b8928d7276efc3cd1831b47b6103adf946d53288135d8",
+        "crumbRequestField": "Jenkins-Crumb",
+    }
+    second_mocked_response = create_autospec(Response)
+    second_mocked_response.json.return_value = {
+        "_class": "hudson.model.FreeStyleProject",
+        "property": [
+            {"_class": "com.coravy.hudson.plugins.github.GithubProjectProperty"},
+            {
+                "_class": "com.dabsquared.gitlabjenkins.connection.GitLabConnectionProperty"
+            },
+            {
+                "_class": "hudson.model.ParametersDefinitionProperty",
+                "parameterDefinitions": [
+                    {
+                        "_class": "hudson.model.StringParameterDefinition",
+                        "defaultParameterValue": {
+                            "_class": "hudson.model.StringParameterValue"
+                        },
+                        "description": "Path to the suite which Ginkgo should run",
+                        "name": "SUITE_PATH",
+                        "type": "StringParameterDefinition",
+                    },
+                    {
+                        "_class": "hudson.model.StringParameterDefinition",
+                        "defaultParameterValue": {
+                            "_class": "hudson.model.StringParameterValue"
+                        },
+                        "description": "Completion timeout for Ginkgo based API tests.",
+                        "name": "TIMEOUT",
+                        "type": "StringParameterDefinition",
+                    },
+                ],
+            },
+        ],
+    }
+    mocked_requests.get.side_effect = [
+        first_mocked_response,
+        second_mocked_response,
+    ]
+
+    jenkins_api.trigger_job("test")
+
+    assert mocked_requests.post.call_args.args[0] == "http://example.com/job/test/buildWithParameters"
