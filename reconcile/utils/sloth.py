8b3b786c2e49617e0280abbe3e6fad9f88a6aced
Drew Welch <dwelch@redhat.com>
2025-08-14 16:17:23 -0400

    decouple sloth processing logic

--- reconcile/utils/sloth.py
+++ reconcile/utils/sloth.py
@@ -19,36 +19,44 @@ class PrometheusRuleSpec(BaseModel):
     groups: list[PrometheusRuleGroup]
 
 
+def get_cleaned_rule_labels(rule: PrometheusRule) -> dict[str, str] | None:
+    if rule.record or not rule.labels:
+        return None
+    # sloth adds several sloth_* labels to rules that are not compliant with prometheus-rule-1 schema
+    # see https://sloth.dev/examples/default/getting-started/#__tabbed_1_2
+    labels = {k: v for k, v in rule.labels.items() if not k.startswith("sloth")}
+    return labels or None
+
+
+def get_cleaned_rule_annotations(rule: PrometheusRule) -> dict[str, str] | None:
+    if rule.alert and rule.annotations:
+        # sloth adds a `title` key within annotations: https://sloth.dev/examples/default/getting-started/#__tabbed_1_2
+        # not supported within schema: https://github.com/app-sre/qontract-schemas/blob/main/schemas/openshift/prometheus-rule-1.yml#L165-L192
+        annotations = {k: v for k, v in rule.annotations.items() if k != "title"}
+        return annotations or None
+    return rule.annotations
+
+
+def get_cleaned_rule(rule: PrometheusRule) -> PrometheusRule:
+    return PrometheusRule(
+        record=rule.record,
+        alert=rule.alert,
+        expr=rule.expr,
+        labels=get_cleaned_rule_labels(rule),
+        annotations=get_cleaned_rule_annotations(rule),
+    )
+
 def process_sloth_output(output_file_path: str) -> str:
-    """Process sloth output to make it compliant with the prometheus-rule-1 schema"""
-    cleaned_rule_spec = PrometheusRuleSpec(groups=[])
     with open(output_file_path, encoding="utf-8") as f:
         data = yaml.safe_load(f)
-    rule_spec = PrometheusRuleSpec(**data)
-    for group in rule_spec.groups:
-        cleaned_group = PrometheusRuleGroup(name=group.name, rules=[])
-        for rule in group.rules:
-            # sloth adds several labels/annotations that are invalid within our rule schema
-            # see: https://sloth.dev/examples/default/getting-started/#__tabbed_1_2
-            cleaned_rule = rule.copy(deep=True)
-            if cleaned_rule.alert:
-                if cleaned_rule.labels:
-                    cleaned_labels = {
-                        k: v
-                        for k, v in cleaned_rule.labels.items()
-                        if not k.startswith("sloth")
-                    }
-                    cleaned_rule.labels = cleaned_labels or None
-                if cleaned_rule.annotations:
-                    cleaned_annotations = {
-                        k: v
-                        for k, v in cleaned_rule.annotations.items()
-                        if k != "title"
-                    }
-                    cleaned_rule.annotations = cleaned_annotations or None
-            # recording rule
-            else:
-                cleaned_rule.labels = None
-            cleaned_group.rules.append(cleaned_rule)
-        cleaned_rule_spec.groups.append(cleaned_group)
+    rule_spec = PrometheusRuleSpec.parse_obj(data)
+    cleaned_rule_spec = PrometheusRuleSpec(
+        groups=[
+            PrometheusRuleGroup(
+                name=group.name,
+                rules=[get_cleaned_rule(r) for r in group.rules],
+            )
+            for group in rule_spec.groups
+        ]
+    )
     return yaml.safe_dump(cleaned_rule_spec.dict(exclude_none=True))
