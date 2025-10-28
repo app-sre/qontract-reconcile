0055ff8aedd0335a2c2be7eb4f37319d1829b12a
Christian Assing <cassing@redhat.com>
2025-10-28 14:22:13 +0100

    :recycle: replace deprecated deep_update

--- reconcile/slack_usergroups.py
+++ reconcile/slack_usergroups.py
@@ -3,17 +3,18 @@ import sys
 from collections.abc import (
     Callable,
     Iterable,
+    MutableMapping,
     Sequence,
 )
 from datetime import datetime
 from typing import (
     Any,
     TypedDict,
+    TypeVar,
 )
 
 from github.GithubException import UnknownObjectException
 from pydantic import BaseModel, ConfigDict
-from pydantic.utils import deep_update
 from sretoolbox.utils import retry
 
 from reconcile import (
@@ -74,6 +75,26 @@ QONTRACT_INTEGRATION = "slack-usergroups"
 INTEGRATION_VERSION = "0.1.0"
 
 error_occurred = False
+
+KeyType = TypeVar("KeyType")
+
+
+def deep_update(
+    mapping: dict[KeyType, Any],
+    *updating_mappings: MutableMapping[KeyType, Any],
+) -> dict[KeyType, Any]:
+    updated_mapping = mapping.copy()
+    for updating_mapping in updating_mappings:
+        for k, v in updating_mapping.items():
+            if (
+                k in updated_mapping
+                and isinstance(updated_mapping[k], dict)
+                and isinstance(v, dict)
+            ):
+                updated_mapping[k] = deep_update(updated_mapping[k], v)
+            else:
+                updated_mapping[k] = v
+    return updated_mapping
 
 
 def get_git_api(url: str) -> GithubRepositoryApi | GitLabApi:
