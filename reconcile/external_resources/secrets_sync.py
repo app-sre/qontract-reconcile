afd569d4688399df70133b3b2abe333d6039c13d
Drew Welch <dwelch@redhat.com>
2025-11-21 13:24:35 -0500

    refactor to leverage copy for avoiding mutation

--- reconcile/external_resources/secrets_sync.py
+++ reconcile/external_resources/secrets_sync.py
@@ -448,8 +448,8 @@ class VaultSecretsReconciler(SecretsReconciler):
         secret_path = self.secret_path(self.vault_path, spec)
         try:
             logging.debug("Reading Secret %s", secret_path)
-            data = self.secrets_reader.read_all({"path": secret_path})
-            spec.metadata[SECRET_UPDATED_AT] = data[SECRET_UPDATED_AT]
-            spec.secret = {k: v for k, v in data.items() if k != SECRET_UPDATED_AT}
+            data = self.secrets_reader.read_all({"path": secret_path}).copy()
+            spec.metadata[SECRET_UPDATED_AT] = data.pop(SECRET_UPDATED_AT)
+            spec.secret = data
         except SecretNotFoundError:
             logging.info("Error getting secret from vault, skipping. [%s]", secret_path)
