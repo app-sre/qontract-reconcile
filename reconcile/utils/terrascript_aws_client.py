12af78ec43f8154ed7c02f54c27f061596bb04a8
Ryan Wallace <rywallac@redhat.com>
2025-11-24 10:35:31 -0500

    fix: add policy to VARIABLE_KEYS

--- reconcile/utils/terrascript_aws_client.py
+++ reconcile/utils/terrascript_aws_client.py
@@ -272,6 +272,7 @@ VARIABLE_KEYS = [
     "lifecycle",
     "max_session_duration",
     "secret_format",
+    "policy"
 ]
 
 EMAIL_REGEX = re.compile(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
