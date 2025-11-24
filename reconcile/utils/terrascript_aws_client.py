4564012d0431f5245dd42c66006bf4cad7eeb00c
Ryan Wallace <rywallac@redhat.com>
2025-11-24 10:50:15 -0500

    fix: test failure

--- reconcile/utils/terrascript_aws_client.py
+++ reconcile/utils/terrascript_aws_client.py
@@ -272,7 +272,7 @@ VARIABLE_KEYS = [
     "lifecycle",
     "max_session_duration",
     "secret_format",
-    "policy"
+    "policy",
 ]
 
 EMAIL_REGEX = re.compile(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
