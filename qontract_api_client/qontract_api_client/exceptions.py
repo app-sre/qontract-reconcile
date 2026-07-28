c46c4f3cb47e01ae274ddbf8f9beb77812e148cd
Christian Assing <cassing@redhat.com>
2026-07-28 14:58:36 +0200

    docs

--- qontract_api_client/qontract_api_client/exceptions.py
+++ qontract_api_client/qontract_api_client/exceptions.py
@@ -1,3 +1,6 @@
+# This file isn't automatically generated, but it is a re-export of the exceptions from the `clientele` package.
+# This allows users of `qontract-api-client` to import exceptions directly from this package
+# without needing to know about the underlying `clientele` package.
 from clientele.api.exceptions import APIException, HTTPStatusError
 
 __all__ = ["APIException", "HTTPStatusError"]
